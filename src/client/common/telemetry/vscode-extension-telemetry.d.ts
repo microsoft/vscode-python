f447ef863aa826115c3bd433ecd967f2242ead48
Brett Cannon <brettcannon@users.noreply.github.com>
2017-11-03 15:43:50 -0700

    Remove copyright header

--- src/client/common/telemetry/vscode-extension-telemetry.d.ts
+++ src/client/common/telemetry/vscode-extension-telemetry.d.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License.
- *--------------------------------------------------------------------------------------------*/
 declare module 'vscode-extension-telemetry' {
     export default class TelemetryReporter {
         /**
