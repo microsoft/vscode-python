612103f01c5a28eccec425d411a70246061bd7b0
Brett Cannon <brettcannon@users.noreply.github.com>
2017-11-03 15:44:08 -0700

    Remove copyright header

--- src/client/debugger/Main.ts
+++ src/client/debugger/Main.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License.
- *--------------------------------------------------------------------------------------------*/
 "use strict";
 
 import { DebugSession, InitializedEvent, TerminatedEvent, StoppedEvent, OutputEvent, Thread, StackFrame, Scope, Source, Handles } from "vscode-debugadapter";
