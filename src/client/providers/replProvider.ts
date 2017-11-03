75ec627d6642b9229b1e0875aeb37358c80933b0
Brett Cannon <brettcannon@users.noreply.github.com>
2017-11-03 15:51:34 -0700

    Remove copyright header

--- src/client/providers/replProvider.ts
+++ src/client/providers/replProvider.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License.
- *--------------------------------------------------------------------------------------------*/
 import { commands, Disposable, window, workspace } from 'vscode';
 import { PythonSettings } from '../common/configSettings';
 import { Commands } from '../common/constants';
