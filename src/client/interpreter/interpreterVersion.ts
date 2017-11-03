9e27b86acb14a500e50eb00721a9aa3c307a4747
Brett Cannon <brettcannon@users.noreply.github.com>
2017-11-03 15:48:55 -0700

    Remove copyright header

--- src/client/interpreter/interpreterVersion.ts
+++ src/client/interpreter/interpreterVersion.ts
@@ -1,5 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
 import * as child_process from 'child_process';
 import { getInterpreterVersion } from '../common/utils';
 
