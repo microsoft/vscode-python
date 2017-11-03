4e923af05fd1b344429dcb8564a68d75d64bfa7d
Brett Cannon <brettcannon@users.noreply.github.com>
2017-11-03 15:56:18 -0700

    Remove copyright header

--- src/client/unittests/display/picker.ts
+++ src/client/unittests/display/picker.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License.
- *--------------------------------------------------------------------------------------------*/
 import * as path from 'path';
 import { QuickPickItem, Uri, window } from 'vscode';
 import * as vscode from 'vscode';
