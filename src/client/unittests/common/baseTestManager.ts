8a06e989769465ed9403460c7e52a4e5a09c5fb4
Don Jayamanne <don.jayamanne@yahoo.com>
2017-11-03 13:52:33 -0700

    fixed license headers

--- src/client/unittests/common/baseTestManager.ts
+++ src/client/unittests/common/baseTestManager.ts
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Licensed under the MIT License.
  *--------------------------------------------------------------------------------------------*/
 // import {TestFolder, TestsToRun, Tests, TestFile, TestSuite, TestFunction, TestStatus, FlattenedTestFunction, FlattenedTestSuite, CANCELLATION_REASON} from './contracts';
 import * as vscode from 'vscode';
--- src/test/interpreters/interpreterVersion.test.ts
+++ src/test/interpreters/interpreterVersion.test.ts
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Licensed under the MIT License.
  *--------------------------------------------------------------------------------------------*/
 import { assert, expect } from 'chai';
 import { execPythonFile } from '../../client/common/utils';
