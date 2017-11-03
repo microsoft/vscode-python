164b27ea2d2bd97c3cf5201e464c630848107e43
Brett Cannon <brettcannon@users.noreply.github.com>
2017-11-03 15:45:45 -0700

    Remove copyright header

--- src/client/interpreter/configuration/services/globalUpdaterService.ts
+++ src/client/interpreter/configuration/services/globalUpdaterService.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License.
- *--------------------------------------------------------------------------------------------*/
 import { workspace } from 'vscode';
 import { IPythonPathUpdaterService } from '../types';
 
