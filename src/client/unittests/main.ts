a89fd8eb1ca042e60ad4e4b90c740421eae89cb4
Brett Cannon <brettcannon@users.noreply.github.com>
2017-11-03 15:56:34 -0700

    Remove copyright header

--- src/client/unittests/main.ts
+++ src/client/unittests/main.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License.
- *--------------------------------------------------------------------------------------------*/
 'use strict';
 import { Uri, window, workspace } from 'vscode';
 import * as vscode from 'vscode';
