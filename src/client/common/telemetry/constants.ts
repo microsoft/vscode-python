a3cff53a2da014eee3d2b844720b02333e5de54b
Brett Cannon <brettcannon@users.noreply.github.com>
2017-11-03 15:42:28 -0700

    Simplify copyright header

--- src/client/common/telemetry/constants.ts
+++ src/client/common/telemetry/constants.ts
@@ -1,7 +1,5 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 export const COMPLETION = 'COMPLETION';
 export const DEFINITION = 'DEFINITION';
 export const HOVER_DEFINITION = 'HOVER_DEFINITION';
