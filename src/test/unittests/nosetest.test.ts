e55c15e335bc8793554843bce2ab0cf5e611a0e8
Brett Cannon <brettcannon@users.noreply.github.com>
2017-11-03 15:59:14 -0700

    Remove copyright header

--- src/test/unittests/nosetest.test.ts
+++ src/test/unittests/nosetest.test.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License.
- *--------------------------------------------------------------------------------------------*/
 import * as assert from 'assert';
 import * as fs from 'fs';
 import * as path from 'path';
