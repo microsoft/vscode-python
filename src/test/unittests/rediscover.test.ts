7c56f5a34a7ee48e345f84b94a247854604bc8bd
Brett Cannon <brettcannon@users.noreply.github.com>
2017-11-03 15:59:47 -0700

    Remove copyright header

--- src/test/unittests/rediscover.test.ts
+++ src/test/unittests/rediscover.test.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
 import { assert } from 'chai';
 import * as fs from 'fs-extra';
 import * as path from 'path';
