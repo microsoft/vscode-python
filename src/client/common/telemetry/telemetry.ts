906e43fa5ec7248bc12ee563838f5752a0bd64ce
Brett Cannon <brettcannon@users.noreply.github.com>
2017-11-03 15:41:27 -0700

    Simplify copyright header

--- src/client/common/telemetry/telemetry.ts
+++ src/client/common/telemetry/telemetry.ts
@@ -1,7 +1,6 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 // tslint:disable-next-line:no-reference
 /// <reference path="./vscode-extension-telemetry.d.ts" />
 import { extensions } from 'vscode';
