74ae430793fd7e87bb3ce5ddef9b4c2155df2e9f
Brett Cannon <brettcannon@users.noreply.github.com>
2017-11-03 15:55:50 -0700

    Remove copyright header

--- src/client/unittests/common/constants.ts
+++ src/client/unittests/common/constants.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License.
- *--------------------------------------------------------------------------------------------*/
 export const CANCELLATION_REASON = 'cancelled_user_request';
 export enum CommandSource {
     auto = 'auto',
