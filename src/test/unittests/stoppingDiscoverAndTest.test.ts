ef467e21367752f0fe4df85169d7a8700a47c6ee
Brett Cannon <brettcannon@users.noreply.github.com>
2017-11-03 16:00:23 -0700

    Simplify copyright header

--- src/test/unittests/stoppingDiscoverAndTest.test.ts
+++ src/test/unittests/stoppingDiscoverAndTest.test.ts
@@ -1,7 +1,6 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 import { expect, use } from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 import * as path from 'path';
