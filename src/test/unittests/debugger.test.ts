c653c0608b96d4c6909a37c2e02975685c0f3bc0
Brett Cannon <brettcannon@users.noreply.github.com>
2017-11-03 15:58:59 -0700

    Remove copyright header

--- src/test/unittests/debugger.test.ts
+++ src/test/unittests/debugger.test.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License.
- *--------------------------------------------------------------------------------------------*/
 import { assert, expect, use } from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 import * as path from 'path';
