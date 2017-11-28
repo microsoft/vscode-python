03f6de33aaf8edcf3854efe11711c2a97705d8d8
Don Jayamanne <don.jayamanne@yahoo.com>
2017-11-28 12:46:01 -0800

    move browser file

--- src/client/extension.ts
+++ src/client/extension.ts
@@ -45,9 +45,7 @@ const activationDeferred = createDeferred<void>();
 export const activated = activationDeferred.promise;
 // tslint:disable-next-line:max-func-body-length
 export async function activate(context: vscode.ExtensionContext) {
-    const jupyterExtInstalled = vscode.extensions.getExtension('donjayamanne.jupyter');
     const persistentStateFactory = new PersistentStateFactory(context.globalState, context.workspaceState);
-    context.subscriptions.push(new FeatureDeprecationManager(persistentStateFactory, !!jupyterExtInstalled));
     const pythonSettings = settings.PythonSettings.getInstance();
     // tslint:disable-next-line:no-floating-promises
     sendStartupTelemetry(activated);
@@ -122,6 +120,7 @@ export async function activate(context: vscode.ExtensionContext) {
     // tslint:disable-next-line:promise-function-async
     const linterProvider = new LintProvider(context, lintingOutChannel, (a, b) => Promise.resolve(false));
     context.subscriptions.push();
+    const jupyterExtInstalled = vscode.extensions.getExtension('donjayamanne.jupyter');
     if (jupyterExtInstalled) {
         if (jupyterExtInstalled.isActive) {
             // tslint:disable-next-line:no-unsafe-any
@@ -153,6 +152,10 @@ export async function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(feedbackService);
     // tslint:disable-next-line:no-unused-expression
     new BannerService(persistentStateFactory);
+
+    const deprecationMgr = new FeatureDeprecationManager(persistentStateFactory, !!jupyterExtInstalled);
+    deprecationMgr.initialize();
+    context.subscriptions.push(new FeatureDeprecationManager(persistentStateFactory, !!jupyterExtInstalled));
 }
 
 async function sendStartupTelemetry(activatedPromise: Promise<void>) {
