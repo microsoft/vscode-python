71c2531a4ad9ec9bf865ec0800ea0114784d2a8c
Don Jayamanne <don.jayamanne@yahoo.com>
2017-11-30 13:14:08 -0800

    bug fix

--- src/client/ioc/container.ts
+++ src/client/ioc/container.ts
@@ -4,8 +4,8 @@ import { Abstract, IServiceContainer, Newable } from './types';
 @injectable()
 export class ServiceContainer implements IServiceContainer {
     constructor(private container: Container) { }
-    public get<T>(serviceIdentifier: interfaces.ServiceIdentifier<T>): T {
-        return this.container.get<T>(serviceIdentifier);
+    public get<T>(serviceIdentifier: interfaces.ServiceIdentifier<T>, name?: string | number | symbol): T {
+        return name ? this.container.getNamed<T>(serviceIdentifier, name) : this.container.get<T>(serviceIdentifier);
     }
     public getAll<T>(serviceIdentifier: string | symbol | Newable<T> | Abstract<T>, name?: string | number | symbol | undefined): T[] {
         return name ? this.container.getAllNamed<T>(serviceIdentifier, name) : this.container.getAll<T>(serviceIdentifier);
