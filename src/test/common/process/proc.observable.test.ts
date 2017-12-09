bdf9ebdea108562e7d12a3afedce36b2cdf78b5b
Don Jayamanne <don.jayamanne@yahoo.com>
2017-12-08 17:34:57 -0800

    remove unwanted test

--- src/test/common/process/proc.observable.test.ts
+++ src/test/common/process/proc.observable.test.ts
@@ -21,19 +21,6 @@ suite('ProcessService', () => {
     setup(initialize);
     teardown(initialize);
 
-    test('execObservable should output print statements', async () => {
-        const procService = new ProcessService(new BufferDecoder());
-        const printOutput = '1234';
-        const result = procService.execObservable(pythonPath, ['-c', `print("${printOutput}")`]);
-
-        expect(result).not.to.be.an('undefined', 'result is undefined');
-        const output = await result.out.toPromise();
-        expect(output.source).to.be.equal('stdout', 'Source is incorrect');
-        expect(output.out).to.have.length.greaterThan(0, 'Invalid output length');
-        const stdOut = output.out.trim();
-        expect(stdOut).to.equal(printOutput, 'Output is incorrect');
-    });
-
     test('execObservable should stream output with new lines', function (done) {
         // tslint:disable-next-line:no-invalid-this
         this.timeout(10000);
