00e3d0d218f9fb85934c62557180906a2efec753
Don Jayamanne <don.jayamanne@yahoo.com>
2017-12-14 10:19:46 -0800

    check how test fails

--- src/test/debugger/portAndHost.test.ts
+++ src/test/debugger/portAndHost.test.ts
@@ -109,15 +109,15 @@ suite('Standard Debugging', () => {
     });
 
     test('Confirm debuggig fails when an invalid host is provided', async () => {
-        const promise = testDebuggingWithProvidedPort(0, 'xyz123409924ple_ewf');
-        expect(promise).to.eventually.be.rejected.and.to.have.property('code', 'ENOTFOUND', 'Debugging failed for some other reason');
+        const promise = await testDebuggingWithProvidedPort(0, 'xyz123409924ple_ewf');
+        // expect(promise).to.eventually.be.rejected.and.to.have.property('code', 'ENOTFOUND', 'Debugging failed for some other reason');
     });
 
     test('Confirm debuggig fails when provided port is in use', async () => {
         // tslint:disable-next-line:no-empty
         const server = net.createServer((s) => { });
         const port = await new Promise<number>((resolve, reject) => server.listen({ host: 'localhost', port: 0 }, () => resolve(server.address().port)));
-        const promise = testDebuggingWithProvidedPort(port);
-        expect(promise).to.eventually.be.rejected.and.to.have.property('code', 'EADDRINUSE', 'Debugging failed for some other reason');
+        const promise = await testDebuggingWithProvidedPort(port);
+        // expect(promise).to.eventually.be.rejected.and.to.have.property('code', 'EADDRINUSE', 'Debugging failed for some other reason');
     });
 });
