66395c5764de0b92e2897e1bdbf0ede22dbe62ad
MikhailArkhipov <mikhaila@microsoft.com>
2018-02-21 16:31:38 -0800

    Basic venv path search tests

---
+++ src/test/interpreters/venv.test.ts
@@ -0,0 +1,68 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { expect } from 'chai';
+import { Container } from 'inversify';
+import * as os from 'os';
+import * as path from 'path';
+import * as TypeMoq from 'typemoq';
+import { Uri, WorkspaceFolder } from 'vscode';
+import { IWorkspaceService } from '../../client/common/application/types';
+import { IConfigurationService, IPythonSettings } from '../../client/common/types';
+import { GlobalVirtualEnvironmentsSearchPathProvider } from '../../client/interpreter/locators/services/globalVirtualEnvService';
+import { WorkspaceVirtualEnvironmentsSearchPathProvider } from '../../client/interpreter/locators/services/workspaceVirtualEnvService';
+import { ServiceContainer } from '../../client/ioc/container';
+import { ServiceManager } from '../../client/ioc/serviceManager';
+
+suite('Virtual environments', () => {
+  let serviceManager: ServiceManager;
+  let serviceContainer: ServiceContainer;
+  let settings: TypeMoq.IMock<IPythonSettings>;
+  let config: TypeMoq.IMock<IConfigurationService>;
+  let workspace: TypeMoq.IMock<IWorkspaceService>;
+
+  setup(async () => {
+    const cont = new Container();
+    serviceManager = new ServiceManager(cont);
+    serviceContainer = new ServiceContainer(cont);
+
+    settings = TypeMoq.Mock.ofType<IPythonSettings>();
+    config = TypeMoq.Mock.ofType<IConfigurationService>();
+    workspace = TypeMoq.Mock.ofType<IWorkspaceService>();
+
+    config.setup(x => x.getSettings(TypeMoq.It.isAny())).returns(() => settings.object);
+
+    serviceManager.addSingletonInstance<IConfigurationService>(IConfigurationService, config.object);
+    serviceManager.addSingletonInstance<IWorkspaceService>(IWorkspaceService, workspace.object);
+  });
+
+  test('Global search paths', async () => {
+    const pathProvider = new GlobalVirtualEnvironmentsSearchPathProvider();
+
+    const folders = ['Envs', '.virtualenvs', '.pyenv', path.join('.pyenv', 'versions')];
+    const paths = pathProvider.getSearchPaths();
+    const homedir = os.homedir();
+    const expected = folders.map(item => path.join(homedir, item));
+    expect(paths).to.deep.equal(expected);
+  });
+
+  test('Workspace search paths', async () => {
+    settings.setup(x => x.venvPath).returns(() => `~${path.sep}foo`);
+
+    const wsRoot = TypeMoq.Mock.ofType<WorkspaceFolder>();
+    wsRoot.setup(x => x.uri).returns(() => Uri.file('root'));
+
+    const folder1 = TypeMoq.Mock.ofType<WorkspaceFolder>();
+    folder1.setup(x => x.uri).returns(() => Uri.file('dir1'));
+
+    workspace.setup(x => x.getWorkspaceFolder(TypeMoq.It.isAny())).returns(() => wsRoot.object);
+    workspace.setup(x => x.workspaceFolders).returns(() => [wsRoot.object, folder1.object]);
+
+    const pathProvider = new WorkspaceVirtualEnvironmentsSearchPathProvider(serviceContainer);
+    const paths = pathProvider.getSearchPaths(Uri.file(''));
+
+    const homedir = os.homedir();
+    const expected = [path.join(homedir, 'foo'), `${path.sep}root`, `${path.sep}root${path.sep}.direnv`];
+    expect(paths).to.deep.equal(expected);
+  });
+});
