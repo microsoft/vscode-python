e0db95a87e29c8f4f87801b7ede97a1898320f48
Don Jayamanne <dojayama@microsoft.com>
2018-02-27 11:21:29 -0800

    use nop

--- src/client/common/net/socket/socketServer.ts
+++ src/client/common/net/socket/socketServer.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from 'events';
 import { injectable } from 'inversify';
 import * as net from 'net';
+import { nop } from '../../constants';
 import { createDeferred, Deferred } from '../../helpers';
 import { ISocketServer } from '../../types';
 
@@ -56,8 +57,7 @@ export class SocketServer extends EventEmitter implements ISocketServer {
         client.on('data', (data: Buffer) => {
             this.emit('data', client, data);
         });
-        // tslint:disable-next-line:no-empty
-        client.on('error', (err: Error) => { });
+        client.on('error', (err: Error) => nop);
 
         client.on('timeout', d => {
             // let msg = "Debugger client timedout, " + d;
