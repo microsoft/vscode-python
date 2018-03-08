240f6fe60204f1bac9c724e5b0884a322579522f
MikhailArkhipov <mikhaila@microsoft.com>
2018-03-07 16:46:20 -0800

    Pass interpreter properties

--- src/client/activation/ptvs.ts
+++ src/client/activation/ptvs.ts
@@ -6,6 +6,7 @@ import * as vscode from 'vscode';
 import * as languageClient from 'vscode-languageclient';
 import { STANDARD_OUTPUT_CHANNEL } from '../common/constants';
 import { IConfigurationService, IOutputChannel, IPythonSettings } from '../common/types';
+import { IInterpreterService } from '../interpreter/contracts';
 import { IServiceContainer } from '../ioc/types';
 import { IExtensionActivator } from './types';
 
@@ -30,18 +31,7 @@ export class PtvsExtensionActivator implements IExtensionActivator {
       debug: { command: 'dotnet', args: [serverModule, '--debug'], options: commandOptions }
     };
 
-    const selector: string[] = [this.PYTHON];
-    const outputChannel = services.get<vscode.OutputChannel>(IOutputChannel, STANDARD_OUTPUT_CHANNEL);
-    // Options to control the language client
-    const clientOptions: languageClient.LanguageClientOptions = {
-      // Register the server for Python documents
-      documentSelector: selector,
-      synchronize: {
-        configurationSection: this.PYTHON
-      },
-      outputChannel: outputChannel
-    };
-
+    const clientOptions = await this.getPtvsOptions(services, pythonSettings);
     // Create the language client and start the client.
     this.languageClent = new languageClient.LanguageClient(this.PYTHON, 'Python Tools', serverOptions, clientOptions);
     context.subscriptions.push(this.languageClent.start());
@@ -54,5 +44,48 @@ export class PtvsExtensionActivator implements IExtensionActivator {
     if (this.languageClent) {
       await this.languageClent.stop();
     }
+  }
+
+  private async getPtvsOptions(services: IServiceContainer, pythonSettings: IPythonSettings): Promise<languageClient.LanguageClientOptions> {
+    // tslint:disable-next-line:no-any
+    const properties = new Map<string, any>();
+    // tslint:disable-next-line:no-string-literal
+    properties['InterpreterPath'] = pythonSettings.pythonPath;
+    // tslint:disable-next-line:no-string-literal
+    properties['UseDefaultDatabase'] = true;
+
+    const interpreserService = services.get<IInterpreterService>(IInterpreterService);
+    const interpreter = await interpreserService.getActiveInterpreter();
+    if (interpreter) {
+      if (interpreter.displayName) {
+        // tslint:disable-next-line:no-string-literal
+        properties['Description'] = interpreter.displayName;
+      }
+
+      if (interpreter.version) {
+        // tslint:disable-next-line:no-string-literal
+        properties['Version'] = interpreter.version;
+      } else if (interpreter.displayName) {
+        // tslint:disable-next-line:no-string-literal
+        properties['Version'] = interpreter.displayName.indexOf('3.') > 0 ? '3.6' : '2.7';
+      }
+    }
+
+    const selector: string[] = [this.PYTHON];
+    const outputChannel = services.get<vscode.OutputChannel>(IOutputChannel, STANDARD_OUTPUT_CHANNEL);
+    // Options to control the language client
+    return {
+      // Register the server for Python documents
+      documentSelector: selector,
+      synchronize: {
+        configurationSection: this.PYTHON
+      },
+      outputChannel: outputChannel,
+      initializationOptions: {
+        interpreter: {
+          properties: properties
+        }
+      }
+    };
   }
 }
