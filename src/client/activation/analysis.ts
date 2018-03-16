a6329bc2eb99c5a60ca5cab5186cf3cf8fe603fb
Mikhail Arkhipov <mikhaila@microsoft.com>
2018-03-16 07:43:45 -0700

    Better handle missing interpreter

--- src/client/activation/analysis.ts
+++ src/client/activation/analysis.ts
@@ -4,6 +4,7 @@
 import * as path from 'path';
 import * as vscode from 'vscode';
 import * as languageClient from 'vscode-languageclient';
+import { IApplicationShell } from '../common/application/types';
 import { isTestExecution, STANDARD_OUTPUT_CHANNEL } from '../common/constants';
 import { IConfigurationService, IOutputChannel, IPythonSettings } from '../common/types';
 import { IInterpreterService, IInterpreterVersionService, PythonInterpreter } from '../interpreter/contracts';
@@ -20,105 +21,112 @@ const languageClientName = 'Python Tools';
 const defaultPythonVersion = '2.7';
 
 export class AnalysisExtensionActivator implements IExtensionActivator {
-  private languageClent: languageClient.LanguageClient | undefined;
+    private languageClent: languageClient.LanguageClient | undefined;
 
-  constructor(private services: IServiceContainer, private pythonSettings: IPythonSettings) {
-  }
-
-  public async activate(context: vscode.ExtensionContext): Promise<boolean> {
-    const configuration = this.services.get<IConfigurationService>(IConfigurationService);
-    const sw = new StopWatch();
-
-    // The server is implemented in C#
-    const commandOptions = { stdio: 'pipe' };
-    const serverModule = path.join(context.extensionPath, analysisEngineFolder, analysisEngineBinaryName);
-
-    // If the extension is launched in debug mode then the debug server options are used
-    // Otherwise the run options are used
-    const serverOptions: languageClient.ServerOptions = {
-      run: { command: dotNetCommand, args: [serverModule], options: commandOptions },
-      debug: { command: dotNetCommand, args: [serverModule, '--debug'], options: commandOptions }
-    };
-
-    const clientOptions = await this.getAnalysisOptions();
-    // tslint:disable-next-line:no-console
-    console.log(`Options determined: ${sw.elapsedTime} ms`);
-    try {
-      // Create the language client and start the client.
-      this.languageClent = new languageClient.LanguageClient(PYTHON, languageClientName, serverOptions, clientOptions);
-      context.subscriptions.push(this.languageClent.start());
-    } catch (ex) {
-      if (!await configuration.checkDependencies()) {
-        throw new Error('.NET Runtime is not installed.');
-      }
-      throw ex;
+    constructor(private services: IServiceContainer, private pythonSettings: IPythonSettings) {
     }
 
-    // tslint:disable-next-line:no-console
-    console.log(`Language server started: ${sw.elapsedTime} ms`);
-    await this.languageClent.onReady();
-    // tslint:disable-next-line:no-console
-    console.log(`Language server ready: ${sw.elapsedTime} ms`);
-    return true;
-  }
+    public async activate(context: vscode.ExtensionContext): Promise<boolean> {
+        const configuration = this.services.get<IConfigurationService>(IConfigurationService);
+        const sw = new StopWatch();
 
-  public async deactivate(): Promise<void> {
-    if (this.languageClent) {
-      await this.languageClent.stop();
-    }
-  }
+        // The server is implemented in C#
+        const commandOptions = { stdio: 'pipe' };
+        const serverModule = path.join(context.extensionPath, analysisEngineFolder, analysisEngineBinaryName);
 
-  private async getAnalysisOptions(): Promise<languageClient.LanguageClientOptions> {
-    // tslint:disable-next-line:no-any
-    const properties = new Map<string, any>();
+        // If the extension is launched in debug mode then the debug server options are used
+        // Otherwise the run options are used
+        const serverOptions: languageClient.ServerOptions = {
+            run: { command: dotNetCommand, args: [serverModule], options: commandOptions },
+            debug: { command: dotNetCommand, args: [serverModule, '--debug'], options: commandOptions }
+        };
 
-    // Microsoft Python code analysis engine needs full path to the interpreter
-    const interpreterService = this.services.get<IInterpreterService>(IInterpreterService);
-    const interpreter = await interpreterService.getActiveInterpreter();
-    if (!interpreter) {
-      throw Error('Interpreter path (pythonPath) is not set or interpreter does not exist.');
-    }
-
-    // tslint:disable-next-line:no-string-literal
-    properties['InterpreterPath'] = interpreter.path;
-    // tslint:disable-next-line:no-string-literal
-    properties['UseDefaultDatabase'] = true;
-
-    if (interpreter.displayName) {
-      // tslint:disable-next-line:no-string-literal
-      properties['Description'] = interpreter.displayName;
-    }
-    // tslint:disable-next-line:no-string-literal
-    properties['Version'] = await this.getInterpreterVersion(interpreter);
-    if (isTestExecution()) {
-      // tslint:disable-next-line:no-string-literal
-      properties['TestEnvironment'] = true;
-    }
-
-    const selector: string[] = [PYTHON];
-    const outputChannel = this.services.get<vscode.OutputChannel>(IOutputChannel, STANDARD_OUTPUT_CHANNEL);
-    // Options to control the language client
-    return {
-      // Register the server for Python documents
-      documentSelector: selector,
-      synchronize: {
-        configurationSection: PYTHON
-      },
-      outputChannel: outputChannel,
-      initializationOptions: {
-        interpreter: {
-          properties: properties
+        const clientOptions = await this.getAnalysisOptions(configuration);
+        if (!clientOptions) {
+            return false;
         }
-      }
-    };
-  }
 
-  private async getInterpreterVersion(interpreter: PythonInterpreter): Promise<string> {
-    if (interpreter.version) {
-      return interpreter.version;
+        // tslint:disable-next-line:no-console
+        console.log(`Options determined: ${sw.elapsedTime} ms`);
+        try {
+            // Create the language client and start the client.
+            this.languageClent = new languageClient.LanguageClient(PYTHON, languageClientName, serverOptions, clientOptions);
+            context.subscriptions.push(this.languageClent.start());
+        } catch (ex) {
+            if (!await configuration.checkDependencies()) {
+                throw new Error('.NET Runtime is not installed.');
+            }
+            throw ex;
+        }
+
+        // tslint:disable-next-line:no-console
+        console.log(`Language server started: ${sw.elapsedTime} ms`);
+        await this.languageClent.onReady();
+        // tslint:disable-next-line:no-console
+        console.log(`Language server ready: ${sw.elapsedTime} ms`);
+        return true;
     }
-    const versionService = this.services.get<IInterpreterVersionService>(IInterpreterVersionService);
-    const ver = await versionService.getVersion(this.pythonSettings.pythonPath, defaultPythonVersion);
-    return versionFromPythonVersionString(ver);
-  }
+
+    public async deactivate(): Promise<void> {
+        if (this.languageClent) {
+            await this.languageClent.stop();
+        }
+    }
+
+    private async getAnalysisOptions(configuration: IConfigurationService): Promise<languageClient.LanguageClientOptions | undefined> {
+        // tslint:disable-next-line:no-any
+        const properties = new Map<string, any>();
+
+        // Microsoft Python code analysis engine needs full path to the interpreter
+        const interpreterService = this.services.get<IInterpreterService>(IInterpreterService);
+        const interpreter = await interpreterService.getActiveInterpreter();
+
+        if (interpreter) {
+            // tslint:disable-next-line:no-string-literal
+            properties['InterpreterPath'] = interpreter.path;
+            if (interpreter.displayName) {
+                // tslint:disable-next-line:no-string-literal
+                properties['Description'] = interpreter.displayName;
+            }
+            // tslint:disable-next-line:no-string-literal
+            properties['Version'] = await this.getInterpreterVersion(interpreter);
+            if (isTestExecution()) {
+                // tslint:disable-next-line:no-string-literal
+                properties['TestEnvironment'] = true;
+            }
+            // tslint:disable-next-line:no-string-literal
+            properties['UseDefaultDatabase'] = true;
+        } else {
+            const appShell = this.services.get<IApplicationShell>(IApplicationShell);
+            const pythonPath = configuration.getSettings().pythonPath;
+            appShell.showErrorMessage(`Interpreter ${pythonPath} does not exist.`);
+            return;
+        }
+
+        const selector: string[] = [PYTHON];
+        const outputChannel = this.services.get<vscode.OutputChannel>(IOutputChannel, STANDARD_OUTPUT_CHANNEL);
+        // Options to control the language client
+        return {
+            // Register the server for Python documents
+            documentSelector: selector,
+            synchronize: {
+                configurationSection: PYTHON
+            },
+            outputChannel: outputChannel,
+            initializationOptions: {
+                interpreter: {
+                    properties: properties
+                }
+            }
+        };
+    }
+
+    private async getInterpreterVersion(interpreter: PythonInterpreter): Promise<string> {
+        if (interpreter.version) {
+            return interpreter.version;
+        }
+        const versionService = this.services.get<IInterpreterVersionService>(IInterpreterVersionService);
+        const ver = await versionService.getVersion(this.pythonSettings.pythonPath, defaultPythonVersion);
+        return versionFromPythonVersionString(ver);
+    }
 }
