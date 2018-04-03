f596c91a2d54a62740e840bd24b1251151bc84df
Don Jayamanne <don.jayamanne@yahoo.com>
2018-04-03 13:17:25 -0700

    :hammer: use utils

--- src/test/debugger/module.test.ts
+++ src/test/debugger/module.test.ts
@@ -9,16 +9,12 @@ import * as path from 'path';
 import { DebugClient } from 'vscode-debugadapter-testsupport';
 import { EXTENSION_ROOT_DIR } from '../../client/common/constants';
 import { noop } from '../../client/common/core.utils';
-import { IS_WINDOWS } from '../../client/common/platform/constants';
 import { DebugOptions, LaunchRequestArguments } from '../../client/debugger/Common/Contracts';
 import { sleep } from '../common';
 import { IS_MULTI_ROOT_TEST, TEST_DEBUGGER } from '../initialize';
-import { DEBUGGER_TIMEOUT } from './common/constants';
-import { DebugClientEx } from './debugClient';
+import { createDebugAdapter } from './utils';
 
-const testAdapterFilePath = path.join(EXTENSION_ROOT_DIR, 'out', 'client', 'debugger', 'mainV2.js');
 const workspaceDirectory = path.join(EXTENSION_ROOT_DIR, 'src', 'testMultiRootWkspc', 'workspace5');
-let testCounter = 0;
 const debuggerType = 'pythonExperimental';
 suite(`Module Debugging - Misc tests: ${debuggerType}`, () => {
     let debugClient: DebugClient;
@@ -26,10 +22,8 @@ suite(`Module Debugging - Misc tests: ${debuggerType}`, () => {
         if (!IS_MULTI_ROOT_TEST || !TEST_DEBUGGER) {
             this.skip();
         }
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        debugClient = createDebugAdapter();
-        debugClient.defaultTimeout = DEBUGGER_TIMEOUT;
-        await debugClient.start();
+        const coverageDirectory = path.join(EXTENSION_ROOT_DIR, 'debug_coverage_module');
+        debugClient = await createDebugAdapter(coverageDirectory);
     });
     teardown(async () => {
         // Wait for a second before starting another test (sometimes, sockets take a while to get closed).
@@ -40,19 +34,6 @@ suite(`Module Debugging - Misc tests: ${debuggerType}`, () => {
         } catch (ex) { }
         await sleep(1000);
     });
-    /**
-     * Creates the debug adapter.
-     * We do not need to support code coverage on AppVeyor, lets use the standard test adapter.
-     * @returns {DebugClient}
-     */
-    function createDebugAdapter(): DebugClient {
-        if (IS_WINDOWS) {
-            return new DebugClient('node', testAdapterFilePath, debuggerType);
-        } else {
-            const coverageDirectory = path.join(EXTENSION_ROOT_DIR, `debug_coverage${testCounter += 1}`);
-            return new DebugClientEx(testAdapterFilePath, debuggerType, coverageDirectory, { cwd: EXTENSION_ROOT_DIR });
-        }
-    }
     function buildLauncArgs(): LaunchRequestArguments {
         const env = {};
         // tslint:disable-next-line:no-string-literal
