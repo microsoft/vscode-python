29afb842fbd32cc70d9c9be2413e8aded275a6be
Derek Keeler <dekeeler@microsoft.com>
2018-06-29 11:49:58 -0700

    Add further tests - determine whether or not cursors are in a string or comment

--- src/test/providers/pythonSignatureProvider.unit.test.ts
+++ src/test/providers/pythonSignatureProvider.unit.test.ts
@@ -12,6 +12,7 @@ import { CancellationToken, Position, SignatureHelp,
     TextDocument, TextLine, Uri } from 'vscode';
 import { JediFactory } from '../../client/languageServices/jediProxyFactory';
 import { IArgumentsResult, JediProxyHandler } from '../../client/providers/jediProxy';
+import { isPositionInsideStringOrComment } from '../../client/providers/providerUtilities';
 import { PythonSignatureProvider } from '../../client/providers/signatureProvider';
 
 use(chaipromise);
@@ -72,6 +73,20 @@ suite('Signature Provider unit tests', () => {
         return pySignatureProvider.provideSignatureHelp(doc.object, position, cancelToken.object);
     }
 
+    function testIsInsideStringOrComment(sourceLine: string, sourcePos: number) : boolean {
+        const textLine: TypeMoq.IMock<TextLine> = TypeMoq.Mock.ofType<TextLine>();
+        textLine.setup(t => t.text).returns(() => sourceLine);
+        const doc: TypeMoq.IMock<TextDocument> = TypeMoq.Mock.ofType<TextDocument>();
+        const pos: Position = new Position(1, sourcePos);
+
+        doc.setup(d => d.fileName).returns(() => '');
+        doc.setup(d => d.getText(TypeMoq.It.isAny())).returns(() => sourceLine);
+        doc.setup(d => d.lineAt(TypeMoq.It.isAny())).returns(() => textLine.object);
+        doc.setup(d => d.offsetAt(TypeMoq.It.isAny())).returns(() => sourcePos);
+
+        return isPositionInsideStringOrComment(doc.object, pos);
+    }
+
     test('Ensure no signature is given within a string.', async () => {
         const source = '  print(\'Python is awesome,\')\n';
         const sigHelp: SignatureHelp = await testSignatureReturns(source, 27);
@@ -103,5 +118,131 @@ suite('Signature Provider unit tests', () => {
         } catch (error) {
             assert(false, `Caught exception ${error}`);
         }
+    });
+    test('Ensure isPositionInsideStringOrComment is behaving as expected.', () => {
+        const sourceLine: string = '  print(\'Hello world!\')\n';
+        const sourcePos: number = sourceLine.length - 1;
+        const isInsideStrComment: boolean = testIsInsideStringOrComment(sourceLine, sourcePos);
+
+        expect(isInsideStrComment).to.not.be.equal(true, [
+            `Position set to the end of ${sourceLine} but `,
+            'is reported as being within a string or comment.'].join(''));
+    });
+    test('Ensure isPositionInsideStringOrComment is behaving as expected at end of source.', () => {
+        const sourceLine: string = '  print(\'Hello world!\')\n';
+        const sourcePos: number = 0;
+        const isInsideStrComment: boolean = testIsInsideStringOrComment(sourceLine, sourcePos);
+
+        expect(isInsideStrComment).to.not.be.equal(true, [
+            `Position set to the end of ${sourceLine} but `,
+            'is reported as being within a string or comment.'].join(''));
+    });
+    test('Ensure isPositionInsideStringOrComment is behaving as expected at beginning of source.', () => {
+        const sourceLine: string = '  print(\'Hello world!\')\n';
+        const sourcePos: number = 0;
+        const isInsideStrComment: boolean = testIsInsideStringOrComment(sourceLine, sourcePos);
+
+        expect(isInsideStrComment).to.not.be.equal(true, [
+            `Position set to the beginning of ${sourceLine} but `,
+            'is reported as being within a string or comment.'].join(''));
+    });
+    test('Ensure isPositionInsideStringOrComment is behaving as expected within a string.', () => {
+        const sourceLine: string = '  print(\'Hello world!\')\n';
+        const sourcePos: number = 16;
+        const isInsideStrComment: boolean = testIsInsideStringOrComment(sourceLine, sourcePos);
+
+        expect(isInsideStrComment).to.be.equal(true, [
+            `Position set within the string in ${sourceLine} (position ${sourcePos}) but `,
+            'is reported as NOT being within a string or comment.'].join(''));
+    });
+    test('Ensure isPositionInsideStringOrComment is behaving as expected immediately before a string.', () => {
+        const sourceLine: string = '  print(\'Hello world!\')\n';
+        const sourcePos: number = 8;
+        const isInsideStrComment: boolean = testIsInsideStringOrComment(sourceLine, sourcePos);
+
+        expect(isInsideStrComment).to.be.equal(false, [
+            `Position set to just before the string in ${sourceLine} (position ${sourcePos}) but `,
+            'is reported as being within a string or comment.'].join(''));
+    });
+    test('Ensure isPositionInsideStringOrComment is behaving as expected immediately in a string.', () => {
+        const sourceLine: string = '  print(\'Hello world!\')\n';
+        const sourcePos: number = 9;
+        const isInsideStrComment: boolean = testIsInsideStringOrComment(sourceLine, sourcePos);
+
+        expect(isInsideStrComment).to.be.equal(true, [
+            `Position set to the start of the string in ${sourceLine} (position ${sourcePos}) but `,
+            'is reported as being within a string or comment.'].join(''));
+    });
+    test('Ensure isPositionInsideStringOrComment is behaving as expected within a comment.', () => {
+        const sourceLine: string = '#  print(\'Hello world!\')\n';
+        const sourcePos: number = 16;
+        const isInsideStrComment: boolean = testIsInsideStringOrComment(sourceLine, sourcePos);
+
+        expect(isInsideStrComment).to.be.equal(true, [
+            `Position set within a full line comment ${sourceLine} (position ${sourcePos}) but `,
+            'is reported as NOT being within a string or comment.'].join(''));
+    });
+    test('Ensure isPositionInsideStringOrComment is behaving as expected within a trailing comment.', () => {
+        const sourceLine: string = '  print(\'Hello world!\') # some comment...\n';
+        const sourcePos: number = 34;
+        const isInsideStrComment: boolean = testIsInsideStringOrComment(sourceLine, sourcePos);
+
+        expect(isInsideStrComment).to.be.equal(true, [
+            `Position set within a trailing line comment ${sourceLine} (position ${sourcePos}) but `,
+            'is reported as NOT being within a string or comment.'].join(''));
+    });
+    test('Ensure isPositionInsideStringOrComment is behaving as expected at the very end of a trailing comment.', () => {
+        const sourceLine: string = '  print(\'Hello world!\') # some comment...\n';
+        const sourcePos: number = sourceLine.length - 1;
+        const isInsideStrComment: boolean = testIsInsideStringOrComment(sourceLine, sourcePos);
+
+        expect(isInsideStrComment).to.be.equal(true, [
+            `Position set within a trailing line comment ${sourceLine} (position ${sourcePos}) but `,
+            'is reported as NOT being within a string or comment.'].join(''));
+    });
+    test('Ensure isPositionInsideStringOrComment is behaving as expected within a multiline string.', () => {
+        const sourceLine: string = '  stringVal = \'\'\'This is a multiline\nstring that you can use\nto test this stuff out with\neveryday!\'\'\'\n';
+        const sourcePos: number = 48;
+        const isInsideStrComment: boolean = testIsInsideStringOrComment(sourceLine, sourcePos);
+
+        expect(isInsideStrComment).to.be.equal(true, [
+            `Position set within a multi-line string ${sourceLine} (position ${sourcePos}) but `,
+            'is reported as NOT being within a string or comment.'].join(''));
+    });
+    test('Ensure isPositionInsideStringOrComment is behaving as expected at the very last quote on a multiline string.', () => {
+        const sourceLine: string = '  stringVal = \'\'\'This is a multiline\nstring that you can use\nto test this stuff out with\neveryday!\'\'\'\n';
+        const sourcePos: number = sourceLine.length - 2; // just at the last '
+        const isInsideStrComment: boolean = testIsInsideStringOrComment(sourceLine, sourcePos);
+
+        expect(isInsideStrComment).to.be.equal(true, [
+            `Position set within a multi-line string ${sourceLine} (position ${sourcePos}) but `,
+            'is reported as NOT being within a string or comment.'].join(''));
+    });
+    test('Ensure isPositionInsideStringOrComment is behaving as expected within a multiline string (double-quoted).', () => {
+        const sourceLine: string = '  stringVal = """This is a multiline\nstring that you can use\nto test this stuff out with\neveryday!"""\n';
+        const sourcePos: number = 48;
+        const isInsideStrComment: boolean = testIsInsideStringOrComment(sourceLine, sourcePos);
+
+        expect(isInsideStrComment).to.be.equal(true, [
+            `Position set within a multi-line string ${sourceLine} (position ${sourcePos}) but `,
+            'is reported as NOT being within a string or comment.'].join(''));
+    });
+    test('Ensure isPositionInsideStringOrComment is behaving as expected at the very last quote on a multiline string (double-quoted).', () => {
+        const sourceLine: string = '  stringVal = """This is a multiline\nstring that you can use\nto test this stuff out with\neveryday!"""\n';
+        const sourcePos: number = sourceLine.length - 2; // just at the last '
+        const isInsideStrComment: boolean = testIsInsideStringOrComment(sourceLine, sourcePos);
+
+        expect(isInsideStrComment).to.be.equal(true, [
+            `Position set within a multi-line string ${sourceLine} (position ${sourcePos}) but `,
+            'is reported as NOT being within a string or comment.'].join(''));
+    });
+    test('Ensure isPositionInsideStringOrComment is behaving as expected during construction of a multiline string (double-quoted).', () => {
+        const sourceLine: string = '  stringVal = """This is a multiline\nstring that you can use\nto test this stuff';
+        const sourcePos: number = sourceLine.length - 1; // just at the last position in the string before it's termination
+        const isInsideStrComment: boolean = testIsInsideStringOrComment(sourceLine, sourcePos);
+
+        expect(isInsideStrComment).to.be.equal(true, [
+            `Position set within a multi-line string ${sourceLine} (position ${sourcePos}) but `,
+            'is reported as NOT being within a string or comment.'].join(''));
     });
 });
