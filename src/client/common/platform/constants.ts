8ff0c1b7485ca9456db6b4138125e7081c6e38a4
Eric Snow <ericsnowcurrently@gmail.com>
2018-08-07 13:47:55 -0600

    Add PlatformService.osVersion().

--- src/client/common/platform/constants.ts
+++ src/client/common/platform/constants.ts
@@ -7,3 +7,4 @@ export const WINDOWS_PATH_VARIABLE_NAME = 'Path';
 export const NON_WINDOWS_PATH_VARIABLE_NAME = 'PATH';
 export const IS_WINDOWS = /^win/.test(process.platform);
 export const IS_64_BIT = arch() === 'x64';
+export const LINUX_INFO_FILE = '/etc/os-release';
--- src/client/common/platform/platformService.ts
+++ src/client/common/platform/platformService.ts
@@ -2,28 +2,43 @@
 // Licensed under the MIT License.
 'use strict';
 
+import * as fs from 'fs';
 import { injectable } from 'inversify';
 import * as os from 'os';
-import { NON_WINDOWS_PATH_VARIABLE_NAME, WINDOWS_PATH_VARIABLE_NAME } from './constants';
+import * as semver from 'semver';
+import { LINUX_INFO_FILE, NON_WINDOWS_PATH_VARIABLE_NAME, WINDOWS_PATH_VARIABLE_NAME } from './constants';
 import { IPlatformService, OSType } from './types';
 
 @injectable()
 export class PlatformService implements IPlatformService {
     private _osType: OSType;
+    private _osVersion: string;
 
     constructor() {
+        this._osVersion = '';
         if (/^win/.test(process.platform)) {
             this._osType = OSType.Windows;
         } else if (/^darwin/.test(process.platform)) {
             this._osType = OSType.OSX;
         } else if (/^linux/.test(process.platform)) {
             this._osType = OSType.Linux;
+            const info = linux_info_from_file();
+            this._osVersion = info.version;
         } else {
             this._osType = OSType.Unsupported;
+        }
+
+        if (this._osVersion === '') {
+            const osVersion = os.release();
+            const clean = semver.clean(osVersion);
+            this._osVersion = clean === null ? '' : clean;
         }
     }
     public get osType(): OSType {
         return this._osType;
+    }
+    public get osVersion(): string {
+        return this._osVersion;
     }
     // XXX deprecate
     public get isWindows(): boolean {
@@ -46,4 +61,39 @@ export class PlatformService implements IPlatformService {
     public get virtualEnvBinName() {
         return this.isWindows ? 'scripts' : 'bin';
     }
+}
+
+// Inspired in part by: https://github.com/juju/os
+class LinuxInfo {
+    public readonly distro: string;
+    public readonly version: string;
+
+    constructor(distro: string, version: string) {
+        this.distro = distro;
+        const clean = semver.clean(version);
+        this.version = clean === null ? '' : clean;
+    }
+}
+
+function linux_info_from_file(filename: string = LINUX_INFO_FILE): LinuxInfo {
+    if (!fs.existsSync(filename)) {
+        return new LinuxInfo('', '');
+    }
+
+    let distro = '';
+    let version = '';
+    const data = fs.readFileSync(filename, 'utf-8');
+    for (const line of data.split(/\n/)) {
+        const parts = line.split('=', 2);
+        switch (parts[0]) {
+            case 'NAME':
+                distro = parts[1];
+                break;
+            case 'VERSION_ID':
+                version = parts[1];
+                break;
+            default:
+        }
+    }
+    return new LinuxInfo(distro, version);
 }
