f99a676f4d7cc024c8bfa696cf8b8a50ae9d67dd
Derek Keeler <dekeeler@microsoft.com>
2018-08-21 11:02:02 -0700

    Add testing to the filesystem implementation.

--- src/test/common/platform/filesystem.unit.test.ts
+++ src/test/common/platform/filesystem.unit.test.ts
@@ -6,7 +6,7 @@ import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as TypeMoq from 'typemoq';
 import { FileSystem } from '../../../client/common/platform/fileSystem';
-import { IFileSystem, IPlatformService } from '../../../client/common/platform/types';
+import { IFileSystem, IPlatformService, TemporaryFile } from '../../../client/common/platform/types';
 // tslint:disable-next-line:no-require-imports no-var-requires
 const assertArrays = require('chai-arrays');
 use(assertArrays);
@@ -87,5 +87,35 @@ suite('FileSystem', () => {
         const expectedFileName = __filename.replace(/\\/g, '/');
         const fileName = files[0].replace(/\\/g, '/');
         expect(fileName).to.equal(expectedFileName);
+    });
+    test('Ensure creating a temporary file results in a unique temp file path', async () => {
+        const tempFile = await fileSystem.createTemporaryFile('.tmp');
+        const tempFile2 = await fileSystem.createTemporaryFile('.tmp');
+        expect(tempFile.filePath).to.not.equal(tempFile2.filePath, 'Temp files must be unique, implementation of createTemporaryFile is off.');
+    });
+    test('Ensure writing to a temp file is supported via file stream', async () => {
+        const tempFile = await fileSystem.createTemporaryFile('.tmp');
+        expect(tempFile).to.be.an('TemporaryFile');
+        const writeStream = fileSystem.createWriteStream(tempFile.filePath);
+        const asyncPromise = new Promise<void>((resolve, reject) => {
+            writeStream.write('hello', 'utf8', (err) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+        await asyncPromise;
+    });
+    test('Ensure chmod works against a temporary file', async () => {
+        await fileSystem.createTemporaryFile('.tmp').then(async (fl: TemporaryFile) => {
+            await fileSystem.chmod(fl.filePath, '7777').then(
+                (success: void) => {
+                    // cannot check for success other than we got here, chmod in Windows won't have any effect on the file itself.
+                },
+                (failReason) => {
+                    expect(failReason).to.equal('There was no error using chmod', `Failed to perform chmod operation successfully, got error ${failReason}`);
+                });
+        });
     });
 });
