8db24474f3018e77d2c47edf919724c933dad585
Brett Cannon <brettcannon@users.noreply.github.com>
2018-08-28 12:10:03 -0700

    Touch up docstring

--- src/test/pythonFiles/linting/threeLineLints.py
+++ src/test/pythonFiles/linting/threeLineLints.py
@@ -9,14 +9,14 @@ class Foo(object):
 
     def meth1(self,arg):
         """missing a space between 'self' and 'arg'. This should trigger the
-        following three line lint warning:
+        following three line lint warning::
 
-        C: 10, 0: Exactly one space required after comma
-            def meth1(self,arg):
-                        ^ (bad-whitespace)
+          C: 10, 0: Exactly one space required after comma
+              def meth1(self,arg):
+                             ^ (bad-whitespace)
 
-        The following three lines should also cause three line lint errors
-        due to "Exactly one space required after comma"
+        The following three lines of tuples should also cause three-line lint
+        errors due to "Exactly one space required after comma" messages.
         """
         a = (1,2)
         b = (1,2)
