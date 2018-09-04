2c350196def571dc5293e18afb97c1ff8e257bc8
Eric Snow <ericsnowcurrently@gmail.com>
2018-09-04 12:45:12 -0600

    Move "text" utils to own file.

--- src/client/refactor/proxy.ts
+++ src/client/refactor/proxy.ts
@@ -7,7 +7,8 @@ import '../common/extensions';
 import { createDeferred, Deferred } from '../common/helpers';
 import { IPythonExecutionFactory } from '../common/process/types';
 import { IPythonSettings } from '../common/types';
-import { getWindowsLineEndingCount, IS_WINDOWS } from '../common/util';
+import { IS_WINDOWS } from '../common/util';
+import { getWindowsLineEndingCount } from '../common/utils/text';
 import { IServiceContainer } from '../ioc/types';
 
 export class RefactorProxy extends Disposable {
