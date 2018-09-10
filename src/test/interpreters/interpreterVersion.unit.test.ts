bfeea9c8a901ce970f604c37c323f02624ec4f10
Don Jayamanne <don.jayamanne@yahoo.com>
2018-09-10 16:23:32 -0700

    Fixed tests

--- src/test/interpreters/interpreterVersion.unit.test.ts
+++ src/test/interpreters/interpreterVersion.unit.test.ts
@@ -1,63 +1,67 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { assert, expect, use } from 'chai';
-import * as chaiAsPromised from 'chai-as-promised';
+import { assert, expect } from 'chai';
+import * as path from 'path';
+import * as typeMoq from 'typemoq';
 import '../../client/common/extensions';
-import { IProcessServiceFactory } from '../../client/common/process/types';
+import { IProcessService, IProcessServiceFactory } from '../../client/common/process/types';
 import { IInterpreterVersionService } from '../../client/interpreter/contracts';
-import { PIP_VERSION_REGEX } from '../../client/interpreter/interpreterVersion';
-import { PYTHON_PATH } from '../common';
-import { UnitTestIocContainer } from '../unittests/serviceRegistry';
-
-use(chaiAsPromised);
+import { InterpreterVersionService } from '../../client/interpreter/interpreterVersion';
 
 suite('Interpreters display version', () => {
-    let ioc: UnitTestIocContainer;
-    setup(initializeDI);
-    teardown(() => ioc.dispose());
+    let processService: typeMoq.IMock<IProcessService>;
+    let interpreterVersionService: IInterpreterVersionService;
 
-    function initializeDI() {
-        ioc = new UnitTestIocContainer();
-        ioc.registerCommonTypes();
-        ioc.registerProcessTypes();
-        ioc.registerVariableTypes();
-        ioc.registerInterpreterTypes();
-    }
+    setup(() => {
+        const processFactory = typeMoq.Mock.ofType<IProcessServiceFactory>();
+        processService = typeMoq.Mock.ofType<IProcessService>();
+        // tslint:disable-next-line:no-any
+        processService.setup((p: any) => p.then).returns(() => undefined);
 
+        processFactory.setup(p => p.create()).returns(() => Promise.resolve(processService.object));
+        interpreterVersionService = new InterpreterVersionService(processFactory.object);
+    });
     test('Must return the Python Version', async () => {
-        const pythonProcess = await ioc.serviceContainer.get<IProcessServiceFactory>(IProcessServiceFactory).create();
-        const output = await pythonProcess.exec(PYTHON_PATH, ['--version'], { cwd: __dirname, mergeStdOutErr: true });
-        const version = output.stdout.splitLines()[0];
-        const interpreterVersion = ioc.serviceContainer.get<IInterpreterVersionService>(IInterpreterVersionService);
-        const pyVersion = await interpreterVersion.getVersion(PYTHON_PATH, 'DEFAULT_TEST_VALUE');
-        assert.equal(pyVersion, version, 'Incorrect version');
+        const pythonPath = path.join('a', 'b', 'python');
+        const pythonVersion = 'Output from the Procecss';
+        processService
+            .setup(p => p.exec(typeMoq.It.isValue(pythonPath), typeMoq.It.isValue(['--version']), typeMoq.It.isAny()))
+            .returns(() => Promise.resolve({ stdout: pythonVersion }))
+            .verifiable(typeMoq.Times.once());
+
+        const pyVersion = await interpreterVersionService.getVersion(pythonPath, 'DEFAULT_TEST_VALUE');
+        assert.equal(pyVersion, pythonVersion, 'Incorrect version');
     });
     test('Must return the default value when Python path is invalid', async () => {
-        const interpreterVersion = ioc.serviceContainer.get<IInterpreterVersionService>(IInterpreterVersionService);
-        const pyVersion = await interpreterVersion.getVersion('INVALID_INTERPRETER', 'DEFAULT_TEST_VALUE');
+        const pythonPath = path.join('a', 'b', 'python');
+        processService
+            .setup(p => p.exec(typeMoq.It.isValue(pythonPath), typeMoq.It.isValue(['--version']), typeMoq.It.isAny()))
+            .returns(() => Promise.reject({}))
+            .verifiable(typeMoq.Times.once());
+
+        const pyVersion = await interpreterVersionService.getVersion(pythonPath, 'DEFAULT_TEST_VALUE');
         assert.equal(pyVersion, 'DEFAULT_TEST_VALUE', 'Incorrect version');
     });
     test('Must return the pip Version.', async () => {
-        const pythonProcess = await ioc.serviceContainer.get<IProcessServiceFactory>(IProcessServiceFactory).create();
-        const result = await pythonProcess.exec(PYTHON_PATH, ['-m', 'pip', '--version'], { cwd: __dirname, mergeStdOutErr: true });
-        const output = result.stdout.splitLines()[0];
-        // Take the second part, see below example.
-        // pip 9.0.1 from /Users/donjayamanne/anaconda3/lib/python3.6/site-packages (python 3.6).
-        const re = new RegExp(PIP_VERSION_REGEX, 'g');
-        const matches = re.exec(output);
-        assert.isNotNull(matches, 'No matches for version found');
-        // tslint:disable-next-line:no-non-null-assertion
-        assert.isAtLeast(matches!.length, 1, 'Version number not found');
+        const pythonPath = path.join('a', 'b', 'python');
+        const pipVersion = '1.2.3';
+        processService
+            .setup(p => p.exec(typeMoq.It.isValue(pythonPath), typeMoq.It.isValue(['-m', 'pip', '--version']), typeMoq.It.isAny()))
+            .returns(() => Promise.resolve({ stdout: pipVersion }))
+            .verifiable(typeMoq.Times.once());
 
-        const interpreterVersion = ioc.serviceContainer.get<IInterpreterVersionService>(IInterpreterVersionService);
-        const pipVersionPromise = interpreterVersion.getPipVersion(PYTHON_PATH);
-        // tslint:disable-next-line:no-non-null-assertion
-        await expect(pipVersionPromise).to.eventually.equal(matches![0].trim());
+        const pyVersion = await interpreterVersionService.getPipVersion(pythonPath);
+        assert.equal(pyVersion, pipVersion, 'Incorrect version');
     });
     test('Must throw an exception when pip version cannot be determined', async () => {
-        const interpreterVersion = ioc.serviceContainer.get<IInterpreterVersionService>(IInterpreterVersionService);
-        const pipVersionPromise = interpreterVersion.getPipVersion('INVALID_INTERPRETER');
+        const pythonPath = path.join('a', 'b', 'python');
+        processService
+            .setup(p => p.exec(typeMoq.It.isValue(pythonPath), typeMoq.It.isValue(['-m', 'pip', '--version']), typeMoq.It.isAny()))
+            .returns(() => Promise.reject())
+            .verifiable(typeMoq.Times.once());
+
+        const pipVersionPromise = interpreterVersionService.getPipVersion(pythonPath);
         await expect(pipVersionPromise).to.be.rejectedWith();
     });
 });
