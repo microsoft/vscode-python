fe1379a8d53dfceafbc5e146e96aa5c5371976ed
Don Jayamanne <don.jayamanne@yahoo.com>
2018-09-10 09:47:58 -0700

    Fixed more tests

--- src/client/interpreter/interpreterService.ts
+++ src/client/interpreter/interpreterService.ts
@@ -22,13 +22,11 @@ export class InterpreterService implements Disposable, IInterpreterService {
     private readonly pythonPathUpdaterService: IPythonPathUpdaterServiceManager;
     private readonly helper: IInterpreterHelper;
     private readonly didChangeInterpreterEmitter = new EventEmitter<void>();
-    private readonly virtualEnvMgr: IVirtualEnvironmentManager;
 
     constructor(@inject(IServiceContainer) private serviceContainer: IServiceContainer) {
         this.locator = serviceContainer.get<IInterpreterLocatorService>(IInterpreterLocatorService, INTERPRETER_LOCATOR_SERVICE);
         this.helper = serviceContainer.get<IInterpreterHelper>(IInterpreterHelper);
         this.pythonPathUpdaterService = this.serviceContainer.get<IPythonPathUpdaterServiceManager>(IPythonPathUpdaterServiceManager);
-        this.virtualEnvMgr = this.serviceContainer.get<IVirtualEnvironmentManager>(IVirtualEnvironmentManager);
     }
 
     public async refresh(resource?: Uri) {
@@ -162,7 +160,8 @@ export class InterpreterService implements Disposable, IInterpreterService {
             // If we do not have the name of the environment, then try to get it again.
             // This can happen based on the context (i.e. resource).
             // I.e. we can determine if an environment is PipEnv only when giving it the right workspacec path (i.e. resource).
-            info.envName = await this.virtualEnvMgr.getEnvironmentName(info.path, resource);
+            const virtualEnvMgr = this.serviceContainer.get<IVirtualEnvironmentManager>(IVirtualEnvironmentManager);
+            info.envName = await virtualEnvMgr.getEnvironmentName(info.path, resource);
         }
         if (info.envName && info.envName.length > 0) {
             envSuffixParts.push(info.envName);
