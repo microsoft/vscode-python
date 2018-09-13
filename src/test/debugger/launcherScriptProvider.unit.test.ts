5089f1322917792a0a56a3777a20b0e2928f80fe
Don Jayamanne <don.jayamanne@yahoo.com>
2018-09-12 17:05:25 -0700

    Remove file test

--- src/test/debugger/launcherScriptProvider.unit.test.ts
+++ src/test/debugger/launcherScriptProvider.unit.test.ts
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 import { expect } from 'chai';
-import * as fs from 'fs';
 import * as path from 'path';
 import { EXTENSION_ROOT_DIR } from '../../client/common/constants';
 import { DebuggerLauncherScriptProvider, NoDebugLauncherScriptProvider } from '../../client/debugger/DebugClients/launcherProvider';
@@ -12,21 +11,10 @@ suite('Debugger - Launcher Script Provider', () => {
         const launcherPath = new DebuggerLauncherScriptProvider().getLauncherFilePath();
         const expectedPath = path.join(EXTENSION_ROOT_DIR, 'pythonFiles', 'experimental', 'ptvsd_launcher.py');
         expect(launcherPath).to.be.equal(expectedPath);
-        console.log(launcherPath);
-        console.log(EXTENSION_ROOT_DIR);
-        const files = fs.readdirSync(path.join(EXTENSION_ROOT_DIR, 'pythonFiles', 'experimental'), 'utf8');
-        files.forEach(item => {
-            console.log(item);
-            const file = path.join(EXTENSION_ROOT_DIR, 'pythonFiles', 'experimental', item);
-            console.log(file);
-            console.log(fs.existsSync(file));
-        });
-        expect(fs.existsSync(launcherPath)).to.be.equal(true, 'file does not exist');
     });
     test('Ensure debugger gets the non debug launcher from PythonTools directory', () => {
         const launcherPath = new NoDebugLauncherScriptProvider().getLauncherFilePath();
         const expectedPath = path.join(EXTENSION_ROOT_DIR, 'pythonFiles', 'experimental', 'ptvsd_launcher.py');
         expect(launcherPath).to.be.equal(expectedPath);
-        expect(fs.existsSync(launcherPath)).to.be.equal(true, 'file does not exist');
     });
 });
