44c9396f42e83412781ad4687438c2161b087b40
Derek Keeler <dekeeler@microsoft.com>
2018-09-14 22:14:57 -0700

    Add telemetry collection for analysis reporting complete.
    - I believe this might fix #2297 (and related issues)
    - Calling 'beginProgress' from LS causes progress reporting to not get cleaned up properly.
    - Related fix for Microsoft/python-language-server#94

--- src/client/activation/progress.ts
+++ src/client/activation/progress.ts
@@ -4,11 +4,18 @@
 import { Progress, ProgressLocation, window } from 'vscode';
 import { Disposable, LanguageClient } from 'vscode-languageclient';
 import { createDeferred, Deferred } from '../../utils/async';
+import { StopWatch } from '../../utils/stopWatch';
+import { sendTelemetryEvent } from '../telemetry';
+import { PYTHON_LANGUAGE_SERVER_ANALYSISTIME } from '../telemetry/constants';
+import { LanguageServerTelemetry } from '../telemetry/types';
 
 export class ProgressReporting {
   private statusBarMessage: Disposable | undefined;
   private progress: Progress<{ message?: string; increment?: number }> | undefined;
   private progressDeferred: Deferred<void> | undefined;
+  private progressTimer: StopWatch | undefined;
+  private progressTimeout: NodeJS.Timer | undefined;
+  private ANALYSIS_TIMEOUT_MS: number = 60000;
 
   constructor(private readonly languageClient: LanguageClient) {
     this.languageClient.onNotification('python/setStatusBarMessage', (m: string) => {
@@ -19,7 +26,14 @@ export class ProgressReporting {
     });
 
     this.languageClient.onNotification('python/beginProgress', async _ => {
+      if (this.progressDeferred) { // if we restarted, no worries as reporting will still funnel to the same place.
+        return;
+      }
+
       this.progressDeferred = createDeferred<void>();
+      this.progressTimer = new StopWatch();
+      this.progressTimeout = setTimeout(this.handleTimeout, this.ANALYSIS_TIMEOUT_MS);
+
       window.withProgress({
         location: ProgressLocation.Window,
         title: ''
@@ -40,7 +54,32 @@ export class ProgressReporting {
       if (this.progressDeferred) {
         this.progressDeferred.resolve();
         this.progressDeferred = undefined;
+        this.completeAnalysisTracking(true);
+        this.progress = undefined;
       }
     });
+  }
+
+  private completeAnalysisTracking(isSuccess: boolean): void {
+    if (this.progressTimer) {
+      const lsAnalysisTelemetry: LanguageServerTelemetry = {
+        success: isSuccess
+      };
+      sendTelemetryEvent(
+        PYTHON_LANGUAGE_SERVER_ANALYSISTIME,
+        this.progressTimer.elapsedTime,
+        lsAnalysisTelemetry
+      );
+      this.progressTimer = undefined;
+    }
+
+    if (this.progressTimeout) {
+      this.progressTimeout = undefined;
+    }
+  }
+
+  // tslint:disable-next-line:no-any
+  private handleTimeout(_args: any[]): void {
+    this.completeAnalysisTracking(false);
   }
 }
