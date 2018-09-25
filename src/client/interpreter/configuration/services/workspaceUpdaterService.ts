921e092980a3204a9c125a21b15dfc1c5c9b6e74
Eric Snow <ericsnowcurrently@gmail.com>
2018-09-25 13:56:05 -0600

    Do not set python.pythonPath to a value with variable substitution.

--- src/client/interpreter/configuration/services/workspaceUpdaterService.ts
+++ src/client/interpreter/configuration/services/workspaceUpdaterService.ts
@@ -1,21 +1,25 @@
-import * as path from 'path';
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
 import { Uri } from 'vscode';
 import { IWorkspaceService } from '../../../common/application/types';
 import { IPythonPathUpdaterService } from '../types';
 
 export class WorkspacePythonPathUpdaterService implements IPythonPathUpdaterService {
-    constructor(private workspace: Uri, private readonly workspaceService: IWorkspaceService) {
-    }
+
+    constructor(
+        private workspace: Uri,
+        private readonly workspaceService: IWorkspaceService
+    ) { }
+
     public async updatePythonPath(pythonPath: string): Promise<void> {
         const pythonConfig = this.workspaceService.getConfiguration('python', this.workspace);
         const pythonPathValue = pythonConfig.inspect<string>('pythonPath');
 
         if (pythonPathValue && pythonPathValue.workspaceValue === pythonPath) {
             return;
-        }
-        if (pythonPath.startsWith(this.workspace.fsPath)) {
-            // tslint:disable-next-line:no-invalid-template-strings
-            pythonPath = path.join('${workspaceFolder}', path.relative(this.workspace.fsPath, pythonPath));
         }
         await pythonConfig.update('pythonPath', pythonPath, false);
     }
