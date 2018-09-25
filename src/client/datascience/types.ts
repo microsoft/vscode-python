dd228f1adf8399c2130eadb804fa92a64bae00a2
Ian Huff <ian.huff@gmail.com>
2018-09-25 14:09:48 -0700

    Small constructor refactor to get services with the service manager and added two small sample unit tests

--- src/client/datascience/types.ts
+++ src/client/datascience/types.ts
@@ -3,9 +3,8 @@
 
 'use strict';
 
-import { ExtensionContext } from 'vscode';
-
 export const IDataScience = Symbol('IDataScience');
 export interface IDataScience {
-    activate(context: ExtensionContext): Promise<void>;
+    activate(): Promise<void>;
+    executeDataScience(): Promise<void>;
 }
