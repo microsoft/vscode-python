269982b1377a6fa32251f02f536b0dfb48a0d5b1
Eric Snow <ericsnowcurrently@gmail.com>
2018-09-26 14:31:57 -0600

    Use matchSetting().

--- src/client/interpreter/configuration/services/workspaceUpdaterService.ts
+++ src/client/interpreter/configuration/services/workspaceUpdaterService.ts
@@ -1,10 +1,12 @@
 import * as path from 'path';
 import { ConfigurationTarget, Uri } from 'vscode';
 import { IWorkspaceService } from '../../../common/application/types';
+import { matchSetting } from '../../../common/configSettings';
 import { IPythonPathUpdaterService } from '../types';
 
 export class WorkspacePythonPathUpdaterService implements IPythonPathUpdaterService {
 
+    private readonly cfgTarget = ConfigurationTarget.Workspace;
     constructor(
         private workspace: Uri,
         private readonly workspaceService: IWorkspaceService
@@ -12,21 +14,23 @@ export class WorkspacePythonPathUpdaterService implements IPythonPathUpdaterServ
 
     public async updatePythonPath(pythonPath: string): Promise<void> {
         const pythonConfig = this.workspaceService.getConfiguration('python', this.workspace);
-        const pythonPathValue = pythonConfig.inspect<string>('pythonPath');
 
-        if (pythonPathValue && pythonPathValue.workspaceValue === pythonPath) {
+        if (matchSetting<string>(pythonConfig, 'pythonPath', this.cfgTarget, pythonPath)) {
             return;
         }
+
         // The workspace folder is guaranteed to be an absolute path.
         if (pythonPath.startsWith(this.workspace.fsPath)) {
             pythonPath = path.relative(this.workspace.fsPath, pythonPath);
         }
-        await pythonConfig.update('pythonPath', pythonPath, false);
+
+        await pythonConfig.update('pythonPath', pythonPath, this.cfgTarget);
     }
 }
 
 export class WorkspaceFolderPythonPathUpdaterService implements IPythonPathUpdaterService {
 
+    private readonly cfgTarget = ConfigurationTarget.WorkspaceFolder;
     constructor(
         private workspaceFolder: Uri,
         private readonly workspaceService: IWorkspaceService
@@ -34,15 +38,16 @@ export class WorkspaceFolderPythonPathUpdaterService implements IPythonPathUpdat
 
     public async updatePythonPath(pythonPath: string): Promise<void> {
         const pythonConfig = this.workspaceService.getConfiguration('python', this.workspaceFolder);
-        const pythonPathValue = pythonConfig.inspect<string>('pythonPath');
 
-        if (pythonPathValue && pythonPathValue.workspaceFolderValue === pythonPath) {
+        if (matchSetting<string>(pythonConfig, 'pythonPath', this.cfgTarget, pythonPath)) {
             return;
         }
+
         // The workspace folder is guaranteed to be an absolute path.
         if (pythonPath.startsWith(this.workspaceFolder.fsPath)) {
             pythonPath = path.relative(this.workspaceFolder.fsPath, pythonPath);
         }
-        await pythonConfig.update('pythonPath', pythonPath, ConfigurationTarget.WorkspaceFolder);
+
+        await pythonConfig.update('pythonPath', pythonPath, this.cfgTarget);
     }
 }
