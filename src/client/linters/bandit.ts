4e95bdfd9492f837bc4eaed2ecb09c3b0527dc4f
Steven Demurjian <steven.demurjian@soroco.com>
2018-10-02 22:42:14 -0400

    Remove custom regex

--- src/client/linters/bandit.ts
+++ src/client/linters/bandit.ts
@@ -8,15 +8,13 @@ import { IServiceContainer } from '../ioc/types';
 import { BaseLinter } from './baseLinter';
 import { ILintMessage } from './types';
 
-const REGEX = '(?<file>.py):(?<line>\\d+): (?<code>B\\S+): (?<type>\\w+): (?<message>.*)\\r?(\\n|$)';
-
 export class Bandit extends BaseLinter {
     constructor(outputChannel: OutputChannel, serviceContainer: IServiceContainer) {
         super(Product.bandit, outputChannel, serviceContainer);
     }
 
     protected async runLinter(document: TextDocument, cancellation: CancellationToken): Promise<ILintMessage[]> {
-        const messages = await this.run(['-f', 'custom', document.uri.fsPath], document, cancellation, REGEX);
+        const messages = await this.run(['-f', 'custom', '--msg-template', '{line},0,{severity},{test_id}:{msg}', document.uri.fsPath], document, cancellation);
         messages.forEach(msg => {
             msg.severity = this.parseMessagesSeverity(msg.type, this.pythonSettings.linting.banditCategorySeverity);
             msg.code = msg.type;
