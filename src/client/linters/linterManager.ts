7c890bbcf47c9e92f3a1184ad54e73266f1192eb
Derek Keeler <dekeeler@microsoft.com>
2018-10-19 12:54:47 -0700

    Relocate logic that checks for and enables available linter.
    - new class `AvailableLinterActivator`
    - DI enable the new class
    - correct some issues with DI usage in the original logic

--- src/client/linters/linterManager.ts
+++ src/client/linters/linterManager.ts
@@ -5,16 +5,10 @@
 
 import { inject, injectable } from 'inversify';
 import {
-    CancellationToken, OutputChannel,
-    TextDocument, Uri
+    CancellationToken, OutputChannel, TextDocument, Uri
 } from 'vscode';
-import { IApplicationShell, IWorkspaceService } from '../common/application/types';
-import { STANDARD_OUTPUT_CHANNEL } from '../common/constants';
-import { LinterInstaller } from '../common/installer/productInstaller';
-import { error as traceError } from '../common/logger';
 import {
-    IConfigurationService, ILogger,
-    IOutputChannel, Product
+    IConfigurationService, ILogger, Product
 } from '../common/types';
 import { IServiceContainer } from '../ioc/types';
 import { Bandit } from './bandit';
@@ -27,8 +21,7 @@ import { PyDocStyle } from './pydocstyle';
 import { PyLama } from './pylama';
 import { Pylint } from './pylint';
 import {
-    ILinter, ILinterInfo,
-    ILinterManager, ILintMessage
+    IAvailableLinterActivator, ILinter, ILinterInfo, ILinterManager, ILintMessage
 } from './types';
 
 class DisabledLinter implements ILinter {
@@ -84,120 +77,15 @@ export class LinterManager implements ILinterManager {
         await this.configService.updateSetting(`linting.${this.lintingEnabledSettingName}`, enable, resource);
     }
 
-    /**
-     * Check if it is possible to enable an otherwise-unconfigured linter in
-     * the current workspace, and if so ask the user if they want that linter
-     * configured explicitly.
-     *
-     * @param linterInfo The linter to check installation status.
-     * @param resource Context for the operation (required when in multi-root workspaces).
-     *
-     * @returns true if configuration was updated in any way, false otherwise.
-     */
-    public async promptUserIfLinterAvailable(linterInfo: ILinterInfo, resource?: Uri): Promise<boolean> {
-        // if we've already checked during this session, don't bother again
-        if (!this.checkedForInstalledLinters) {
-            this.checkedForInstalledLinters = true;
-        } else {
-            return false;
-        }
-
-        // If linting is disabled, we are finished.
-        if (!await this.isLintingEnabled(false, resource)) {
-            return false;
-        }
-
-        // Has the linter in question has been configured explicitly? If so, no need to continue.
-        if (!this.isLinterUsingDefaultConfiguration(linterInfo, resource)) {
-            return false;
-        }
-
-        // Is the linter available in the current workspace?
-        if (await this.isLinterAvailable(linterInfo.product, resource)) {
-            // ...ask the user if they would like to enable it.
-            return this.notifyUserAndConfigureLinter(linterInfo);
-        } else {
-            return false;
-        }
-
-    }
-
-    /// Raise a dialog asking the user if they would like to explicitly configure a linter or not.
-    /// Return true if a config change was made.
-    public async notifyUserAndConfigureLinter(linterInfo: ILinterInfo): Promise<boolean> {
-        const appShell = this.serviceContainer.get<IApplicationShell>(IApplicationShell);
-
-        type ConfigureLinterMessage = {
-            enabled: boolean;
-            title: string;
-        };
-
-        const optButtons: ConfigureLinterMessage[] = [
-            {
-                title: `Enable ${linterInfo.id}`,
-                enabled: true
-            },
-            {
-                title: `Disable ${linterInfo.id}`,
-                enabled: false
-            }
-        ];
-        const pick = await appShell.showInformationMessage(`Linter ${linterInfo.id} is available but not enabled.`, ...optButtons);
-        if (pick) {
-            await linterInfo.enableAsync(pick.enabled);
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * Check if the linter itself is available in the workspace's Python environment or
-     * not.
-     *
-     * @param linterProduct Linter to check in the current workspace environment.
-     * @param resource Context information for workspace.
-     */
-    public async isLinterAvailable(linterProduct: Product, resource?: Uri): Promise<boolean | undefined> {
-        const outputChannel = this.serviceContainer.get<IOutputChannel>(IOutputChannel, STANDARD_OUTPUT_CHANNEL);
-        const linterInstaller = new LinterInstaller(this.serviceContainer, outputChannel);
-
-        return linterInstaller.isInstalled(linterProduct, resource)
-            .catch((reason) => {
-                // report and continue, assume the linter is unavailable.
-                traceError(`[WARNING]: Failed to discover if linter ${linterProduct} is installed.`, reason);
-                return false;
-            });
-    }
-
-    /**
-     * Check if the given linter has been configured by the user in this workspace or not.
-     *
-     * @param linterInfo Linter to check for configuration status.
-     * @param resource Context information.
-     *
-     * @returns true if the linter has not been configured at the user, workspace, or workspace-folder scope. false otherwise.
-     */
-    public isLinterUsingDefaultConfiguration(linterInfo: ILinterInfo, resource?: Uri) {
-        const workspaceConfig = this.serviceContainer.get<IWorkspaceService>(IWorkspaceService);
-        const ws = workspaceConfig.getConfiguration('python.linting', resource);
-        const pe = ws!.inspect(linterInfo.enabledSettingName);
-        return (pe!.globalValue === undefined && pe!.workspaceValue === undefined && pe!.workspaceFolderValue === undefined);
-    }
-
     public async getActiveLinters(silent: boolean, resource?: Uri): Promise<ILinterInfo[]> {
-        if (silent) {
-            // only ask the user if they'd like to enable pylint when it is available... others may follow.
-            const pylintInfo = this.linters.find((linter: ILinterInfo) => linter.id === 'pylint');
-            if (pylintInfo) {
-                await this.promptUserIfLinterAvailable(pylintInfo, resource);
-            }
+        if (!silent) {
+            await this.enableUnconfiguredLinters(resource);
         }
         return this.linters.filter(x => x.isEnabled(resource));
     }
 
     public async setActiveLintersAsync(products: Product[], resource?: Uri): Promise<void> {
-        const active = await this.getActiveLinters(false, resource);
+        const active = await this.getActiveLinters(true, resource);
         for (const x of active) {
             await x.enableAsync(false, resource);
         }
@@ -211,7 +99,7 @@ export class LinterManager implements ILinterManager {
     }
 
     public async createLinter(product: Product, outputChannel: OutputChannel, serviceContainer: IServiceContainer, resource?: Uri): Promise<ILinter> {
-        if (!await this.isLintingEnabled(false, resource)) {
+        if (!await this.isLintingEnabled(true, resource)) {
             return new DisabledLinter(this.configService);
         }
         const error = 'Linter manager: Unknown linter';
@@ -237,5 +125,25 @@ export class LinterManager implements ILinterManager {
                 break;
         }
         throw new Error(error);
+    }
+
+    private async enableUnconfiguredLinters(resource?: Uri): Promise<boolean> {
+        // if we've already checked during this session, don't bother again
+        if (this.checkedForInstalledLinters) {
+            return false;
+        }
+        this.checkedForInstalledLinters = true;
+
+        // only check & ask the user if they'd like to enable pylint
+        const pylintInfo = this.linters.find(
+            (linter: ILinterInfo) => linter.id === 'pylint'
+        );
+
+        // If linting is disabled, don't bother checking further.
+        if (pylintInfo && await this.isLintingEnabled(true, resource)) {
+            const activator = this.serviceContainer.get<IAvailableLinterActivator>(IAvailableLinterActivator);
+            return activator.promptIfLinterAvailable(pylintInfo, resource);
+        }
+        return false;
     }
 }
