5c04a0f6445c3ec676ca5a8c5bc1ea7210e1e6cb
Don Jayamanne <don.jayamanne@yahoo.com>
2018-10-19 17:09:10 -0700

    without header

--- src/client/jupyter/blah.ts
+++ src/client/jupyter/blah.ts
@@ -1,8 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-'use strict';
-
 import { Range, window, TextDocument, Position } from 'vscode';
 
 
@@ -17,6 +12,8 @@ export class JupyterProvider {
         window.showErrorMessage('Hello', 'one', 'two');
         return /^(# %%|#%%|# \<codecell\>|# In\[\d*?\]|# In\[ \])(.*)/i;
     }
+
+
 
     /**
      * Returns the selected code
