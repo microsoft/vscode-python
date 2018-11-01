ba2f99a9d1ff500eaca70b5dca5fb4c040a5679e
Rich Chiodo <rchiodo@microsoft.com>
2018-11-01 11:28:42 -0700

    Implement more notebook tests

--- src/test/unittests.ts
+++ src/test/unittests.ts
@@ -16,7 +16,7 @@ process.env.VSC_PYTHON_UNIT_TEST = '1';
 // this allows us to run hygiene as a git pre-commit hook or via debugger.
 if (require.main === module) {
     // When running from debugger, allow custom args.
-    const args = extractParams();
+    const args = extractParams(1000);
 
     runTests({ filePattern: '**/**.unit.test.js', grep: args.grep, timeout: args.timeout });
 }
