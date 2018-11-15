f4dd06ba040b4a9e5669eb19e059c0849535a462
Don Jayamanne <don.jayamanne@yahoo.com>
2018-11-14 17:15:40 -0800

    Update comments

--- src/client/constants.ts
+++ src/client/constants.ts
@@ -6,6 +6,6 @@
 import * as path from 'path';
 
 // This file is also used by the debug adapter.
-// When bundling, the bundle file for the da ends up elsewhere.
+// When bundling, the bundle file for the debug adapter ends up elsewhere.
 const folderName = path.basename(__dirname);
 export const EXTENSION_ROOT_DIR = folderName === 'client' ? path.join(__dirname, '..', '..') : path.join(__dirname, '..', '..', '..', '..');
