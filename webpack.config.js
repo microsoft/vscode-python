ff02e2f7d0a16daaebadd0f5e1ab43f8f37a7990
Don Jayamanne <don.jayamanne@yahoo.com>
2018-11-14 17:10:58 -0800

    Unified webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
 const merge = require('webpack-merge');
 const datascience = require('./webpack.datascience-ui.config.js');
+const extension = require('./build/webpack/webpack.extension.config.js').default;
 
-module.exports = [merge(datascience, {
-    devtool: 'eval'
-})];
+module.exports = [
+    merge(datascience, {
+        devtool: 'eval'
+    }),
+    merge(extension, {
+        mode: 'production',
+        devtool: 'source-map',
+    })
+];
