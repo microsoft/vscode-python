564f7c532570c42ec8a2b004ec335a123329b03a
Don Jayamanne <don.jayamanne@yahoo.com>
2018-11-27 11:43:52 -0800

    Add tests

--- src/client/application/diagnostics/surceMapSupportService.ts
+++ src/client/application/diagnostics/surceMapSupportService.ts
@@ -5,22 +5,31 @@
 
 import { inject, injectable } from 'inversify';
 import { ConfigurationTarget } from 'vscode';
-import { ICommandManager } from '../../common/application/types';
+import { IApplicationShell, ICommandManager } from '../../common/application/types';
 import { Commands } from '../../common/constants';
 import { IConfigurationService, IDisposableRegistry } from '../../common/types';
+import { Diagnostics } from '../../common/utils/localize';
 import { ISourceMapSupportService } from './types';
 
 @injectable()
 export class SourceMapSupportService implements ISourceMapSupportService {
     constructor(@inject(ICommandManager) private readonly commandManager: ICommandManager,
         @inject(IDisposableRegistry) private readonly disposables: IDisposableRegistry,
-        @inject(IConfigurationService) private readonly configurationService: IConfigurationService) {
+        @inject(IConfigurationService) private readonly configurationService: IConfigurationService,
+        @inject(IApplicationShell) private readonly shell: IApplicationShell) {
 
     }
     public register(): void {
-        this.disposables.push(this.commandManager.registerCommand(Commands.Enable_SourceMap_Support, this.enable, this));
+        this.disposables.push(this.commandManager.registerCommand(Commands.Enable_SourceMap_Support, this.onEnable, this));
     }
     public async enable(): Promise<void> {
         await this.configurationService.updateSetting('diagnostics.sourceMapsEnabled', true, undefined, ConfigurationTarget.Global);
+    }
+    protected async onEnable(): Promise<void> {
+        const enableSourceMapsAndReloadVSC = Diagnostics.enableSourceMapsAndReloadVSC();
+        const selection = await this.shell.showWarningMessage(Diagnostics.warnBeforeEnablingSourceMaps(), enableSourceMapsAndReloadVSC);
+        if (selection === enableSourceMapsAndReloadVSC) {
+            await this.enable();
+        }
     }
 }
--- src/test/application/diagnostics/sourceMapSupportService.unit.test.ts
+++ src/test/application/diagnostics/sourceMapSupportService.unit.test.ts
@@ -5,26 +5,57 @@
 
 // tslint:disable:no-any
 
-import { anyFunction, instance, mock, verify, when } from 'ts-mockito';
+import { anyFunction, anything, instance, mock, verify, when } from 'ts-mockito';
 import { ConfigurationTarget } from 'vscode';
 import { SourceMapSupportService } from '../../../client/application/diagnostics/surceMapSupportService';
+import { ApplicationShell } from '../../../client/common/application/applicationShell';
 import { CommandManager } from '../../../client/common/application/commandManager';
 import { ConfigurationService } from '../../../client/common/configuration/service';
 import { Commands } from '../../../client/common/constants';
+import { Diagnostics } from '../../../client/common/utils/localize';
 
 suite('Diagnostisc - Source Maps', () => {
     test('Command is registered', async () => {
         const commandManager = mock(CommandManager);
-        const service = new SourceMapSupportService(instance(commandManager), [], undefined as any);
+        const service = new SourceMapSupportService(instance(commandManager), [], undefined as any, undefined as any);
         service.register();
         verify(commandManager.registerCommand(Commands.Enable_SourceMap_Support, anyFunction(), service)).once();
     });
     test('Setting is turned on', async () => {
         const configService = mock(ConfigurationService);
-        const service = new SourceMapSupportService(undefined as any, [], instance(configService));
+        const service = new SourceMapSupportService(undefined as any, [], instance(configService), undefined as any);
         when(configService.updateSetting('diagnostics.sourceMapsEnabled', true, undefined, ConfigurationTarget.Global)).thenResolve();
 
         await service.enable();
+        verify(configService.updateSetting('diagnostics.sourceMapsEnabled', true, undefined, ConfigurationTarget.Global)).once();
+    });
+    test('Display prompt and do not enable', async () => {
+        const shell = mock(ApplicationShell);
+        const service = new class extends SourceMapSupportService {
+            public async enable() {
+                throw new Error('Should not be invokved');
+            }
+            public async onEnable() {
+                await super.onEnable();
+            }
+        }(undefined as any, [], undefined as any, instance(shell));
+        when(shell.showWarningMessage(anything(), anything())).thenResolve();
+
+        await service.onEnable();
+    });
+    test('Display prompt and must enable', async () => {
+        const configService = mock(ConfigurationService);
+        const shell = mock(ApplicationShell);
+        const service = new class extends SourceMapSupportService {
+            public async onEnable() {
+                await super.onEnable();
+            }
+        }(undefined as any, [], instance(configService), instance(shell));
+
+        when(configService.updateSetting('diagnostics.sourceMapsEnabled', true, undefined, ConfigurationTarget.Global)).thenResolve();
+        when(shell.showWarningMessage(anything(), anything())).thenResolve(Diagnostics.enableSourceMapsAndReloadVSC() as any);
+
+        await service.onEnable();
         verify(configService.updateSetting('diagnostics.sourceMapsEnabled', true, undefined, ConfigurationTarget.Global)).once();
     });
 });
