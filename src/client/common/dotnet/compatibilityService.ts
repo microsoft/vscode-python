415f1876d474ec65c840e797bb13f878b9edeabb
Don Jayamanne <don.jayamanne@yahoo.com>
2018-12-07 10:21:10 -0800

    Update with comments

--- src/client/common/dotnet/compatibilityService.ts
+++ src/client/common/dotnet/compatibilityService.ts
@@ -8,6 +8,14 @@ import { IPlatformService } from '../platform/types';
 import { OSType } from '../utils/platform';
 import { IDotNetCompatibilityService, IOSDotNetCompatibilityService } from './types';
 
+/**
+ * .NET Core 2.1 OS Requirements
+ * https://github.com/dotnet/core/blob/master/release-notes/2.1/2.1-supported-os.md
+ * We are using the versions provided in the above .NET 2.1 Core requirements page as minimum required versions.
+ * Why, cuz getting distros, mapping them to the ones listd on .NET 2.1 Core requirements are entirely accurate.
+ * Due to the inaccuracy, its easier and safer to just assume futur versions of an OS are also supported.
+ * We will need to regularly update the requirements over time, when using .NET Core 2.2 or 3, etc.
+ */
 @injectable()
 export class DotNetCompatibilityService implements IDotNetCompatibilityService {
     private readonly mappedServices = new Map<OSType, IDotNetCompatibilityService>();
