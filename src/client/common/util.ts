c0e431dde687d572c99224ff487a5039baf9f19a
Don Jayamanne <don.jayamanne@yahoo.com>
2018-12-10 12:49:34 -0800

    Remove dup exports of IS_WINDOWS

--- src/client/common/util.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-'use strict';
-
-// tslint:disable-next-line: no-suspicious-comment
-// TODO: Usage of these should be replaced by OSInfo.* or
-// IPlatformService.* (from src/client/common/platform).
-export const IS_WINDOWS = /^win/.test(process.platform);
