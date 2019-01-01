7b89e6c505e689317c8df24f055eaeff2381dd95
Derek Keeler <dekeeler@microsoft.com>
2018-12-31 20:06:06 -0700

    pep526 is meant for Python >= 3.6 only. Disable pep526 tests for other versions.

--- src/test/languageServers/jedi/autocomplete/pep526.test.ts
+++ src/test/languageServers/jedi/autocomplete/pep526.test.ts
@@ -20,17 +20,16 @@ const filePep526 = path.join(autoCompPath, 'pep526.py');
 
 // tslint:disable-next-line:max-func-body-length
 suite('Autocomplete PEP 526', () => {
-    let isPython2: boolean;
     let ioc: UnitTestIocContainer;
     suiteSetup(async function () {
+        // Pep526 only valid for 3.6+ (#2545)
+        if (await isPythonVersion('2', '3.4', '3.5')) {
+            // tslint:disable-next-line:no-invalid-this
+            return this.skip();
+        }
+
         await initialize();
         initializeDI();
-        isPython2 = await ioc.getPythonMajorVersion(rootWorkspaceUri) === 2;
-        if (isPython2) {
-            // tslint:disable-next-line:no-invalid-this
-            this.skip();
-            return;
-        }
     });
     setup(initializeTest);
     suiteTeardown(closeActiveWindows);
@@ -44,17 +43,7 @@ suite('Autocomplete PEP 526', () => {
         ioc.registerVariableTypes();
         ioc.registerProcessTypes();
     }
-
-    test('variable (abc:str)', async function () {
-        // This test has not been working for many months in Python 3.4 and 3.5 under
-        // Windows and macOS.Tracked by #2545.
-        if (isOs(OSType.Windows, OSType.OSX)) {
-            if (await isPythonVersion('3.4', '3.5')) {
-                // tslint:disable-next-line:no-invalid-this
-                return this.skip();
-            }
-        }
-
+    test('variable (abc:str)', async () => {
         const textDocument = await vscode.workspace.openTextDocument(filePep526);
         await vscode.window.showTextDocument(textDocument);
         assert(vscode.window.activeTextEditor, 'No active editor');
@@ -65,16 +54,7 @@ suite('Autocomplete PEP 526', () => {
         assert.notEqual(list!.items.filter(item => item.label === 'lower').length, 0, 'lower not found');
     });
 
-    test('variable (abc: str = "")', async function () {
-        // This test has not been working for many months in Python 3.4 and 3.5 under
-        // Windows and macOS.Tracked by #2545.
-        if (isOs(OSType.Windows, OSType.OSX)) {
-            if (await isPythonVersion('3.4', '3.5')) {
-                // tslint:disable-next-line:no-invalid-this
-                return this.skip();
-            }
-        }
-
+    test('variable (abc: str = "")', async () => {
         const textDocument = await vscode.workspace.openTextDocument(filePep526);
         await vscode.window.showTextDocument(textDocument);
         assert(vscode.window.activeTextEditor, 'No active editor');
@@ -85,16 +65,7 @@ suite('Autocomplete PEP 526', () => {
         assert.notEqual(list!.items.filter(item => item.label === 'lower').length, 0, 'lower not found');
     });
 
-    test('variable (abc = UNKNOWN # type: str)', async function () {
-        // This test has not been working for many months in Python 3.4 and 3.5 under
-        // Windows and macOS.Tracked by #2545.
-        if (isOs(OSType.Windows, OSType.OSX)) {
-            if (await isPythonVersion('3.4', '3.5')) {
-                // tslint:disable-next-line:no-invalid-this
-                return this.skip();
-            }
-        }
-
+    test('variable (abc = UNKNOWN # type: str)', async () => {
         const textDocument = await vscode.workspace.openTextDocument(filePep526);
         await vscode.window.showTextDocument(textDocument);
         assert(vscode.window.activeTextEditor, 'No active editor');
@@ -105,16 +76,7 @@ suite('Autocomplete PEP 526', () => {
         assert.notEqual(list!.items.filter(item => item.label === 'lower').length, 0, 'lower not found');
     });
 
-    test('class methods', async function () {
-        // This test has not been working for many months in Python 3.4 and 3.5 under
-        // Windows and macOS.Tracked by #2545.
-        if (isOs(OSType.Windows, OSType.OSX)) {
-            if (await isPythonVersion('3.4', '3.5')) {
-                // tslint:disable-next-line:no-invalid-this
-                return this.skip();
-            }
-        }
-
+    test('class methods', async () => {
         const textDocument = await vscode.workspace.openTextDocument(filePep526);
         await vscode.window.showTextDocument(textDocument);
         assert(vscode.window.activeTextEditor, 'No active editor');
