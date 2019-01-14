ad72a0cd23411ca821d52a30146b2ceb559b29c6
Eric Snow <ericsnowcurrently@gmail.com>
2019-01-14 15:04:54 -0700

    Re-enable the tests.

--- src/test/linters/lint.unit.test.ts
+++ src/test/linters/lint.unit.test.ts
@@ -109,11 +109,6 @@ suite('Linting - functional (mocked)', () => {
     let configService: IConfigurationService;
 
     suiteSetup(async function () {
-        // these tests are currently flakey, and need some refactoring to become reliable
-        // See #3914
-        // tslint:disable-next-line:no-invalid-this
-        return this.skip();
-
         await initialize();
     });
     setup(async () => {
@@ -165,27 +160,26 @@ suite('Linting - functional (mocked)', () => {
     }
 
     async function testEnablingDisablingOfLinter(product: Product, enabled: boolean, file?: string) {
-        // Reenable this after fixing #3922
-        // const setting = makeSettingKey(product);
-        // const output = ioc.serviceContainer.get<MockOutputChannel>(IOutputChannel, STANDARD_OUTPUT_CHANNEL);
+        const setting = makeSettingKey(product);
+        const output = ioc.serviceContainer.get<MockOutputChannel>(IOutputChannel, STANDARD_OUTPUT_CHANNEL);
 
-        // await configService.updateSetting(setting, enabled, rootWorkspaceUri,
-        //     IS_MULTI_ROOT_TEST ? ConfigurationTarget.WorkspaceFolder : ConfigurationTarget.Workspace);
+        await configService.updateSetting(setting, enabled, rootWorkspaceUri,
+            IS_MULTI_ROOT_TEST ? ConfigurationTarget.WorkspaceFolder : ConfigurationTarget.Workspace);
 
-        // file = file ? file : fileToLint;
-        // const document = await workspace.openTextDocument(file);
-        // const cancelToken = new CancellationTokenSource();
+        file = file ? file : fileToLint;
+        const document = await workspace.openTextDocument(file);
+        const cancelToken = new CancellationTokenSource();
 
-        // await linterManager.setActiveLintersAsync([product]);
-        // await linterManager.enableLintingAsync(enabled);
-        // const linter = await linterManager.createLinter(product, output, ioc.serviceContainer);
+        await linterManager.setActiveLintersAsync([product]);
+        await linterManager.enableLintingAsync(enabled);
+        const linter = await linterManager.createLinter(product, output, ioc.serviceContainer);
 
-        // const messages = await linter.lint(document, cancelToken.token);
-        // if (enabled) {
-        //     assert.notEqual(messages.length, 0, `No linter errors when linter is enabled, Output - ${output.output}`);
-        // } else {
-        //     assert.equal(messages.length, 0, `Errors returned when linter is disabled, Output - ${output.output}`);
-        // }
+        const messages = await linter.lint(document, cancelToken.token);
+        if (enabled) {
+            assert.notEqual(messages.length, 0, `No linter errors when linter is enabled, Output - ${output.output}`);
+        } else {
+            assert.equal(messages.length, 0, `Errors returned when linter is disabled, Output - ${output.output}`);
+        }
     }
 
     test('Disable Pylint and test linter', async () => {
