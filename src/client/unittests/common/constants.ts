b9a1754e739a40f0e45feb44a2b64f960ef1b196
Eric Snow <ericsnowcurrently@gmail.com>
2019-02-06 12:49:29 -0700

    Add UNIT_TEST_PRODUCTS.

--- src/client/unittests/common/constants.ts
+++ src/client/unittests/common/constants.ts
@@ -1,4 +1,5 @@
-import { TestProvider } from './types';
+import { Product } from '../../common/types';
+import { TestProvider, UnitTestProduct } from './types';
 
 export const CANCELLATION_REASON = 'cancelled_user_request';
 export enum CommandSource {
@@ -8,6 +9,12 @@ export enum CommandSource {
     commandPalette = 'commandpalette'
 }
 export const TEST_OUTPUT_CHANNEL = 'TEST_OUTPUT_CHANNEL';
+
+export const UNIT_TEST_PRODUCTS: UnitTestProduct[] = [
+    Product.pytest,
+    Product.unittest,
+    Product.nosetest
+];
 export const NOSETEST_PROVIDER: TestProvider = 'nosetest';
 export const PYTEST_PROVIDER: TestProvider = 'pytest';
 export const UNITTEST_PROVIDER: TestProvider = 'unittest';
