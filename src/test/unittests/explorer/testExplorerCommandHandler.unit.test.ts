bba7871ea34223ecabd82d897877ff15a745f412
Derek Keeler <dekeeler@microsoft.com>
2019-02-14 11:22:20 -0800

    Update Test Explorer command handler for use with the correct type.
    - Correct unit tests for command handler

--- src/test/unittests/explorer/testExplorerCommandHandler.unit.test.ts
+++ src/test/unittests/explorer/testExplorerCommandHandler.unit.test.ts
@@ -10,21 +10,25 @@ import { Uri } from 'vscode';
 import { CommandManager } from '../../../client/common/application/commandManager';
 import { ICommandManager } from '../../../client/common/application/types';
 import { Commands } from '../../../client/common/constants';
+import { ITestDataItemResource, TestDataItem } from '../../../client/providers/types';
 import { CommandSource } from '../../../client/unittests/common/constants';
 import { TestsHelper } from '../../../client/unittests/common/testUtils';
 import { TestFile, TestFunction, TestsToRun, TestSuite } from '../../../client/unittests/common/types';
 import { TestExplorerCommandHandler } from '../../../client/unittests/explorer/commandHandlers';
 import { TestTreeItem } from '../../../client/unittests/explorer/testTreeViewItem';
+import { TestTreeViewProvider } from '../../../client/unittests/explorer/testTreeViewProvider';
 import { ITestExplorerCommandHandler } from '../../../client/unittests/navigation/types';
 
 // tslint:disable:no-any max-func-body-length
 suite('Unit Tests - Test Explorer Command Handler', () => {
     let commandHandler: ITestExplorerCommandHandler;
     let cmdManager: ICommandManager;
+    let testResourceMapper: ITestDataItemResource;
 
     setup(() => {
         cmdManager = mock(CommandManager);
-        commandHandler = new TestExplorerCommandHandler(instance(cmdManager));
+        testResourceMapper = mock(TestTreeViewProvider);
+        commandHandler = new TestExplorerCommandHandler(instance(cmdManager), instance(testResourceMapper));
     });
     test('Commands are registered', () => {
         commandHandler.register();
@@ -50,16 +54,15 @@ suite('Unit Tests - Test Explorer Command Handler', () => {
         disposable3.verify(d => d.dispose(), typemoq.Times.once());
     });
     async function testOpeningTestNode(data: TestFile | TestSuite | TestFunction, expectedCommand: string) {
-        const treeItem = mock(TestTreeItem);
+        //const treeItem = mock(TestTreeItem);
         const resource = Uri.file(__filename);
-        when(treeItem.data).thenReturn(data);
-        when(treeItem.resource).thenReturn(resource);
-        when(treeItem.testType).thenReturn(TestsHelper.getTestType(data));
+        when(testResourceMapper.getResource(data)).thenReturn(resource);
 
         commandHandler.register();
 
         const handler = capture(cmdManager.registerCommand).last()[1];
-        await handler.bind(commandHandler)(instance(treeItem));
+        await handler.bind(commandHandler)(data);
+        const cap = capture(cmdManager.executeCommand).last();
 
         verify(cmdManager.executeCommand(expectedCommand, resource, data, true)).once();
     }
@@ -78,17 +81,14 @@ suite('Unit Tests - Test Explorer Command Handler', () => {
     });
     async function testRunOrDebugTestNode(data: TestFile | TestSuite | TestFunction,
         expectedTestRun: TestsToRun, runType: 'run' | 'debug') {
-        const treeItem = mock(TestTreeItem);
         const resource = Uri.file(__filename);
-        when(treeItem.data).thenReturn(data);
-        when(treeItem.testType).thenReturn(TestsHelper.getTestType(data));
-        when(treeItem.resource).thenReturn(resource);
+        when(testResourceMapper.getResource(data)).thenReturn(resource);
 
         commandHandler.register();
 
         const capturedCommand = capture(cmdManager.registerCommand);
         const handler = runType === 'run' ? capturedCommand.first()[1] : capturedCommand.second()[1];
-        await handler.bind(commandHandler)(instance(treeItem));
+        await handler.bind(commandHandler)(data);
 
         const cmd = runType === 'run' ? Commands.Tests_Run : Commands.Tests_Debug;
         verify(cmdManager.executeCommand(cmd, undefined, CommandSource.testExplorer, resource, deepEqual(expectedTestRun))).once();
