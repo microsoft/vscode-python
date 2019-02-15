efecd3a239e6b2363c3a967d8e244bd7ca65b842
Kartik Raj <karraj@microsoft.com>
2019-02-15 14:06:51 -0800

    corrections

--- src/test/common/socketStream.test.ts
+++ src/test/common/socketStream.test.ts
@@ -4,41 +4,45 @@
 //
 
 // Place this right on top
-import { initialize } from './../initialize';
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
-import { SocketStream } from '../../client/common/net/socket/SocketStream';
 import * as net from 'net';
-const uint64be = require("uint64be");
+import { SocketStream } from '../../client/common/net/socket/SocketStream';
+// tslint:disable:no-require-imports no-var-requires
+const uint64be = require('uint64be');
 
 class MockSocket {
+    private _data: string;
+    // tslint:disable-next-line:no-any
+    private _rawDataWritten: any;
     constructor() {
         this._data = '';
     }
-    private _data: string;
-    private _rawDataWritten: any;
     public get dataWritten(): string {
         return this._data;
     }
+    // tslint:disable-next-line:no-any
     public get rawDataWritten(): any {
         return this._rawDataWritten;
     }
-    write(data: any) {
-        this._data = data + '';
+    // tslint:disable-next-line:no-any
+    public write(data: any) {
+        this._data = `${data}` + '';
         this._rawDataWritten = data;
     }
 }
 // Defines a Mocha test suite to group tests of similar kind together
+// tslint:disable-next-line:max-func-body-length
 suite('SocketStream', () => {
     test('Read Byte', done => {
-        let buffer = new Buffer("X");
+        const buffer = new Buffer('X');
         const byteValue = buffer[0];
         const socket = new MockSocket();
-        const stream = new SocketStream((socket as any) as net.Socket, buffer)
+        // tslint:disable-next-line:no-any
+        const stream = new SocketStream((socket as any) as net.Socket, buffer);
 
         assert.equal(stream.ReadByte(), byteValue);
         done();
@@ -46,8 +50,9 @@ suite('SocketStream', () => {
     test('Read Int32', done => {
         const num = 1234;
         const socket = new MockSocket();
-        let buffer = uint64be.encode(num);
-        const stream = new SocketStream((socket as any) as net.Socket, buffer)
+        const buffer = uint64be.encode(num);
+        // tslint:disable-next-line:no-any
+        const stream = new SocketStream((socket as any) as net.Socket, buffer);
 
         assert.equal(stream.ReadInt32(), num);
         done();
@@ -55,8 +60,9 @@ suite('SocketStream', () => {
     test('Read Int64', done => {
         const num = 9007199254740993;
         const socket = new MockSocket();
-        let buffer = uint64be.encode(num);
-        const stream = new SocketStream((socket as any) as net.Socket, buffer)
+        const buffer = uint64be.encode(num);
+        // tslint:disable-next-line:no-any
+        const stream = new SocketStream((socket as any) as net.Socket, buffer);
 
         assert.equal(stream.ReadInt64(), num);
         done();
@@ -64,8 +70,9 @@ suite('SocketStream', () => {
     test('Read Ascii String', done => {
         const message = 'Hello World';
         const socket = new MockSocket();
-        let buffer = Buffer.concat([new Buffer('A'), uint64be.encode(message.length), new Buffer(message)]);
-        const stream = new SocketStream((socket as any) as net.Socket, buffer)
+        const buffer = Buffer.concat([new Buffer('A'), uint64be.encode(message.length), new Buffer(message)]);
+        // tslint:disable-next-line:no-any
+        const stream = new SocketStream((socket as any) as net.Socket, buffer);
 
         assert.equal(stream.ReadString(), message);
         done();
