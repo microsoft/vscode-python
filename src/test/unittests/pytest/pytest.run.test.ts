f88b64342101d2887c50f37040f91126cb84649b
Kartik Raj <karraj@microsoft.com>
2019-02-15 18:16:00 -0800

    corrections

--- src/test/unittests/pytest/pytest.run.test.ts
+++ src/test/unittests/pytest/pytest.run.test.ts
@@ -57,7 +57,7 @@ async function getScenarioTestsToRun(scenario: ITestScenarioDetails, tests: Test
             scenario.testsToRun.testFunction!.push(tests.testSuites[scenario.testFunctionIndex].testSuite);
         }
     }
-    return scenario.testsToRun;
+    return scenario.testsToRun!;
 }
 
 /**
@@ -371,14 +371,14 @@ suite('Unit Tests - pytest - run with mocked process output', () => {
                 await ioc.dispose();
                 await updateSetting('unitTest.pyTestArgs', [], rootWorkspaceUri, configTarget);
             });
-            const shouldRunProperly = (suiteName, failedRun = false) => {
+            const shouldRunProperly = (suiteName: string, failedRun = false) => {
                 suite(suiteName, () => {
                     testDetails = getScenarioTestDetails(scenario, failedRun);
                     const uniqueIssueFiles = getUniqueIssueFilesFromTestDetails(testDetails);
-                    let expectedSummaryCount;
+                    let expectedSummaryCount: IResultsSummaryCount;
                     suiteSetup(async () => {
                         testDetails = getScenarioTestDetails(scenario, failedRun);
-                        results = await getResultsFromTestManagerRunTest(testManager, scenario.testsToRun, failedRun);
+                        results = await getResultsFromTestManagerRunTest(testManager, scenario.testsToRun!, failedRun);
                         expectedSummaryCount = getExpectedSummaryCount(testDetails, failedRun);
                     });
                     test('Test results summary', async () => { await testResultsSummary(results, expectedSummaryCount); });
