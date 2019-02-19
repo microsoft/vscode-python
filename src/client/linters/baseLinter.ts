428dec1614dc0286ebe38f19165654fc1c702af9
Peter Cock <p.j.a.cock@googlemail.com>
2019-02-19 16:15:51 +0000

    Allow codes like ABC123 for flake8 v3 plugins
    Quoting the flake8 plugin author instructions:
    
    "Please Note: Your entry point does not need to be exactly 4 characters as of Flake8 3.0. Consider using an entry point with 3 letters followed by 3 numbers (i.e. ABC123 )."
    
    http://flake8.pycqa.org/en/latest/plugin-development/registering-plugins.html
    
    This change was in last part motivated by the increasing number of error code collisions with flake8 plugins using the original one letter and three numbers pattern.
    
    This regular expression fix was suggested by @jraygauthier on issue #4074.

--- src/client/linters/baseLinter.ts
+++ src/client/linters/baseLinter.ts
@@ -19,7 +19,8 @@ import {
 // tslint:disable-next-line:no-require-imports no-var-requires no-any
 const namedRegexp = require('named-js-regexp');
 // Allow negative column numbers (https://github.com/PyCQA/pylint/issues/1822)
-const REGEX = '(?<line>\\d+),(?<column>-?\\d+),(?<type>\\w+),(?<code>\\w\\d+):(?<message>.*)\\r?(\\n|$)';
+// Allow codes with more than one letter (e.g. plugins for flake8 v3 onwards)
+const REGEX = '(?<line>\\d+),(?<column>-?\\d+),(?<type>\\w+),(?<code>\\w+\\d+):(?<message>.*)\\r?(\\n|$)';
 
 export interface IRegexGroup {
     line: number;
