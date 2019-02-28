02e024ea3178421ff9df9e9c63a16b26a7a61d6f
Eric Snow <ericsnowcurrently@gmail.com>
2019-02-28 14:10:33 -0700

    Implement pytest.discover().

--- pythonFiles/testing_tools/adapter/pytest.py
+++ pythonFiles/testing_tools/adapter/pytest.py
@@ -1,7 +1,13 @@
+import os.path
+
+import pytest
+
 from .errors import UnsupportedCommandError
+from .info import TestInfo, TestPath
 
 
 def add_cli_subparser(cmd, name, parent):
+    """Add a new subparser to the given parent and add args to it."""
     parser = parent.add_parser(name)
     if cmd == 'discover':
         # For now we don't have any tool-specific CLI options to add.
@@ -11,6 +17,97 @@ def add_cli_subparser(cmd, name, parent):
     return parser
 
 
-def discover(pytestargs=None):
+def discover(pytestargs=None, pytest_main=None, plugin=None):
     """Return the results of test discovery."""
-    raise NotImplementedError
+    if pytest_main is None:
+        pytest_main = pytest.main
+    if plugin is None:
+        plugin = TestCollector()
+    # ensure --collect-only
+    # -pno:terminal
+    ec = pytest_main(pytestargs or [], [plugin])
+    if ec != 0:
+        raise Exception('pytest discovery failed (exit code {})'.format(ec))
+    if plugin.discovered is None:
+        raise Exception('pytest discovery did not start')
+    return plugin.discovered
+
+
+class TestCollector(object):
+    """This is a pytest plugin that collects the discovered tests."""
+
+    discovered = None
+
+    # Relevant plugin hooks:
+    #  https://docs.pytest.org/en/latest/reference.html#collection-hooks
+
+    def pytest_collection_modifyitems(self, session, config, items):
+        self.discovered = []
+        for item in items:
+            info = _parse_item(item)
+            self.discovered.append(info)
+
+    # This hook is not specified in the docs, so we also provide
+    # the "modifyitems" hook just in case.
+    def pytest_collection_finish(self, session):
+        try:
+            items = session.items
+        except AttributeError:
+            # Is there an alternative?
+            return
+        self.discovered = []
+        for item in items:
+            info = _parse_item(item)
+            self.discovered.append(info)
+
+
+def _parse_item(item):
+    """
+    (pytest.Collector)
+        pytest.Session
+        pytest.Package
+        pytest.Module
+        pytest.Class
+        (pytest.File)
+    (pytest.Item)
+        pytest.Function
+    """
+    # Figure out the file.
+    filename, lineno, fullname = item.location
+    if not str(item.fspath).endswith(os.path.sep + filename):
+        raise NotImplementedError
+    testroot = str(item.fspath)[:-len(filename)].rstrip(os.path.sep)
+    if os.path.sep in filename:
+        relfile = filename
+    else:
+        relfile = os.path.join('.', filename)
+
+    # Figure out the func (and subs).
+    funcname = item.function.__name__
+    parts = item.nodeid.split('::')
+    if parts.pop(0) != filename:
+        raise NotImplementedError
+    suites = []
+    while parts[0] != funcname:
+        suites.append(parts.pop(0))
+    if suites:
+        testfunc = '.'.join(suites) + '.' + funcname
+    else:
+        testfunc = funcname
+    subs = parts[1:]
+    if len(subs) > 1:
+        raise NotImplementedError
+    if fullname != testfunc:
+        raise NotImplementedError
+
+    return TestInfo(
+        id=item.nodeid,
+        name=item.name,
+        path=TestPath(
+            root=testroot,
+            relfile=relfile,
+            func=testfunc,
+            sub=subs or None,
+            ),
+        lineno=lineno,
+        )
--- pythonFiles/tests/testing_tools/adapter/test_pytest.py
+++ pythonFiles/tests/testing_tools/adapter/test_pytest.py
@@ -1,8 +1,12 @@
+import os.path
 import unittest
 
 from ...util import Stub, StubProxy
 from testing_tools.adapter.errors import UnsupportedCommandError
-from testing_tools.adapter.pytest import discover, add_cli_subparser
+from testing_tools.adapter.info import TestInfo, TestPath
+from testing_tools.adapter.pytest import (
+        discover, add_cli_subparser, TestCollector
+        )
 
 
 class AddCLISubparser(unittest.TestCase):
@@ -39,8 +43,164 @@ class AddCLISubparser(unittest.TestCase):
 class DiscoverTests(unittest.TestCase):
 
     def test_basic(self):
-        with self.assertRaises(NotImplementedError):
-            discover()
+        stub = Stub()
+        pytest = StubPyTest(stub)
+        plugin = StubPlugin(stub)
+        expected = []
+        plugin.discovered = expected
+
+        discovered = discover([], pytest.main, plugin)
+
+        self.assertEqual(discovered, expected)
+        self.assertEqual(stub.calls, [
+            ('pytest.main', None, {'args': [], 'plugins': [plugin]}),
+            ])
+
+    def test_failure(self):
+        stub = Stub()
+        pytest = StubPyTest(stub)
+        pytest.return_main = 2
+        plugin = StubPlugin(stub)
+
+        with self.assertRaises(Exception):
+            discover([], pytest.main, plugin)
+
+        self.assertEqual(stub.calls, [
+            ('pytest.main', None, {'args': [], 'plugins': [plugin]}),
+            ])
+
+
+class CollectorTests(unittest.TestCase):
+
+    def test_modifyitems(self):
+        stub = Stub()
+        session = StubPytestSession(stub)
+        config = StubPytestConfig(stub)
+        collector = TestCollector()
+
+        testroot = '/a/b/c'.replace('/', os.path.sep)
+        relfile1 = './test_spam.py'.replace('/', os.path.sep)
+        relfile2 = 'x/y/z/test_eggs.py'.replace('/', os.path.sep)
+
+        collector.pytest_collection_modifyitems(session, config, [
+            StubPytestItem(
+                stub,
+                nodeid='test_spam.py::SpamTests::test_one',
+                name='test_one',
+                location=('test_spam.py', 12, 'SpamTests.test_one'),
+                fspath=os.path.join(testroot, 'test_spam.py'),
+                function=FakeFunc('test_one'),
+                ),
+            StubPytestItem(
+                stub,
+                nodeid='test_spam.py::SpamTests::test_other',
+                name='test_other',
+                location=('test_spam.py', 19, 'SpamTests.test_other'),
+                fspath=os.path.join(testroot, 'test_spam.py'),
+                function=FakeFunc('test_other'),
+                ),
+            StubPytestItem(
+                stub,
+                nodeid='test_spam.py::test_all',
+                name='test_all',
+                location=('test_spam.py', 144, 'test_all'),
+                fspath=os.path.join(testroot, 'test_spam.py'),
+                function=FakeFunc('test_all'),
+                ),
+            StubPytestItem(
+                stub,
+                nodeid=relfile2 + '::All::BasicTests::test_first',
+                name='test_first',
+                location=(relfile2, 31, 'All.BasicTests.test_first'),
+                fspath=os.path.join(testroot, relfile2),
+                function=FakeFunc('test_first'),
+                ),
+            # TODO: Add an example of subtests!
+            ])
+
+        self.maxDiff = None
+        self.assertEqual(collector.discovered, [
+            TestInfo(
+                id='test_spam.py::SpamTests::test_one',
+                name='test_one',
+                path=TestPath(
+                    root=testroot,
+                    relfile=relfile1,
+                    func='SpamTests.test_one',
+                    sub=None,
+                    ),
+                lineno=12,
+                ),
+            TestInfo(
+                id='test_spam.py::SpamTests::test_other',
+                name='test_other',
+                path=TestPath(
+                    root=testroot,
+                    relfile=relfile1,
+                    func='SpamTests.test_other',
+                    sub=None,
+                    ),
+                lineno=19,
+                ),
+            TestInfo(
+                id='test_spam.py::test_all',
+                name='test_all',
+                path=TestPath(
+                    root=testroot,
+                    relfile=relfile1,
+                    func='test_all',
+                    sub=None,
+                    ),
+                lineno=144,
+                ),
+            TestInfo(
+                id=relfile2 + '::All::BasicTests::test_first',
+                name='test_first',
+                path=TestPath(
+                    root=testroot,
+                    relfile=relfile2,
+                    func='All.BasicTests.test_first',
+                    sub=None,
+                    ),
+                lineno=31,
+                ),
+            ])
+        self.assertEqual(stub.calls, [])
+
+    def test_finish(self):
+        stub = Stub()
+        session = StubPytestSession(stub)
+        testroot = '/a/b/c'.replace('/', os.path.sep)
+        relfile = 'x/y/z/test_eggs.py'.replace('/', os.path.sep)
+        session.items = [
+            StubPytestItem(
+                stub,
+                nodeid=relfile + '::SpamTests::test_spam',
+                name='test_spam',
+                location=(relfile, 12, 'SpamTests.test_spam'),
+                fspath=os.path.join(testroot, relfile),
+                function=FakeFunc('test_spam'),
+                ),
+            ]
+        collector = TestCollector()
+
+        collector.pytest_collection_finish(session)
+
+        self.maxDiff = None
+        self.assertEqual(collector.discovered, [
+            TestInfo(
+                id=relfile + '::SpamTests::test_spam',
+                name='test_spam',
+                path=TestPath(
+                    root=testroot,
+                    relfile=relfile,
+                    func='SpamTests.test_spam',
+                    sub=None,
+                    ),
+                lineno=12,
+                ),
+            ])
+        self.assertEqual(stub.calls, [])
 
 
 class StubSubparsers(StubProxy):
@@ -60,3 +220,70 @@ class StubArgParser(StubProxy):
 
     def add_argument(self, *args, **kwargs):
         self.add_call('add_argument', args, kwargs)
+
+
+class StubPyTest(StubProxy):
+
+    def __init__(self, stub=None):
+        super().__init__(stub, 'pytest')
+        self.return_main = 0
+
+    def main(self, args, plugins):
+        self.add_call('main', None, {'args': args, 'plugins': plugins})
+        return self.return_main
+
+
+class StubPlugin(StubProxy):
+
+    def __init__(self, stub=None):
+        super().__init__(stub, 'plugin')
+
+    def __getattr__(self, name):
+        if not name.startswith('pytest_'):
+            raise AttributeError(name)
+        def func(*args, **kwargs):
+            self.add_call(name, args or None, kwargs or None)
+        return func
+
+
+class FakeFunc(object):
+
+    def __init__(self, name):
+        self.__name__ = name
+
+
+class StubPytestItem(StubProxy):
+
+    def __init__(self, stub=None, **attrs):
+        super().__init__(stub, 'pytest.Item')
+        self.__dict__.update(attrs)
+
+    def __getattr__(self, name):
+        self.add_call(name + ' (attr)', None, None)
+        def func(*args, **kwargs):
+            self.add_call(name, args or None, kwargs or None)
+        return func
+
+
+class StubPytestSession(StubProxy):
+
+    def __init__(self, stub=None):
+        super().__init__(stub, 'pytest.Session')
+
+    def __getattr__(self, name):
+        self.add_call(name + ' (attr)', None, None)
+        def func(*args, **kwargs):
+            self.add_call(name, args or None, kwargs or None)
+        return func
+
+
+class StubPytestConfig(StubProxy):
+
+    def __init__(self, stub=None):
+        super().__init__(stub, 'pytest.Config')
+
+    def __getattr__(self, name):
+        self.add_call(name + ' (attr)', None, None)
+        def func(*args, **kwargs):
+            self.add_call(name, args or None, kwargs or None)
+        return func
