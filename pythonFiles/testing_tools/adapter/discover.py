fb33dae26d57a3d37da098f367a4f3ac0c681ebb
Eric Snow <ericsnowcurrently@gmail.com>
2019-02-28 13:09:50 -0700

    Implement discover.report().

--- pythonFiles/testing_tools/adapter/discover.py
+++ pythonFiles/testing_tools/adapter/discover.py
@@ -1,5 +1,25 @@
+import json
 
 
-def report(discovered):
+def report(tests, debug=False, _send=print):
     """Serialize the discovered tests and write to stdout."""
-    raise NotImplementedError
+
+    data = [{
+            'id': test.id,
+            'name': test.name,
+            'testroot': test.path.root,
+            'relfile': test.path.relfile,
+            'lineno': test.lineno,
+            'testfunc': test.path.func,
+            'subtest': test.path.sub or None,
+            } for test in tests]
+    kwargs = {}
+    if debug:
+        # human-formatted
+        kwargs = dict(
+            sort_keys=True,
+            indent=4,
+            separators=(',', ': '),
+            )
+    serialized = json.dumps(data, **kwargs)
+    _send(serialized)
