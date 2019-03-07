e02882310370fdcec2c0c0bb163e2d660f285252
Derek Keeler <dekeeler@microsoft.com>
2019-03-07 14:24:39 -0800

    Fixups

--- src/test/unittests/pytest/pytest.testparser.unit.test.ts
+++ src/test/unittests/pytest/pytest.testparser.unit.test.ts
@@ -8,12 +8,11 @@ import * as chaipromise from 'chai-as-promised';
 import * as typeMoq from 'typemoq';
 import { CancellationToken, OutputChannel, Uri } from 'vscode';
 import { IApplicationShell, ICommandManager } from '../../../client/common/application/types';
-import { OSType } from '../../../client/common/utils/platform';
 import { IServiceContainer } from '../../../client/ioc/types';
 import { DiscoveredTestData } from '../../../client/unittests/common/discoveredTests';
 import { TestsHelper } from '../../../client/unittests/common/testUtils';
 import { TestFlatteningVisitor } from '../../../client/unittests/common/testVisitors/flatteningVisitor';
-import { FlattenedTestFunction, TestDiscoveryOptions, Tests } from '../../../client/unittests/common/types';
+import { TestDiscoveryOptions, Tests } from '../../../client/unittests/common/types';
 import { TestsParser as PyTestsParser } from '../../../client/unittests/pytest/services/parserService';
 import { pytestScenario } from './pytest.testparser.testdata';
 
@@ -73,76 +72,4 @@ suite('PyTest parser used in discovery', () => {
             expect(testScenario.expectedResult).to.deep.equal(tests);
         });
     });
-
-    // pytestScenarioData.forEach((testScenario) => {
-    //     if (testPlatformType === testScenario.platform) {
-
-    //         const testDescription: string =
-    //             `PyTest${testScenario.pytest_version_spec}: ${testScenario.description}`;
-
-    // test(testDescription, async () => {
-    //     // Setup the service container for use by the parser.
-    //     const serviceContainer = typeMoq.Mock.ofType<IServiceContainer>();
-    //     const appShell = typeMoq.Mock.ofType<IApplicationShell>();
-    //     const cmdMgr = typeMoq.Mock.ofType<ICommandManager>();
-    //     serviceContainer.setup(s => s.get(typeMoq.It.isValue(IApplicationShell), typeMoq.It.isAny()))
-    //         .returns(() => {
-    //             return appShell.object;
-    //         });
-    //     serviceContainer.setup(s => s.get(typeMoq.It.isValue(ICommandManager), typeMoq.It.isAny()))
-    //         .returns(() => {
-    //             return cmdMgr.object;
-    //         });
-
-    //     // Create mocks used in the test discovery setup.
-    //     const outChannel = typeMoq.Mock.ofType<OutputChannel>();
-    //     const cancelToken = typeMoq.Mock.ofType<CancellationToken>();
-    //     cancelToken.setup(c => c.isCancellationRequested).returns(() => false);
-    //     const wsFolder = typeMoq.Mock.ofType<Uri>();
-
-    //     // Create the test options for the mocked-up test. All data is either
-    //     // mocked or is taken from the JSON test data itself.
-    //     const options: TestDiscoveryOptions = {
-    //         args: [],
-    //         cwd: testScenario.rootdir,
-    //         ignoreCache: true,
-    //         outChannel: outChannel.object,
-    //         token: cancelToken.object,
-    //         workspaceFolder: wsFolder.object
-    //     };
-
-    //     // Setup the parser.
-    //     const testFlattener: TestFlatteningVisitor = new TestFlatteningVisitor();
-    //     const testHlp: TestsHelper = new TestsHelper(testFlattener, serviceContainer.object);
-    //     const parser = new PyTestsParser(testHlp);
-
-    //     // Each test scenario has a 'stdout' member that is an array of
-    //     // stdout lines. Join them here such that the parser can operate
-    //     // on stdout-like data.
-    //     const stdout: string = testScenario.stdout.join('\n');
-
-    //     const parsedTests: Tests = parser.parse(stdout, options);
-
-    //     // Now we can actually perform tests.
-    //     expect(parsedTests).is.not.equal(
-    //         undefined,
-    //         'Should have gotten tests extracted from the parsed pytest result content.');
-
-    //     expect(parsedTests.testFunctions.length).equals(
-    //         testScenario.functionCount,
-    //         `Parsed pytest summary contained ${testScenario.functionCount} test functions.`);
-
-    //     testScenario.test_functions.forEach((funcName: string) => {
-    //         const findAllTests: FlattenedTestFunction[] | undefined = parsedTests.testFunctions.filter(
-    //             (tstFunc: FlattenedTestFunction) => {
-    //                 return tstFunc.testFunction.nameToRun === funcName;
-    //             });
-    //         // Each test identified in the testScenario should exist once and only once.
-    //         expect(findAllTests).is.not.equal(undefined, `Could not find "${funcName}" in tests.`);
-    //         expect(findAllTests.length).is.equal(1, 'There should be exactly one instance of each test.');
-    //     });
-
-    // });
-    //     }
-    // });
 });
