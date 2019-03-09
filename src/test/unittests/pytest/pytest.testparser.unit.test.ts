a96660dac4b39b54cd452ecb8967ea0a9f4e076a
Derek Keeler <dekeeler@microsoft.com>
2019-03-08 17:41:58 -0800

    Adding/fixing further tests
    - Fix bug with parser throwing on empty input
    - Tests for the discoveredTests
    - Tests fixed for the discoveryService
    - simplify tests for the parserService
    - remove a non-unit test from running during unit test runs

--- src/test/unittests/pytest/pytest.testparser.unit.test.ts
+++ src/test/unittests/pytest/pytest.testparser.unit.test.ts
@@ -25,37 +25,70 @@ use(chaipromise);
 // tslint:disable-next-line:max-func-body-length
 suite('PyTest parser used in discovery', () => {
 
+    // Type for keeping a test function's TestsHelper dependencies intact.
+    type TestHelperDependents = {
+        serviceContainer: typeMoq.IMock<IServiceContainer>;
+        appShell: typeMoq.IMock<IApplicationShell>;
+        cmdMgr: typeMoq.IMock<ICommandManager>;
+        flattener: TestFlatteningVisitor;
+    };
+
+    // Type for keeping test discovery options & dependencies intact.
+    type TestParserDiscoveryOptions = {
+        outChannel: typeMoq.IMock<OutputChannel>;
+        cancelToken: typeMoq.IMock<CancellationToken>;
+        wsFolder: typeMoq.IMock<Uri>;
+    };
+
+    // Create a TestsHelper and return it (and its dependancies)
+    function createTestsHelper(): [TestHelperDependents, TestsHelper] {
+        // Setup the service container for use by the parser.
+        const tDeps: TestHelperDependents = {
+            serviceContainer: typeMoq.Mock.ofType<IServiceContainer>(),
+            appShell: typeMoq.Mock.ofType<IApplicationShell>(),
+            cmdMgr: typeMoq.Mock.ofType<ICommandManager>(),
+            flattener: new TestFlatteningVisitor()
+        };
+
+        tDeps.serviceContainer.setup(s => s.get(typeMoq.It.isValue(IApplicationShell), typeMoq.It.isAny()))
+            .returns(() => {
+                return tDeps.appShell.object;
+            });
+        tDeps.serviceContainer.setup(s => s.get(typeMoq.It.isValue(ICommandManager), typeMoq.It.isAny()))
+            .returns(() => {
+                return tDeps.cmdMgr.object;
+            });
+
+        const testHelper: TestsHelper = new TestsHelper(tDeps.flattener, tDeps.serviceContainer.object);
+        return [tDeps, testHelper];
+    }
+
+    function createParserOptions(): [TestParserDiscoveryOptions, TestDiscoveryOptions] {
+        // Create mocks used in the test discovery setup.
+        const oDeps: TestParserDiscoveryOptions = {
+            outChannel: typeMoq.Mock.ofType<OutputChannel>(),
+            cancelToken: typeMoq.Mock.ofType<CancellationToken>(),
+            wsFolder: typeMoq.Mock.ofType<Uri>()
+        };
+        oDeps.cancelToken.setup(c => c.isCancellationRequested).returns(() => false);
+
+        // Create the test options for the mocked-up test. All data is either
+        // mocked or is taken from the JSON test data itself.
+        // tslint:disable-next-line:no-unnecessary-local-variable
+        const options: TestDiscoveryOptions = {
+            args: [],
+            cwd: '.',
+            ignoreCache: true,
+            outChannel: oDeps.outChannel.object,
+            token: oDeps.cancelToken.object,
+            workspaceFolder: oDeps.wsFolder.object
+        };
+
+        return [oDeps, options];
+    }
+
     pytestScenario.forEach((testScenario) => {
         test(`${testScenario.scenarioDescription} (convert to TestFiles)`, () => {
-            // Setup the service container for use by the parser.
-            const serviceContainer = typeMoq.Mock.ofType<IServiceContainer>();
-            const appShell = typeMoq.Mock.ofType<IApplicationShell>();
-            const cmdMgr = typeMoq.Mock.ofType<ICommandManager>();
-            serviceContainer.setup(s => s.get(typeMoq.It.isValue(IApplicationShell), typeMoq.It.isAny()))
-                .returns(() => {
-                    return appShell.object;
-                });
-            serviceContainer.setup(s => s.get(typeMoq.It.isValue(ICommandManager), typeMoq.It.isAny()))
-                .returns(() => {
-                    return cmdMgr.object;
-                });
-
-            // Create mocks used in the test discovery setup.
-            const outChannel = typeMoq.Mock.ofType<OutputChannel>();
-            const cancelToken = typeMoq.Mock.ofType<CancellationToken>();
-            cancelToken.setup(c => c.isCancellationRequested).returns(() => false);
-            const wsFolder = typeMoq.Mock.ofType<Uri>();
-
-            // Create the test options for the mocked-up test. All data is either
-            // mocked or is taken from the JSON test data itself.
-            const options: TestDiscoveryOptions = {
-                args: [],
-                cwd: '.',
-                ignoreCache: true,
-                outChannel: outChannel.object,
-                token: cancelToken.object,
-                workspaceFolder: wsFolder.object
-            };
 
             let testFilesParsed: TestFile[];
             // set up the test flattener, but extact the TestFiles for inspection here instead of actually flattening them.
@@ -67,10 +100,35 @@ suite('PyTest parser used in discovery', () => {
                 });
 
             const parser = new PyTestsParser(testHelper.object);
+            const [_, options] = createParserOptions();
             parser.parse(testScenario.json, options);
 
             expect(testFilesParsed).to.deep.equal(testScenario.expectedTestFiles);
         });
+    });
 
+    test('Parser handles the case when there is no test information given', () => {
+        const [_, testHelper] = createTestsHelper();
+        const [__, options] = createParserOptions();
+        const parser = new PyTestsParser(testHelper);
+
+        let result: Tests = parser.parse('', options);
+        expect(result).to.be.equal(undefined, 'Received tests structure when input was an empty string?');
+        result = parser.parse('      ', options);
+        expect(result).to.be.equal(undefined, 'Received tests structure when input was a blank string?');
+        result = parser.parse(undefined, options);
+        expect(result).to.be.equal(undefined, 'Received tests structure when input was undefined?');
+    });
+
+    test('Parser throws error when given invalid JSON string', () => {
+        const [_, testHelper] = createTestsHelper();
+        const [__, options] = createParserOptions();
+        const parser = new PyTestsParser(testHelper);
+        try {
+            parser.parse('This is not JSON, it is just a string.', options);
+            expect('Should_not_get_here').to.be.equal('here', 'Parser did not throw an exception for invalid JSON input.');
+        } catch (ex) {
+            expect(ex.message).to.contain('Could not discover tests');
+        }
     });
 });
