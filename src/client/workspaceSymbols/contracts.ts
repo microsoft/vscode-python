b5f19caa95c4baf2354b5326503f8f85c62beb32
Eric Snow <ericsnowcurrently@gmail.com>
2019-03-18 15:51:12 -0600

    Fix src/client/workspaceSymbols/contracts.ts.

--- src/client/workspaceSymbols/contracts.ts
+++ src/client/workspaceSymbols/contracts.ts
@@ -1,6 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
 import { Position, SymbolKind } from 'vscode';
 
-export interface Tag {
+export interface ITag {
     fileName: string;
     symbolName: string;
     symbolKind: SymbolKind;
