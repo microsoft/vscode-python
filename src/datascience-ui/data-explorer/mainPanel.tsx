68eb3bfb5f5a0c16d22c76b444234064ec78fdb0
rchiodo <rchiodo@microsoft.com>
2019-03-18 16:55:54 -0700

    Switch to just AdazzleReactDataGrid

--- src/datascience-ui/data-explorer/mainPanel.tsx
+++ src/datascience-ui/data-explorer/mainPanel.tsx
@@ -1,24 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 'use strict';
-import '@blueprintjs/core/lib/css/blueprint.css';
-import '@blueprintjs/select/lib/css/blueprint-select.css';
-import 'react-table/react-table.css';
 
-import DataExplorer from '@nteract/data-explorer';
-import { DataProps } from '@nteract/data-explorer/lib/types';
 import * as React from 'react';
 
 import { DataExplorerMessages, IDataExplorerMapping } from '../../client/datascience/data-viewing/types';
 import { IMessageHandler, PostOffice } from '../react-common/postOffice';
 import { generateTestData } from './testData';
-
-import 'react-tabulator/css/tabulator.css';
-import 'react-tabulator/lib/styles.css';
-
-import { React15Tabulator } from 'react-tabulator';
-
-//import { ReactTabulator } from 'react-tabulator';
 
 import * as AdazzleReactDataGrid from 'react-data-grid';
 import { Toolbar, Data } from 'react-data-grid-addons';
@@ -30,15 +18,11 @@ const defaultColumnProperties = {
     filterable: true,
     sortable: true    
 }
-
 export interface IMainPanelProps {
     skipDefault?: boolean;
 }
 
 interface IMainPanelState {
-    nteractDataProps:  DataProps;
-    tabulatorColumns: {}[];
-    tabulatorData: {}[];
     gridColumns: {key:string, name: string}[];
     gridRows: IGridRow[];
     initialGridRows: IGridRow[];
@@ -53,44 +37,21 @@ class DataExplorerPostOffice extends PostOffice<IDataExplorerMapping> {};
 
 export class MainPanel extends React.Component<IMainPanelProps, IMainPanelState> implements IMessageHandler {
     private postOffice: DataExplorerPostOffice | undefined;
-    private de : DataExplorer | undefined;
 
     // tslint:disable-next-line:max-func-body-length
     constructor(props: IMainPanelProps, state: IMainPanelState) {
         super(props);
 
         if (!this.props.skipDefault) {
-            const data = generateTestData(500);
+            const data = generateTestData(5000);
             this.state = {
-                nteractDataProps: {
-                    schema: {
-                       fields: data.fields,
-                       primaryKey: data.primaryKeys,
-                       pandas_version: '0.21.0'
-                    },
-                    data: [] //data.rows
-                },
-                tabulatorColumns: data.columns,
-                tabulatorData: [], // data.tabulatorRows
-                gridColumns: data.gridColumns,
-                gridRows: data.tabularRows,
-                initialGridRows: data.tabularRows,
+                gridColumns: data.columns,
+                gridRows: data.rows,
+                initialGridRows: data.rows,
                 filters: {}
             };
         } else {
             this.state = {
-                nteractDataProps: {
-                    schema: {
-                       fields: [
-                       ],
-                       primaryKey: [],
-                       pandas_version: '1'
-                    },
-                    data: [
-                    ]
-                },
-                tabulatorColumns: [],
-                tabulatorData: [],
                 gridColumns: [],
                 gridRows: [],
                 initialGridRows: [],
@@ -100,20 +61,12 @@ export class MainPanel extends React.Component<IMainPanelProps, IMainPanelState>
     }
 
     public render = () => {
-        const options = {
-            height: 300,
-            movableRows: false
-          };
 
         const filteredRows = this.getRows(this.state.gridRows, this.state.filters);
-      
+        
         return (
             <div className='main-panel'>
                 <DataExplorerPostOffice messageHandlers={[this]} ref={this.updatePostOffice} />
-                <div>Hello from Data Explorer</div>
-                <DataExplorer data={this.state.nteractDataProps} initialView={"grid"} ref={this.updateDataExplorer}/>
-                <div>Hello from Tabulator</div>
-                <React15Tabulator columns={this.state.tabulatorColumns} data={this.state.tabulatorData} options={options} />
                 <div>Hello from React Data Grid</div>
                 <AdazzleReactDataGrid
                     columns={this.state.gridColumns.map(c => { return {...c, ...defaultColumnProperties };})}
@@ -138,12 +91,6 @@ export class MainPanel extends React.Component<IMainPanelProps, IMainPanelState>
         }
 
         return false;
-    }
-
-    private updateDataExplorer = (de: DataExplorer) => {
-        if (this.de !== de) {
-            this.de = de;
-        }
     }
 
     private updatePostOffice = (postOffice: DataExplorerPostOffice) => {
