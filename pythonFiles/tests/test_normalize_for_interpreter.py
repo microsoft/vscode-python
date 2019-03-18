ab92c45f5df5b8c5a049e29e1f0a10d424fa3613
Derek Keeler <dekeeler@microsoft.com>
2019-03-18 10:11:22 -0700

    Correct python version specification
    - was running Python 3.7 in all cases before
    - Updated AGENT_OS usage
    - Add display name to show dependency versions (was 'Bash')
    - skip Python unittests for normalizeForInterpreter in python 2.7

--- pythonFiles/tests/test_normalize_for_interpreter.py
+++ pythonFiles/tests/test_normalize_for_interpreter.py
@@ -11,18 +11,16 @@ import normalizeForInterpreter
 class TestNormalizationScript(object):
     """Basic unit tests for the normalization script."""
 
-
     @pytest.mark.skipif(sys.version_info.major == 2,
-                    reason="normalizeForInterpreter not working for 2.7, see GH #4805")
+                        reason="normalizeForInterpreter not working for 2.7, see GH #4805")
     def test_basicNormalization(self, capsys):
         src = 'print("this is a test")'
         normalizeForInterpreter.normalize_lines(src)
         captured = capsys.readouterr()
         assert captured.out == src
 
-
     @pytest.mark.skipif(sys.version_info.major == 2,
-                    reason="normalizeForInterpreter not working for 2.7, see GH #4805")
+                        reason="normalizeForInterpreter not working for 2.7, see GH #4805")
     def test_moreThanOneLine(self, capsys):
         src = textwrap.dedent("""\
             # Some rando comment
@@ -30,14 +28,13 @@ class TestNormalizationScript(object):
             def show_something():
                 print("Something")
             """
-        )
+                              )
         normalizeForInterpreter.normalize_lines(src)
         captured = capsys.readouterr()
         assert captured.out == src
 
-
     @pytest.mark.skipif(sys.version_info.major == 2,
-                    reason="normalizeForInterpreter not working for 2.7, see GH #4805")
+                        reason="normalizeForInterpreter not working for 2.7, see GH #4805")
     def test_withHangingIndent(self, capsys):
         src = textwrap.dedent("""\
             x = 22
@@ -48,14 +45,13 @@ class TestNormalizationScript(object):
             if result == 42:
                 print("The answer to life, the universe, and everything")
             """
-        )
+                              )
         normalizeForInterpreter.normalize_lines(src)
         captured = capsys.readouterr()
         assert captured.out == src
 
-
     @pytest.mark.skipif(sys.version_info.major == 2,
-                    reason="normalizeForInterpreter not working for 2.7, see GH #4805")
+                        reason="normalizeForInterpreter not working for 2.7, see GH #4805")
     def test_clearOutExtraneousNewlines(self, capsys):
         src = textwrap.dedent("""\
             value_x = 22
@@ -67,7 +63,7 @@ class TestNormalizationScript(object):
             print(value_x + value_y + value_z)
 
             """
-        )
+                              )
         expectedResult = textwrap.dedent("""\
             value_x = 22
             value_y = 30
@@ -75,14 +71,13 @@ class TestNormalizationScript(object):
             print(value_x + value_y + value_z)
 
             """
-        )
+                                         )
         normalizeForInterpreter.normalize_lines(src)
         result = capsys.readouterr()
         assert result.out == expectedResult
 
-
     @pytest.mark.skipif(sys.version_info.major == 2,
-                    reason="normalizeForInterpreter not working for 2.7, see GH #4805")
+                        reason="normalizeForInterpreter not working for 2.7, see GH #4805")
     def test_clearOutExtraLinesAndWhitespace(self, capsys):
         src = textwrap.dedent("""\
             if True:
@@ -95,7 +90,7 @@ class TestNormalizationScript(object):
             print(x + y + z)
 
             """
-        )
+                              )
         expectedResult = textwrap.dedent("""\
             if True:
                 x = 22
@@ -105,7 +100,7 @@ class TestNormalizationScript(object):
             print(x + y + z)
 
             """
-        )
+                                         )
         normalizeForInterpreter.normalize_lines(src)
         result = capsys.readouterr()
         assert result.out == expectedResult
