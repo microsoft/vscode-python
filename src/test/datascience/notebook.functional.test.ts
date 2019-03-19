7f9d9963b4180c9bd6b7b4e66c9663d9ab09c7bc
Derek Keeler <dekeeler@microsoft.com>
2019-03-19 11:09:47 -0700

    Fixed test prior to my PR being merged, unskip test.

--- src/test/datascience/notebook.functional.test.ts
+++ src/test/datascience/notebook.functional.test.ts
@@ -195,16 +195,7 @@ suite('Jupyter notebook tests', () => {
     }
 
     function runTest(name: string, func: () => Promise<void>, notebookProc?: ChildProcess) {
-        test(name, async function () {
-            if (name === 'Cancel execution') {
-                // Failing on 'Cancel did not cancel getusable after 30ms'
-                // and 'Cancel did not cancel isNotebook after 10ms' on AzDO in Linux & Windows.
-
-                // Tracked by GH #4818
-                // tslint:disable-next-line:no-invalid-this
-                return this.skip();
-            }
-
+        test(name, async () => {
             console.log(`Starting test ${name} ...`);
             if (ioc.mockJupyter) {
                 ioc.mockJupyter.addInterpreter(workingPython, SupportedCommands.all, undefined, notebookProc);
