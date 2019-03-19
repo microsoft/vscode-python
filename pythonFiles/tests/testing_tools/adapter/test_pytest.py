e195e4a24180b2662b24f8f3be7b0e301cc66eb9
Eric Snow <ericsnowcurrently@gmail.com>
2019-03-19 10:15:46 -0600

    Fix Python 2.

--- pythonFiles/tests/testing_tools/adapter/test_pytest.py
+++ pythonFiles/tests/testing_tools/adapter/test_pytest.py
@@ -1,6 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
 
 try:
     from io import StringIO
