77b296f165620072457e56d0974a67afa5bcbb80
Don Jayamanne <don.jayamanne@yahoo.com>
2019-04-19 13:01:18 -0700

    Ooops

--- src/test/testing/explorer/testTreeViewProvider.unit.test.ts
+++ src/test/testing/explorer/testTreeViewProvider.unit.test.ts
@@ -53,7 +53,7 @@ class TestExplorerCaptureRefresh implements IDisposable {
 }
 
 // tslint:disable:max-func-body-length
-suite('xUnit Tests Test Explorer TestTreeViewProvider', () => {
+suite('Unit Tests Test Explorer TestTreeViewProvider', () => {
     suite('Misc', () => {
         const testResource: Uri = Uri.parse('anything');
         let disposables: IDisposable[] = [];
