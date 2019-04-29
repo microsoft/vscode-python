2a8701c883b04ce1e3fe6016e30fc5558b53c994
Don Jayamanne <don.jayamanne@yahoo.com>
2019-04-29 15:37:18 -0700

    Revert

--- src/test/testing/serviceRegistry.ts
+++ src/test/testing/serviceRegistry.ts
@@ -19,19 +19,16 @@ import {
     INotebookServer
 } from '../../client/datascience/types';
 import { IServiceContainer } from '../../client/ioc/types';
-import { NOSETEST_PROVIDER, PYTEST_PROVIDER, UNITTEST_PROVIDER } from '../../client/testing/common/constants';
-import { TestContextService } from '../../client/testing/common/services/contextService';
-import { TestDiscoveredTestParser } from '../../client/testing/common/services/discoveredTestParser';
-import { TestsDiscoveryService } from '../../client/testing/common/services/discovery';
-import { TestCollectionStorageService } from '../../client/testing/common/services/storageService';
-import { TestManagerService } from '../../client/testing/common/services/testManagerService';
-import { TestResultsService } from '../../client/testing/common/services/testResultsService';
-import { TestsStatusUpdaterService } from '../../client/testing/common/services/testsStatusService';
-import { ITestDiscoveredTestParser } from '../../client/testing/common/services/types';
-import { UnitTestDiagnosticService } from '../../client/testing/common/services/unitTestDiagnosticService';
-import { TestsHelper } from '../../client/testing/common/testUtils';
-import { TestFlatteningVisitor } from '../../client/testing/common/testVisitors/flatteningVisitor';
-import { TestResultResetVisitor } from '../../client/testing/common/testVisitors/resultResetVisitor';
+import { NOSETEST_PROVIDER, PYTEST_PROVIDER, UNITTEST_PROVIDER } from '../../client/unittests/common/constants';
+import { TestContextService } from '../../client/unittests/common/services/contextService';
+import { TestCollectionStorageService } from '../../client/unittests/common/services/storageService';
+import { TestManagerService } from '../../client/unittests/common/services/testManagerService';
+import { TestResultsService } from '../../client/unittests/common/services/testResultsService';
+import { TestsStatusUpdaterService } from '../../client/unittests/common/services/testsStatusService';
+import { UnitTestDiagnosticService } from '../../client/unittests/common/services/unitTestDiagnosticService';
+import { TestsHelper } from '../../client/unittests/common/testUtils';
+import { TestFlatteningVisitor } from '../../client/unittests/common/testVisitors/flatteningVisitor';
+import { TestResultResetVisitor } from '../../client/unittests/common/testVisitors/resultResetVisitor';
 import {
     ITestCollectionStorageService,
     ITestContextService,
@@ -47,14 +44,15 @@ import {
     ITestVisitor,
     IUnitTestSocketServer,
     TestProvider
-} from '../../client/testing/common/types';
-import { TestManager as NoseTestManager } from '../../client/testing/nosetest/main';
-import { TestDiscoveryService as NoseTestDiscoveryService } from '../../client/testing/nosetest/services/discoveryService';
-import { TestsParser as NoseTestTestsParser } from '../../client/testing/nosetest/services/parserService';
-import { TestManager as PyTestTestManager } from '../../client/testing/pytest/main';
-import { TestDiscoveryService as PytestTestDiscoveryService } from '../../client/testing/pytest/services/discoveryService';
-import { ITestDiagnosticService } from '../../client/testing/types';
-import { TestManager as UnitTestTestManager } from '../../client/testing/unittest/main';
+} from '../../client/unittests/common/types';
+import { TestManager as NoseTestManager } from '../../client/unittests/nosetest/main';
+import { TestDiscoveryService as NoseTestDiscoveryService } from '../../client/unittests/nosetest/services/discoveryService';
+import { TestsParser as NoseTestTestsParser } from '../../client/unittests/nosetest/services/parserService';
+import { TestManager as PyTestTestManager } from '../../client/unittests/pytest/main';
+import { TestDiscoveryService as PytestTestDiscoveryService } from '../../client/unittests/pytest/services/discoveryService';
+import { TestsParser as PytestTestsParser } from '../../client/unittests/pytest/services/parserService';
+import { IUnitTestDiagnosticService } from '../../client/unittests/types';
+import { TestManager as UnitTestTestManager } from '../../client/unittests/unittest/main';
 import {
     TestDiscoveryService as UnitTestTestDiscoveryService
 } from '../../client/testing/unittest/services/discoveryService';
@@ -99,6 +97,7 @@ export class UnitTestIocContainer extends IocContainer {
 
     public registerTestParsers() {
         this.serviceManager.add<ITestsParser>(ITestsParser, UnitTestTestsParser, UNITTEST_PROVIDER);
+        this.serviceManager.add<ITestsParser>(ITestsParser, PytestTestsParser, PYTEST_PROVIDER);
         this.serviceManager.add<ITestsParser>(ITestsParser, NoseTestTestsParser, NOSETEST_PROVIDER);
     }
 
@@ -106,8 +105,6 @@ export class UnitTestIocContainer extends IocContainer {
         this.serviceManager.add<ITestDiscoveryService>(ITestDiscoveryService, UnitTestTestDiscoveryService, UNITTEST_PROVIDER);
         this.serviceManager.add<ITestDiscoveryService>(ITestDiscoveryService, PytestTestDiscoveryService, PYTEST_PROVIDER);
         this.serviceManager.add<ITestDiscoveryService>(ITestDiscoveryService, NoseTestDiscoveryService, NOSETEST_PROVIDER);
-        this.serviceManager.add<ITestDiscoveryService>(ITestDiscoveryService, TestsDiscoveryService, 'common');
-        this.serviceManager.add<ITestDiscoveredTestParser>(ITestDiscoveredTestParser, TestDiscoveredTestParser);
     }
 
     public registerTestDiagnosticServices() {
