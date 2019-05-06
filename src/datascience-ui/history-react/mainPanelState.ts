62153a463bfe6994415ff329ad13c06962499bc5
Rich Chiodo <rchiodo@microsoft.com>
2019-05-06 09:29:09 -0700

    Add actual table to test state.

--- src/datascience-ui/history-react/mainPanelState.ts
+++ src/datascience-ui/history-react/mainPanelState.ts
@@ -174,7 +174,7 @@ function generateCellData() : (nbformat.ICodeCell | nbformat.IMarkdownCell | nbf
             source: [],
             metadata: {},
             message: 'You have this python data:',
-            connection: 'https:\\localhost'
+            connection: 'https:\\localhost\\token?=9343p0843084039483084308430984038403840938409384098304983094803948093848034809384'
         },
         {
             cell_type: 'code',
@@ -187,21 +187,199 @@ function generateCellData() : (nbformat.ICodeCell | nbformat.IMarkdownCell | nbf
             outputs: [
                 {
                     data: {
-                        'text/plain': [
-                            '   num_preg  glucose_conc  diastolic_bp  thickness  insulin   bmi  diab_pred  \\\n',
-                            '0         6           148            72         35        0  33.6      0.627   \n',
-                            '1         1            85            66         29        0  26.6      0.351   \n',
-                            '2         8           183            64          0        0  23.3      0.672   \n',
-                            '3         1            89            66         23       94  28.1      0.167   \n',
-                            '4         0           137            40         35      168  43.1      2.288   \n',
-                            '\n',
-                            '   age    skin  diabetes  \n',
-                            '0   50  1.3790      True  \n',
-                            '1   31  1.1426     False  \n',
-                            '2   32  0.0000      True  \n',
-                            '3   21  0.9062     False  \n',
-                            '4   33  1.3790      True  '
-                        ]
+// tslint:disable-next-line: no-multiline-string
+                        'text/html': [`
+                            <div style="
+                            overflow: auto;
+                        ">
+                        <style scoped="">
+                            .dataframe tbody tr th:only-of-type {
+                                vertical-align: middle;
+                            }
+                            .dataframe tbody tr th {
+                                vertical-align: top;
+                            }
+                            .dataframe thead th {
+                                text-align: right;
+                            }
+                        </style>
+                        <table border="1" class="dataframe">
+                          <thead>
+                            <tr style="text-align: right;">
+                              <th></th>
+                              <th>0</th>
+                              <th>1</th>
+                              <th>2</th>
+                              <th>3</th>
+                              <th>4</th>
+                              <th>5</th>
+                              <th>6</th>
+                              <th>7</th>
+                              <th>8</th>
+                              <th>9</th>
+                              <th>...</th>
+                              <th>2990</th>
+                              <th>2991</th>
+                              <th>2992</th>
+                              <th>2993</th>
+                              <th>2994</th>
+                              <th>2995</th>
+                              <th>2996</th>
+                              <th>2997</th>
+                              <th>2998</th>
+                              <th>2999</th>
+                            </tr>
+                            <tr>
+                              <th>idx</th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <th>2007-01-31</th>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>...</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                              <td>37.060604</td>
+                            </tr>
+                            <tr>
+                              <th>2007-02-28</th>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>...</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                              <td>20.603407</td>
+                            </tr>
+                            <tr>
+                              <th>2007-03-31</th>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>...</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                              <td>6.142031</td>
+                            </tr>
+                            <tr>
+                              <th>2007-04-30</th>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>...</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                              <td>6.931635</td>
+                            </tr>
+                            <tr>
+                              <th>2007-05-31</th>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>...</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                              <td>52.642243</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        <p>5 rows × 3000 columns</p>
+                        </div>`
+                    ]
                     },
                     execution_count: 4,
                     metadata: {},
