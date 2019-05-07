49f2b7d1477e42db0c9674106f47f95b57f5d3d3
Kartik Raj <karraj@microsoft.com>
2019-05-07 15:53:31 -0700

    Updated tests

--- src/test/debugger/extension/hooks/childProcessAttachService.unit.test.ts
+++ src/test/debugger/extension/hooks/childProcessAttachService.unit.test.ts
@@ -7,7 +7,7 @@
 
 import { expect } from 'chai';
 import { anything, capture, instance, mock, verify, when } from 'ts-mockito';
-import { Uri, WorkspaceFolder } from 'vscode';
+import { DebugSession, Uri, WorkspaceFolder } from 'vscode';
 import { ApplicationShell } from '../../../../client/common/application/applicationShell';
 import { DebugService } from '../../../../client/common/application/debugService';
 import { WorkspaceService } from '../../../../client/common/application/workspace';
@@ -38,12 +38,13 @@ suite('Debug - Attach to Child Process', () => {
                 command: 'request'
             }
         };
-
+        const parentDebugSession = { id: 'id' };
         when(workspaceService.hasWorkspaceFolders).thenReturn(false);
-        when(debugService.startDebugging(anything(), anything())).thenResolve(true as any);
+        when(debugService.activeDebugSession).thenReturn(parentDebugSession as DebugSession);
+        when(debugService.startDebugging(anything(), anything(), anything())).thenResolve(true as any);
         await service.attach(data);
         verify(workspaceService.hasWorkspaceFolders).once();
-        verify(debugService.startDebugging(anything(), anything())).once();
+        verify(debugService.startDebugging(anything(), anything(), anything())).once();
     });
     test('Message is displayed if debugger is not launched', async () => {
         const shell = mock(ApplicationShell);
@@ -69,13 +70,14 @@ suite('Debug - Attach to Child Process', () => {
         };
 
         when(workspaceService.hasWorkspaceFolders).thenReturn(false);
-        when(debugService.startDebugging(anything(), anything())).thenResolve(false as any);
+        when(debugService.activeDebugSession).thenReturn(undefined);
+        when(debugService.startDebugging(anything(), anything(), anything())).thenResolve(false as any);
         when(shell.showErrorMessage(anything())).thenResolve();
 
         await service.attach(data);
 
         verify(workspaceService.hasWorkspaceFolders).once();
-        verify(debugService.startDebugging(anything(), anything())).once();
+        verify(debugService.startDebugging(anything(), anything(), anything())).once();
         verify(shell.showErrorMessage(anything())).once();
     });
     test('Use correct workspace folder', async () => {
@@ -108,12 +110,13 @@ suite('Debug - Attach to Child Process', () => {
 
         when(workspaceService.hasWorkspaceFolders).thenReturn(true);
         when(workspaceService.workspaceFolders).thenReturn([wkspace1, rightWorkspaceFolder, wkspace2]);
-        when(debugService.startDebugging(rightWorkspaceFolder, anything())).thenResolve(true as any);
+        when(debugService.activeDebugSession).thenReturn(undefined);
+        when(debugService.startDebugging(rightWorkspaceFolder, anything(), anything())).thenResolve(true as any);
 
         await service.attach(data);
 
         verify(workspaceService.hasWorkspaceFolders).once();
-        verify(debugService.startDebugging(rightWorkspaceFolder, anything())).once();
+        verify(debugService.startDebugging(rightWorkspaceFolder, anything(), anything())).once();
         verify(shell.showErrorMessage(anything())).never();
     });
     test('Use empty workspace folder if right one is not found', async () => {
@@ -146,12 +149,13 @@ suite('Debug - Attach to Child Process', () => {
 
         when(workspaceService.hasWorkspaceFolders).thenReturn(true);
         when(workspaceService.workspaceFolders).thenReturn([wkspace1, wkspace2]);
-        when(debugService.startDebugging(undefined, anything())).thenResolve(true as any);
+        when(debugService.activeDebugSession).thenReturn(undefined);
+        when(debugService.startDebugging(undefined, anything(), anything())).thenResolve(true as any);
 
         await service.attach(data);
 
         verify(workspaceService.hasWorkspaceFolders).once();
-        verify(debugService.startDebugging(undefined, anything())).once();
+        verify(debugService.startDebugging(undefined, anything(), anything())).once();
         verify(shell.showErrorMessage(anything())).never();
     });
     test('Validate debug config when parent/root parent was launched', async () => {
@@ -185,13 +189,13 @@ suite('Debug - Attach to Child Process', () => {
         debugConfig.request = 'attach';
 
         when(workspaceService.hasWorkspaceFolders).thenReturn(false);
-        when(debugService.startDebugging(undefined, anything())).thenResolve(true as any);
-        // when(debugService.startDebugging(undefined, debugConfig)).thenResolve(true as any);
+        when(debugService.activeDebugSession).thenReturn(undefined);
+        when(debugService.startDebugging(undefined, anything(), anything())).thenResolve(true as any);
 
         await service.attach(data);
 
         verify(workspaceService.hasWorkspaceFolders).once();
-        verify(debugService.startDebugging(undefined, anything())).once();
+        verify(debugService.startDebugging(undefined, anything(), anything())).once();
         const [, secondArg] = capture(debugService.startDebugging).last();
         expect(secondArg).to.deep.equal(debugConfig);
         verify(shell.showErrorMessage(anything())).never();
@@ -228,13 +232,14 @@ suite('Debug - Attach to Child Process', () => {
         debugConfig.request = 'attach';
 
         when(workspaceService.hasWorkspaceFolders).thenReturn(false);
-        when(debugService.startDebugging(undefined, anything())).thenResolve(true as any);
+        when(debugService.activeDebugSession).thenReturn(undefined);
+        when(debugService.startDebugging(undefined, anything(), anything())).thenResolve(true as any);
         // when(debugService.startDebugging(undefined, debugConfig)).thenResolve(true as any);
 
         await service.attach(data);
 
         verify(workspaceService.hasWorkspaceFolders).once();
-        verify(debugService.startDebugging(undefined, anything())).once();
+        verify(debugService.startDebugging(undefined, anything(), anything())).once();
         const [, secondArg] = capture(debugService.startDebugging).last();
         expect(secondArg).to.deep.equal(debugConfig);
         verify(shell.showErrorMessage(anything())).never();
