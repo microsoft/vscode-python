df3ea0290bc3069cfea58f5ab0584753ede5b554
Rich Chiodo <rchiodo@microsoft.com>
2019-05-24 11:07:21 -0700

    Fix hyperlinks

--- src/datascience-ui/history-react/contentPanel.tsx
+++ src/datascience-ui/history-react/contentPanel.tsx
@@ -27,6 +27,7 @@ export interface IContentPanelProps {
     deleteCell(index: number): void;
     onCodeChange(changes: monacoEditor.editor.IModelContentChange[], cellId: string, modelId: string): void;
     onCodeCreated(code: string, file: string, cellId: string, modelId: string): void;
+    openLink(uri: monacoEditor.Uri): void;
 }
 
 export class ContentPanel extends React.Component<IContentPanelProps> {
@@ -82,6 +83,7 @@ export class ContentPanel extends React.Component<IContentPanelProps> {
                     onCodeChange={this.props.onCodeChange}
                     onCodeCreated={this.props.onCodeCreated}
                     monacoTheme={this.props.monacoTheme}
+                    openLink={this.props.openLink}
                     />
             </ErrorBoundary>
         );
