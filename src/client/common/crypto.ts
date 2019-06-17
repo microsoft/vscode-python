6468d47c79bc130bc49bd58a757bbd9fe23eddb5
Kartik Raj <karraj@microsoft.com>
2019-06-17 15:54:05 -0700

    Fix CI

--- src/client/common/crypto.ts
+++ src/client/common/crypto.ts
@@ -2,9 +2,10 @@
 // Licensed under the MIT License.
 'use strict';
 
-import { injectable } from 'inversify';
+// tslint:disable: no-any
 
 import { createHash, HexBase64Latin1Encoding } from 'crypto';
+import { injectable } from 'inversify';
 import { ICryptoUtils, IHashFormat } from './types';
 
 /**
@@ -14,6 +15,6 @@ import { ICryptoUtils, IHashFormat } from './types';
 export class CryptoUtils implements ICryptoUtils {
     public createHash<E extends keyof IHashFormat>(data: string, encoding: HexBase64Latin1Encoding, hashFormat: E): IHashFormat[E] {
         const hash = createHash('sha512').update(data).digest(encoding);
-        return hashFormat === 'number' ? parseInt(hash, undefined) : hash;
+        return hashFormat === 'number' ? parseInt(hash, undefined) : hash as any;
     }
 }
