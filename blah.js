e3681ca08b0bd3b54f6a5c9fb8512835d22a6fe6
Don Jayamanne <don.jayamanne@yahoo.com>
2019-06-21 11:25:51 -0700

    Revert unnecessary changes

--- blah.js
+++
@@ -1,7 +0,0 @@
-const coveralls = require('coveralls');
-console.log(
-    coveralls.getOptions(function(ex, options) {
-        console.error(ex);
-        console.log(JSON.stringify(options, undefined, 4));
-    })
-);
