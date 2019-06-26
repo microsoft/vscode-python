fcbaa83d9a2cd261b836a5db6014318c4eb998e5
Kim-Adeline Miguel <kimiguel@microsoft.com>
2019-06-26 16:45:52 -0700

    Don't exclude first arg from being formatted

--- src/client/common/process/logger.ts
+++ src/client/common/process/logger.ts
@@ -13,7 +13,10 @@ export class ProcessLogger implements IProcessLogger {
     constructor(@inject(IOutputChannel) @named(STANDARD_OUTPUT_CHANNEL) private readonly outputChannel: IOutputChannel, @inject(IPathUtils) private readonly pathUtils: IPathUtils) { }
 
     public logProcess(file: string, args: string[], options?: SpawnOptions) {
-        const formattedArgs = args.reduce((accumulator, current, index) => index === 0 ? current : `${accumulator} ${this.pathUtils.getDisplayName(current.toCommandArgument())}`, '');
+        const formattedArgs = args.reduce((accumulator, current, index) => {
+            const formattedArg = this.pathUtils.getDisplayName(current.toCommandArgument());
+            return index === 0 ? formattedArg : `${accumulator} ${formattedArg}`;
+        }, '');
         const info = [`> ${this.pathUtils.getDisplayName(file)} ${formattedArgs}`];
         if (options && options.cwd) {
             info.push(`${Logging.currentWorkingDirectory()} ${this.pathUtils.getDisplayName(options.cwd)}`);
