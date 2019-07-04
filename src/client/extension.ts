f34ca57806e8c9c4d27fc92c0cc7a09c8d407017
Kim-Adeline Miguel <kimiguel@microsoft.com>
2019-07-04 11:14:29 -0700

    Undo prettier changes

--- src/client/extension.ts
+++ src/client/extension.ts
@@ -75,7 +75,13 @@ import { IDebugConfigurationService, IDebuggerBanner } from './debugger/extensio
 import { registerTypes as formattersRegisterTypes } from './formatters/serviceRegistry';
 import { AutoSelectionRule, IInterpreterAutoSelectionRule, IInterpreterAutoSelectionService } from './interpreter/autoSelection/types';
 import { IInterpreterSelector } from './interpreter/configuration/types';
-import { ICondaService, IInterpreterLocatorProgressService, IInterpreterService, InterpreterLocatorProgressHandler, PythonInterpreter } from './interpreter/contracts';
+import {
+    ICondaService,
+    IInterpreterLocatorProgressService,
+    IInterpreterService,
+    InterpreterLocatorProgressHandler,
+    PythonInterpreter
+} from './interpreter/contracts';
 import { registerTypes as interpretersRegisterTypes } from './interpreter/serviceRegistry';
 import { ServiceContainer } from './ioc/container';
 import { ServiceManager } from './ioc/serviceManager';
@@ -150,8 +156,7 @@ async function activateUnsafe(context: ExtensionContext): Promise<IExtensionApi>
 
     const workspaceService = serviceContainer.get<IWorkspaceService>(IWorkspaceService);
     const interpreterManager = serviceContainer.get<IInterpreterService>(IInterpreterService);
-    interpreterManager
-        .refresh(workspaceService.hasWorkspaceFolders ? workspaceService.workspaceFolders![0].uri : undefined)
+    interpreterManager.refresh(workspaceService.hasWorkspaceFolders ? workspaceService.workspaceFolders![0].uri : undefined)
         .catch(ex => console.error('Python Extension: interpreterManager.refresh', ex));
 
     const jupyterExtension = extensions.getExtension('donjayamanne.jupyter');
@@ -331,11 +336,9 @@ function isUsingGlobalInterpreterInWorkspace(currentPythonPath: string, serviceC
 function hasUserDefinedPythonPath(resource: Resource, serviceContainer: IServiceContainer) {
     const workspaceService = serviceContainer.get<IWorkspaceService>(IWorkspaceService);
     const settings = workspaceService.getConfiguration('python', resource)!.inspect<string>('pythonPath')!;
-    return (settings.workspaceFolderValue && settings.workspaceFolderValue !== 'python') ||
+    return ((settings.workspaceFolderValue && settings.workspaceFolderValue !== 'python') ||
         (settings.workspaceValue && settings.workspaceValue !== 'python') ||
-        (settings.globalValue && settings.globalValue !== 'python')
-        ? true
-        : false;
+        (settings.globalValue && settings.globalValue !== 'python')) ? true : false;
 }
 
 function getPreferredWorkspaceInterpreter(resource: Resource, serviceContainer: IServiceContainer) {
@@ -364,10 +367,7 @@ async function getActivationTelemetryProps(serviceContainer: IServiceContainer):
     const mainWorkspaceUri = workspaceService.hasWorkspaceFolders ? workspaceService.workspaceFolders![0].uri : undefined;
     const settings = configurationService.getSettings(mainWorkspaceUri);
     const [condaVersion, interpreter, interpreters] = await Promise.all([
-        condaLocator
-            .getCondaVersion()
-            .then(ver => (ver ? ver.raw : ''))
-            .catch<string>(() => ''),
+        condaLocator.getCondaVersion().then(ver => ver ? ver.raw : '').catch<string>(() => ''),
         interpreterService.getActiveInterpreter().catch<PythonInterpreter | undefined>(() => undefined),
         interpreterService.getInterpreters(mainWorkspaceUri).catch<PythonInterpreter[]>(() => [])
     ]);
@@ -401,7 +401,8 @@ async function getActivationTelemetryProps(serviceContainer: IServiceContainer):
 function handleError(ex: Error) {
     notifyUser('Extension activation failed, run the \'Developer: Toggle Developer Tools\' command for more information.');
     traceError('extension activation failed', ex);
-    sendErrorTelemetry(ex).ignoreErrors();
+    sendErrorTelemetry(ex)
+        .ignoreErrors();
 }
 
 interface IAppShell {
@@ -411,12 +412,13 @@ interface IAppShell {
 function notifyUser(msg: string) {
     try {
         // tslint:disable-next-line:no-any
-        let appShell: IAppShell = (window as any) as IAppShell;
+        let appShell: IAppShell = (window as any as IAppShell);
         if (activatedServiceContainer) {
             // tslint:disable-next-line:no-any
-            appShell = (activatedServiceContainer.get<IApplicationShell>(IApplicationShell) as any) as IAppShell;
+            appShell = activatedServiceContainer.get<IApplicationShell>(IApplicationShell) as any as IAppShell;
         }
-        appShell.showErrorMessage(msg).ignoreErrors();
+        appShell.showErrorMessage(msg)
+            .ignoreErrors();
     } catch (ex) {
         // ignore
     }
