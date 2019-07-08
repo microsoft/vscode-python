7c248da1b40d24ac68ebdaca1d38bd06a87cab13
Brett Cannon <brcan@microsoft.com>
2019-07-08 15:42:48 -0700

    Remove PYTHON_LANGUAGE_SERVER.ANALYSIS_TIME telemetry

--- src/client/telemetry/index.ts
+++ src/client/telemetry/index.ts
@@ -299,7 +299,6 @@ export interface IEventNamePropertyMapping {
     [EventName.PYTHON_INTERPRETER_DISCOVERY]: InterpreterDiscovery;
     [EventName.PYTHON_INTERPRETER_ACTIVATE_ENVIRONMENT_PROMPT]: { selection: 'Yes' | 'No' | 'Ignore' | undefined };
     [EventName.PYTHON_LANGUAGE_SERVER_SWITCHED]: { change: 'Switch to Jedi from LS' | 'Switch to LS from Jedi' };
-    [EventName.PYTHON_LANGUAGE_SERVER_ANALYSISTIME]: { success: boolean };
     [EventName.PYTHON_LANGUAGE_SERVER_DOWNLOADED]: LanguageServerVersionTelemetry;
     [EventName.PYTHON_LANGUAGE_SERVER_ENABLED]: never | undefined;
     [EventName.PYTHON_LANGUAGE_SERVER_ERROR]: LanguageServerErrorTelemetry;
