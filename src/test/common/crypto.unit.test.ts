e899f2112aac423dc2a1f3e1b8eddfdd73a37dd7
Kim-Adeline Miguel <kimiguel@microsoft.com>
2019-07-08 11:45:00 -0700

    Add tests

--- src/test/common/crypto.unit.test.ts
+++ src/test/common/crypto.unit.test.ts
@@ -19,4 +19,30 @@ suite('Crypto Utils', async () => {
         const hash = crypto.createHash('blabla', 'string');
         assert.typeOf(hash, 'string', 'Type should be a string');
     });
+    test('If hashFormat equals `number`, the hash should not be NaN', async () => {
+        let hash = crypto.createHash('test', 'number');
+        assert.isNotNaN(hash, 'Number hash should not be NaN');
+        hash = crypto.createHash('hash', 'number');
+        assert.isNotNaN(hash, 'Number hash should not be NaN');
+        hash = crypto.createHash('HASH1', 'number');
+        assert.isNotNaN(hash, 'Number hash should not be NaN');
+    });
+    test('If hashFormat equals `string`, the hash should not be undefined', async () => {
+        let hash = crypto.createHash('test', 'string');
+        assert.isDefined(hash, 'String hash should not be undefined');
+        hash = crypto.createHash('hash', 'string');
+        assert.isDefined(hash, 'String hash should not be undefined');
+        hash = crypto.createHash('HASH1', 'string');
+        assert.isDefined(hash, 'String hash should not be undefined');
+    });
+    test('If hashFormat equals `number`, hashes with different data should return different number hashes', async () => {
+        const hash1 = crypto.createHash('hash1', 'number');
+        const hash2 = crypto.createHash('hash2', 'number');
+        assert.notEqual(hash1, hash2, 'Hashes should be different numbers');
+    });
+    test('If hashFormat equals `string`, hashes with different data should return different string hashes', async () => {
+        const hash1 = crypto.createHash('hash1', 'string');
+        const hash2 = crypto.createHash('hash2', 'string');
+        assert.notEqual(hash1, hash2, 'Hashes should be different strings');
+    });
 });
