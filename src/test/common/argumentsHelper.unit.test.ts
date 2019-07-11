e447185a3da2d1ee01ceabeeb17f9b7ffb78cbe0
Kim-Adeline Miguel <kimiguel@microsoft.com>
2019-07-11 08:00:40 -0700

    Add tests for getPositionalArguments only

--- src/test/common/argumentsHelper.unit.test.ts
+++ src/test/common/argumentsHelper.unit.test.ts
@@ -24,39 +24,6 @@ suite('ArgumentsHelper tests', () => {
         argumentsHelper = new ArgumentsHelper(instance(serviceContainer));
     });
 
-    test('getOptionValues with an option should return the arg value after the option', () => {
-        const args = ['arg1', '--foo', 'arg2'];
-        const result = argumentsHelper.getOptionValues(args, '--foo');
-
-        expect(result).to.be.a('string');
-        expect(result).to.equal('arg2');
-    });
-
-    test('getOptionValues with a non-existent option should return nothing', () => {
-        const args = ['arg1', '--foo', 'arg2'];
-        const result = argumentsHelper.getOptionValues(args, '--bar');
-
-        expect(result).to.be.a('undefined');
-    });
-
-    test('getOptionValues with an option followed by = should return the value part of the arg', () => {
-        const args = ['arg1', '--foo=arg2', 'arg2'];
-        const result = argumentsHelper.getOptionValues(args, '--foo');
-
-        expect(result).to.be.a('string');
-        expect(result).to.equal('arg2');
-    });
-
-    test('getOptionValues with an option repeated multiple times should return an array of values', () => {
-        const args = ['arg1', '--foo', 'arg2', '--bar', '--foo', 'arg3'];
-        const result = argumentsHelper.getOptionValues(args, '--foo') as string[];
-
-        expect(result).to.be.a('array');
-        expect(result).to.have.length(2);
-        expect(result[0]).to.equal('arg2');
-        expect(result[1]).to.equal('arg3');
-    });
-
     test('getPositionalArguments with both options parameters should return correct positional arguments', () => {
         const args = ['arg1', '--foo', 'arg2', '--bar', 'arg3', 'arg4'];
         const optionsWithArguments = ['--bar'];
@@ -86,46 +53,5 @@ suite('ArgumentsHelper tests', () => {
         expect(result).to.deep.equal(argumentsHelper.getPositionalArguments(args));
         expect(result).to.have.length(4);
         expect(result).to.deep.equal(['arg1', 'arg2', 'arg3', 'arg4']);
-    });
-
-    test('filterArguments with both options parameters should return correct filtered arguments', () => {
-        const args = ['arg1', '--foo', 'arg2', '--bar', 'arg3', 'arg4'];
-        const optionsWithArguments = ['--bar'];
-        const optionsWithoutArguments = ['--foo'];
-        const result = argumentsHelper.filterArguments(args, optionsWithArguments, optionsWithoutArguments);
-
-        expect(result).to.have.length(3);
-        expect(result).to.deep.equal(['arg1', 'arg2', 'arg4']);
-    });
-
-    test('filterArguments with optionsWithArguments with inline `option=value` syntax should return correct filtered arguments', () => {
-        const args = ['arg1', '--foo', 'arg2', '--bar=arg3', 'arg4'];
-        const optionsWithArguments = ['--bar'];
-        const optionsWithoutArguments = ['--foo'];
-        const result = argumentsHelper.filterArguments(args, optionsWithArguments, optionsWithoutArguments);
-
-        expect(result).to.have.length(3);
-        expect(result).to.deep.equal(['arg1', 'arg2', 'arg4']);
-    });
-
-    test('filterArguments should ignore wildcard args', () => {
-        const args = ['arg1', '--foo', 'arg2', '--bar', 'arg3', 'arg4'];
-        const optionsWithArguments = ['--bar'];
-        const optionsWithoutArguments = ['--fo*'];
-        const result = argumentsHelper.filterArguments(args, optionsWithArguments, optionsWithoutArguments);
-
-        expect(result).to.have.length(3);
-        expect(result).to.deep.equal(['arg1', 'arg2', 'arg4']);
-    });
-
-    test('filterArguments with no options parameter should be the same as passing empty arrays', () => {
-        const args = ['arg1', '--foo', 'arg2', '--bar', 'arg3', 'arg4'];
-        const optionsWithArguments: string[] = [];
-        const optionsWithoutArguments: string[] = [];
-        const result = argumentsHelper.filterArguments(args, optionsWithArguments, optionsWithoutArguments);
-
-        expect(result).to.deep.equal(argumentsHelper.filterArguments(args));
-        expect(result).to.have.length(6);
-        expect(result).to.deep.equal(args);
     });
 });
