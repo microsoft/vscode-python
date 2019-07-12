8e695c35b1bca50dd793f845a06f94b604c4aa40
Don Jayamanne <don.jayamanne@yahoo.com>
2019-07-12 09:36:10 -0700

    Take screenshots

--- src/smoke/src/steps/testing.ts
+++ src/smoke/src/steps/testing.ts
@@ -91,8 +91,10 @@ When('I navigate to the code associated with the test node {string}', async (lab
 When('I debug the node {string} from the test explorer', CucumberRetryMax20Seconds, async function (this: any, label: string) {
     const counter = this.retryCounter = ((this.retryCounter || 0) + 1);
     console.log(`Start debugging node, counter ${counter}`);
+    await context.app.captureScreenshot(`take_a_screenshot_${new Date().getTime().toString()}`);
     await context.app.workbench.testExplorer.selectActionForNode(label, 'debug', counter * 2);
     console.log(`Clicked debug ${counter}`);
+    await context.app.captureScreenshot(`take_a_screenshot_${new Date().getTime().toString()}`);
     await context.app.code.waitForElement('div.debug-toolbar', ele => ele ? !ele.attributes.style.includes('[aria-hidden="true"]') : false, 50);
     console.log(`Debugger started ${counter}`);
 });
