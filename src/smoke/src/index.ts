433651947c049ad550e7453f377d609b4947c23f
Don Jayamanne <don.jayamanne@yahoo.com>
2019-07-11 17:33:37 -0700

    Capture times

--- src/smoke/src/index.ts
+++ src/smoke/src/index.ts
@@ -6,6 +6,7 @@
 import { After, AfterAll, Before, BeforeAll, HookScenarioResult, setDefaultTimeout, setDefinitionFunctionWrapper, setWorldConstructor, Status } from 'cucumber';
 import * as fs from 'fs-extra';
 import * as path from 'path';
+import { performance, PerformanceObserver } from 'perf_hooks';
 import { Application, context } from './application';
 import { featurePath, maxHookTimeout, maxStepTimeout } from './constants';
 import { AsyncFunction, noop, RetryOptions, retryWrapper } from './helpers';
@@ -13,6 +14,17 @@ import { activateAndDismissMessages, clearWorkspace, dismissMessages, resetWorks
 import { initialize, initializeBasePaths, restoreDefaultUserSettings } from './setup/setup';
 
 // tslint:disable: no-invalid-this mocha-no-side-effect-code no-any non-literal-require no-function-expression no-console
+const times: string[] = [];
+const obs = new PerformanceObserver((list) => {
+    const entry = list.getEntries()[0]!;
+    times.push(`Time for ('${entry.name}') ${entry.duration.toLocaleString()}`);
+    console.log(times[times.length - 1]);
+});
+obs.observe({ entryTypes: ['measure'], buffered: false });  //we want to react to full measurements and not individual marks
+
+// type ScenarioTimes = Record<string, { start: string; stop?: string; durations: { before?: number; steps?: number; after?: number; total?: number } }>;
+// const times: { beforeAll?: number; afterAll?: number; scenarios: ScenarioTimes } = { scenarios: {} };
+// const stopWatch = new StopWatch();
 
 setWorldConstructor(function (options: { attach: Function }) {
     const hook = (buffer: Buffer) => options.attach(buffer, 'image/png');
@@ -27,16 +39,21 @@ setDefaultTimeout(maxStepTimeout);
 // Wait for a max of 2 minutes (download VSC, install, activate python extension).
 // All of this takes time when running the first time, hence allow max timeout of 2 minutes.
 BeforeAll({ timeout: maxHookTimeout }, async function () {
+    performance.mark('BeforeAll-start');
     console.log('Before all');
     const testOptions = await initialize();
     const app = new Application(testOptions);
     await app.start();
     await activateAndDismissMessages(app);
     await app.stop();
+    performance.mark('BeforeAll-end');
+    performance.measure('BeforeAll', 'BeforeAll-start', 'BeforeAll-end');
 });
 
 // const lastSetWorkspaceFolder = '';
 Before(async function (scenario: HookScenarioResult) {
+    performance.mark(`${scenario.pickle.name}-start`);
+    performance.mark(`${scenario.pickle.name}-before-start`);
     const location = scenario.pickle.locations[0].line;
     const sourceLocation = path.relative(featurePath, scenario.sourceLocation.uri);
     const scenarioLogsPath = path.join(sourceLocation, `${scenario.pickle.name}:${location}`.replace(/[^a-z0-9\-]/gi, '_'));
@@ -62,9 +79,12 @@ Before(async function (scenario: HookScenarioResult) {
     }
     // Sometimes it fails due to connectivity issues.
     await retryWrapper({ count: 3 }, beforeHook);
+    performance.mark(`${scenario.pickle.name}-before-end`);
+    performance.measure(`${scenario.pickle.name}-before`, `${scenario.pickle.name}-before-start`, `${scenario.pickle.name}-before-end`);
 });
 
 After(async function (scenario: HookScenarioResult) {
+    performance.mark(`${scenario.pickle.name}-after-start`);
     try {
         const name = `After_${new Date().getTime()}`.replace(/[^a-z0-9\-]/gi, '_');
         // Ignore errors, as its possible app hasn't started.
@@ -85,14 +105,24 @@ After(async function (scenario: HookScenarioResult) {
             await fs.emptyDir(context.options.logsPath).catch(noop);
         }
     }
+    performance.mark(`${scenario.pickle.name}-after-end`);
+    performance.mark(`${scenario.pickle.name}-end`);
+    performance.measure(`${scenario.pickle.name}-after`, `${scenario.pickle.name}-after-start`, `${scenario.pickle.name}-after-end`);
+    performance.measure(scenario.pickle.name, `${scenario.pickle.name}-start`, `${scenario.pickle.name}-end`);
+
 });
 
 AfterAll({ timeout: maxHookTimeout }, async function () {
+    performance.mark('AfterAll-start');
     // Possible it has already died.
     await clearWorkspace().catch(noop);
     await context.app.stop().catch(ex => {
         console.error('Failed to shutdown gracefully', ex);
     });
+
+    performance.mark('AfterAll-end');
+    performance.measure('AfterAll', 'AfterAll-start', 'AfterAll-end');
+    console.log(times.join('\n'));
 });
 
 /*
