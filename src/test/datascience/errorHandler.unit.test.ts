2b4656f4a63736a319ba253f4623f4ff5a5490ca
David Kutugata <dakutuga@microsoft.com>
2019-07-24 13:54:25 -0700

    fixed unit tests to use typemoq

--- src/test/datascience/errorHandler.unit.test.ts
+++ src/test/datascience/errorHandler.unit.test.ts
@@ -1,60 +1,96 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 'use strict';
-import { expect } from 'chai';
-import { mock } from 'ts-mockito';
-import { InstallationChannelManager } from '../../client/common/installer/channelManager';
-import { ProductNames } from '../../client/common/installer/productNames';
-import { Product } from '../../client/common/types';
+import * as typemoq from 'typemoq';
+import { IApplicationShell } from '../../client/common/application/types';
+import { IInstallationChannelManager, IModuleInstaller } from '../../client/common/installer/types';
+import { ILogger } from '../../client/common/types';
+import * as localize from '../../client/common/utils/localize';
+import { DataScienceErrorHandler } from '../../client/datascience/errorHandler/errorHandler';
 import { JupyterInstallError } from '../../client/datascience/jupyter/jupyterInstallError';
 import { JupyterSelfCertsError } from '../../client/datascience/jupyter/jupyterSelfCertsError';
-import { IDataScienceErrorHandler } from '../../client/datascience/types';
-
-class MockErrorHandler implements IDataScienceErrorHandler {
-    private channels = mock(InstallationChannelManager);
-
-    public async handleError(err: Error): Promise<string> {
-        if (err instanceof JupyterInstallError) {
-            const installers = await this.channels.getInstallationChannels();
-            if (installers.length > 0) {
-                // If Conda is available, always pick it as the user must have a Conda Environment
-                const installer = installers.find(ins => ins.name === 'Conda');
-                const product = ProductNames.get(Product.jupyter);
-
-                if (installer && product) {
-                    return 'installing';
-                } else if (installers[0] && product) {
-                    return 'installing';
-                }
-            }
-            return 'error';
-        } else if (err instanceof JupyterSelfCertsError) {
-            return 'noop';
-        } else if (err.message) {
-            return err.message;
-        } else {
-            return err.toString();
-        }
-    }
-}
 
 suite('DataScience Error Handler Unit Tests', () => {
-    const dataScienceErrorHandler = new MockErrorHandler();
+    let applicationShell: typemoq.IMock<IApplicationShell>;
+    let logger: typemoq.IMock<ILogger>;
+    let channels: typemoq.IMock<IInstallationChannelManager>;
+    let dataScienceErrorHandler: DataScienceErrorHandler;
+
+    setup(() => {
+        applicationShell = typemoq.Mock.ofType<IApplicationShell>();
+        logger = typemoq.Mock.ofType<ILogger>();
+        channels = typemoq.Mock.ofType<IInstallationChannelManager>();
+
+        dataScienceErrorHandler = new DataScienceErrorHandler(applicationShell.object, logger.object, channels.object);
+    });
     const message = 'Test error message.';
 
-    test('Default error', async () => {
+    test('Default error', () => {
+        applicationShell.setup(app => app.showErrorMessage(typemoq.It.isAny()))
+            .returns(() => Promise.resolve(message))
+            .verifiable(typemoq.Times.once());
+
+        logger.setup(log => log.logError(typemoq.It.isAny()))
+            .verifiable(typemoq.Times.once());
+
         const err = new Error(message);
-        const result = await dataScienceErrorHandler.handleError(err);
-        expect(result).to.be.equal(message);
+        dataScienceErrorHandler.handleError(err);
+
+        applicationShell.verifyAll();
+        logger.verifyAll();
     });
-    test('Jupyter Self Certificates Error', async () => {
+
+    test('Jupyter Self Certificates Error', () => {
+        applicationShell.setup(app => app.showErrorMessage(typemoq.It.isAny()))
+            .returns(() => Promise.resolve(message))
+            .verifiable(typemoq.Times.never());
+
+        logger.setup(log => log.logError(typemoq.It.isAny()))
+            .verifiable(typemoq.Times.once());
+
         const err = new JupyterSelfCertsError(message);
-        const result = await dataScienceErrorHandler.handleError(err);
-        expect(result).to.be.equal('noop');
+        dataScienceErrorHandler.handleError(err);
+
+        applicationShell.verifyAll();
+        logger.verifyAll();
     });
-    test('Jupyter Install Error', async () => {
+
+    test('Jupyter Install Error', () => {
+        applicationShell.setup(app => app.showInformationMessage(typemoq.It.isAny(),
+            typemoq.It.isValue(localize.DataScience.jupyterInstall()),
+            typemoq.It.isValue(localize.DataScience.notebookCheckForImportNo())))
+            .returns(() => Promise.resolve(localize.DataScience.jupyterInstall()))
+            .verifiable(typemoq.Times.once());
+
+        logger.setup(log => log.logError(typemoq.It.isAny()))
+            .verifiable(typemoq.Times.once());
+
+        const installers: IModuleInstaller[] = [
+            {
+                name: 'Pip',
+                displayName: 'Pip',
+                priority: 0,
+                isSupported: () => Promise.resolve(true),
+                installModule: () => Promise.resolve()
+            },
+            {
+                name: 'Conda',
+                displayName: 'Conda',
+                priority: 0,
+                isSupported: () => Promise.resolve(true),
+                installModule: () => Promise.resolve()
+            }
+        ];
+
+        channels.setup(ch => ch.getInstallationChannels())
+            .returns(() => Promise.resolve(installers))
+            .verifiable(typemoq.Times.once());
+
         const err = new JupyterInstallError(message, 'test.com');
-        const result = await dataScienceErrorHandler.handleError(err);
-        expect(result).to.be.equal('installing');
+        dataScienceErrorHandler.handleError(err);
+
+        applicationShell.verifyAll();
+        logger.verifyAll();
+        channels.verifyAll();
     });
 });
