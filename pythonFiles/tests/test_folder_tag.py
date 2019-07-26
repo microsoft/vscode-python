370e895a2e36b3008fec9ab63d6badb9da0aec1d
Kim-Adeline Miguel <kimiguel@microsoft.com>
2019-07-25 21:48:14 -0700

    add tests for get_folder_tag

---
+++ pythonFiles/tests/test_folder_tag.py
@@ -0,0 +1,32 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import platform
+import pytest
+
+from folder_tag import get_folder_tag
+
+
+@pytest.mark.parametrize(
+    "os,arch,expected",
+    [
+        ("Darwin", 32, "mac-64"),
+        ("Darwin", 64, "mac-64"),
+        ("Windows", 32, "win-32"),
+        ("Windows", 64, "win-64"),
+        ("Linux", 64, "linux-64"),
+        ("Linux", 32, "linux-32"),
+        ("Anything", 64, "linux-64"),
+    ],
+)
+def test_folder(monkeypatch, os, arch, expected):
+    def mock_os():
+        return os
+
+    def mock_arch():
+        return (f"{arch}bit", "")
+
+    monkeypatch.setattr(platform, "system", mock_os)
+    monkeypatch.setattr(platform, "architecture", mock_arch)
+    assert get_folder_tag() == expected
+
