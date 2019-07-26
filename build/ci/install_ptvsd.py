4334a36412995eaaa052000f17346515bc7913c1
Kim-Adeline Miguel <kimiguel@microsoft.com>
2019-07-25 20:29:18 -0700

    Remove linebreaks and move comment

--- build/ci/install_ptvsd.py
+++ build/ci/install_ptvsd.py
@@ -58,7 +58,6 @@ def install_ptvsd_wheels(version):
     }
 
     for folder in platforms:
-        # Remove the platform folder and its content if it exists.
         dirpath = os.path.join(
             os.path.dirname(__file__),
             "..",
@@ -67,13 +66,12 @@ def install_ptvsd_wheels(version):
             "lib",
             f"python-{folder}-{version}",
         )
+        # Remove the platform folder and its content if it exists.
         delete_folder(dirpath)
 
         # Download and install the appropriate PTVSD wheel.
         os.makedirs(dirpath)
-
         download_wheel(platforms[folder], dirpath)
-
         wheel = get_wheel_name(dirpath)
         install_wheel(wheel, dirpath)
 
