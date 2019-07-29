4dae8acc91e9135fef4d2da8459bccddc533a4f6
Eric Snow <ericsnowcurrently@gmail.com>
2019-07-29 11:44:49 -0600

    Follow existing pattern for testing registerTypes().

--- src/test/terminals/serviceRegistry.unit.test.ts
+++ src/test/terminals/serviceRegistry.unit.test.ts
@@ -1,10 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { expect } from 'chai';
-import { interfaces } from 'inversify';
+import * as typemoq from 'typemoq';
 import { IExtensionActivationService } from '../../client/activation/types';
-import { ClassType } from '../../client/ioc/types';
+import { IServiceManager } from '../../client/ioc/types';
 import { ExtensionActivationForTerminalActivation, TerminalAutoActivation } from '../../client/terminals/activation';
 import { CodeExecutionManager } from '../../client/terminals/codeExecution/codeExecutionManager';
 import { DjangoShellCodeExecutionProvider } from '../../client/terminals/codeExecution/djangoShellCodeExecution';
@@ -19,49 +18,35 @@ import {
 
 suite('Terminal - Service Registry', () => {
     test('Ensure all services get registered', () => {
-        // tslint:disable-next-line:no-use-before-declare
-        const stub = new StubRegistry();
-
-        registerTypes(stub);
-
-        expect(stub.sort()).to.deep.equal([
-            [ICodeExecutionHelper, CodeExecutionHelper, undefined],
-            [ICodeExecutionManager, CodeExecutionManager, undefined],
+        const services = typemoq.Mock.ofType<IServiceManager>(undefined, typemoq.MockBehavior.Strict);
+        [
+            [ICodeExecutionHelper, CodeExecutionHelper],
+            [ICodeExecutionManager, CodeExecutionManager],
             [ICodeExecutionService, DjangoShellCodeExecutionProvider, 'djangoShell'],
-            [IExtensionActivationService, ExtensionActivationForTerminalActivation, undefined],
+            [IExtensionActivationService, ExtensionActivationForTerminalActivation],
             [ICodeExecutionService, ReplProvider, 'repl'],
-            [ITerminalAutoActivation, TerminalAutoActivation, undefined],
+            [ITerminalAutoActivation, TerminalAutoActivation],
             [ICodeExecutionService, TerminalCodeExecutionProvider, 'standard']
-        ], 'wrong services registered');
+        ].forEach(args => {
+            if (args.length === 2) {
+                services.setup(s => s.addSingleton(
+                    // tslint:disable-next-line:no-any
+                    typemoq.It.isValue(args[0] as any),
+                    typemoq.It.is(value => args[1] === value))
+                ).verifiable(typemoq.Times.once());
+            } else {
+                services.setup(s => s.addSingleton(
+                    // tslint:disable-next-line:no-any
+                    typemoq.It.isValue(args[0] as any),
+                    typemoq.It.is(value => args[1] === value),
+                    // tslint:disable-next-line:no-any
+                    typemoq.It.isValue(args[2] as any))
+                ).verifiable(typemoq.Times.once());
+            }
+        });
+
+        registerTypes(services.object);
+
+        services.verifyAll();
     });
 });
-
-class StubRegistry {
-    // tslint:disable-next-line:no-any
-    private registered: [any, any, any][];
-    constructor() {
-        this.registered = [];
-    }
-    public addSingleton<T>(
-        svc: interfaces.ServiceIdentifier<T>,
-        ctor: ClassType<T>,
-        name?: string | number | symbol
-    ): void {
-        this.registered.push([svc, ctor, name]);
-    }
-
-    // tslint:disable-next-line:no-any
-    public sort(): [any, any, any][] {
-        return this.registered.sort((r1, r2) => {
-            const c1 = r1[1];
-            const c2 = r2[1];
-            if (c1 > c2) {
-                return 1;
-            }
-            if (c1 < c2) {
-                return -1;
-            }
-            return 0;
-        });
-    }
-}
