9add75cfdfa806b1fd8e903d29397b980db85392
Eric Snow <ericsnowcurrently@gmail.com>
2019-08-01 15:21:26 -0600

    Inline the regexes.

--- src/client/language/languageConfiguration.ts
+++ src/client/language/languageConfiguration.ts
@@ -4,28 +4,17 @@
 
 import { IndentAction } from 'vscode';
 
-export const MULTILINE_SEPARATOR_INDENT_REGEX = /^(?!\s+\\)[^#\n]+\\$/;
-/**
- * This does not handle all cases. However, it does handle nearly all usage.
- * Here's what it does not cover:
- * - the statement is split over multiple lines (and hence the ":" is on a different line)
- * - the code block is inlined (after the ":")
- * - there are multiple statements on the line (separated by semicolons)
- * Also note that `lambda` is purposefully excluded.
- */
-export const INCREASE_INDENT_REGEX = /^\s*(?:(?:async|class|def|elif|except|for|if|while|with)\b.*|(else|finally|try))\s*:\s*(#.*)?$/;
-export const DECREASE_INDENT_REGEX = /^\s*(?:else|finally|(?:elif|except)\b.*)\s*:\s*(#.*)?$/;
-export const OUTDENT_ONENTER_REGEX = /^\s*(?:break|continue|pass|(?:raise|return)\b.*)\s*(#.*)?$/;
-
 export function getLanguageConfiguration() {
     return {
         onEnterRules: [
+            // multi-line separator
             {
-                beforeText: MULTILINE_SEPARATOR_INDENT_REGEX,
+                beforeText: /^(?!\s+\\)[^#\n]+\\$/,
                 action: {
                     indentAction: IndentAction.Indent
                 }
             },
+            // continue comments
             {
                 beforeText: /^\s*#.*/,
                 afterText: /.+$/,
@@ -34,16 +23,25 @@ export function getLanguageConfiguration() {
                     appendText: '# '
                 }
             },
+            // outdent on enter
             {
-                beforeText: OUTDENT_ONENTER_REGEX,
+                beforeText: /^\s*(?:break|continue|pass|(?:raise|return)\b.*)\s*(#.*)?$/,
                 action: {
                     indentAction: IndentAction.Outdent
                 }
             }
         ],
         indentationRules: {
-            increaseIndentPattern: INCREASE_INDENT_REGEX,
-            decreaseIndentPattern: DECREASE_INDENT_REGEX
+            /**
+             * This does not handle all cases. However, it does handle nearly all usage.
+             * Here's what it does not cover:
+             * - the statement is split over multiple lines (and hence the ":" is on a different line)
+             * - the code block is inlined (after the ":")
+             * - there are multiple statements on the line (separated by semicolons)
+             * Also note that `lambda` is purposefully excluded.
+             */
+            increaseIndentPattern: /^\s*(?:(?:async|class|def|elif|except|for|if|while|with)\b.*|(else|finally|try))\s*:\s*(#.*)?$/,
+            decreaseIndentPattern: /^\s*(?:else|finally|(?:elif|except)\b.*)\s*:\s*(#.*)?$/
         }
     };
 }
