0ccf9cda3040317e13eb05c473d8d2cb4aae47bc
Eric Snow <ericsnowcurrently@gmail.com>
2019-08-12 10:05:06 -0600

    Fill in language config regex test coverage.

--- src/test/language/languageConfiguration.unit.test.ts
+++ src/test/language/languageConfiguration.unit.test.ts
@@ -3,18 +3,67 @@
 
 'use strict';
 
+// tslint:disable:max-func-body-length
+
 import { expect } from 'chai';
 
 import {
     getLanguageConfiguration
 } from '../../client/language/languageConfiguration';
 
+const NEEDS_INDENT = [
+    /^break$/,
+    /^continue$/,
+    /^raise$/,  // only re-raise
+    /^return\b/
+];
+const INDENT_ON_MATCH = [
+    /^async\s+def\b/,
+    /^class\b/,
+    /^def\b/,
+    /^with\b/,
+    /^try\b/,
+    /^except\b/,
+    /^finally\b/,
+    /^while\b/,
+    /^for\b/,
+    /^if\b/,
+    /^elif\b/,
+    /^else\b/
+];
+const DEDENT_ON_MATCH = [
+    /^elif\b/,
+    /^else\b/,
+    /^except\b/,
+    /^finally\b/
+];
+const DEDENT_ON_ENTER = [
+    /^break$/,
+    /^continue$/,
+    /^raise\b/,
+    /^return\b/,
+    /^pass\b/
+];
+
+function isMember(line: string, regexes: RegExp[]): boolean {
+    for (const regex of regexes) {
+        if (regex.test(line)) {
+            return true;
+        }
+    }
+    return false;
+}
+
 suite('Language configuration regexes', () => {
     const cfg = getLanguageConfiguration();
+    const MULTILINE_SEPARATOR_INDENT_REGEX = cfg.onEnterRules[0].beforeText;
     const DECREASE_INDENT_REGEX = cfg.indentationRules.decreaseIndentPattern;
     const INCREASE_INDENT_REGEX = cfg.indentationRules.increaseIndentPattern;
-    const MULTILINE_SEPARATOR_INDENT_REGEX = cfg.onEnterRules[0].beforeText;
     const OUTDENT_ONENTER_REGEX = cfg.onEnterRules[2].beforeText;
+    // To see the actual (non-verbose) regex patterns, un-comment here:
+    //console.log(DECREASE_INDENT_REGEX.source);
+    //console.log(INCREASE_INDENT_REGEX.source);
+    //console.log(OUTDENT_ONENTER_REGEX.source);
 
     test('Multiline separator indent regex should not pick up strings with no multiline separator', async () => {
         const result = MULTILINE_SEPARATOR_INDENT_REGEX.test('a = "test"');
@@ -32,77 +81,151 @@ suite('Language configuration regexes', () => {
     });
 
     [
+        // compound statements
         'async def test(self):',
-        'class TestClass:',
-        'def foo(self, node, namespace=""):',
+        'async def :',
+        'async :',
+        'class Test:',
+        'class Test(object):',
+        'class :',
+        'def spam():',
+        'def spam(self, node, namespace=""):',
+        'def :',
         'for item in items:',
+        'for item in :',
+        'for :',
         'if foo is None:',
+        'if :',
         'try:',
         'while \'::\' in macaddress:',
-        'with self.test:'
-    ].forEach(example => {
-        const keyword = example.split(' ')[0];
-
-        test(`Increase indent regex should pick up lines containing the ${keyword} keyword`, async () => {
-            const result = INCREASE_INDENT_REGEX.test(example);
-            expect(result).to.be.equal(true, `Increase indent regex should pick up lines containing the ${keyword} keyword`);
-        });
-
-        test(`Decrease indent regex should not pick up lines containing the ${keyword} keyword`, async () => {
-            const result = DECREASE_INDENT_REGEX.test(example);
-            expect(result).to.be.equal(false, `Decrease indent regex should not pick up lines containing the ${keyword} keyword`);
-        });
-    });
-
-    [
+        'while :',
+        'with self.test:',
+        'with :',
         'elif x < 5:',
+        'elif :',
         'else:',
+        //'else if x < 5:',
         'except TestError:',
-        'finally:'
-    ].forEach(example => {
-        const keyword = example.split(' ')[0];
+        'except :',
+        'finally:',
+        // simple statemenhts
+        'pass',
+        'raise Exception(msg)',
+        'raise Exception',
+        'raise',  // re-raise
+        'break',
+        'continue',
+        'return True',
+        'return (True, False, False)',
+        'return [True, False, False]',
+        'return {True, False, False}',
+        'return (',
+        'return [',
+        'return {',
+        'return',
+        // bogus
+        '',
+        ' ',
+        '  '
+    ].forEach(base => {
+        [
+            ['', '', '', ''],
+            // leading
+            ['    ', '', '', ''],
+            ['   ', '', '', ''],  // unusual indent
+            ['\t\t', '', '', ''],
+            // pre-keyword
+            ['x', '', '', ''],
+            // post-keyword
+            ['', 'x', '', ''],
+            // pre-colon
+            ['', '', ' ', ''],
+            // trailing
+            ['', '', '', ' '],
+            ['', '', '', '# a comment'],
+            ['', '', '', ' # ...']
+        ].forEach(whitespace => {
+            const [leading, postKeyword, preColon, trailing] = whitespace;
+            let invalid: string | undefined;
+            if (base.trim() === '') {
+                invalid = 'blank line';
+            } else if (leading === '' && isMember(base, NEEDS_INDENT)) {
+                invalid = 'expected indent';
+            } else if (leading.trim() !== '') {
+                invalid = 'look-alike - pre-keyword';
+            } else if (postKeyword.trim() !== '') {
+                invalid = 'look-alike - post-keyword';
+            }
 
-        test(`Increase indent regex should pick up lines containing the ${keyword} keyword`, async () => {
-            const result = INCREASE_INDENT_REGEX.test(example);
-            expect(result).to.be.equal(true, `Increase indent regex should pick up lines containing the ${keyword} keyword`);
-        });
+            let resolvedBase = base;
+            if (postKeyword !== '') {
+                if (resolvedBase.includes(' ')) {
+                    const kw = resolvedBase.split(' ', 1)[0];
+                    const remainder = resolvedBase.substring(kw.length);
+                    resolvedBase = `${kw}${postKeyword} ${remainder}`;
+                } else {
+                    if (resolvedBase.endsWith(':')) {
+                        resolvedBase = `${resolvedBase.substring(0, resolvedBase.length - 1)}${postKeyword}:`;
+                    } else {
+                        resolvedBase = `${resolvedBase}${postKeyword}`;
+                    }
+                }
+            }
+            if (preColon !== '') {
+                if (resolvedBase.endsWith(':')) {
+                    resolvedBase = `${resolvedBase.substring(0, resolvedBase.length - 1)}${preColon}:`;
+                } else {
+                    return;
+                }
+            }
+            const example = `${leading}${resolvedBase}${trailing}`;
 
-        test(`Decrease indent regex should pick up lines containing the ${keyword} keyword`, async () => {
-            const result = DECREASE_INDENT_REGEX.test(example);
-            expect(result).to.be.equal(true, `Decrease indent regex should pick up lines containing the ${keyword} keyword`);
-        });
-    });
+            if (invalid) {
+                test(`Line "${example}" ignored (${invalid})`, () => {
+                    let result = INCREASE_INDENT_REGEX.test(example);
+                    expect(result).to.be.equal(false, 'unexpected match');
 
-    test('Increase indent regex should not pick up lines without keywords', async () => {
-        const result = INCREASE_INDENT_REGEX.test('a = \'hello \\n \'');
-        expect(result).to.be.equal(false, 'Increase indent regex should not pick up lines without keywords');
-    });
+                    result = DECREASE_INDENT_REGEX.test(example);
+                    expect(result).to.be.equal(false, 'unexpected match');
 
-    test('Decrease indent regex should not pick up lines without keywords', async () => {
-        const result = DECREASE_INDENT_REGEX.test('a = \'hello \\n \'');
-        expect(result).to.be.equal(false, 'Decrease indent regex should not pick up lines without keywords');
-    });
+                    result = OUTDENT_ONENTER_REGEX.test(example);
+                    expect(result).to.be.equal(false, 'unexpected match');
+                });
+                return;
+            }
 
-    [
-        '    break',
-        '\t\t continue',
-        ' pass',
-        'raise Exception(\'Unknown Exception\'',
-        '    return',
-        '    return [ True, False, False ]'
-    ].forEach(example => {
-        const keyword = example.trim().split(' ')[0];
+            test(`Check indent-on-match for line "${example}"`, () => {
+                let expected = false;
+                if (isMember(base, INDENT_ON_MATCH)) {
+                    expected = true;
+                }
 
-        const testWithoutComments = `Outdent regex for on enter rule should pick up lines containing the ${keyword} keyword`;
-        test(testWithoutComments, () => {
-            const result = OUTDENT_ONENTER_REGEX.test(example);
-            expect(result).to.be.equal(true, testWithoutComments);
-        });
+                const result = INCREASE_INDENT_REGEX.test(example);
 
-        const testWithComments = `Outdent regex on enter should pick up lines containing the ${keyword} keyword and ending with comments`;
-        test(testWithComments, () => {
-            const result = OUTDENT_ONENTER_REGEX.test(`${example} # test comment`);
-            expect(result).to.be.equal(true, testWithComments);
+                expect(result).to.be.equal(expected, 'unexpected result');
+            });
+
+            test(`Check dedent-on-match for line "${example}"`, () => {
+                let expected = false;
+                if (isMember(base, DEDENT_ON_MATCH)) {
+                    expected = true;
+                }
+
+                const result = DECREASE_INDENT_REGEX.test(example);
+
+                expect(result).to.be.equal(expected, 'unexpected result');
+            });
+
+            test(`Check dedent-on-enter for line "${example}"`, () => {
+                let expected = false;
+                if (isMember(base, DEDENT_ON_ENTER)) {
+                    expected = true;
+                }
+
+                const result = OUTDENT_ONENTER_REGEX.test(example);
+
+                expect(result).to.be.equal(expected, 'unexpected result');
+            });
         });
     });
 });
