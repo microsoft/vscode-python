5acebc1301e46af9c4ed31c29f89f9136dfb0b5c
David Kutugata <dakutuga@microsoft.com>
2019-08-13 11:12:13 -0700

    added an exception when PTVSD fails to attach

--- src/client/datascience/jupyter/jupyterDebugger.ts
+++ src/client/datascience/jupyter/jupyterDebugger.ts
@@ -88,6 +88,9 @@ export class JupyterDebugger implements IJupyterDebugger, ICellHashListener {
             //     };
             //     await this.debugService.activeDebugSession.customRequest('setExceptionBreakpoints', args);
             // }
+        } else {
+            // if we cannot connect to the server, throw so we exit out of debugging
+            throw new JupyterDebuggerNotInstalledError(localize.DataScience.environmentVariablePtvsdLogDirIsSet());
         }
     }
 
