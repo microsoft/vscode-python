538feb813d189dcd160290ea5af6135b3dfe4d38
Kim-Adeline Miguel <kimiguel@microsoft.com>
2019-08-23 09:16:27 -0700

    more comments stuff

--- build/ci/install_ptvsd.py
+++ build/ci/install_ptvsd.py
@@ -21,9 +21,9 @@ if __name__ == "__main__":
         ptvsd_version = releases_keys[-1]
 
         for wheel_info in releases[ptvsd_version]:
-            # Download only if it's a 3.7 wheel .
+            # Download only if it's a 3.7 wheel.
             if wheel_info["python_version"].endswith("37"):
-                wheel_filename = wheel_info["filename"][:-4]  # trim the file extension
+                wheel_filename = wheel_info["filename"][:-4]  # Trim the file extension
                 ptvsd_path = path.join(PYTHONFILES_PATH, wheel_filename)
 
                 with urllib.request.urlopen(wheel_info["url"]) as wheel_response:
