b79ef05620039025f71951af12e9cd103e2fa2be
Kim-Adeline Miguel <kimiguel@microsoft.com>
2019-08-23 09:06:04 -0700

    Flatten folder structure

--- build/ci/install_ptvsd.py
+++ build/ci/install_ptvsd.py
@@ -22,12 +22,20 @@ if __name__ == "__main__":
 
         for wheel_info in releases[ptvsd_version]:
             # Download only if it's a 3.7 wheel (aka don't download the source code).
-            if wheel_info["filename"].endswith(".whl") and wheel_info[
-                "python_version"
-            ].endswith("37"):
-                filename = wheel_info["filename"][:-4]
+            if wheel_info["python_version"].endswith("37"):
+                wheel_filename = wheel_info["filename"][:-4]  # trim .whl
+                ptvsd_path = path.join(PYTHONFILES_PATH, wheel_filename)
 
                 with urllib.request.urlopen(wheel_info["url"]) as wheel_response:
                     wheel_file = BytesIO(wheel_response.read())
+                    # Extract only the contents of the ptvsd subfolder.
+                    prefix = path.join(
+                        f"ptvsd-{ptvsd_version}.data", "purelib", "ptvsd"
+                    )
+
                     with ZipFile(wheel_file, "r") as wheel:
-                        wheel.extractall(path.join(PYTHONFILES_PATH, filename))
+                        for zip_info in wheel.infolist():
+                            if zip_info.filename.startswith(prefix):
+                                # Flatten the folder structure.
+                                zip_info.filename = zip_info.filename.split(prefix)[-1]
+                                wheel.extract(zip_info, ptvsd_path)
