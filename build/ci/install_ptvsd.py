fcc8834ec0447baa7a4e1fa6efcd5d12100f80e4
Kim-Adeline Miguel <kimiguel@microsoft.com>
2019-08-26 13:55:00 -0700

    Dedent wherever possible

--- build/ci/install_ptvsd.py
+++ build/ci/install_ptvsd.py
@@ -13,29 +13,29 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen(PYPI_PTVSD_URL) as response:
         json_response = json.loads(response.read())
-        releases = json_response["releases"]
+    releases = json_response["releases"]
 
-        # Remove these lines when the version of PTVSD in requirements.txt gets updated.
-        # (and add code leveraging the packaging module to parse requirements.txt)
-        releases_keys = list(releases)
-        ptvsd_version = releases_keys[-1]
+    # Remove these lines when the version of PTVSD in requirements.txt gets updated.
+    # (and add code leveraging the packaging module to parse requirements.txt)
+    releases_keys = list(releases)
+    ptvsd_version = releases_keys[-1]
 
-        for wheel_info in releases[ptvsd_version]:
-            # Download only if it's a 3.7 wheel.
-            if wheel_info["python_version"].endswith("37"):
-                wheel_filename = wheel_info["filename"][:-4]  # Trim the file extension
-                ptvsd_path = path.join(PYTHONFILES_PATH, wheel_filename)
+    for wheel_info in releases[ptvsd_version]:
+        # Download only if it's a 3.7 wheel.
+        if not wheel_info["python_version"].endswith("37"):
+            continue
+        wheel_filename = wheel_info["filename"][:-4]  # Trim the file extension
+        ptvsd_path = path.join(PYTHONFILES_PATH, wheel_filename)
 
-                with urllib.request.urlopen(wheel_info["url"]) as wheel_response:
-                    wheel_file = BytesIO(wheel_response.read())
-                    # Extract only the contents of the ptvsd subfolder.
-                    prefix = path.join(
-                        f"ptvsd-{ptvsd_version}.data", "purelib", "ptvsd"
-                    )
+        with urllib.request.urlopen(wheel_info["url"]) as wheel_response:
+            wheel_file = BytesIO(wheel_response.read())
+        # Extract only the contents of the ptvsd subfolder.
+        prefix = path.join(f"ptvsd-{ptvsd_version}.data", "purelib", "ptvsd")
 
-                    with ZipFile(wheel_file, "r") as wheel:
-                        for zip_info in wheel.infolist():
-                            if zip_info.filename.startswith(prefix):
-                                # Flatten the folder structure.
-                                zip_info.filename = zip_info.filename.split(prefix)[-1]
-                                wheel.extract(zip_info, ptvsd_path)
+        with ZipFile(wheel_file, "r") as wheel:
+            for zip_info in wheel.infolist():
+                if not zip_info.filename.startswith(prefix):
+                    continue
+                # Flatten the folder structure.
+                zip_info.filename = zip_info.filename.split(prefix)[-1]
+                wheel.extract(zip_info, ptvsd_path)
