577ec911f4d2c4b0b2ef47a1384933bf16bf2a49
Kim-Adeline Miguel <kimiguel@microsoft.com>
2019-09-04 15:51:51 -0700

    Mock things in setup() function when possible

--- src/test/debugger/extension/adapter/factory.unit.test.ts
+++ src/test/debugger/extension/adapter/factory.unit.test.ts
@@ -38,14 +38,24 @@ suite('Debugging - Adapter Factory', () => {
     let executionFactory: IPythonExecutionFactory;
     let pythonProcess: IPythonExecutionService;
     let stateFactory: IPersistentStateFactory;
-    let debugAdapterPersistentState: IPersistentState<DebugAdapterPtvsdPathInfo>;
+    let debugAdapterPersistentState: IPersistentState<DebugAdapterPtvsdPathInfo | undefined>;
     let extensions: IExtensions;
+
     const nodeExecutable = { command: 'node', args: [] };
     const mockExtensionVersion = new SemVer('2019.9.0');
+    const ptvsdPath = path.join('path', 'to', 'ptvsd');
+    const pythonPath = path.join('path', 'to', 'python', 'interpreter');
+    const interpreter = {
+        architecture: Architecture.Unknown,
+        path: pythonPath,
+        sysPrefix: '',
+        sysVersion: '',
+        type: InterpreterType.Unknown,
+        version: new SemVer('3.7.4-test')
+    };
 
     setup(() => {
         interpreterService = mock(InterpreterService);
-        appShell = mock(ApplicationShell);
         appShell = mock(ApplicationShell);
         experimentsManager = mock(ExperimentsManager);
         executionFactory = mock(PythonExecutionFactory);
@@ -54,14 +64,7 @@ suite('Debugging - Adapter Factory', () => {
         debugAdapterPersistentState = mock(PersistentState);
         extensions = mock(Extensions);
 
-        const interpreter = {
-            architecture: Architecture.Unknown,
-            path: path.join('path', 'to', 'active', 'interpreter'),
-            sysPrefix: '',
-            sysVersion: '',
-            type: InterpreterType.Unknown
-        };
-
+        when(interpreterService.getInterpreterDetails(pythonPath)).thenResolve(interpreter);
         when(interpreterService.getInterpreters(anything())).thenResolve([interpreter]);
         when(executionFactory.create(anything())).thenResolve(instance(pythonProcess));
         when(extensions.getExtension(anything())).thenReturn({ packageJSON: { version: mockExtensionVersion } } as any);
@@ -76,10 +79,10 @@ suite('Debugging - Adapter Factory', () => {
         );
     });
 
-    function mockPtvsdInfoPersistentState(sameVersion: boolean, ptvsdPath: string) {
+    function mockPtvsdInfoPersistentState(sameVersion: boolean) {
         const debugAdapterInfo: DebugAdapterPtvsdPathInfo = { extensionVersion: sameVersion ? mockExtensionVersion.raw : '2019.10.0-dev', ptvsdPath };
 
-        when(stateFactory.createGlobalPersistentState<DebugAdapterPtvsdPathInfo>(ptvsdPathStorageKey, undefined)).thenReturn(instance(debugAdapterPersistentState));
+        when(stateFactory.createGlobalPersistentState<DebugAdapterPtvsdPathInfo | undefined>(ptvsdPathStorageKey, undefined)).thenReturn(instance(debugAdapterPersistentState));
         when(debugAdapterPersistentState.value).thenReturn(debugAdapterInfo);
     }
 
@@ -95,21 +98,10 @@ suite('Debugging - Adapter Factory', () => {
     }
 
     test('Return the value of configuration.pythonPath as the current python path if it exists and if we are in the experiment', async () => {
-        const ptvsdPath = path.join('path', 'to', 'ptvsd');
-        const pythonPath = path.join('session', 'path', 'to', 'python');
         const session = createSession({ pythonPath });
-        const interpreterDetails = {
-            architecture: Architecture.Unknown,
-            path: pythonPath,
-            sysPrefix: '',
-            sysVersion: '',
-            type: InterpreterType.Unknown,
-            version: new SemVer('3.7.4-test')
-        };
         const debugExecutable = new DebugAdapterExecutable(pythonPath, [path.join(ptvsdPath, 'ptvsd', 'adapter')]);
 
-        mockPtvsdInfoPersistentState(true, ptvsdPath);
-        when(interpreterService.getInterpreterDetails(pythonPath)).thenResolve(interpreterDetails);
+        mockPtvsdInfoPersistentState(true);
         when(experimentsManager.inExperiment(DebugAdapterNewPtvsd.experiment)).thenReturn(true);
 
         const descriptor = await factory.createDebugAdapterDescriptor(session, nodeExecutable);
@@ -118,23 +110,12 @@ suite('Debugging - Adapter Factory', () => {
     });
 
     test('Return the path of the active interpreter as the current python path if we are in the experiment, it exists and configuration.pythonPath is not defined', async () => {
-        const ptvsdPath = path.join('path', 'to', 'ptvsd');
-        const pythonPath = path.join('active', 'python', 'interpreter');
         const session = createSession({});
-        const interpreterDetails = {
-            architecture: Architecture.Unknown,
-            path: pythonPath,
-            sysPrefix: '',
-            sysVersion: '',
-            type: InterpreterType.Unknown,
-            version: new SemVer('3.7.4-test')
-        };
         const debugExecutable = new DebugAdapterExecutable(pythonPath, [path.join(ptvsdPath, 'ptvsd', 'adapter')]);
 
-        mockPtvsdInfoPersistentState(true, ptvsdPath);
-        when(interpreterService.getActiveInterpreter(anything())).thenResolve(interpreterDetails);
-        when(interpreterService.getInterpreterDetails(pythonPath)).thenResolve(interpreterDetails);
+        mockPtvsdInfoPersistentState(true);
         when(experimentsManager.inExperiment(DebugAdapterNewPtvsd.experiment)).thenReturn(true);
+        when(interpreterService.getActiveInterpreter(anything())).thenResolve(interpreter);
 
         const descriptor = await factory.createDebugAdapterDescriptor(session, nodeExecutable);
 
@@ -142,22 +123,10 @@ suite('Debugging - Adapter Factory', () => {
     });
 
     test('Return the path of the first available interpreter as the current python path if we are in the experiment, configuration.pythonPath is not defined and there is no active interpreter', async () => {
-        const ptvsdPath = path.join('path', 'to', 'ptvsd');
-        const pythonPath = path.join('first', 'available', 'interpreter');
         const session = createSession({});
-        const interpreterDetails = {
-            architecture: Architecture.Unknown,
-            path: pythonPath,
-            sysPrefix: '',
-            sysVersion: '',
-            type: InterpreterType.Unknown,
-            version: new SemVer('3.7.4-test')
-        };
         const debugExecutable = new DebugAdapterExecutable(pythonPath, [path.join(ptvsdPath, 'ptvsd', 'adapter')]);
 
-        mockPtvsdInfoPersistentState(true, ptvsdPath);
-        when(interpreterService.getInterpreters(anything())).thenResolve([interpreterDetails]);
-        when(interpreterService.getInterpreterDetails(pythonPath)).thenResolve(interpreterDetails);
+        mockPtvsdInfoPersistentState(true);
         when(experimentsManager.inExperiment(DebugAdapterNewPtvsd.experiment)).thenReturn(true);
 
         const descriptor = await factory.createDebugAdapterDescriptor(session, nodeExecutable);
@@ -182,8 +151,8 @@ suite('Debugging - Adapter Factory', () => {
     });
 
     test('Return old node debugger when the active interpreter is not Python 3.7', async () => {
-        const pythonPath = path.join('path', 'to', 'active', 'interpreter');
-        const interpreterDetails = {
+        const python36Path = path.join('path', 'to', 'active', 'interpreter');
+        const interpreterPython36Details = {
             architecture: Architecture.Unknown,
             path: pythonPath,
             sysPrefix: '',
@@ -193,7 +162,7 @@ suite('Debugging - Adapter Factory', () => {
         };
         const session = createSession({});
 
-        when(interpreterService.getInterpreterDetails(pythonPath)).thenResolve(interpreterDetails);
+        when(interpreterService.getInterpreterDetails(python36Path)).thenResolve(interpreterPython36Details);
 
         const descriptor = await factory.createDebugAdapterDescriptor(session, nodeExecutable);
 
@@ -201,21 +170,10 @@ suite('Debugging - Adapter Factory', () => {
     });
 
     test('Return Python debug adapter executable when in the experiment and with the active interpreter being Python 3.7', async () => {
-        const ptvsdPath = path.join('path', 'to', 'ptvsd');
-        const pythonPath = path.join('path', 'to', 'active', 'interpreter');
-        const interpreterDetails = {
-            architecture: Architecture.Unknown,
-            path: pythonPath,
-            sysPrefix: '',
-            sysVersion: '',
-            type: InterpreterType.Unknown,
-            version: { raw: '3.7.4', major: 3, minor: 7, build: ['foo'], patch: 0, prerelease: ['bar'] }
-        };
         const debugExecutable = new DebugAdapterExecutable(pythonPath, [path.join(ptvsdPath, 'ptvsd', 'adapter')]);
         const session = createSession({});
 
-        mockPtvsdInfoPersistentState(true, ptvsdPath);
-        when(interpreterService.getInterpreterDetails(pythonPath)).thenResolve(interpreterDetails);
+        mockPtvsdInfoPersistentState(true);
         when(experimentsManager.inExperiment(DebugAdapterNewPtvsd.experiment)).thenReturn(true);
 
         const descriptor = await factory.createDebugAdapterDescriptor(session, nodeExecutable);
