44d3372b22edfe5c1f96bbac53dadcaedcf0941e
Kim-Adeline Miguel <kimiguel@microsoft.com>
2019-09-04 15:21:26 -0700

    Fix existing tests

--- src/test/debugger/extension/adapter/factory.unit.test.ts
+++ src/test/debugger/extension/adapter/factory.unit.test.ts
@@ -15,18 +15,20 @@ import { Extensions } from '../../../../client/common/application/extensions';
 import { IApplicationShell } from '../../../../client/common/application/types';
 import { DebugAdapterNewPtvsd } from '../../../../client/common/experimentGroups';
 import { ExperimentsManager } from '../../../../client/common/experiments';
-import { PersistentStateFactory } from '../../../../client/common/persistentState';
+import { PersistentState, PersistentStateFactory } from '../../../../client/common/persistentState';
 import { PythonExecutionFactory } from '../../../../client/common/process/pythonExecutionFactory';
-import { IPythonExecutionFactory } from '../../../../client/common/process/types';
-import { IExperimentsManager, IExtensions, IPersistentStateFactory } from '../../../../client/common/types';
+import { PythonExecutionService } from '../../../../client/common/process/pythonProcess';
+import { IPythonExecutionFactory, IPythonExecutionService } from '../../../../client/common/process/types';
+import { IExperimentsManager, IExtensions, IPersistentState, IPersistentStateFactory } from '../../../../client/common/types';
 import { Architecture } from '../../../../client/common/utils/platform';
-import { DebugAdapterDescriptorFactory } from '../../../../client/debugger/extension/adapter/factory';
-import { IDebugAdapterDescriptorFactory } from '../../../../client/debugger/extension/types';
+import { DebugAdapterDescriptorFactory, ptvsdPathStorageKey } from '../../../../client/debugger/extension/adapter/factory';
+import { DebugAdapterPtvsdPathInfo, IDebugAdapterDescriptorFactory } from '../../../../client/debugger/extension/types';
 import { IInterpreterService, InterpreterType } from '../../../../client/interpreter/contracts';
 import { InterpreterService } from '../../../../client/interpreter/interpreterService';
 
 use(chaiAsPromised);
 
+// tslint:disable: no-any
 // tslint:disable-next-line: max-func-body-length
 suite('Debugging - Adapter Factory', () => {
     let factory: IDebugAdapterDescriptorFactory;
@@ -34,12 +36,24 @@ suite('Debugging - Adapter Factory', () => {
     let appShell: IApplicationShell;
     let experimentsManager: IExperimentsManager;
     let executionFactory: IPythonExecutionFactory;
+    let pythonProcess: IPythonExecutionService;
     let stateFactory: IPersistentStateFactory;
+    let debugAdapterPersistentState: IPersistentState<DebugAdapterPtvsdPathInfo>;
     let extensions: IExtensions;
     const nodeExecutable = { command: 'node', args: [] };
+    const mockExtensionVersion = new SemVer('2019.9.0');
 
     setup(() => {
         interpreterService = mock(InterpreterService);
+        appShell = mock(ApplicationShell);
+        appShell = mock(ApplicationShell);
+        experimentsManager = mock(ExperimentsManager);
+        executionFactory = mock(PythonExecutionFactory);
+        pythonProcess = mock(PythonExecutionService);
+        stateFactory = mock(PersistentStateFactory);
+        debugAdapterPersistentState = mock(PersistentState);
+        extensions = mock(Extensions);
+
         const interpreter = {
             architecture: Architecture.Unknown,
             path: path.join('path', 'to', 'active', 'interpreter'),
@@ -47,13 +61,11 @@ suite('Debugging - Adapter Factory', () => {
             sysVersion: '',
             type: InterpreterType.Unknown
         };
+
         when(interpreterService.getInterpreters(anything())).thenResolve([interpreter]);
-        appShell = mock(ApplicationShell);
-        appShell = mock(ApplicationShell);
-        experimentsManager = mock(ExperimentsManager);
-        executionFactory = mock(PythonExecutionFactory);
-        stateFactory = mock(PersistentStateFactory);
-        extensions = mock(Extensions);
+        when(executionFactory.create(anything())).thenResolve(instance(pythonProcess));
+        when(extensions.getExtension(anything())).thenReturn({ packageJSON: { version: mockExtensionVersion } } as any);
+
         factory = new DebugAdapterDescriptorFactory(
             instance(interpreterService),
             instance(appShell),
@@ -63,6 +75,13 @@ suite('Debugging - Adapter Factory', () => {
             instance(extensions)
         );
     });
+
+    function mockPtvsdInfoPersistentState(sameVersion: boolean, ptvsdPath: string) {
+        const debugAdapterInfo: DebugAdapterPtvsdPathInfo = { extensionVersion: sameVersion ? mockExtensionVersion.raw : '2019.10.0-dev', ptvsdPath };
+
+        when(stateFactory.createGlobalPersistentState<DebugAdapterPtvsdPathInfo>(ptvsdPathStorageKey, undefined)).thenReturn(instance(debugAdapterPersistentState));
+        when(debugAdapterPersistentState.value).thenReturn(debugAdapterInfo);
+    }
 
     function createSession(config: Partial<DebugConfiguration>, workspaceFolder?: WorkspaceFolder): DebugSession {
         return {
@@ -76,6 +95,7 @@ suite('Debugging - Adapter Factory', () => {
     }
 
     test('Return the value of configuration.pythonPath as the current python path if it exists and if we are in the experiment', async () => {
+        const ptvsdPath = path.join('path', 'to', 'ptvsd');
         const pythonPath = path.join('session', 'path', 'to', 'python');
         const session = createSession({ pythonPath });
         const interpreterDetails = {
@@ -86,8 +106,9 @@ suite('Debugging - Adapter Factory', () => {
             type: InterpreterType.Unknown,
             version: new SemVer('3.7.4-test')
         };
-        const debugExecutable = new DebugAdapterExecutable(pythonPath);
+        const debugExecutable = new DebugAdapterExecutable(pythonPath, [path.join(ptvsdPath, 'ptvsd', 'adapter')]);
 
+        mockPtvsdInfoPersistentState(true, ptvsdPath);
         when(interpreterService.getInterpreterDetails(pythonPath)).thenResolve(interpreterDetails);
         when(experimentsManager.inExperiment(DebugAdapterNewPtvsd.experiment)).thenReturn(true);
 
@@ -97,6 +118,7 @@ suite('Debugging - Adapter Factory', () => {
     });
 
     test('Return the path of the active interpreter as the current python path if we are in the experiment, it exists and configuration.pythonPath is not defined', async () => {
+        const ptvsdPath = path.join('path', 'to', 'ptvsd');
         const pythonPath = path.join('active', 'python', 'interpreter');
         const session = createSession({});
         const interpreterDetails = {
@@ -107,8 +129,9 @@ suite('Debugging - Adapter Factory', () => {
             type: InterpreterType.Unknown,
             version: new SemVer('3.7.4-test')
         };
-        const debugExecutable = new DebugAdapterExecutable(pythonPath);
+        const debugExecutable = new DebugAdapterExecutable(pythonPath, [path.join(ptvsdPath, 'ptvsd', 'adapter')]);
 
+        mockPtvsdInfoPersistentState(true, ptvsdPath);
         when(interpreterService.getActiveInterpreter(anything())).thenResolve(interpreterDetails);
         when(interpreterService.getInterpreterDetails(pythonPath)).thenResolve(interpreterDetails);
         when(experimentsManager.inExperiment(DebugAdapterNewPtvsd.experiment)).thenReturn(true);
@@ -119,6 +142,7 @@ suite('Debugging - Adapter Factory', () => {
     });
 
     test('Return the path of the first available interpreter as the current python path if we are in the experiment, configuration.pythonPath is not defined and there is no active interpreter', async () => {
+        const ptvsdPath = path.join('path', 'to', 'ptvsd');
         const pythonPath = path.join('first', 'available', 'interpreter');
         const session = createSession({});
         const interpreterDetails = {
@@ -129,8 +153,9 @@ suite('Debugging - Adapter Factory', () => {
             type: InterpreterType.Unknown,
             version: new SemVer('3.7.4-test')
         };
-        const debugExecutable = new DebugAdapterExecutable(pythonPath);
+        const debugExecutable = new DebugAdapterExecutable(pythonPath, [path.join(ptvsdPath, 'ptvsd', 'adapter')]);
 
+        mockPtvsdInfoPersistentState(true, ptvsdPath);
         when(interpreterService.getInterpreters(anything())).thenResolve([interpreterDetails]);
         when(interpreterService.getInterpreterDetails(pythonPath)).thenResolve(interpreterDetails);
         when(experimentsManager.inExperiment(DebugAdapterNewPtvsd.experiment)).thenReturn(true);
@@ -175,8 +200,8 @@ suite('Debugging - Adapter Factory', () => {
         assert.deepEqual(descriptor, nodeExecutable);
     });
 
-    test('Return debug adapter executable when in the experiment and with the active interpreter being Python 3.7', async () => {
-        // will be updated when we support the new debug adapter
+    test('Return Python debug adapter executable when in the experiment and with the active interpreter being Python 3.7', async () => {
+        const ptvsdPath = path.join('path', 'to', 'ptvsd');
         const pythonPath = path.join('path', 'to', 'active', 'interpreter');
         const interpreterDetails = {
             architecture: Architecture.Unknown,
@@ -186,9 +211,10 @@ suite('Debugging - Adapter Factory', () => {
             type: InterpreterType.Unknown,
             version: { raw: '3.7.4', major: 3, minor: 7, build: ['foo'], patch: 0, prerelease: ['bar'] }
         };
-        const debugExecutable = new DebugAdapterExecutable(pythonPath);
+        const debugExecutable = new DebugAdapterExecutable(pythonPath, [path.join(ptvsdPath, 'ptvsd', 'adapter')]);
         const session = createSession({});
 
+        mockPtvsdInfoPersistentState(true, ptvsdPath);
         when(interpreterService.getInterpreterDetails(pythonPath)).thenResolve(interpreterDetails);
         when(experimentsManager.inExperiment(DebugAdapterNewPtvsd.experiment)).thenReturn(true);
 
@@ -197,7 +223,7 @@ suite('Debugging - Adapter Factory', () => {
         assert.deepEqual(descriptor, debugExecutable);
     });
 
-    test('Throw an error if the executable has not been defined', async () => {
+    test('Throw an error if the Node debugger adapter executable has not been defined', async () => {
         const session = createSession({});
         const promise = factory.createDebugAdapterDescriptor(session, undefined);
 
