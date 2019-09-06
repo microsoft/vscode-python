f7a07ab0c951d756ec597fdfb593382c2004728a
Kim-Adeline Miguel <kimiguel@microsoft.com>
2019-09-06 09:20:00 -0700

    Renaming in python scripts (code review)

--- pythonFiles/ptvsd_folder_name.py
+++ pythonFiles/ptvsd_folder_name.py
@@ -1,11 +1,11 @@
 import sys
 from os import path
 
-ROOT_DIRNAME = path.dirname(path.dirname(path.abspath(__file__)))
-PYTHONFILES_PATH = path.join(ROOT_DIRNAME, "pythonFiles", "lib", "python")
-REQUIREMENTS_PATH = path.join(ROOT_DIRNAME, "requirements.txt")
+ROOT = path.dirname(path.dirname(path.abspath(__file__)))
+PYTHONFILES = path.join(ROOT, "pythonFiles", "lib", "python")
+REQUIREMENTS = path.join(ROOT, "requirements.txt")
 
-sys.path.insert(0, PYTHONFILES_PATH)
+sys.path.insert(0, PYTHONFILES)
 
 from packaging.tags import sys_tags
 from packaging.requirements import Requirement
@@ -14,27 +14,25 @@ from packaging.requirements import Requirement
 def ptvsd_folder_name():
     """Return the folder name for the bundled PTVSD wheel compatible with the new debug adapter."""
 
-    with open(REQUIREMENTS_PATH, "r", encoding="utf-8") as requirements:
-        for line in requirements:
-            package_requirement = Requirement(line)
-            if package_requirement.name != "ptvsd":
-                continue
-            requirement_specifier = package_requirement.specifier
-            ptvsd_version = next(iter(requirement_specifier)).version
+    with open(REQUIREMENTS, "r", encoding="utf-8") as reqsfile:
+        for line in reqsfile:
+            pkgreq = Requirement(line)
+            if pkgreq.name == "ptvsd":
+                specs = pkgreq.specifier
+                version = next(iter(specs)).version
+                break
 
-    sys.path.remove(PYTHONFILES_PATH)
+    sys.path.remove(PYTHONFILES)
 
     for tag in sys_tags():
-        folder_name = (
-            f"ptvsd-{ptvsd_version}-{tag.interpreter}-{tag.abi}-{tag.platform}"
-        )
-        folder_path = path.join(PYTHONFILES_PATH, folder_name)
+        folder_name = f"ptvsd-{version}-{tag.interpreter}-{tag.abi}-{tag.platform}"
+        folder_path = path.join(PYTHONFILES, folder_name)
         if path.exists(folder_path):
             print(folder_path)
             return
 
     # Fallback to use base PTVSD path.
-    print(PYTHONFILES_PATH)
+    print(PYTHONFILES)
 
 
 if __name__ == "__main__":
