4f0ab82a9fac390d5fba6643be70cd5c1b510f4b
Kim-Adeline Miguel <kimiguel@microsoft.com>
2019-09-18 13:59:19 -0700

    Test this

--- src/test/extension.unit.test.ts
+++ src/test/extension.unit.test.ts
@@ -6,8 +6,10 @@
 // tslint:disable:no-any
 
 import { expect } from 'chai';
+import { parse } from 'semver';
+import { extensions } from 'vscode';
 import { buildApi } from '../client/api';
-import { EXTENSION_ROOT_DIR } from '../client/common/constants';
+import { EXTENSION_ROOT_DIR, PVSC_EXTENSION_ID } from '../client/common/constants';
 
 const expectedPath = `${EXTENSION_ROOT_DIR.fileToCommandArgument()}/pythonFiles/ptvsd_launcher.py`;
 
@@ -21,5 +23,32 @@ suite('Extension API Debugger', () => {
         const args = await buildApi(Promise.resolve()).debug.getRemoteLauncherCommand('something', 1234, true);
         const expectedArgs = [expectedPath, '--default', '--host', 'something', '--port', '1234', '--wait'];
         expect(args).to.be.deep.equal(expectedArgs);
+    });
+});
+
+suite('Extension version tests', () => {
+    let version: string;
+
+    setup(() => {
+        const extension = extensions.getExtension(PVSC_EXTENSION_ID)!;
+        version = parse(extension.packageJSON.version)!.raw;
+    });
+
+    test('If we are running a pipeline in the master branch, the extension version in `package.json` should have the "-dev" suffix', async function() {
+        if (process.env.CI_BRANCH_NAME !== 'master') {
+            // tslint:disable-next-line: no-invalid-this
+            return this.skip();
+        }
+
+        return expect(version.endsWith('-dev'), 'When running a pipeline in the master branch, the extension version in package.json should have the -dev suffix').to.be.true;
+    });
+
+    test('If we are running a pipeline in the release branch, the extension version in `package.json` should not have the "-dev" suffix', async function() {
+        if (process.env.CI_BRANCH_NAME !== 'release') {
+            // tslint:disable-next-line: no-invalid-this
+            return this.skip();
+        }
+
+        return expect(version.endsWith('-dev'), 'When running a pipeline in the release branch, the extension version in package.json should not have the -dev suffix').to.be.false;
     });
 });
