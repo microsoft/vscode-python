53ac82029b3cc5fac56e6a864f9fe1f907aecd3e
Don Jayamanne <don.jayamanne@yahoo.com>
2019-09-28 10:08:05 -0700

    Fixes

--- uitests/src/steps/testing.ts
+++ uitests/src/steps/testing.ts
@@ -17,12 +17,22 @@ Then('the test explorer icon will be visible', async function() {
 
 // Surely tests can't take more than 30s to get discovered.
 When('I wait for test discovery to complete', async function() {
-    // Wait either for 5 seconds (we know tests would have stated discoverying by then or until we can see the discovering message).
-    await Promise.race([sleep(5000), this.app.statusbar.waitUntilStatusBarItemWithText('Discovering Tests', 3000).catch(noop)]);
-    // Ensure the `stop` icon is not visible in the explorer toolbar.
-    await this.app.testExplorer.waitUntilTestsStop(30_000);
-    // Also ensure there is no statubar item with the text 'Discovering Tests'
-    await this.app.statusbar.waitUntilNoStatusBarItemWithText('Discovering Tests', 30_000);
+    // Wait for tests discovery to first start.
+    await Promise.race([
+        // Wait either for 3 seconds (we know tests would have stated discoverying by then).
+        sleep(3_000),
+        // Or until we can see the discovering icon.
+        this.app.testExplorer.waitUntilToolbarIconVisible('Stop', 3_000).catch(noop),
+        // Or until we can see the discovering message.
+        this.app.statusbar.waitUntilStatusBarItemWithText('Discovering Tests', 3_000).catch(noop)
+    ]);
+
+    await Promise.all([
+        // Ensure the `stop` icon is not visible in the explorer toolbar.
+        this.app.testExplorer.waitUntilTestsStop(30_000),
+        // Also ensure there is no statubar item with the text 'Discovering Tests'
+        this.app.statusbar.waitUntilNoStatusBarItemWithText('Discovering Tests', 30_000)
+    ]);
 });
 
 // Surely pythonn tests (in our UI Tests) can't take more than 30s to run.
