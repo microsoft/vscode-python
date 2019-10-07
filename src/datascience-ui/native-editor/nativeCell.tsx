92b3b24f18dc58ba51e97c9cacf8979852a4b2b5
Rich Chiodo <rchiodo@microsoft.com>
2019-10-07 15:58:46 -0700

    Also fix add cell to put focus in the new cell

--- src/datascience-ui/native-editor/nativeCell.tsx
+++ src/datascience-ui/native-editor/nativeCell.tsx
@@ -487,7 +487,7 @@ export class NativeCell extends React.Component<INativeCellProps> {
         const newCell = this.props.stateController.insertBelow(this.props.cellVM.cell.id, true);
         this.props.stateController.sendCommand(NativeCommandType.AddToEnd, 'mouse');
         if (newCell) {
-            this.props.selectCell(newCell);
+            this.props.focusCell(newCell, true);
         }
     }
 
