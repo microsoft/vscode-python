a900f69ad448d4b2bc33c163df375f87edb07247
Karthik Nadig <karthik.nadig@live.com>
2019-10-16 17:29:43 -0700

    Custom debugger packaging and path fixes

---
+++ pythonFiles/tests/debug_adapter/test_install_ptvsd.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+import sys
+from ...install_ptvsd import main
+
+def _check_binaries(dir_path):
+    expected_endswith = (
+        "win_amd64.pyd",
+        "win32.pyd",
+        "darwin.so",
+        "i386-linux-gnu.so",
+        "x86_64-linux-gnu.so",
+    )
+
+    binaries = list(
+        p for p in os.listdir(dir_path)
+        if p.endswith(expected_endswith)
+    )
+
+    assert len(binaries) == len(expected_endswith)
+
+
+def test_install_ptvsd(tmpdir):
+    main(str(tmpdir))
+    dir_path = os.path.join(
+        str(tmpdir), "ptvsd", "_vendored", "pydevd", "_pydevd_bundle"
+    )
+    _check_binaries(dir_path)
+
+    dir_path = os.path.join(
+        str(tmpdir), "ptvsd", "_vendored", "pydevd", "_pydevd_frame_eval"
+    )
+    _check_binaries(dir_path)
