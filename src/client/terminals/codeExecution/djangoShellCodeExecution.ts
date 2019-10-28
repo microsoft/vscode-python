4fda3b0645a4bbec6e2e31b9afb246b200790263
Kim-Adeline Miguel <kimiguel@microsoft.com>
2019-10-28 13:55:44 -0700

    Forgot djangoShellCodeExecution.ts

--- src/client/terminals/codeExecution/djangoShellCodeExecution.ts
+++ src/client/terminals/codeExecution/djangoShellCodeExecution.ts
@@ -11,6 +11,7 @@ import '../../common/extensions';
 import { IFileSystem, IPlatformService } from '../../common/platform/types';
 import { ITerminalServiceFactory } from '../../common/terminal/types';
 import { IConfigurationService, IDisposableRegistry } from '../../common/types';
+import { ICondaService, IInterpreterService } from '../../interpreter/contracts';
 import { DjangoContextInitializer } from './djangoContext';
 import { TerminalCodeExecutionProvider } from './terminalCodeExecution';
 
@@ -20,19 +21,28 @@ export class DjangoShellCodeExecutionProvider extends TerminalCodeExecutionProvi
         @inject(IConfigurationService) configurationService: IConfigurationService,
         @inject(IWorkspaceService) workspace: IWorkspaceService,
         @inject(IDocumentManager) documentManager: IDocumentManager,
+        @inject(IInterpreterService) interpreterService: IInterpreterService,
+        @inject(ICondaService) condaService: ICondaService,
         @inject(IPlatformService) platformService: IPlatformService,
         @inject(ICommandManager) commandManager: ICommandManager,
         @inject(IFileSystem) fileSystem: IFileSystem,
-        @inject(IDisposableRegistry) disposableRegistry: Disposable[]) {
-
-        super(terminalServiceFactory, configurationService, workspace, disposableRegistry, platformService);
+        @inject(IDisposableRegistry) disposableRegistry: Disposable[]
+    ) {
+        super(terminalServiceFactory, configurationService, workspace, disposableRegistry, interpreterService, condaService, platformService);
         this.terminalTitle = 'Django Shell';
         disposableRegistry.push(new DjangoContextInitializer(documentManager, workspace, fileSystem, commandManager));
     }
-    public getReplCommandArgs(resource?: Uri): { command: string; args: string[] } {
+
+    public async getReplCommandArgs(resource?: Uri, replArgs: string[] = []): Promise<{ command: string; args: string[] }> {
+        const { command } = await this.getExecutableInfo(resource);
         const pythonSettings = this.configurationService.getSettings(resource);
-        const command = this.platformService.isWindows ? pythonSettings.pythonPath.replace(/\\/g, '/') : pythonSettings.pythonPath;
-        const args = pythonSettings.terminal.launchArgs.slice();
+        const args = pythonSettings.terminal.launchArgs.slice().concat(replArgs);
+
+        return { command, args };
+    }
+
+    public async getExecutableInfo(resource?: Uri): Promise<{ command: string; args: string[] }> {
+        const { command, args } = await super.getExecutableInfo(resource);
 
         const workspaceUri = resource ? this.workspace.getWorkspaceFolder(resource) : undefined;
         const defaultWorkspace = Array.isArray(this.workspace.workspaceFolders) && this.workspace.workspaceFolders.length > 0 ? this.workspace.workspaceFolders[0].uri.fsPath : '';
