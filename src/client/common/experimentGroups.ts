ce6b22000830fae30e361ec860d2503ed5e72aed
Luciana <luabud@microsoft.com>
2019-10-28 15:33:04 -0700

    Changing NewDebugAdapter experiment to be on for 50% of insiders users

--- src/client/common/experimentGroups.ts
+++ src/client/common/experimentGroups.ts
@@ -14,9 +14,9 @@ export enum ShowExtensionSurveyPrompt {
 }
 
 // Experiment to check whether the extension should use the new VS Code debug adapter API.
-export enum DebugAdapterDescriptorFactory {
-    control = 'DebugAdapterFactory - control',
-    experiment = 'DebugAdapterFactory - experiment'
+export enum DebugAdapterDescriptorFactoryInsiders {
+    control = 'DebugAdapterFactoryInsiders - control',
+    experiment = 'DebugAdapterFactoryInsiders - experiment'
 }
 
 // Experiment to check whether the ptvsd launcher should use pre-installed ptvsd wheels for debugging.
