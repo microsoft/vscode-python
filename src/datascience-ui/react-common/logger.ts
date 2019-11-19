efbb0874c8d7c0920d3370a5d4761623769f42b6
Don Jayamanne <don.jayamanne@yahoo.com>
2019-11-19 08:57:52 -0800

    Disable logging

--- src/datascience-ui/react-common/logger.ts
+++ src/datascience-ui/react-common/logger.ts
@@ -1,17 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 'use strict';
-import { isTestExecution } from '../../client/common/constants';
+// import { isTestExecution } from '../../client/common/constants';
 
-// The logs generated here are super massive, hence CI logs are unsable, Azure Pipelines don't even display the test results, just one line in the log stating that it has failed.
-const enableLogger = !process.env.VSCODE_PYTHON_ROLLING && (!isTestExecution() || process.env.VSC_PYTHON_FORCE_LOGGING || process.env.VSC_PYTHON_LOG_FILE);
+// // The logs generated here are super massive, hence CI logs are unsable, Azure Pipelines don't even display the test results, just one line in the log stating that it has failed.
+// const enableLogger = !process.env.VSCODE_PYTHON_ROLLING && (!isTestExecution() || process.env.VSC_PYTHON_FORCE_LOGGING || process.env.VSC_PYTHON_LOG_FILE);
 
-export function logMessage(message: string) {
-    // Might want to post this back to the other side too. This was
-    // put here to prevent having to disable the console log warning
+export function logMessage(_message: string) {
+    // // Might want to post this back to the other side too. This was
+    // // put here to prevent having to disable the console log warning
 
-    if (enableLogger) {
-        // tslint:disable-next-line: no-console
-        console.log(message);
-    }
+    // if (enableLogger) {
+    //     // tslint:disable-next-line: no-console
+    //     console.log(message);
+    // }
 }
