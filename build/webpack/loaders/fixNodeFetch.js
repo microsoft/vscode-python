ee91b603d69a157ec7321626a0c05c97148cbf4d
Don Jayamanne <don.jayamanne@yahoo.com>
2019-11-19 14:41:53 -0800

    Changes to how node-fetch is bundled in extension

---
+++ build/webpack/loaders/fixNodeFetch.js
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
+const path = require('path');
+const constants = require('../../constants');
+const nodeFetchIndex = path.join(constants.ExtensionRootDir, 'node_modules', '@jupyterlab','services','node_modules','node-fetch','lib','index.js');
+
+/**
+ * Node fetch has a commonjs module file. That gets bundled into @jupyterlab/services.
+ * However @jupyterlab/services/serverconnection.js is written such that it uses fetch from either node or browser.
+ * We need to force the browser version for things to work correctly.
+ *
+ * @export
+ * @param {string} source
+ * @returns
+ */
+exports.default = function (source) {
+    if (source.indexOf('require(\'node-fetch\')') > 0) {
+        source = source.replace(/require\('node-fetch'\)/g, `require('${nodeFetchIndex}')`);
+    }
+    return source;
+}
