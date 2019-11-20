d59d20b749983122ca3b9ea118acb2c8d6b8dfc8
Don Jayamanne <don.jayamanne@yahoo.com>
2019-11-19 16:38:42 -0800

    Tests

--- src/test/smoke/datascience.smoke.test.ts
+++ src/test/smoke/datascience.smoke.test.ts
@@ -10,7 +10,7 @@ import * as path from 'path';
 import * as vscode from 'vscode';
 import { openFile, waitForCondition } from '../common';
 import { EXTENSION_ROOT_DIR_FOR_TESTS, IS_SMOKE_TEST } from '../constants';
-import { sleep } from '../core';
+import { noop, sleep } from '../core';
 import { closeActiveWindows, initialize, initializeTest } from '../initialize';
 
 const timeoutForCellToRun = 3 * 60 * 1_000;
@@ -36,25 +36,28 @@ suite('Smoke Test: Interactive Window', () => {
 
         // Wait for code lenses to get detected.
         await sleep(1_000);
+
         await vscode.commands.executeCommand<void>('python.datascience.runallcells', textDocument.uri);
         const checkIfFileHasBeenCreated = () => fs.pathExists(outputFile);
         await waitForCondition(checkIfFileHasBeenCreated, timeoutForCellToRun, `"${outputFile}" file not created`);
     }).timeout(timeoutForCellToRun);
+
     test('Run Cell in native editor', async () => {
         const file = path.join(EXTENSION_ROOT_DIR_FOR_TESTS, 'src', 'test', 'pythonFiles', 'datascience', 'simple_nb.ipynb');
         const fileContents = await fs.readFile(file, { encoding: 'utf-8' });
-        await fs.writeFile(file, fileContents.replace('\'ds.log\'', `'${file}'`), { encoding: 'utf-8' });
         const outputFile = path.join(path.dirname(file), 'ds.log');
+        await fs.writeFile(file, fileContents.replace('\'ds.log\'', `'${outputFile}'`), { encoding: 'utf-8' });
         if (await fs.pathExists(outputFile)) {
             await fs.unlink(outputFile);
         }
-        const textDocument = await openFile(file);
+        // Ignore exceptions (as native editor closes the document as soon as its opened);
+        await openFile(file).catch(noop);
 
         // Wait for 15 seconds for notebook to launch.
         // Unfortunately there's no way to know for sure it has completely loaded.
         await sleep(15_000);
 
-        await vscode.commands.executeCommand<void>('python.datascience.runallcells', textDocument.uri);
+        await vscode.commands.executeCommand<void>('python.datascience.notebookeditor.runallcells');
         const checkIfFileHasBeenCreated = () => fs.pathExists(outputFile);
         await waitForCondition(checkIfFileHasBeenCreated, timeoutForCellToRun, `"${outputFile}" file not created`);
     }).timeout(timeoutForCellToRun);
