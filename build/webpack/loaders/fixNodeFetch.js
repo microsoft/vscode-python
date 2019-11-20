a92144a8616eb12661e06618d49496b5d590aaab
Don Jayamanne <don.jayamanne@yahoo.com>
2019-11-19 21:57:13 -0800

    Fixes for windows

--- build/webpack/loaders/fixNodeFetch.js
+++ build/webpack/loaders/fixNodeFetch.js
@@ -5,7 +5,10 @@
 
 const path = require('path');
 const constants = require('../../constants');
-const nodeFetchIndex = path.join(constants.ExtensionRootDir, 'node_modules', '@jupyterlab','services','node_modules','node-fetch','lib','index.js');
+
+const nodeFetchIndexFile = path.join(constants.ExtensionRootDir, 'node_modules', '@jupyterlab','services','node_modules','node-fetch','lib','index.js');
+// On windows replace `\` with `\\`, else we get an error in webpack (Module parse failed: Octal literal in strict mode).
+const nodeFetchFile = constants.isWindows ? nodeFetchIndexFile.replace(/\\/g, '\\\\') : nodeFetchIndexFile;
 
 /**
  * Node fetch has an es6 module file. That gets bundled into @jupyterlab/services.
@@ -18,7 +21,7 @@ const nodeFetchIndex = path.join(constants.ExtensionRootDir, 'node_modules', '@j
  */
 exports.default = function (source) {
     if (source.indexOf('require(\'node-fetch\')') > 0) {
-        source = source.replace(/require\('node-fetch'\)/g, `require('${nodeFetchIndex}')`);
+        source = source.replace(/require\('node-fetch'\)/g, `require('${nodeFetchFile}')`);
     }
     return source;
 }
