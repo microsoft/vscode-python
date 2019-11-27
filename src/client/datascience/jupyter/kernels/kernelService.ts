3fc2f147d1a482d4eac90129359ecd7f23c5c408
Don Jayamanne <don.jayamanne@yahoo.com>
2019-11-27 10:42:04 -0800

    Add tests

--- src/client/datascience/jupyter/kernels/kernelService.ts
+++ src/client/datascience/jupyter/kernels/kernelService.ts
@@ -259,7 +259,7 @@ export class KernelService {
 
         // If no active interpreter, just act like everything is okay as we can't find a new spec anyway
         return true;
-    }
+    };
 
     private async addMatchingSpec(bestInterpreter: PythonInterpreter, cancelToken?: CancellationToken): Promise<void> {
         const displayName = localize.DataScience.historyTitle();
@@ -320,7 +320,7 @@ export class KernelService {
                 return js && js.name === specName;
             }) as JupyterKernelSpec;
         return match ? match.specFile : undefined;
-    }
+    };
 
     //tslint:disable-next-line:cyclomatic-complexity
     private findSpecMatch = async (enumerator: () => Promise<(IJupyterKernelSpec | undefined)[]>): Promise<IJupyterKernelSpec | undefined> => {
@@ -410,7 +410,7 @@ export class KernelService {
 
         traceInfo(`Found kernelspec match ${bestSpec ? `${bestSpec.name}' '${bestSpec.path}` : 'undefined'}`);
         return bestSpec;
-    }
+    };
 
     private enumerateSpecs = async (_cancelToken?: CancellationToken): Promise<JupyterKernelSpec[]> => {
         // Ignore errors if there are no kernels.
@@ -451,7 +451,7 @@ export class KernelService {
             // This is failing for some folks. In that case return nothing
             return [];
         }
-    }
+    };
     private async getInterpreterDetailsFromProcess(baseProcessName: string): Promise<PythonInterpreter | undefined> {
         if (path.basename(baseProcessName) !== baseProcessName) {
             // This function should only be called with a non qualified path. We're using this
