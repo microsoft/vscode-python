6279deb6d6149fe7ceca74797a0802c15c7441cb
Kartik Raj <karraj@microsoft.com>
2019-12-02 15:17:49 -0800

    Fix smoke tests

--- src/test/performanceTest.ts
+++ src/test/performanceTest.ts
@@ -71,7 +71,7 @@ class TestRunner {
         await this.runPerfTest(devLogFiles, releaseLogFiles, languageServerLogFiles);
     }
     private async enableLanguageServer(enable: boolean) {
-        const settings = `{ "python.languageServer": ${enable ? 'jedi' : 'microsoft'} }`;
+        const settings = `{ "python.languageServer": ${enable ? '"microsoft"' : '"jedi"'} }`;
         await fs.writeFile(path.join(EXTENSION_ROOT_DIR, 'src', 'test', 'performance', 'settings.json'), settings);
     }
 
