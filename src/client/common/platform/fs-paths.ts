e7d4ee0a6581b5737a44ceccae61dd2d43bae0a8
Eric Snow <ericsnowcurrently@gmail.com>
2019-12-19 15:52:22 -0700

    Factor out IFileSystemPathUtils and friends.

---
+++ src/client/common/platform/fs-paths.ts
@@ -0,0 +1,96 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import * as nodepath from 'path';
+import { getOSType, OSType } from '../utils/platform';
+import {
+    IExecutables,
+    IFileSystemPaths
+} from './types';
+// tslint:disable-next-line:no-var-requires no-require-imports
+const untildify = require('untildify');
+
+// The parts of node's 'path' module used by FileSystemPaths.
+interface INodePath {
+    sep: string;
+    basename(filename: string, ext?: string): string;
+}
+
+// The file path operations used by the extension.
+export class FileSystemPaths {
+    constructor(
+        private raw: INodePath
+    ) { }
+    // Create a new object using common-case default values.
+    // We do not use an alternate constructor because defaults in the
+    // constructor runs counter to our typical approach.
+    public static withDefaults(): FileSystemPaths {
+        return new FileSystemPaths(
+            nodepath
+        );
+    }
+
+    public get sep(): string {
+        return this.raw.sep;
+    }
+
+    public basename(filename: string, suffix?: string): string {
+        return this.raw.basename(filename, suffix);
+    }
+}
+
+export class Executables {
+    constructor(
+        public readonly delimiter: string,
+        private readonly osType: OSType
+    ) { }
+    // Create a new object using common-case default values.
+    // We do not use an alternate constructor because defaults in the
+    // constructor runs counter to our typical approach.
+    public static withDefaults(): Executables {
+        return new Executables(
+            nodepath.delimiter,
+            getOSType()
+        );
+    }
+
+    public get envVar(): string {
+        return this.osType === OSType.Windows
+            ? 'Path'
+            : 'PATH';
+    }
+}
+
+interface IRawPaths {
+    relative(relpath: string, rootpath: string): string;
+}
+
+export class FileSystemPathUtils {
+    constructor(
+        public readonly home: string,
+        public readonly paths: IFileSystemPaths,
+        public readonly executables: IExecutables,
+        private readonly raw: IRawPaths
+    ) { }
+    // Create a new object using common-case default values.
+    // We do not use an alternate constructor because defaults in the
+    // constructor runs counter to our typical approach.
+    public static withDefaults(): FileSystemPathUtils {
+        return new FileSystemPathUtils(
+            untildify('~'),
+            FileSystemPaths.withDefaults(),
+            Executables.withDefaults(),
+            nodepath
+        );
+    }
+
+    public getDisplayName(pathValue: string, cwd?: string): string {
+        if (cwd && pathValue.startsWith(cwd)) {
+            return `.${this.paths.sep}${this.raw.relative(cwd, pathValue)}`;
+        } else if (pathValue.startsWith(this.home)) {
+            return `~${this.paths.sep}${this.raw.relative(this.home, pathValue)}`;
+        } else {
+            return pathValue;
+        }
+    }
+}
