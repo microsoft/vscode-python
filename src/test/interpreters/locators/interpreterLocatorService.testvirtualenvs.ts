613903218a42075002b3bf78328fc843d3ebbf49
Kartik Raj <karraj@microsoft.com>
2020-01-15 10:48:20 -0800

    Added logging

--- src/test/interpreters/locators/interpreterLocatorService.testvirtualenvs.ts
+++ src/test/interpreters/locators/interpreterLocatorService.testvirtualenvs.ts
@@ -28,16 +28,26 @@ suite('Python interpreter locator service', () => {
         ioc.registerUnitTestTypes();
         ioc.registerVariableTypes();
         ioc.registerInterpreterTypes();
-        ioc.registerMockProcessTypes();
         ioc.serviceManager.addSingleton<IRegistry>(IRegistry, RegistryImplementation);
     }
 
-    test('Ensure we are getting all conda environments', async () => {
+    test('xEnsure we are getting all conda environments', async () => {
+        // tslint:disable: no-console
+        console.time('Time taken to run test');
         const locator = ioc.serviceContainer.get<IInterpreterLocatorService>(IInterpreterLocatorService, INTERPRETER_LOCATOR_SERVICE);
         const interpreters = await locator.getInterpreters();
+        interpreters.forEach(i => {
+            // tslint:disable: no-console
+            console.log(`${i.path} | ${i.envName}`);
+        });
         // Created in CI using command `conda create -n "test_env1" -y python`
         let filteredInterpreters = interpreters.filter(i => i.envName === 'test_env1' && i.type === InterpreterType.Conda);
+        filteredInterpreters.forEach(i => {
+            // tslint:disable-next-line: no-console
+            console.log(`1 ${i.path} | ${i.envName}`);
+        });
         expect(filteredInterpreters.length).to.be.greaterThan(0, 'Environment test_env1 not found');
+        console.log('After 1');
 
         // Created in CI using command `conda create -p "./test_env2" -y python`
         filteredInterpreters = interpreters.filter(i => {
@@ -47,7 +57,12 @@ suite('Python interpreter locator service', () => {
             }
             return dirName.endsWith('test_env2') && i.type === InterpreterType.Conda;
         });
+        filteredInterpreters.forEach(i => {
+            // tslint:disable-next-line: no-console
+            console.log(`2 ${i.path} | ${i.envName}`);
+        });
         expect(filteredInterpreters.length).to.be.greaterThan(0, 'Environment test_env2 not found');
+        console.log('After 2');
 
         // Created in CI using command `conda create -p "<HOME>/test_env3" -y python`
         filteredInterpreters = interpreters.filter(i => {
@@ -57,14 +72,25 @@ suite('Python interpreter locator service', () => {
             }
             return dirName.endsWith('test_env3') && i.type === InterpreterType.Conda;
         });
+        filteredInterpreters.forEach(i => {
+            // tslint:disable-next-line: no-console
+            console.log(`3 ${i.path} | ${i.envName}`);
+        });
         expect(filteredInterpreters.length).to.be.greaterThan(0, 'Environment test_env3 not found');
+        console.log('After 3');
 
         // Base conda environment in CI
         filteredInterpreters = interpreters.filter(i => (i.envName === 'base' || i.envName === 'miniconda') && i.type === InterpreterType.Conda);
+        filteredInterpreters.forEach(i => {
+            // tslint:disable-next-line: no-console
+            console.log(`4 ${i.path} | ${i.envName}`);
+        });
         expect(filteredInterpreters.length).to.be.greaterThan(0, 'Environment base not found');
+        console.log('After 4');
         interpreters.forEach(i => {
             // tslint:disable-next-line: no-console
             console.log(`${i.path} | ${i.envName}`);
         });
+        console.timeEnd('Time taken to run test');
     });
 });
