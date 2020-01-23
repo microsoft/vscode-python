ecdc560a821bb999e28aa58cea31e4c99332138c
David Kutugata <dakutuga@microsoft.com>
2020-01-23 15:55:01 -0800

    changed ts files to js to avoid compiling. added a opts file to only run some test suites

--- build/ci/performance/checkPerformanceResults.js
+++ build/ci/performance/checkPerformanceResults.js
@@ -1,18 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 'use strict';
-import fs from 'fs';
-import { PerformanceResults } from './savePerformanceResults';
-import { TestBenchmark } from './xml2json';
+const fs = require('fs');
 
 const performanceResultsFile = './performance-results.json';
-const baseFile = './test-results.json';
+const resultsFile = './test-results.json';
 const errorMargin = 0.01;
 let testsWereAdded = false;
+let failedTests = '';
 
-fs.readFile(baseFile, 'utf8', (baseFileError, baseData) => {
-    if (baseFileError) {
-        throw baseFileError;
+fs.readFile(resultsFile, 'utf8', (resultsFileError, resultsData) => {
+    if (resultsFileError) {
+        throw resultsFileError;
     }
 
     fs.readFile(performanceResultsFile, 'utf8', (performanceResultsFileError, performanceData) => {
@@ -20,39 +19,41 @@ fs.readFile(baseFile, 'utf8', (baseFileError, baseData) => {
             throw performanceResultsFileError;
         }
 
-        const baseJson: TestBenchmark[] = JSON.parse(baseData);
-        const performanceJson: PerformanceResults[] = JSON.parse(performanceData);
+        const resultsJson = JSON.parse(resultsData);
+        const performanceJson = JSON.parse(performanceData);
 
         performanceJson.forEach(result => {
-            const avg = result.times.reduce((a, b) => a + b) / result.times.length;
-            const testcase = baseJson.find(x => x.name === result.name);
+            const avg = result.times.reduce((a, b) => parseFloat(a) + parseFloat(b)) / result.times.length;
+            const testcase = resultsJson.find(x => x.name === result.name);
 
             if (testcase) {
                 // compare the average result to the base JSON
                 if (avg > testcase.time + errorMargin) {
-                    // test performance is slow
-                } else {
-                    // test performance is ok
+                    failedTests += 'Performance is slow in: ' + testcase.name + ' , Benchmark time: ' + testcase.time + ' , Average test time: ' + avg + '\n';
                 }
             } else {
                 // since there's no data, add the average result to the base JSON
                 testsWereAdded = true;
-                const newTest: TestBenchmark = {
+                const newTest = {
                     name: result.name,
                     time: avg
                 };
-                baseJson.push(newTest);
+                resultsJson.push(newTest);
             }
         });
 
         if (testsWereAdded) {
-            fs.writeFile('./test-results.json', JSON.stringify(baseJson, null, 2), writeResultsError => {
+            fs.writeFile('./test-results.json', JSON.stringify(resultsJson, null, 2), writeResultsError => {
                 if (writeResultsError) {
                     throw writeResultsError;
                 }
                 // tslint:disable-next-line: no-console
-                console.log('The file was saved!');
+                console.log('test-results.json was updated!');
             });
+        }
+
+        if (failedTests.length > 0) {
+            throw new Error(failedTests);
         }
     });
 });
--- build/ci/performance/savePerformanceResults.js
+++ build/ci/performance/savePerformanceResults.js
@@ -1,17 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 'use strict';
-import fastXmlParser from 'fast-xml-parser';
-import fs from 'fs';
+const fastXmlParser = require('fast-xml-parser');
+const fs = require('fs');
 
-export class PerformanceResults {
-    public name: string;
-    public times: number[];
-}
-
-const xmlFile = '../test-results.xml';
+const xmlFile = '../../../test-results.xml';
 const jsonFile = './performance-results.json';
-let jsonData: PerformanceResults[] = [];
+let performanceData = [];
+const resultsData = [];
 
 fs.readFile(xmlFile, 'utf8', (xmlReadError, xmlData) => {
     if (xmlReadError) {
@@ -28,49 +24,64 @@ fs.readFile(xmlFile, 'utf8', (xmlReadError, xmlData) => {
         fs.readFile(jsonFile, 'utf8', (jsonReadError, data) => {
             if (jsonReadError) {
                 // File doesn't exist, so we create it
-
                 jsonObj.testsuites.testsuite.forEach(suite => {
                     if (parseInt(suite.tests, 10) > 0) {
                         if (Array.isArray(suite.testcase)) {
                             suite.testcase.forEach(testcase => {
-                                const test: PerformanceResults = {
+                                const test = {
                                     name: testcase.name,
-                                    times: [testcase.time]
+                                    times: [parseFloat(testcase.time)]
                                 };
-                                jsonData.push(test);
+                                performanceData.push(test);
+                                resultsData.push({
+                                    name: testcase.name,
+                                    time: parseFloat(testcase.time)
+                                });
                             });
                         } else {
                             const test = {
                                 name: suite.testcase.name,
-                                times: [suite.testcase.time]
+                                times: [parseFloat(suite.testcase.time)]
                             };
-                            jsonData.push(test);
+                            performanceData.push(test);
+                            resultsData.push({
+                                name: suite.testcase.name,
+                                time: parseFloat(suite.testcase.time)
+                            });
                         }
                     }
                 });
+
+                fs.writeFile('./test-results.json', JSON.stringify(resultsData, null, 2), writeResultsError => {
+                    if (writeResultsError) {
+                        throw writeResultsError;
+                    }
+                    // tslint:disable-next-line: no-console
+                    console.log('test-results.json was saved!');
+                });
             } else {
-                jsonData = JSON.parse(data);
+                performanceData = JSON.parse(data);
 
                 jsonObj.testsuites.testsuite.forEach(suite => {
                     if (parseInt(suite.tests, 10) > 0) {
                         if (Array.isArray(suite.testcase)) {
                             suite.testcase.forEach(testcase => {
-                                let test = jsonData.find(x => x.name === testcase.name);
+                                let test = performanceData.find(x => x.name === testcase.name);
                                 if (test) {
                                     // if the test name is already there, we add the new time
                                     test.times.push(parseFloat(testcase.time));
                                 } else {
                                     // if its not there, we add the whole thing
-                                    test = {
+                                    const test = {
                                         name: testcase.name,
                                         times: [parseFloat(testcase.time)]
                                     };
 
-                                    jsonData.push(test);
+                                    performanceData.push(test);
                                 }
                             });
                         } else {
-                            let test = jsonData.find(x => x.name === suite.testcase.name);
+                            let test = performanceData.find(x => x.name === suite.testcase.name);
                             if (test) {
                                 // if the test name is already there, we add the new time
                                 test.times.push(parseFloat(suite.testcase.time));
@@ -81,20 +92,20 @@ fs.readFile(xmlFile, 'utf8', (xmlReadError, xmlData) => {
                                     times: [parseFloat(suite.testcase.time)]
                                 };
 
-                                jsonData.push(test);
+                                performanceData.push(test);
                             }
                         }
                     }
                 });
             }
+
+            fs.writeFile('./performance-results.json', JSON.stringify(performanceData, null, 2), writeResultsError => {
+                if (writeResultsError) {
+                    throw writeResultsError;
+                }
+                // tslint:disable-next-line: no-console
+                console.log('performance-results.json was saved!');
+            });
         });
     }
-
-    fs.writeFile('./performance-results.json', JSON.stringify(jsonData, null, 2), writeResultsError => {
-        if (writeResultsError) {
-            throw writeResultsError;
-        }
-        // tslint:disable-next-line: no-console
-        console.log('The file was saved!');
-    });
 });
