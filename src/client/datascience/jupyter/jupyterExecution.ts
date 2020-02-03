85fdda92e3dbf0cafaab81bf5be225211fc8407a
Don Jayamanne <don.jayamanne@yahoo.com>
2020-02-03 11:40:08 -0800

    More logging

--- src/client/datascience/jupyter/jupyterExecution.ts
+++ src/client/datascience/jupyter/jupyterExecution.ts
@@ -297,12 +297,7 @@ export class JupyterExecutionBase implements IJupyterExecution {
     // tslint:disable-next-line: max-func-body-length
     @captureTelemetry(Telemetry.StartJupyter)
     private async startNotebookServer(useDefaultConfig: boolean, cancelToken?: CancellationToken): Promise<IConnection> {
-        console.clear();
-        console.error('Start Tracking Total Time');
-        const stopWatch = new StopWatch();
-        const connection = await this.notebookStarter.start(useDefaultConfig, cancelToken);
-        console.error(`Total Time = ${stopWatch.elapsedTime} `);
-        return connection;
+        return this.notebookStarter.start(useDefaultConfig, cancelToken);
     }
     private onSettingsChanged() {
         // Clear our usableJupyterInterpreter so that we recompute our values
