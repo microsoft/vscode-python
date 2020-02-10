fc05183b549817a196cf88af5ae0f457e1d8bbdb
Karthik Nadig <kanadig@microsoft.com>
2020-02-10 11:07:06 -0800

    Undo logging changes to analysisEngineTest.ts.

--- src/test/analysisEngineTest.ts
+++ src/test/analysisEngineTest.ts
@@ -3,9 +3,8 @@
 
 'use strict';
 
-// tslint:disable: no-require-imports no-var-requires
+// tslint:disable:no-console no-require-imports no-var-requires
 import * as path from 'path';
-import { traceVerbose } from '../client/common/logger';
 
 process.env.CODE_TESTS_WORKSPACE = path.join(__dirname, '..', '..', 'src', 'test');
 process.env.IS_CI_SERVER_TEST_DEBUGGER = '';
@@ -13,8 +12,8 @@ process.env.VSC_PYTHON_LANGUAGE_SERVER = '1';
 process.env.TEST_FILES_SUFFIX = 'ls.test';
 
 function start() {
-    traceVerbose('*'.repeat(100));
-    traceVerbose('Start Language Server tests');
+    console.log('*'.repeat(100));
+    console.log('Start Language Server tests');
     require('../../node_modules/vscode/bin/test');
 }
 start();
