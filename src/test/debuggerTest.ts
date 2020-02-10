1fb39f1bb4fe85fc67946196d40653090e4b4b46
Karthik Nadig <karthik.nadig@live.com>
2020-02-09 20:37:17 -0800

    Undo trace changes in debuggerTests.ts

--- src/test/debuggerTest.ts
+++ src/test/debuggerTest.ts
@@ -1,9 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+// tslint:disable:no-console
+
 import * as path from 'path';
 import { runTests } from 'vscode-test';
-import { traceError, traceVerbose } from '../client/common/logger';
 import { EXTENSION_ROOT_DIR_FOR_TESTS } from './constants';
 
 const workspacePath = path.join(__dirname, '..', '..', 'src', 'testMultiRootWkspc', 'multi.code-workspace');
@@ -11,15 +12,15 @@ process.env.IS_CI_SERVER_TEST_DEBUGGER = '1';
 process.env.VSC_PYTHON_CI_TEST = '1';
 
 function start() {
-    traceVerbose('*'.repeat(100));
-    traceVerbose('Start Debugger tests');
+    console.log('*'.repeat(100));
+    console.log('Start Debugger tests');
     runTests({
         extensionDevelopmentPath: EXTENSION_ROOT_DIR_FOR_TESTS,
         extensionTestsPath: path.join(EXTENSION_ROOT_DIR_FOR_TESTS, 'out', 'test', 'index'),
         launchArgs: [workspacePath],
         version: 'stable'
     }).catch(ex => {
-        traceError('End Debugger tests (with errors)', ex);
+        console.error('End Debugger tests (with errors)', ex);
         process.exit(1);
     });
 }
