6f3aea1c339e37a2aab226ea728392a585ef4534
Ian Huff <ian.huff@gmail.com>
2020-02-19 14:45:10 -0800

    better comment and news

--- src/datascience-ui/interactive-common/buildSettingsCss.ts
+++ src/datascience-ui/interactive-common/buildSettingsCss.ts
@@ -5,7 +5,8 @@
 import { IDataScienceExtraSettings } from '../../client/datascience/types';
 
 // From a set of data science settings build up any settings that need to be
-// inserted into our StyleSetter divs
+// inserted into our StyleSetter divs some things like pseudo elements
+// can't be put into inline styles
 export function buildSettingsCss(settings: IDataScienceExtraSettings): string {
     return `#main-panel-content::-webkit-scrollbar {
     width: ${settings.extraSettings.verticalScrollbarSize}px;
