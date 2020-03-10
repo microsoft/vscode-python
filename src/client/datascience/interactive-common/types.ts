e66f21d82d3b7ba95ae5b47c00cfe81b99d7d934
Don Jayamanne <don.jayamanne@yahoo.com>
2020-03-10 12:49:35 -0700

    Fix formatting

--- src/client/datascience/interactive-common/types.ts
+++ src/client/datascience/interactive-common/types.ts
@@ -32,11 +32,11 @@ type BaseDataWithPayload<T> = {
 // This forms the base content of every payload in all dispatchers.
 export type BaseReduxActionPayload<T = never | undefined> = T extends never
     ? T extends undefined
-    ? BaseData
-    : BaseDataWithPayload<T>
+        ? BaseData
+        : BaseDataWithPayload<T>
     : BaseDataWithPayload<T>;
-// tslint:disable-next-line: no-any
 export type SyncPayload = {
     type: InteractiveWindowMessages | SharedMessages | CommonActionType | CssMessages;
+    // tslint:disable-next-line: no-any
     payload: BaseReduxActionPayload<any>;
 };
