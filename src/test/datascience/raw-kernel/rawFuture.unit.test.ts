f37eb98cd3a34c9b3ac01b44b16249b7473c308a
Ian Huff <ianhuff@Ians-MacBook-Pro.local>
2020-03-19 17:11:22 -0700

    empty files

---
+++ src/test/datascience/raw-kernel/rawFuture.unit.test.ts
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//import { assert } from 'chai';
+//import { anything, deepEqual, instance, mock, verify, when } from 'ts-mockito';
+//import * as typemoq from 'typemoq';
+//import { noop } from '../../../client/common/utils/misc';
+//import { RawKernel } from '../../../client/datascience/raw-kernel/rawKernel';
+//import { IJMPConnection } from '../../../client/datascience/types';
+
+// tslint:disable: max-func-body-length
+suite('Data Science - RawFuture', () => {
+    //let rawKernel: RawKernel;
+    //let jmpConnection: IJMPConnection;
+
+    setup(() => {
+        //jmpConnection = mock<IJMPConnection>();
+    });
+
+    //test('Start a session when connecting', async () => {
+    //await connect();
+    //assert.isTrue(jupyterSession.isConnected);
+    //verify(sessionManager.startNew(anything())).once();
+    //verify(contentsManager.newUntitled(anything())).once();
+    //});
+});
--- src/test/datascience/raw-kernel/rawKernel.unit.test.ts
+++ src/test/datascience/raw-kernel/rawKernel.unit.test.ts
@@ -1,16 +1,16 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { assert } from 'chai';
-import { anything, deepEqual, instance, mock, verify, when } from 'ts-mockito';
-import * as typemoq from 'typemoq';
-import { noop } from '../../../client/common/utils/misc';
-import { RawKernel } from '../../../client/datascience/raw-kernel/rawKernel';
-import { IJMPConnection } from '../../../client/datascience/types';
+//import { assert } from 'chai';
+//import { anything, deepEqual, instance, mock, verify, when } from 'ts-mockito';
+//import * as typemoq from 'typemoq';
+//import { noop } from '../../../client/common/utils/misc';
+//import { RawKernel } from '../../../client/datascience/raw-kernel/rawKernel';
+//import { IJMPConnection } from '../../../client/datascience/types';
 
 // tslint:disable: max-func-body-length
 suite('Data Science - RawKernel', () => {
-    let rawKernel: RawKernel;
-    let jmpConnection: IJMPConnection;
+    //let rawKernel: RawKernel;
+    //let jmpConnection: IJMPConnection;
 
     setup(() => {
         //jmpConnection = mock<IJMPConnection>();
