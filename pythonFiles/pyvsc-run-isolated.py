9fab8b1ce6db4b54eed26860bd2de841ea5841ad
Eric Snow <ericsnowcurrently@gmail.com>
2020-03-24 12:51:12 -0600

    Fix lint.

--- pythonFiles/pyvsc-run-isolated.py
+++ pythonFiles/pyvsc-run-isolated.py
@@ -1,8 +1,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-if __name__ != '__main__':
-    raise Exception('{} cannot be imported'.format(__name__))
+if __name__ != "__main__":
+    raise Exception("{} cannot be imported".format(__name__))
 
 import runpy
 import sys
@@ -13,7 +13,7 @@ import sys
 del sys.path[0]
 del sys.argv[0]
 module = sys.argv[0]
-if module.endswith('.py'):
+if module.endswith(".py"):
     runpy.run_path(module)
 else:
     runpy.run_module(module, alter_sys=True)
