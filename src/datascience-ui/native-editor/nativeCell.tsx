33a47890d2a73bc5cb3a1c2c016d8300bdd36393
Rich Chiodo <rchiodo@microsoft.com>
2020-03-26 09:35:12 -0700

    Fix typing in output of cells to not delete or modify any cells

--- src/datascience-ui/native-editor/nativeCell.tsx
+++ src/datascience-ui/native-editor/nativeCell.tsx
@@ -686,7 +686,7 @@ export class NativeCell extends React.Component<INativeCellProps> {
 
     private onOuterKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
         // Handle keydown events for the entire cell when we don't have focus
-        if (event.key !== 'Tab' && !this.isFocused()) {
+        if (event.key !== 'Tab' && !this.isFocused() && !this.focusInOutput()) {
             this.keyDownInput(this.props.cellVM.cell.id, {
                 code: event.key,
                 shiftKey: event.shiftKey,
@@ -699,6 +699,14 @@ export class NativeCell extends React.Component<INativeCellProps> {
             });
         }
     };
+
+    private focusInOutput(): boolean {
+        const focusedElement = document.activeElement as HTMLElement;
+        if (focusedElement) {
+            return focusedElement.closest('.cell-output-wrapper') !== null;
+        }
+        return false;
+    }
 
     private renderCollapseBar = (input: boolean) => {
         let classes = 'collapse-bar';
