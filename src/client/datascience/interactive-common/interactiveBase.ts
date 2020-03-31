a85d14c6726cb006b73eefd2cc5a280d9f746ec4
Rich Chiodo <rchiodo@microsoft.com>
2020-03-30 17:25:16 -0700

    Prettier format

--- src/client/datascience/interactive-common/interactiveBase.ts
+++ src/client/datascience/interactive-common/interactiveBase.ts
@@ -4,7 +4,6 @@
 import '../../common/extensions';
 
 import { nbformat } from '@jupyterlab/coreutils';
-// tslint:disable-next-line: prettier
 import type { KernelMessage } from '@jupyterlab/services';
 import { injectable, unmanaged } from 'inversify';
 import * as os from 'os';
