e52451180f95477f91f847aaa5859cbe684b236f
Ian Huff <ianhuff@Ians-MacBook-Pro.local>
2020-04-01 09:16:43 -0700

    unit tests added and working

--- src/test/datascience/interactive-common/notebookServerProvider.unit.test.ts
+++ src/test/datascience/interactive-common/notebookServerProvider.unit.test.ts
@@ -1,16 +1,26 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { expect } from 'chai';
-//import { SemVer } from 'semver';
+import { SemVer } from 'semver';
 import { anything, instance, mock, verify, when } from 'ts-mockito';
+import * as typemoq from 'typemoq';
 import { IApplicationShell } from '../../../client/common/application/types';
 import { IConfigurationService, IDataScienceSettings, IPythonSettings } from '../../../client/common/types';
-//import { Architecture } from '../../../client/common/utils/platform';
+import { Architecture } from '../../../client/common/utils/platform';
 import { NotebookServerProvider } from '../../../client/datascience/interactive-common/notebookServerProvider';
 import { ProgressReporter } from '../../../client/datascience/progress/progressReporter';
 import { IJupyterExecution, INotebookServer } from '../../../client/datascience/types';
-//import { IInterpreterService, InterpreterType, PythonInterpreter } from '../../../client/interpreter/contracts';
-import { IInterpreterService } from '../../../client/interpreter/contracts';
+import { IInterpreterService, InterpreterType, PythonInterpreter } from '../../../client/interpreter/contracts';
+
+// tslint:disable:no-any
+function createTypeMoq<T>(tag: string): typemoq.IMock<T> {
+    // Use typemoqs for those things that are resolved as promises. mockito doesn't allow nesting of mocks. ES6 Proxy class
+    // is the problem. We still need to make it thenable though. See this issue: https://github.com/florinn/typemoq/issues/67
+    const result = typemoq.Mock.ofType<T>();
+    (result as any).tag = tag;
+    result.setup((x: any) => x.then).returns(() => undefined);
+    return result;
+}
 
 // tslint:disable: max-func-body-length
 suite('Data Science - NotebookServerProvider', () => {
@@ -22,14 +32,14 @@ suite('Data Science - NotebookServerProvider', () => {
     let interpreterService: IInterpreterService;
     let pythonSettings: IPythonSettings;
     let dataScienceSettings: IDataScienceSettings;
-    //const workingPython: PythonInterpreter = {
-    //path: '/foo/bar/python.exe',
-    //version: new SemVer('3.6.6-final'),
-    //sysVersion: '1.0.0.0',
-    //sysPrefix: 'Python',
-    //type: InterpreterType.Unknown,
-    //architecture: Architecture.x64
-    //};
+    const workingPython: PythonInterpreter = {
+        path: '/foo/bar/python.exe',
+        version: new SemVer('3.6.6-final'),
+        sysVersion: '1.0.0.0',
+        sysPrefix: 'Python',
+        type: InterpreterType.Unknown,
+        architecture: Architecture.x64
+    };
 
     setup(() => {
         progressReporter = mock(ProgressReporter);
@@ -73,14 +83,13 @@ suite('Data Science - NotebookServerProvider', () => {
         verify(jupyterExecution.getServer(anything())).once();
     });
 
-    // IANHU: Look before checkin?
-    //test('NotebookServerProvider - Get Or Create', async () => {
-    //when(jupyterExecution.getUsableJupyterPython()).thenResolve(workingPython);
-    //const notebookServer = mock<INotebookServer>();
-    //when(jupyterExecution.connectToNotebookServer(anything(), undefined)).thenResolve(instance(notebookServer));
+    test('NotebookServerProvider - Get Or Create', async () => {
+        when(jupyterExecution.getUsableJupyterPython()).thenResolve(workingPython);
+        const notebookServer = createTypeMoq<INotebookServer>('jupyter server');
+        when(jupyterExecution.connectToNotebookServer(anything(), anything())).thenResolve(notebookServer.object);
 
-    //// Disable UI just lets us skip mocking the progress reporter
-    //const server = await serverProvider.getOrCreateServer({ getOnly: false, disableUI: true });
-    //expect(server).to.not.equal(undefined, 'Server expected to be defined');
-    //});
+        // Disable UI just lets us skip mocking the progress reporter
+        const server = await serverProvider.getOrCreateServer({ getOnly: false, disableUI: true });
+        expect(server).to.not.equal(undefined, 'Server expected to be defined');
+    });
 });
