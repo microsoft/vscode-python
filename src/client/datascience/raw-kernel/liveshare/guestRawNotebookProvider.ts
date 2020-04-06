588897795d833023e40827e497efac04dad11413
Ian Huff <ianhuff@Ians-MacBook-Pro.local>
2020-04-06 14:42:41 -0700

    stub out liveshare

--- src/client/datascience/raw-kernel/liveshare/guestRawNotebookProvider.ts
+++ src/client/datascience/raw-kernel/liveshare/guestRawNotebookProvider.ts
@@ -26,9 +26,6 @@ import { INotebook, IRawConnection, IRawNotebookProvider } from '../../types';
 export class GuestRawNotebookProvider
     extends LiveShareParticipantGuest(LiveShareParticipantDefault, LiveShare.RawNotebookProviderService)
     implements IRawNotebookProvider, ILiveShareParticipant {
-    //private notebooks = new Map<string, Promise<INotebook>>();
-
-    // IANHU: Clean up constructor ordering to not have unused types here?
     constructor(
         liveShare: ILiveShareApi,
         _disposableRegistry: IDisposableRegistry,
@@ -60,81 +57,23 @@ export class GuestRawNotebookProvider
         throw new Error('Not implemented');
     }
 
-    //public async createNotebook(resource: Resource, identity: Uri): Promise<INotebook> {
-    //// Remember we can have multiple native editors opened against the same ipynb file.
-    //if (this.notebooks.get(identity.toString())) {
-    //return this.notebooks.get(identity.toString())!;
-    //}
-
-    //const deferred = createDeferred<INotebook>();
-    //this.notebooks.set(identity.toString(), deferred.promise);
-    //// Tell the host side to generate a notebook for this uri
-    //const service = await this.waitForService();
-    //if (service) {
-    //const resourceString = resource ? resource.toString() : undefined;
-    //const identityString = identity.toString();
-    //await service.request(LiveShareCommands.createNotebook, [resourceString, identityString]);
-    //}
-
-    //// Return a new notebook to listen to
-    //const result = new GuestJupyterNotebook(
-    //this.liveShare,
-    //this.disposableRegistry,
-    //this.configService,
-    //resource,
-    //identity,
-    //this.launchInfo,
-    //this.dataScience.activationStartTime
-    //);
-    //deferred.resolve(result);
-    //const oldDispose = result.dispose.bind(result);
-    //result.dispose = () => {
-    //this.notebooks.delete(identity.toString());
-    //return oldDispose();
-    //};
-
-    //return result;
-    //}
-
-    public async onSessionChange(api: vsls.LiveShare | null): Promise<void> {
-        await super.onSessionChange(api);
-
-        //this.notebooks.forEach(async notebook => {
-        //const guestNotebook = (await notebook) as GuestJupyterNotebook;
-        //if (guestNotebook) {
-        //await guestNotebook.onSessionChange(api);
-        //}
-        //});
+    public async onSessionChange(_api: vsls.LiveShare | null): Promise<void> {
+        throw new Error('Not implemented');
     }
 
     public async getNotebook(_resource: Uri): Promise<INotebook | undefined> {
         throw new Error('Not implemented');
-        //return this.notebooks.get(resource.toString());
     }
 
     public async shutdown(): Promise<void> {
-        // Send this across to the other side. Otherwise the host server will remain running (like during an export)
-        //const service = await this.waitForService();
-        //if (service) {
-        //await service.request(LiveShareCommands.disposeServer, []);
-        //}
+        throw new Error('Not implemented');
     }
 
     public dispose(): Promise<void> {
-        return this.shutdown();
+        throw new Error('Not implemented');
     }
 
-    public async onAttach(api: vsls.LiveShare | null): Promise<void> {
-        await super.onAttach(api);
-
-        //if (api) {
-        //const service = await this.waitForService();
-
-        //// Wait for sync up
-        //const synced = service ? await service.request(LiveShareCommands.syncRequest, []) : undefined;
-        //if (!synced && api.session && api.session.role !== vsls.Role.None) {
-        //throw new Error(localize.DataScience.liveShareSyncFailure());
-        //}
-        //}
+    public async onAttach(_api: vsls.LiveShare | null): Promise<void> {
+        throw new Error('Not implemented');
     }
 }
--- src/client/datascience/raw-kernel/liveshare/hostRawNotebookProvider.ts
+++ src/client/datascience/raw-kernel/liveshare/hostRawNotebookProvider.ts
@@ -36,8 +36,6 @@ export class HostRawNotebookProvider
     extends LiveShareParticipantHost(RawNotebookProviderBase, LiveShare.RawNotebookProviderService)
     implements IRoleBasedObject, IRawNotebookProvider {
     private disposed = false;
-    //private portToForward = 0;
-    //private sharedPort: vscode.Disposable | undefined;
     constructor(
         private liveShare: ILiveShareApi,
         private disposableRegistry: IDisposableRegistry,
@@ -61,86 +59,20 @@ export class HostRawNotebookProvider
         }
     }
 
-    // IANHU: Port forwarding?
-    //public async connect(launchInfo: INotebookServerLaunchInfo, cancelToken?: CancellationToken): Promise<void> {
-    //if (launchInfo.connectionInfo && launchInfo.connectionInfo.localLaunch) {
-    //const portMatch = RegExpValues.ExtractPortRegex.exec(launchInfo.connectionInfo.baseUrl);
-    //if (portMatch && portMatch.length > 1) {
-    //const port = parseInt(portMatch[1], 10);
-    //await this.attemptToForwardPort(this.finishedApi, port);
-    //}
-    //}
-    //return super.connect(launchInfo, cancelToken);
-    //}
-
-    public async onAttach(api: vsls.LiveShare | null): Promise<void> {
-        await super.onAttach(api);
-
-        //if (api && !this.disposed) {
-        //const service = await this.waitForService();
-
-        //// Attach event handlers to different requests
-        //if (service) {
-        //// Requests return arrays
-        //service.onRequest(LiveShareCommands.syncRequest, (_args: any[], _cancellation: CancellationToken) =>
-        //this.onSync()
-        //);
-        //service.onRequest(LiveShareCommands.disposeServer, (_args: any[], _cancellation: CancellationToken) =>
-        //this.dispose()
-        //);
-        //service.onRequest(
-        //LiveShareCommands.createNotebook,
-        //async (args: any[], cancellation: CancellationToken) => {
-        //const resource = this.parseUri(args[0]);
-        //const identity = this.parseUri(args[1]);
-        //// Don't return the notebook. We don't want it to be serialized. We just want its live share server to be started.
-        //const notebook = (await this.createNotebook(
-        //resource,
-        //identity!,
-        //undefined,
-        //cancellation
-        //)) as HostJupyterNotebook;
-        //await notebook.onAttach(api);
-        //}
-        //);
-
-        //// See if we need to forward the port
-        //await this.attemptToForwardPort(api, this.portToForward);
-        //}
-        //}
+    public async onAttach(_api: vsls.LiveShare | null): Promise<void> {
+        throw new Error('Not implemented');
     }
 
-    public async onSessionChange(api: vsls.LiveShare | null): Promise<void> {
-        await super.onSessionChange(api);
-
-        //this.getNotebooks().forEach(async notebook => {
-        //const hostNotebook = (await notebook) as HostJupyterNotebook;
-        //if (hostNotebook) {
-        //await hostNotebook.onSessionChange(api);
-        //}
-        //});
+    public async onSessionChange(_api: vsls.LiveShare | null): Promise<void> {
+        throw new Error('Not implemented');
     }
 
-    public async onDetach(api: vsls.LiveShare | null): Promise<void> {
-        await super.onDetach(api);
-
-        // Make sure to unshare our port
-        //if (api && this.sharedPort) {
-        //this.sharedPort.dispose();
-        //this.sharedPort = undefined;
-        //}
+    public async onDetach(_api: vsls.LiveShare | null): Promise<void> {
+        throw new Error('Not implemented');
     }
 
     public async waitForServiceName(): Promise<string> {
         throw new Error('Not implemented');
-        // First wait for connect to occur
-        //const launchInfo = await this.waitForConnect();
-        //// Use our base name plus our purpose. This means one unique server per purpose
-        //if (!launchInfo) {
-        //return LiveShare.JupyterServerSharedService;
-        //}
-        //// tslint:disable-next-line:no-suspicious-comment
-        //return `${LiveShare.JupyterServerSharedService}${launchInfo.purpose}`;
     }
 
     protected async createNotebookInstance(
@@ -233,20 +165,4 @@ export class HostRawNotebookProvider
             purpose: undefined
         };
     }
-
-    //private async attemptToForwardPort(api: vsls.LiveShare | null | undefined, port: number): Promise<void> {
-    //if (port !== 0 && api && api.session && api.session.role === vsls.Role.Host) {
-    //this.portToForward = 0;
-    //this.sharedPort = await api.shareServer({
-    //port,
-    //displayName: localize.DataScience.liveShareHostFormat().format(os.hostname())
-    //});
-    //} else {
-    //this.portToForward = port;
-    //}
-    //}
-
-    //private onSync(): Promise<any> {
-    //return Promise.resolve(true);
-    //}
 }
