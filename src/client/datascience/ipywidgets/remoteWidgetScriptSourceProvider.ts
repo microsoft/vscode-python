51b9803899d48df76b1266dad72ead5d1533ea71
Don Jayamanne <don.jayamanne@yahoo.com>
2020-04-09 16:32:23 -0700

    Oops

--- src/client/datascience/ipywidgets/remoteWidgetScriptSourceProvider.ts
+++ src/client/datascience/ipywidgets/remoteWidgetScriptSourceProvider.ts
@@ -3,6 +3,8 @@
 
 'use strict';
 
+import { traceWarning } from '../../common/logger';
+import { IHttpClient } from '../../common/types';
 import { IConnection } from '../types';
 import { IWidgetScriptSourceProvider, WidgetScriptSource } from './types';
 
@@ -11,11 +13,27 @@ import { IWidgetScriptSourceProvider, WidgetScriptSource } from './types';
  * `<remote url>/nbextensions/moduleName/index`
  */
 export class RemoteWidgetScriptSourceProvider implements IWidgetScriptSourceProvider {
-    constructor(private readonly connection: IConnection) {}
+    public static validUrls = new Map<string, boolean>();
+    constructor(private readonly connection: IConnection, private readonly httpClient: IHttpClient) {}
     public dispose() {
         // Noop.
     }
-    public async getWidgetScriptSource(moduleName: string): Promise<WidgetScriptSource> {
-        return { moduleName, scriptUri: `${this.connection.baseUrl}/nbextensions/${moduleName}/index` };
+    public async getWidgetScriptSource(moduleName: string, moduleVersion: string): Promise<WidgetScriptSource> {
+        const scriptUri = `${this.connection.baseUrl}/nbextensions/${moduleName}/index`;
+        const exists = await this.getUrlForWidget(`${scriptUri}.js`);
+        if (exists) {
+            return { moduleName, scriptUri, source: 'cdn' };
+        }
+        traceWarning(`Widget Script not found for ${moduleName}@${moduleVersion}`);
+        return { moduleName };
+    }
+    private async getUrlForWidget(url: string): Promise<boolean> {
+        if (RemoteWidgetScriptSourceProvider.validUrls.has(url)) {
+            return RemoteWidgetScriptSourceProvider.validUrls.get(url)!;
+        }
+
+        const exists = await this.httpClient.exists(url);
+        RemoteWidgetScriptSourceProvider.validUrls.set(url, exists);
+        return exists;
     }
 }
