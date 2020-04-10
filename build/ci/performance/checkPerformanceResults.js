fea0e0a57a74c9819b1c539eb4b5b0641210ac7c
DavidKutu <david.kutu@gmail.com>
2020-04-10 00:00:54 -0700

    increase margin of error to 20%

--- build/ci/performance/checkPerformanceResults.js
+++ build/ci/performance/checkPerformanceResults.js
@@ -13,7 +13,7 @@ const performanceResultsFile = path.join(
     'performance',
     'performance-results.json'
 );
-const errorMargin = 1.1;
+const errorMargin = 1.2;
 let failedTests = '';
 
 function getFailingTimesString(missedTimes) {
