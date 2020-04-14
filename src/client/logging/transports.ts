2f7461a23e3cef7e72247b0d4b420e73995e6dec
Eric Snow <ericsnowcurrently@gmail.com>
2020-04-14 16:04:40 -0600

    Move ConsoleTransport to module scope.

--- src/client/logging/transports.ts
+++ src/client/logging/transports.ts
@@ -13,25 +13,34 @@ import { ConsoleStreams } from './types';
 // tslint:disable-next-line: no-var-requires no-require-imports
 const TransportStream = require('winston-transport');
 
+const formattedMessage = Symbol.for('message');
+
+// A winston-compatible transport type.
+class ConsoleTransport extends TransportStream {
+    constructor(
+        // logToConsole() is used to emit the log entry.
+        private readonly logToConsole: (stream: ConsoleStreams, ...args: any[]) => void,
+        options?: any
+    ) {
+        super(options);
+    }
+
+    public log?(info: { level: string; message: string; [formattedMessage]: string }, next: () => void): any {
+        setImmediate(() => this.emit('logged', info));
+        const msg = info[formattedMessage] || info.message;
+        this.logToConsole(info.level as any, msg);
+        if (next) {
+            next();
+        }
+    }
+}
+
 // Create a console-targeting transport that can be added to a winston logger.
 export function getConsoleTransport(
     logToConsole: (stream: ConsoleStreams, ...args: any[]) => void,
     formatter: logform.Format
 ) {
-    const formattedMessage = Symbol.for('message');
-    class ConsoleTransport extends TransportStream {
-        constructor(options?: any) {
-            super(options);
-        }
-        public log?(info: { level: string; message: string; [formattedMessage]: string }, next: () => void): any {
-            setImmediate(() => this.emit('logged', info));
-            logToConsole(info.level as any, info[formattedMessage] || info.message);
-            if (next) {
-                next();
-            }
-        }
-    }
-    return new ConsoleTransport({
+    return new ConsoleTransport(logToConsole, {
         // We minimize customization.
         format: formatter
     });
