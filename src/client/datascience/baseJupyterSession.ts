fe5f270d582d796657fbacbbd3781f41dd17d3ed
Ian Huff <ianhuff@Ians-MacBook-Pro.local>
2020-04-20 16:05:03 -0700

    sonar skip

--- src/client/datascience/baseJupyterSession.ts
+++ src/client/datascience/baseJupyterSession.ts
@@ -191,7 +191,7 @@ export abstract class BaseJupyterSession implements IJupyterSession {
         // This is just like doing a restart, kill the old session (and the old restart session), and start new ones
         if (this.session) {
             this.shutdownSession(this.session, this.statusHandler).ignoreErrors();
-            this.restartSessionPromise?.then((r) => this.shutdownSession(r, undefined)).ignoreErrors();
+            this.restartSessionPromise?.then((r) => this.shutdownSession(r, undefined)).ignoreErrors(); // NOSONAR
         }
 
         // Update our kernel spec
