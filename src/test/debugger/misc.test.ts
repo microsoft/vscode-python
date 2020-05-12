8a08b061391004420d87f805cc542df13230e84c
Karthik Nadig <karthik.nadig@live.com>
2020-05-12 13:42:00 -0700

    Remove DebugAdapterNewPtvsd and DebugAdapterDescriptorFactory experiment.

--- src/test/debugger/misc.test.ts
+++ src/test/debugger/misc.test.ts
@@ -6,7 +6,7 @@
 import * as path from 'path';
 import { DebugClient } from 'vscode-debugadapter-testsupport';
 import { noop } from '../../client/common/utils/misc';
-import { DebuggerTypeName, PTVSD_PATH } from '../../client/debugger/constants';
+import { DebuggerTypeName } from '../../client/debugger/constants';
 import { DebugOptions, LaunchRequestArguments } from '../../client/debugger/types';
 import { PYTHON_PATH, sleep } from '../common';
 import { IS_MULTI_ROOT_TEST, TEST_DEBUGGER } from '../initialize';
@@ -55,7 +55,6 @@ suite(`Standard Debugging - Misc tests: ${debuggerType}`, () => {
         stopOnEntry: boolean = false,
         showReturnValue: boolean = true
     ): LaunchRequestArguments {
-        const env = { PYTHONPATH: PTVSD_PATH };
         // tslint:disable-next-line:no-unnecessary-local-variable
         const options = ({
             program: path.join(debugFilesPath, pythonFile),
@@ -65,7 +64,6 @@ suite(`Standard Debugging - Misc tests: ${debuggerType}`, () => {
             debugOptions: [DebugOptions.RedirectOutput],
             pythonPath: PYTHON_PATH,
             args: [],
-            env,
             envFile: '',
             logToFile: false,
             type: debuggerType
