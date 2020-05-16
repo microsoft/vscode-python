8f74de1ed2e966f9a76a4e18b21858cd7b9b1ffb
Don Jayamanne <don.jayamanne@yahoo.com>
2020-05-15 17:23:48 -0700

    Fixes

--- src/client/datascience/notebook/serviceRegistry.ts
+++ src/client/datascience/notebook/serviceRegistry.ts
@@ -5,11 +5,18 @@
 
 import { IExtensionSingleActivationService } from '../../activation/types';
 import { IServiceManager } from '../../ioc/types';
+import { INotebookEditorProvider } from '../types';
 import { NotebookContentProvider } from './contentProvider';
 import { NotebookIntegration } from './integration';
+import { NotebookEditorProvider, NotebookEditorProviderActivation } from './notebookEditorProvider';
 import { NotebookKernel } from './notebookKernel';
 
 export function registerTypes(serviceManager: IServiceManager) {
+    serviceManager.addSingleton<IExtensionSingleActivationService>(
+        IExtensionSingleActivationService,
+        NotebookEditorProviderActivation
+    );
+    serviceManager.rebindSingleton<INotebookEditorProvider>(INotebookEditorProvider, NotebookEditorProvider);
     serviceManager.addSingleton<NotebookKernel>(NotebookKernel, NotebookKernel);
     serviceManager.addSingleton<IExtensionSingleActivationService>(
         IExtensionSingleActivationService,
