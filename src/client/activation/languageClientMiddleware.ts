ce64798f4e6bf8c1ca924967da9994ed29b28f50
Kim-Adeline Miguel <kimiguel@microsoft.com>
2020-05-19 08:57:52 -0700

    Replace slashes before sending telemetry instead

--- src/client/activation/languageClientMiddleware.ts
+++ src/client/activation/languageClientMiddleware.ts
@@ -444,9 +444,12 @@ function captureTelemetryForLSPMethod(method: string, debounceMilliseconds: numb
             this.lastCaptured.set(method, now);
             this.eventCount += 1;
 
+            // Replace all slashes in the method name, or the property will be swallowed by vscode-extension-telemetry.
+            const formattedMethod = method.replace(/\//g, '.');
+
             const properties = {
                 lsVersion: this.serverVersion || 'unknown',
-                method: method
+                method: formattedMethod
             };
 
             const stopWatch = new StopWatch();
