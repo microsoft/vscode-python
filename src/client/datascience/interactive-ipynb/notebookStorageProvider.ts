6361d40eaba514f320cbf1b1f5448aa0c116f8ea
Don Jayamanne <don.jayamanne@yahoo.com>
2020-05-20 08:44:40 -0700

    Revert changes

--- src/client/datascience/interactive-ipynb/notebookStorageProvider.ts
+++ src/client/datascience/interactive-ipynb/notebookStorageProvider.ts
@@ -10,6 +10,7 @@ import { IDisposable, IDisposableRegistry } from '../../common/types';
 import { DataScience } from '../../common/utils/localize';
 import { noop } from '../../common/utils/misc';
 import { INotebookModel, INotebookStorage } from '../types';
+import { isUntitled } from './nativeEditorStorage';
 
 // tslint:disable-next-line:no-require-imports no-var-requires
 const debounce = require('lodash/debounce') as typeof import('lodash/debounce');
@@ -27,7 +28,6 @@ export class NotebookStorageProvider implements INotebookStorageProvider {
     private readonly storageAndModels = new Map<string, Promise<INotebookModel>>();
     private models = new Set<INotebookModel>();
     private readonly disposables: IDisposable[] = [];
-    private nextUntitledNotebookCounter = 1;
     private readonly _autoSaveNotebookInHotExitFile = new WeakMap<INotebookModel, Function>();
     constructor(
         @inject(INotebookStorage) private readonly storage: INotebookStorage,
@@ -72,9 +72,13 @@ export class NotebookStorageProvider implements INotebookStorageProvider {
     }
 
     private async getNextNewNotebookUri(): Promise<Uri> {
-        const fileName = `${DataScience.untitledNotebookFileName()}-${this.nextUntitledNotebookCounter}.ipynb`;
-        // Next time we should use a new value.
-        this.nextUntitledNotebookCounter = this.nextUntitledNotebookCounter + 1;
+        // tslint:disable-next-line: no-suspicious-comment
+        // TODO: This will not work, if we close an untitled document.
+        // See if we have any untitled storage already
+        const untitledStorage = Array.from(this.models.values()).filter(isUntitled);
+        // Just use the length (don't bother trying to fill in holes). We never remove storage objects from
+        // our map, so we'll keep creating new untitled notebooks.
+        const fileName = `${DataScience.untitledNotebookFileName()}-${untitledStorage.length + 1}.ipynb`;
         const fileUri = Uri.file(fileName);
         // Turn this back into an untitled
         return fileUri.with({ scheme: 'untitled', path: fileName });
