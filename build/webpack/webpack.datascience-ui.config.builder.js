5382c65df5c67d561efe8bd8784afd2f0a432d27
Don Jayamanne <don.jayamanne@yahoo.com>
2020-05-22 14:22:03 -0700

    Split jobs

--- build/webpack/webpack.datascience-ui.config.builder.js
+++ build/webpack/webpack.datascience-ui.config.builder.js
@@ -99,10 +99,6 @@ function buildConfiguration(isNotebook) {
         filesToCopy.push(
             ...[
                 {
-                    from: path.join(constants.ExtensionRootDir, 'out/ipywidgets/dist/ipywidgets.js'),
-                    to: path.join(constants.ExtensionRootDir, 'out', 'datascience-ui', bundleFolder)
-                },
-                {
                     from: path.join(constants.ExtensionRootDir, 'node_modules/font-awesome/**/*'),
                     to: path.join(constants.ExtensionRootDir, 'out', 'datascience-ui', 'common', 'node_modules')
                 }
