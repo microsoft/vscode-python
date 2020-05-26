7d7890882eb7faa1ed0ebcb912e6c9d1013a6887
Mikhail Arkhipov <mikhaila@microsoft.com>
2020-05-26 12:08:49 -0700

    Put test back

--- src/test/linters/linterinfo.unit.test.ts
+++ src/test/linters/linterinfo.unit.test.ts
@@ -62,4 +62,40 @@ suite('Linter Info - Pylint', () => {
 
         expect(linterInfo.isEnabled()).to.be.false;
     });
+    const testsForisEnabled = [
+        {
+            testName: 'When workspaceFolder setting is provided',
+            inspection: { workspaceFolderValue: true }
+        },
+        {
+            testName: 'When workspace setting is provided',
+            inspection: { workspaceValue: true }
+        },
+        {
+            testName: 'When global setting is provided',
+            inspection: { globalValue: true }
+        }
+    ];
+
+    suite('Test is enabled when using Language Server and Pylint is configured', () => {
+        testsForisEnabled.forEach((testParams) => {
+            test(testParams.testName, async () => {
+                // tslint:disable-next-line:no-shadowed-variable
+                const config = mock(ConfigurationService);
+                const workspaceService = mock(WorkspaceService);
+                const linterInfo = new PylintLinterInfo(instance(config), instance(workspaceService), []);
+
+                const pythonConfig = {
+                    inspect: () => testParams.inspection
+                };
+                when(config.getSettings(anything())).thenReturn({
+                    linting: { pylintEnabled: true },
+                    languageServer: LanguageServerType.Microsoft
+                } as any);
+                when(workspaceService.getConfiguration('python', anything())).thenReturn(pythonConfig as any);
+
+                expect(linterInfo.isEnabled()).to.be.true;
+            });
+        });
+    });
 });
