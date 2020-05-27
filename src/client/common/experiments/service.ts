e73f68e02fe86f214ba6e81078ef6e05a4494343
Kim-Adeline Miguel <kimiguel@microsoft.com>
2020-05-27 16:17:38 -0700

    Forgot 'use strict' in service.ts

--- src/client/common/experiments/service.ts
+++ src/client/common/experiments/service.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+'use strict';
+
 import { inject, named } from 'inversify';
 import { Memento } from 'vscode';
 import { getExperimentationService, IExperimentationService, TargetPopulation } from 'vscode-tas-client';
