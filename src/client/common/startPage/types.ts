99b33f50b5047c820682819d652d927ff8f1f716
DavidKutu <david.kutu@gmail.com>
2020-05-28 21:50:00 -0700

    added license

--- src/client/common/startPage/types.ts
+++ src/client/common/startPage/types.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 import { SharedMessages } from '../../datascience/messages';
 
 export const IStartPage = Symbol('IStartPage');
