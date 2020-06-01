98a60cd9602c3642ebfb2c30e421adf5fa900423
Tim Ruscica <mrtechguy234@gmail.com>
2020-06-01 13:53:28 -0400

    clean up

--- src/client/datascience/interactive-common/interactiveBase.ts
+++ src/client/datascience/interactive-common/interactiveBase.ts
@@ -1092,10 +1092,10 @@ export abstract class InteractiveBase extends WebViewHost<IInteractiveWindowMapp
             try {
                 notebook = identity
                     ? await this.notebookProvider.getOrCreateNotebook({
-                        identity: identity.resource,
-                        resource,
-                        metadata
-                    })
+                          identity: identity.resource,
+                          resource,
+                          metadata
+                      })
                     : undefined;
                 if (notebook) {
                     const executionActivation = { ...identity, owningResource: resource };
@@ -1373,11 +1373,11 @@ export abstract class InteractiveBase extends WebViewHost<IInteractiveWindowMapp
         const response: IJupyterVariablesResponse = this._notebook
             ? await this.jupyterVariables.getVariables(this._notebook, args)
             : {
-                totalCount: 0,
-                pageResponse: [],
-                pageStartIndex: args?.startIndex,
-                executionCount: args?.executionCount
-            };
+                  totalCount: 0,
+                  pageResponse: [],
+                  pageStartIndex: args?.startIndex,
+                  executionCount: args?.executionCount
+              };
 
         this.postMessage(InteractiveWindowMessages.GetVariablesResponse, response).ignoreErrors();
         sendTelemetryEvent(Telemetry.VariableExplorerVariableCount, undefined, { variableCount: response.totalCount });
