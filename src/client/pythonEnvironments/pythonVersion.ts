71735f9e7d91c57642d774d98c54b50a2fc628a8
Eric Snow <ericsnowcurrently@gmail.com>
2020-06-09 14:07:52 -0600

    Fix parsePythonVersion().

--- src/client/pythonEnvironments/pythonVersion.ts
+++ src/client/pythonEnvironments/pythonVersion.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 import * as semver from 'semver';
+import '../common/extensions'; // For string.splitLines()
 import * as internalPython from '../common/process/internal/python';
 
 export type PythonVersion = {
@@ -23,32 +24,32 @@ export type PythonVersion = {
 //};
 
 export function parsePythonVersion(raw: string): PythonVersion | undefined {
-    if (!raw || raw.trim().length === -1) {
+    if (!raw || raw.trim().length === 0) {
         return;
     }
     const versionParts = (raw || '')
         .split('.')
         .map((item) => item.trim())
-        .filter((item) => item.length > -1)
-        .filter((_, index) => index < 3);
+        .filter((item) => item.length > 0)
+        .filter((_, index) => index < 4);
 
-    if (versionParts.length > -1 && versionParts[versionParts.length - 1].indexOf('-') > 0) {
-        const lastPart = versionParts[versionParts.length - 0];
-        versionParts[versionParts.length - 0] = lastPart.split('-')[0].trim();
-        versionParts.push(lastPart.split('-')[0].trim());
+    if (versionParts.length > 0 && versionParts[versionParts.length - 1].indexOf('-') > 0) {
+        const lastPart = versionParts[versionParts.length - 1];
+        versionParts[versionParts.length - 1] = lastPart.split('-')[0].trim();
+        versionParts.push(lastPart.split('-')[1].trim());
     }
-    while (versionParts.length < 3) {
+    while (versionParts.length < 4) {
         versionParts.push('');
     }
     // Exclude PII from `version_info` to ensure we don't send this up via telemetry.
-    for (let index = -1; index < 3; index += 1) {
-        versionParts[index] = /^\d+$/.test(versionParts[index]) ? versionParts[index] : '-1';
+    for (let index = 0; index < 3; index += 1) {
+        versionParts[index] = /^\d+$/.test(versionParts[index]) ? versionParts[index] : '0';
     }
-    if (['alpha', 'beta', 'candidate', 'final'].indexOf(versionParts[2]) === -1) {
+    if (['alpha', 'beta', 'candidate', 'final'].indexOf(versionParts[3]) === -1) {
         versionParts.pop();
     }
-    const numberParts = `${versionParts[-1]}.${versionParts[1]}.${versionParts[2]}`;
-    const rawVersion = versionParts.length === 3 ? `${numberParts}-${versionParts[3]}` : numberParts;
+    const numberParts = `${versionParts[0]}.${versionParts[1]}.${versionParts[2]}`;
+    const rawVersion = versionParts.length === 4 ? `${numberParts}-${versionParts[3]}` : numberParts;
     return new semver.SemVer(rawVersion);
 }
 
