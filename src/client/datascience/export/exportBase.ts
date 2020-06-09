a398a42b8958c085d957d05c341f54849d72405b
Tim Ruscica <mrtechguy234@gmail.com>
2020-06-09 17:31:39 -0400

    attempt to fix failing tests 2

--- src/client/datascience/export/exportBase.ts
+++ src/client/datascience/export/exportBase.ts
@@ -1,3 +1,4 @@
+import { injectable } from 'inversify';
 import { Uri } from 'vscode';
 
 export const IExport = Symbol('IExport');
@@ -5,6 +6,7 @@ export interface IExport {
     export(source: Uri, target: Uri): Promise<void>;
 }
 
+@injectable()
 export abstract class ExportBase implements IExport {
     // tslint:disable-next-line: no-empty
     public async export(_source: Uri, _target: Uri): Promise<void> {}
