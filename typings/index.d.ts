51024cbf626ecea13ad4da085f816175111ea824
Don Jayamanne <don.jayamanne@yahoo.com>
2020-06-10 12:07:48 -0700

    Remove types

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,11 +2,6 @@
 // Licensed under the MIT License.
 
 
-// Added to allow compilation of backbone types pulled in from ipywidgets (@jupyterlab/widgets).
-declare module JQuery {
-    type TriggeredEvent = any;
-}
-
 declare module '@phosphor/coreutils' {
     export class PromiseDelegate<T> {
         /**
