eae4d450fa81a57e06ab5f358c43c8e76ce93f25
Eric Snow <ericsnowcurrently@gmail.com>
2020-06-16 10:56:54 -0600

    Drop commented-out code.

--- src/client/pythonEnvironments/pythonVersion.ts
+++ src/client/pythonEnvironments/pythonVersion.ts
@@ -10,18 +10,13 @@ export type PythonVersion = {
     major: number;
     minor: number;
     patch: number;
+    // Eventually it may be useful to match what sys.version_info
+    // provides for the remainder here:
+    // * releaseLevel: 'alpha' | 'beta' | 'candidate' | 'final';
+    // * serial: number;
     build: string[];
     prerelease: string[];
 };
-
-//export type PythonVersion = {
-//    raw: string;
-//    major: number;
-//    minor: number;
-//    micro: number;
-//    releaseLevel: 'alpha' | 'beta' | 'candidate' | 'final';
-//    serial: number;
-//};
 
 export function parsePythonVersion(raw: string): PythonVersion | undefined {
     if (!raw || raw.trim().length === 0) {
