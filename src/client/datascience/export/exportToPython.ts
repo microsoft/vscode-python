caa1858deeafb3764754370ed4725614aec94342
Tim <mrtechguy234@gmail.com>
2020-06-18 15:07:43 -0400

    fixed export to python telemetry

--- src/client/datascience/export/exportToPython.ts
+++ src/client/datascience/export/exportToPython.ts
@@ -6,8 +6,8 @@ import { ExportBase } from './exportBase';
 
 @injectable()
 export class ExportToPython extends ExportBase {
-    public sendTelemetryEvent(Telemetry.ExportNotebookAsPython);
     public async export(source: Uri, target: Uri): Promise<void> {
+        sendTelemetryEvent(Telemetry.ExportNotebookAsPython);
         const contents = await this.importer.importFromFile(source.fsPath);
         await this.fileSystem.writeFile(target.fsPath, contents);
     }
