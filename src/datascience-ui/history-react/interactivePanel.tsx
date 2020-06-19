a5965d6fd2e9f571c0b55570ae4b7ba2d7860f52
DavidKutu <david.kutu@gmail.com>
2020-06-19 12:25:21 -0700

    Add the experiment, as well as IExperimentService on everything that extends WebViewHost

--- src/datascience-ui/history-react/interactivePanel.tsx
+++ src/datascience-ui/history-react/interactivePanel.tsx
@@ -210,18 +210,24 @@ ${buildSettingsCss(this.props.settings)}`}</style>
     }
 
     private renderKernelSelection() {
-        if (this.props.settings && this.props.settings.showKernelSelectionOnInteractiveWindow) {
-            return (
-                <KernelSelection
-                    baseTheme={this.props.baseTheme}
-                    font={this.props.font}
-                    kernel={this.props.kernel}
-                    selectServer={this.props.selectServer}
-                    selectKernel={this.props.selectKernel}
-                />
-            );
-        } else if (this.props.kernel.localizedUri === getLocString('DataScience.localJupyterServer', 'local')) {
-            return;
+        if (
+            this.props.settings &&
+            this.props.settings.webviewExperiments &&
+            this.props.settings.webviewExperiments.removeKernelToolbarInInteractiveWindow
+        ) {
+            if (this.props.settings.showKernelSelectionOnInteractiveWindow) {
+                return (
+                    <KernelSelection
+                        baseTheme={this.props.baseTheme}
+                        font={this.props.font}
+                        kernel={this.props.kernel}
+                        selectServer={this.props.selectServer}
+                        selectKernel={this.props.selectKernel}
+                    />
+                );
+            } else if (this.props.kernel.localizedUri === getLocString('DataScience.localJupyterServer', 'local')) {
+                return;
+            }
         }
 
         return (
