c4b69c63260dd0864224ac0e0f607e386b3528c4
Kartik Raj <karraj@microsoft.com>
2020-06-26 11:21:03 -0700

    Modified the API

--- src/client/api.ts
+++ src/client/api.ts
@@ -3,11 +3,13 @@
 
 'use strict';
 
+import { Event, Uri } from 'vscode';
 import { isTestExecution } from './common/constants';
 import { traceError } from './common/logger';
 import { IConfigurationService, Resource } from './common/types';
 import { IDataViewerDataProvider, IDataViewerFactory } from './datascience/data-viewing/types';
 import { getDebugpyLauncherArgs, getDebugpyPackagePath } from './debugger/extension/adapter/remoteLaunchers';
+import { IInterpreterService } from './interpreter/contracts';
 import { IServiceContainer, IServiceManager } from './ioc/types';
 
 /*
@@ -45,20 +47,32 @@ export interface IExtensionApi {
      */
     settings: {
         /**
-         * Returns the Python execution command corresponding to the specified resource, taking into account
+         * An event that is emitted when the interpreter configuration changes.
+         */
+        readonly onDidChangeExecDetails: Event<Uri | undefined>;
+        /**
+         * Returns the Python execution object corresponding to the specified resource, taking into account
          * any workspace-specific settings for the workspace to which this resource belongs.
-         * E.g of execution commands returned could be,
-         * * `['<path to the interpreter set in settings>']`
-         * * `['<path to the interpreter selected by the extension when setting is not set>']`
-         * * `['conda', 'run', 'python']` which is used to run from within Conda environments.
-         * or something similar for some other Python environments.
          * @param {Resource} [resource] A resource for which the setting is asked for.
          * * When no resource is provided, the setting scoped to the first workspace folder is returned.
          * * If no folder is present, it returns the global setting.
-         * @returns {(string[] | undefined)} When return value is `undefined`, it means no interpreter is set.
-         * Otherwise, join the items returned using space to construct the full execution command.
+         * @returns {({ execCommand: string[] | undefined })}
          */
-        getExecutionCommand(resource?: Resource): string[] | undefined;
+        getExecutionDetails(
+            resource?: Resource
+        ): {
+            /**
+             * E.g of execution commands returned could be,
+             * * `['<path to the interpreter set in settings>']`
+             * * `['<path to the interpreter selected by the extension when setting is not set>']`
+             * * `['conda', 'run', 'python']` which is used to run from within Conda environments.
+             * or something similar for some other Python environments.
+             *
+             * @type {(string[] | undefined)} When return value is `undefined`, it means no interpreter is set.
+             * Otherwise, join the items returned using space to construct the full execution command.
+             */
+            execCommand: string[] | undefined;
+        };
     };
     datascience: {
         /**
@@ -77,7 +91,8 @@ export function buildApi(
     serviceContainer: IServiceContainer
 ): IExtensionApi {
     const configurationService = serviceContainer.get<IConfigurationService>(IConfigurationService);
-    const api = {
+    const interpreterService = serviceContainer.get<IInterpreterService>(IInterpreterService);
+    const api: IExtensionApi = {
         // 'ready' will propagate the exception, but we must log it here first.
         ready: ready.catch((ex) => {
             traceError('Failure during activation.', ex);
@@ -100,10 +115,11 @@ export function buildApi(
             }
         },
         settings: {
-            getExecutionCommand(resource?: Resource) {
+            onDidChangeExecDetails: interpreterService.onDidChangeInterpreterConfiguration,
+            getExecutionDetails(resource?: Resource) {
                 const pythonPath = configurationService.getSettings(resource).pythonPath;
                 // If pythonPath equals an empty string, no interpreter is set.
-                return pythonPath === '' ? undefined : [pythonPath];
+                return { execCommand: pythonPath === '' ? undefined : [pythonPath] };
             }
         },
         datascience: {
