1f76143247996b6fc7676e4d86b958d2cbc82961
Rich Chiodo <rchiodo@microsoft.com>
2020-06-25 17:58:26 -0700

    Run by line telemetry

--- src/client/telemetry/index.ts
+++ src/client/telemetry/index.ts
@@ -2170,6 +2170,9 @@ export interface IEventNamePropertyMapping {
     [Telemetry.StartPageOpenFileBrowser]: never | undefined;
     [Telemetry.StartPageOpenFolder]: never | undefined;
     [Telemetry.StartPageOpenWorkspace]: never | undefined;
+    [Telemetry.RunByLineStart]: never | undefined;
+    [Telemetry.RunByLineStep]: never | undefined;
+    [Telemetry.RunByLineStop]: never | undefined;
     [VSCodeNativeTelemetry.AddCell]: never | undefined;
     [VSCodeNativeTelemetry.DeleteCell]: never | undefined;
     [VSCodeNativeTelemetry.MoveCell]: never | undefined;
