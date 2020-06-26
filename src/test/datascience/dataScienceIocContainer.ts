5b3ab35696a1fdbcb82271bc536db13e13ce3075
Don Jayamanne <don.jayamanne@yahoo.com>
2020-06-26 16:00:47 -0700

    FIxes

--- src/test/datascience/dataScienceIocContainer.ts
+++ src/test/datascience/dataScienceIocContainer.ts
@@ -270,6 +270,7 @@ import { KernelFinder } from '../../client/datascience/kernel-launcher/kernelFin
 import { KernelLauncher } from '../../client/datascience/kernel-launcher/kernelLauncher';
 import { IKernelFinder, IKernelLauncher } from '../../client/datascience/kernel-launcher/types';
 import { NotebookAndInteractiveWindowUsageTracker } from '../../client/datascience/notebookAndInteractiveTracker';
+import { NotebookModelFactory } from '../../client/datascience/notebookStorage/factory';
 import { PlotViewer } from '../../client/datascience/plotting/plotViewer';
 import { PlotViewerProvider } from '../../client/datascience/plotting/plotViewerProvider';
 import { ProgressReporter } from '../../client/datascience/progress/progressReporter';
@@ -616,6 +617,7 @@ export class DataScienceIocContainer extends UnitTestIocContainer {
             ExportManagerDependencyChecker,
             ExportManagerDependencyChecker
         );
+        this.serviceManager.addSingleton<NotebookModelFactory>(NotebookModelFactory, NotebookModelFactory);
         this.serviceManager.addSingleton<IExportManager>(IExportManager, ExportManagerFileOpener);
         this.serviceManager.addSingleton<IExport>(IExport, ExportToPDF, ExportFormat.pdf);
         this.serviceManager.addSingleton<IExport>(IExport, ExportToHTML, ExportFormat.html);
