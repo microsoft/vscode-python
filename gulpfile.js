b4af150e9d87746c59968d1264f8c05c1ac12fbb
Don Jayamanne <don.jayamanne@yahoo.com>
2020-07-07 13:51:33 -0700

    Oops

--- gulpfile.js
+++ gulpfile.js
@@ -138,7 +138,7 @@ gulp.task('compile-viewers', async () => {
     await buildWebPackForDevOrProduction('./build/webpack/webpack.datascience-ui-viewers.config.js');
 });
 
-gulp.task('compile-webviews', gulp.series('compile-ipywidgets', 'compile-notebooks', 'compile-viewers', 'compile-renderers'));
+gulp.task('compile-webviews', gulp.series('compile-ipywidgets', 'compile-notebooks', 'compile-viewers'));
 
 gulp.task(
     'check-datascience-dependencies',
@@ -177,6 +177,7 @@ gulp.task('webpack', async () => {
     await buildWebPackForDevOrProduction('./build/webpack/webpack.datascience-ui-notebooks.config.js', 'production');
     await buildWebPackForDevOrProduction('./build/webpack/webpack.datascience-ui-viewers.config.js', 'production');
     await buildWebPackForDevOrProduction('./build/webpack/webpack.extension.config.js', 'extension');
+    await buildWebPackForDevOrProduction('./build/webpack/webpack.datascience-ui-renderers.config.js', 'production');
 });
 
 gulp.task('updateBuildNumber', async () => {
