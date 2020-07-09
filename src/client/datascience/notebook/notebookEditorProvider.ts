7a1378f4a92fbf48983e7e80e75392111ebbb3bc
Don Jayamanne <don.jayamanne@yahoo.com>
2020-07-09 15:02:47 -0700

    Fixes

--- src/client/datascience/notebook/notebookEditorProvider.ts
+++ src/client/datascience/notebook/notebookEditorProvider.ts
@@ -180,6 +180,9 @@ export class NotebookEditorProvider implements INotebookEditorProvider {
         const deferred = this.notebooksWaitingToBeOpenedByUri.get(uri.toString())!;
         deferred.resolve(editor);
         this.notebookEditorsByUri.set(uri.toString(), editor);
+        if (!model.isTrusted) {
+            await this.commandManager.executeCommand(Commands.TrustNotebook, model.file);
+        }
     }
     private onDidChangeActiveVsCodeNotebookEditor(editor: VSCodeNotebookEditor | undefined) {
         if (!editor) {
