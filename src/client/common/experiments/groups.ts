b3c85fc398f4eb6ddfcdced71eea997c51810188
Mikhail Arkhipov <mikhaila@microsoft.com>
2020-07-20 17:36:26 -0700

    Remove obsolete experiments, add Pylance

--- src/client/common/experiments/groups.ts
+++ src/client/common/experiments/groups.ts
@@ -1,6 +1,3 @@
-export const LSControl = 'LS - control';
-export const LSEnabled = 'LS - enabled';
-
 // Experiment to check whether to always display the test explorer.
 export enum AlwaysDisplayTestExplorerGroups {
     control = 'AlwaysDisplayTestExplorer - control',
