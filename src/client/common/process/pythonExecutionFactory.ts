aa275d95620466fc1891740e0c0b7f4d7218d3fa
Ian Huff <ianhuff@ravikun-dev2.redmond.corp.microsoft.com>
2020-08-10 16:49:20 -0700

    remove funky old cast

--- src/client/common/process/pythonExecutionFactory.ts
+++ src/client/common/process/pythonExecutionFactory.ts
@@ -93,7 +93,7 @@ export class PythonExecutionFactory implements IPythonExecutionFactory {
         });
         // No daemon support in Python 2.7 or during shutdown
         if (!interpreterService || (interpreter?.version && interpreter.version.major < 3)) {
-            return (activatedProcPromise! as unknown) as T;
+            return activatedProcPromise;
         }
 
         // Ensure we do not start multiple daemons for the same interpreter.
