a2c1d52b683ab350ccc64e49430dbfa517d245bf
Karthik Nadig <kanadig@microsoft.com>
2020-08-13 23:35:28 -0700

    Rebase with master and some more cleanup

--- src/client/pythonEnvironments/info/environmentInfoService.ts
+++ src/client/pythonEnvironments/info/environmentInfoService.ts
@@ -3,49 +3,11 @@
 
 'use strict';
 
-import { Architecture } from '../../common/utils/platform';
+import { EnvironmentType, PythonEnvironment } from '.';
 import { shellExecute } from '../common/externalDependencies';
 import { buildPythonExecInfo } from '../exec';
 import { getInterpreterInfo } from './interpreter';
-import { PythonVersion } from './pythonVersion';
 import { IWorkerPool, QueuePosition, WorkerPool } from './workerPool';
-
-export enum InterpreterType {
-    unknown,
-    cpython
-}
-
-export enum EnvironmentType {
-    Unknown,
-    Conda,
-    Poetry,
-    PyEnv,
-    PipEnv,
-    WindowsStore,
-    Venv,
-    VirtualEnvWrapper,
-    VirtualEnv,
-    Global,
-    System
-}
-
-export interface IEnvironmentInfo {
-    interpreterPath: string;
-    interpreterType: InterpreterType;
-    environmentType: EnvironmentType;
-    architecture: Architecture;
-    version?: PythonVersion;
-}
-
-class EnvironmentInfo implements IEnvironmentInfo {
-    public constructor(
-        public interpreterPath: string,
-        public interpreterType: InterpreterType,
-        public environmentType: EnvironmentType,
-        public architecture: Architecture,
-        public version?: PythonVersion
-    ) {}
-}
 
 export enum EnvironmentInfoServiceQueuePriority {
     Default,
@@ -56,31 +18,40 @@ export interface IEnvironmentInfoService {
     getEnvironmentInfo(
         interpreterPath: string,
         priority?: EnvironmentInfoServiceQueuePriority
-    ): Promise<IEnvironmentInfo | undefined>;
+    ): Promise<PythonEnvironment | undefined>;
 }
 
 export class EnvironmentInfoService implements IEnvironmentInfoService {
-    private readonly cache: Map<string, IEnvironmentInfo>;
-    public constructor(private readonly workerPool?: IWorkerPool<string, IEnvironmentInfo | undefined>) {
-        this.cache = new Map<string, IEnvironmentInfo>();
+    private readonly cache: Map<string, PythonEnvironment>;
+    public constructor(private readonly workerPool?: IWorkerPool<string, PythonEnvironment | undefined>) {
+        this.cache = new Map<string, PythonEnvironment>();
         if (!this.workerPool) {
-            this.workerPool = new WorkerPool<string, IEnvironmentInfo | undefined>(async (interpreterPath: string) => {
+            this.workerPool = new WorkerPool<string, PythonEnvironment | undefined>(async (interpreterPath: string) => {
                 const interpreterInfo = await getInterpreterInfo(buildPythonExecInfo(interpreterPath), shellExecute);
                 if (interpreterInfo && interpreterInfo.version) {
-                    return new EnvironmentInfo(
-                        interpreterPath,
-                        InterpreterType.cpython,
-                        EnvironmentType.Unknown, // This will be handled later
-                        interpreterInfo.architecture,
-                        {
+                    return {
+                        path: interpreterInfo.path,
+                        // Have to do this because the type returned by getInterpreterInfo is SemVer
+                        // But we expect this to be PythonVersion
+                        version: {
                             raw: interpreterInfo.version.raw,
                             major: interpreterInfo.version.major,
                             minor: interpreterInfo.version.minor,
                             patch: interpreterInfo.version.patch,
                             build: interpreterInfo.version.build,
                             prerelease: interpreterInfo.version.prerelease
-                        }
-                    );
+                        },
+                        sysVersion: interpreterInfo.sysVersion,
+                        architecture: interpreterInfo.architecture,
+                        sysPrefix: interpreterInfo.sysPrefix,
+                        pipEnvWorkspaceFolder: interpreterInfo.pipEnvWorkspaceFolder,
+                        companyDisplayName: '',
+                        displayName: '',
+                        envType: EnvironmentType.Unknown, // Code to handle This will be added later.
+                        envName: '',
+                        envPath: '',
+                        cachedEntry: false
+                    };
                 }
                 return undefined;
             });
@@ -90,7 +61,7 @@ export class EnvironmentInfoService implements IEnvironmentInfoService {
     public async getEnvironmentInfo(
         interpreterPath: string,
         priority?: EnvironmentInfoServiceQueuePriority
-    ): Promise<IEnvironmentInfo | undefined> {
+    ): Promise<PythonEnvironment | undefined> {
         let result = this.cache.get(interpreterPath);
         if (!result) {
             if (priority === EnvironmentInfoServiceQueuePriority.High) {
