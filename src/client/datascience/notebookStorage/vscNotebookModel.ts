f049ed4740ac93c047bfdda8d46bf5d7dcbcce7c
Don Jayamanne <don.jayamanne@yahoo.com>
2020-08-17 11:13:24 -0700

    Fixes

--- src/client/datascience/notebookStorage/vscNotebookModel.ts
+++ src/client/datascience/notebookStorage/vscNotebookModel.ts
@@ -101,7 +101,7 @@ export class VSCodeNotebookModel extends BaseNotebookModel {
                 return {
                     ...cell,
                     metadata
-                // tslint:disable-next-line: no-any (because ts-node sucks).
+                    // tslint:disable-next-line: no-any (because ts-node sucks).
                 } as any;
             });
         }
