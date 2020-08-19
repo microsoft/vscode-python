2a0d84921df4ae1764bec01e631eec33f3d142bb
Eric Snow <ericsnowcurrently@gmail.com>
2020-08-19 17:45:19 -0600

    Move the base info types to their own directory.

--- src/client/pythonEnvironments/base/info/index.ts
+++ src/client/pythonEnvironments/base/info/index.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { Architecture } from '../../common/utils/platform';
-import { Version, VersionInfo } from '../../common/utils/version';
+import { Architecture } from '../../../common/utils/platform';
+import { Version, VersionInfo } from '../../../common/utils/version';
 
 /**
  * IDs for the various supported Python environments.
