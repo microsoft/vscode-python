142274138fbd50f4466ee1adc1ae18b8c3c66753
Eric Snow <ericsnowcurrently@gmail.com>
2020-08-25 14:54:13 -0600

    Improve handling of abnormal versions.

--- src/client/common/utils/version.ts
+++ src/client/common/utils/version.ts
@@ -10,58 +10,133 @@ import * as semver from 'semver';
 
 /**
  * basic version information
+ *
+ * A normalized object will only have non-negative numbers, or `-1`,
+ * in its properties.  A `-1` value is an indicator that the property
+ * is not set.  Lower properties will not be set if a higher property
+ * is not.
+ *
+ * Note that any object can be forced to look like a VersionInfo and
+ * any of the properties may be forced to hold a non-number value.
+ * To resolve this situation, pass the object through
+ * `normalizeVersionInfo()` and then `validateVersionInfo()`.
  */
-export type VersionInfo = {
+export type BasicVersionInfo = {
     major: number;
     minor: number;
     micro: number;
+    // There is also a hidden `unnormalized` property.
+};
+
+function normalizeVersionPart(part: number): number {
+    // Any -1 values where the original is not a number are handled in validation.
+    if (typeof part === 'number') {
+        if (isNaN(part) || part < 0) {
+            // We leave this as a marker.
+            return -1;
+        }
+        return part;
+    }
+    if (typeof part === 'string') {
+        const parsed = parseInt((part as unknown) as string, 10);
+        if (isNaN(parsed) || parsed < 0) {
+            return -1;
+        }
+        return parsed;
+    }
+    if (part === undefined || part === null) {
+        return -1;
+    }
+    return -1;
+}
+
+type RawBasicVersionInfo = BasicVersionInfo & {
+    unnormalized?: {
+        // tslint:disable:no-any
+        major?: any;
+        minor?: any;
+        micro?: any;
+        // tslint:enable:no-any
+    };
 };
 
 /**
  * Make a copy and set all the properties properly.
  */
-export function normalizeVersionInfo(info: VersionInfo): VersionInfo {
-    const norm = { ...info };
-    if (!norm.major || norm.major < 0) {
-        norm.major = 0;
-    }
-    if (!norm.minor || norm.minor < 0) {
-        norm.minor = 0;
-    }
-    if (!norm.micro || norm.micro < 0) {
-        norm.micro = 0;
+export function normalizeBasicVersionInfo<T extends BasicVersionInfo>(info: T): T {
+    const norm: T = { ...info };
+    const raw = (norm as unknown) as RawBasicVersionInfo;
+    if (raw.unnormalized === undefined) {
+        raw.unnormalized = { ...norm };
+        norm.major = normalizeVersionPart(norm.major);
+        norm.minor = normalizeVersionPart(norm.minor);
+        norm.micro = normalizeVersionPart(norm.micro);
     }
     return norm;
+}
+
+function validateVersionPart(prop: string, part: number, unnormalized?: unknown) {
+    if (typeof part !== 'number' || isNaN(part)) {
+        throw Error(`invalid ${prop} version (not normalized)`);
+    }
+    if (part === 0 || part > 0) {
+        return;
+    }
+    if (typeof unnormalized === 'number') {
+        return;
+    }
+    if (unnormalized === undefined || unnormalized === null) {
+        return;
+    }
+    if (typeof unnormalized === 'string' && !isNaN(parseInt(unnormalized, 10))) {
+        return;
+    }
+    throw Error(`invalid ${prop} version (failed to normalize)`);
 }
 
 /**
  * Fail if any properties are not set properly.
  *
- * Optional properties that are not set are ignored.
- *
- * This assumes that the info has already been normalized.
+ * The info is expected to be normalized already.
  */
-export function validateVersionInfo(_info: VersionInfo) {
-    // For now we don't do any checks.
+export function validateBasicVersionInfo<T extends BasicVersionInfo>(info: T) {
+    const raw = (info as unknown) as RawBasicVersionInfo;
+    validateVersionPart('major', info.major, raw.unnormalized?.major);
+    validateVersionPart('minor', info.minor, raw.unnormalized?.minor);
+    validateVersionPart('micro', info.micro, raw.unnormalized?.micro);
+    if (info.major < 0) {
+        throw Error('missing major version');
+    }
+    if (info.minor < 0) {
+        if (info.micro === 0 || info.micro > 0) {
+            throw Error('missing minor version');
+        }
+    }
 }
 
 /**
  * Convert the info to a simple string.
+ *
+ * Any negative parts are ignored.
+ *
+ * The object is expected to be normalized.
  */
-export function getVersionString(ver: VersionInfo): string {
-    if (ver.major === undefined) {
+export function getVersionString<T extends BasicVersionInfo>(info: T): string {
+    if (typeof info.major !== 'number' || typeof info.minor !== 'number' || typeof info.micro !== 'number') {
         return '';
-    } else if (ver.minor === undefined) {
-        return `${ver.major}`;
     }
-    if (ver.micro === undefined) {
-        return `${ver.major}.${ver.minor}`;
+    if (info.major < 0) {
+        return '';
+    } else if (info.minor < 0) {
+        return `${info.major}`;
+    } else if (info.micro < 0) {
+        return `${info.major}.${info.minor}`;
     }
-    return `${ver.major}.${ver.minor}.${ver.micro}`;
+    return `${info.major}.${info.minor}.${info.micro}`;
 }
 
-export type ParseResult = {
-    version: VersionInfo;
+export type ParseResult<T extends BasicVersionInfo = BasicVersionInfo> = {
+    version: T;
     before: string;
     after: string;
 };
@@ -72,7 +147,7 @@ export type ParseResult = {
  * If the version is surrounded by other text then that is provided
  * as well.
  */
-export function parseVersionInfo(verStr: string): ParseResult | undefined {
+export function parseBasicVersionInfo<T extends BasicVersionInfo>(verStr: string): ParseResult<T> | undefined {
     // ^
     // (.*?)
     // (\d+)
@@ -108,14 +183,11 @@ export function parseVersionInfo(verStr: string): ParseResult | undefined {
         }
     }
     const major = parseInt(majorStr, 10);
-    const minor = minorStr ? parseInt(minorStr, 10) : undefined;
-    const micro = microStr ? parseInt(microStr, 10) : undefined;
+    const minor = minorStr ? parseInt(minorStr, 10) : -1;
+    const micro = microStr ? parseInt(microStr, 10) : -1;
     return {
-        version: {
-            major,
-            minor: (minor as unknown) as number,
-            micro: (micro as unknown) as number
-        },
+        // This is effectively normalized.
+        version: ({ major, minor, micro } as unknown) as T,
         before: before || '',
         after: after || ''
     };
@@ -123,28 +195,33 @@ export function parseVersionInfo(verStr: string): ParseResult | undefined {
 
 /**
  * Returns true if the given version appears to be not set.
+ *
+ * The object is expected to already be normalized.
  */
-export function isVersionEmpty(ver: VersionInfo): boolean {
-    return ver.major === 0 && ver.minor === 0 && ver.micro === 0;
+export function isVersionInfoEmpty<T extends BasicVersionInfo>(info: T): boolean {
+    if (typeof info.major !== 'number' || typeof info.minor !== 'number' || typeof info.micro !== 'number') {
+        return false;
+    }
+    return info.major < 0 && info.minor < 0 && info.micro < 0;
 }
 
 //===========================
-// basic version
+// base version info
 
 /**
  * basic version information
  *
  * @prop raw - the unparsed version string, if any
  */
-export type Version = VersionInfo & {
+export type VersionInfo = BasicVersionInfo & {
     raw?: string;
 };
 
 /**
  * Make a copy and set all the properties properly.
  */
-export function normalizeVersion(info: Version): Version {
-    const norm = { ...info, ...normalizeVersionInfo(info) };
+export function normalizeVersionInfo<T extends VersionInfo>(info: T): T {
+    const norm = { ...info, ...normalizeBasicVersionInfo(info) };
     if (!norm.raw) {
         norm.raw = '';
     }
@@ -158,9 +235,24 @@ export function normalizeVersion(info: Version): Version {
  *
  * This assumes that the info has already been normalized.
  */
-export function validateVersion(info: Version) {
-    validateVersionInfo(info);
+export function validateVersionInfo<T extends VersionInfo>(info: T) {
+    validateBasicVersionInfo(info);
     // `info.raw` can be anything.
+}
+
+/**
+ * Extract a version from the given text.
+ *
+ * If the version is surrounded by other text then that is provided
+ * as well.
+ */
+export function parseVersionInfo<T extends VersionInfo>(verStr: string): ParseResult<T> | undefined {
+    const result = parseBasicVersionInfo<T>(verStr);
+    if (result === undefined) {
+        return undefined;
+    }
+    result.version.raw = verStr;
+    return result;
 }
 
 //===========================
--- src/test/common/utils/version.unit.test.ts
+++ src/test/common/utils/version.unit.test.ts
@@ -3,57 +3,307 @@
 
 import * as assert from 'assert';
 
-import { ParseResult, parseVersionInfo } from '../../../client/common/utils/version';
+import {
+    getVersionString,
+    isVersionInfoEmpty,
+    normalizeVersionInfo,
+    ParseResult,
+    parseVersionInfo,
+    validateVersionInfo,
+    VersionInfo
+} from '../../../client/common/utils/version';
+
+const NOT_USED = {};
+
+function ver(
+    // tslint:disable:no-any
+    major: any,
+    minor: any = NOT_USED,
+    micro: any = NOT_USED,
+    // tslint:enable:no-any
+    unnormalized?: VersionInfo
+): VersionInfo {
+    if (minor === NOT_USED) {
+        minor = -1;
+    }
+    if (micro === NOT_USED) {
+        micro = -1;
+    }
+    const info = {
+        major: (major as unknown) as number,
+        minor: (minor as unknown) as number,
+        micro: (micro as unknown) as number,
+        raw: undefined
+    };
+    if (unnormalized !== undefined) {
+        // tslint:disable-next-line:no-any
+        ((info as unknown) as any).unnormalized = unnormalized;
+    }
+    return info;
+}
 
 function res(
+    // These go into the VersionInfo:
     major: number,
-    minor: number | undefined,
-    micro: number | undefined,
+    minor: number,
+    micro: number,
+    // These are the remainder of the ParseResult:
     before: string,
     after: string
-): ParseResult {
+): ParseResult<VersionInfo> {
     return {
         before,
         after,
-        version: {
-            major,
-            minor: (minor as unknown) as number,
-            micro: (micro as unknown) as number
-        }
+        version: ver(major, minor, micro)
     };
 }
 
-const VERSIONS: [string, ParseResult][] = [
-    // plain
-    ['2.7.0', res(2, 7, 0, '', '')],
-    ['2.7', res(2, 7, undefined, '', '')],
-    ['02.7', res(2, 7, undefined, '', '')],
-    ['2.07', res(2, 7, undefined, '', '')],
-    ['2.7.01', res(2, 7, 1, '', '')],
-    ['2.7.11', res(2, 7, 11, '', '')],
-    ['3.11.1', res(3, 11, 1, '', '')],
-    ['0.0.0', res(0, 0, 0, '', '')],
-    // with before/after
-    [' 2.7.9 ', res(2, 7, 9, ' ', ' ')],
-    ['2.7.9-3.2.7', res(2, 7, 9, '', '-3.2.7')],
-    ['python2.7.exe', res(2, 7, undefined, 'python', '.exe')],
-    ['1.2.3.4.5-x2.2', res(1, 2, 3, '', '.4.5-x2.2')],
-    ['3.8.1a2', res(3, 8, 1, '', 'a2')],
-    ['3.8.1-alpha2', res(3, 8, 1, '', '-alpha2')],
-    [
-        '3.7.5 (default, Nov  7 2019, 10:50:52) \\n[GCC 8.3.0]',
-        res(3, 7, 5, '', ' (default, Nov  7 2019, 10:50:52) \\n[GCC 8.3.0]')
-    ],
-    ['2', res(2, undefined, undefined, '', '')],
-    ['python2', res(2, undefined, undefined, 'python', '')],
-    // without the "before" the following won't match.
-    ['python2.a', res(2, undefined, undefined, 'python', '.a')],
-    ['python2.b7', res(2, undefined, undefined, 'python', '.b7')]
+const VERSIONS: [VersionInfo, string][] = [
+    [ver(2, 7, 0), '2.7.0'],
+    [ver(2, 7, -1), '2.7'],
+    [ver(2, -1, -1), '2'],
+    [ver(-1, -1, -1), ''],
+    [ver(2, 7, 11), '2.7.11'],
+    [ver(3, 11, 1), '3.11.1'],
+    [ver(0, 0, 0), '0.0.0']
 ];
+const INVALID: VersionInfo[] = [
+    ver(undefined, undefined, undefined),
+    ver(null, null, null),
+    ver({}, {}, {}),
+    //ver('-1', '-1', '-1'),
+    ver('x', 'y', 'z')
+];
+
+suite('common utils - getVersionString', () => {
+    suite('valid', () => {
+        VERSIONS.forEach((data) => {
+            const [info, expected] = data;
+            test(`${expected}`, () => {
+                const result = getVersionString(info);
+
+                assert.equal(result, expected);
+            });
+        });
+    });
+
+    suite('invalid', () => {
+        INVALID.forEach((info) => {
+            test(`[${info.major}, ${info.minor}, ${info.micro}]`, () => {
+                const result = getVersionString(info);
+
+                assert.equal(result, '');
+            });
+        });
+    });
+});
+
+suite('common utils - isVersionEmpty', () => {
+    [
+        ver(-1, -1, -1),
+        // normalization failed:
+        ver(-1, -1, -1, ver(null, null, null)),
+        // not normalized by still empty
+        ver(-10, -10, -10)
+    ].forEach((data: VersionInfo) => {
+        const info = data;
+        test(`empty: ${info}`, () => {
+            const result = isVersionInfoEmpty(info);
+
+            assert.ok(result);
+        });
+    });
+
+    [
+        // clearly not empty:
+        ver(3, 4, 5),
+        ver(3, 4, -1),
+        ver(3, -1, -1),
+        // 0 is not empty:
+        ver(0, 0, 0),
+        ver(0, 0, -1),
+        ver(0, -1, -1)
+    ].forEach((data: VersionInfo) => {
+        const info = data;
+        test(`not empty: ${info.major}.${info.minor}.${info.micro}`, () => {
+            const result = isVersionInfoEmpty(info);
+
+            assert.equal(result, false);
+        });
+    });
+
+    INVALID.forEach((data: VersionInfo) => {
+        const info = data;
+        test(`bogus: ${info.major}`, () => {
+            const result = isVersionInfoEmpty(info);
+
+            assert.equal(result, false);
+        });
+    });
+});
+
+suite('common utils - normalizeVersionInfo', () => {
+    test(`noop`, () => {
+        const info = ver(1, 2, 3);
+        info.raw = '1.2.3';
+        // tslint:disable-next-line:no-any
+        ((info as unknown) as any).unnormalized = { ...info };
+        const expected = info;
+
+        const normalized = normalizeVersionInfo(info);
+
+        assert.deepEqual(normalized, expected);
+    });
+
+    test(`same`, () => {
+        const info = ver(1, 2, 3);
+        info.raw = '1.2.3';
+        // tslint:disable-next-line:no-any
+        const expected: any = { ...info };
+        expected.unnormalized = { ...info };
+
+        const normalized = normalizeVersionInfo(info);
+
+        assert.deepEqual(normalized, expected);
+    });
+
+    test(`NaN`, () => {
+        const info = ver(NaN, 2, 3);
+        info.raw = '';
+        // tslint:disable-next-line:no-any
+        const expected: any = { ...info };
+        expected.major = -1;
+        expected.unnormalized = { ...info };
+
+        const normalized = normalizeVersionInfo(info);
+
+        // tslint:disable-next-line:no-any
+        const raw = (normalized as unknown) as any;
+        assert.ok(isNaN(raw.unnormalized?.major));
+        raw.unnormalized.major = 1;
+        expected.unnormalized.major = 1;
+        assert.deepEqual(normalized, expected);
+    });
+
+    test('valid', () => {
+        const info = ver(3, -1, -10);
+        // tslint:disable-next-line:no-any
+        const expected: any = ver(3, -1, -1);
+        expected.raw = '';
+        expected.unnormalized = { ...info };
+
+        const normalized = normalizeVersionInfo(info);
+
+        assert.deepEqual(normalized, expected);
+    });
+
+    test('empty', () => {
+        const info = ver(-1, -5, -10);
+        // tslint:disable-next-line:no-any
+        const expected: any = ver(-1, -1, -1);
+        expected.raw = '';
+        expected.unnormalized = { ...info };
+
+        const normalized = normalizeVersionInfo(info);
+
+        assert.deepEqual(normalized, expected);
+    });
+
+    [
+        [ver(3, null, undefined), ver(3, -1, -1)],
+        [ver('', 4, '5'), ver(-1, 4, 5)],
+        [ver({}, [], 5), ver(-1, -1, 5)]
+    ].forEach((data) => {
+        const [info, expected] = data;
+        // tslint:disable-next-line:no-any
+        ((expected as unknown) as any).unnormalized = info;
+        expected.raw = '';
+        test(`partially invalid: [${info.major}, ${info.minor}, ${info.micro}]`, () => {
+            const normalized = normalizeVersionInfo(info);
+
+            assert.deepEqual(normalized, expected);
+        });
+    });
+});
+
+suite('common utils - validateVersionInfo', () => {
+    suite('valid', () => {
+        [
+            ver(3, 4, 5),
+            ver(3, 4, -1),
+            ver(3, -1, -1),
+            // unnormalized but still valid:
+            ver(3, -7, -11)
+        ].forEach((info) => {
+            test(`as-is: [${info.major}, ${info.minor}, ${info.micro}]`, () => {
+                validateVersionInfo(info);
+            });
+        });
+
+        [
+            // Each of these normalizes to -1:
+            NaN,
+            '-1',
+            '-10',
+            -11,
+            undefined,
+            null
+        ].forEach((value) => {
+            const raw = ver(3, 4, value);
+            const info = ver(3, 4, -1, raw);
+            test(`normalization worked: [${raw.major}, ${raw.minor}, ${raw.micro}]`, () => {
+                validateVersionInfo(info);
+            });
+        });
+    });
+
+    suite('invalid', () => {
+        [
+            // missing major:
+            ver(-1, -1, -1),
+            ver(-1, -1, 5),
+            ver(-1, 4, -1),
+            ver(-1, 4, 5),
+            // missing minor:
+            ver(3, -1, 5)
+        ].forEach((info) => {
+            test(`missing parts: [${info.major}.${info.minor}.${info.micro}]`, () => {
+                assert.throws(() => validateVersionInfo(info));
+            });
+        });
+
+        [
+            // parseInt() -> NaN:
+            '',
+            ' ',
+            '\n',
+            'foo',
+            // no equivalent number and no special-case:
+            {},
+            []
+        ].forEach((value) => {
+            const raw = ver(3, 4, value);
+            const info = ver(3, 4, -1, raw);
+            test(`normalization failed: [${raw.major}, ${raw.minor}, ${raw.micro}]`, () => {
+                assert.throws(() => validateVersionInfo(info));
+            });
+        });
+
+        [
+            ...INVALID,
+            // extra cases:
+            ver(NaN, 4, 5)
+        ].forEach((info) => {
+            test(`bogus: [${info.major}, ${info.minor}, ${info.micro}]`, () => {
+                assert.throws(() => validateVersionInfo(info));
+            });
+        });
+    });
+});
 
 suite('common utils - parseVersionInfo', () => {
     suite('invalid versions', () => {
-        const INVALID = [
+        const BOGUS = [
             // Note that some of these are *almost* valid.
             '2.',
             '.2',
@@ -65,7 +315,7 @@ suite('common utils - parseVersionInfo', () => {
             '2.7rc1',
             ''
         ];
-        for (const verStr of INVALID) {
+        for (const verStr of BOGUS) {
             test(`invalid - '${verStr}'`, () => {
                 const result = parseVersionInfo(verStr);
 
@@ -74,12 +324,40 @@ suite('common utils - parseVersionInfo', () => {
         }
     });
 
-    for (const info of VERSIONS) {
-        const [verStr, expected] = info;
-        test(`valid - '${verStr}'`, () => {
-            const result = parseVersionInfo(verStr);
+    suite('valid versions', () => {
+        ([
+            // plain
+            ...VERSIONS.map(([v, s]) => [s, { version: v, before: '', after: '' }]),
+            ['02.7', res(2, 7, -1, '', '')],
+            ['2.07', res(2, 7, -1, '', '')],
+            ['2.7.01', res(2, 7, 1, '', '')],
+            // with before/after
+            [' 2.7.9 ', res(2, 7, 9, ' ', ' ')],
+            ['2.7.9-3.2.7', res(2, 7, 9, '', '-3.2.7')],
+            ['python2.7.exe', res(2, 7, -1, 'python', '.exe')],
+            ['1.2.3.4.5-x2.2', res(1, 2, 3, '', '.4.5-x2.2')],
+            ['3.8.1a2', res(3, 8, 1, '', 'a2')],
+            ['3.8.1-alpha2', res(3, 8, 1, '', '-alpha2')],
+            [
+                '3.7.5 (default, Nov  7 2019, 10:50:52) \\n[GCC 8.3.0]',
+                res(3, 7, 5, '', ' (default, Nov  7 2019, 10:50:52) \\n[GCC 8.3.0]')
+            ],
+            ['python2', res(2, -1, -1, 'python', '')],
+            // without the "before" the following won't match.
+            ['python2.a', res(2, -1, -1, 'python', '.a')],
+            ['python2.b7', res(2, -1, -1, 'python', '.b7')]
+        ] as [string, ParseResult<VersionInfo>][]).forEach((data) => {
+            const [verStr, result] = data;
+            if (verStr === '') {
+                return;
+            }
+            const expected = { ...result, version: { ...result.version } };
+            expected.version.raw = verStr;
+            test(`valid - '${verStr}'`, () => {
+                const parsed = parseVersionInfo(verStr);
 
-            assert.deepEqual(result, expected);
+                assert.deepEqual(parsed, expected);
+            });
         });
-    }
+    });
 });
