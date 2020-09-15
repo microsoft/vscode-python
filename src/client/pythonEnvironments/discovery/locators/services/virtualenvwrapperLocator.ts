26df20c63a868ecb63b75a8baf1643aec370019d
Kim-Adeline Miguel <kimiguel@microsoft.com>
2020-09-15 10:18:00 -0700

    Change how the locator works + move util func out

--- src/client/pythonEnvironments/discovery/locators/services/virtualenvwrapperLocator.ts
+++ src/client/pythonEnvironments/discovery/locators/services/virtualenvwrapperLocator.ts
@@ -3,34 +3,27 @@
 
 import * as path from 'path';
 import {
-    getEnvironmentVariable, getOSType, getUserHomeDir, OSType,
+    getEnvironmentVariable, getOSType, OSType,
 } from '../../../../common/utils/platform';
 import { pathExists } from '../../../common/externalDependencies';
-
-export function getDefaultVirtualenvwrapperDir(): string {
-    const homeDir = getUserHomeDir() || '';
-
-    // In Windows, the default path for WORKON_HOME is %USERPROFILE%\Envs.
-    if (getOSType() === OSType.Windows) {
-        return path.join(homeDir, 'Envs');
-    }
-    return path.join(homeDir, '.virtualenvs');
-}
+import { getDefaultVirtualenvwrapperDir } from '../../../common/virtualenvwrapperUtils';
 
 /**
  * Checks if the given interpreter belongs to a virtualenvWrapper based environment.
  * @param {string} interpreterPath: Absolute path to the python interpreter.
- * @returns {boolean} : Returns true if the interpreter belongs to a virtualenvWrapper environment.
+ * @returns {boolean}: Returns true if the interpreter belongs to a virtualenvWrapper environment.
  */
 export async function isVirtualenvwrapperEnvironment(interpreterPath:string): Promise<boolean> {
     // The WORKON_HOME variable contains the path to the root directory of all virtualenvwrapper environments.
     // If the interpreter path belongs to one of them then it is a virtualenvwrapper type of environment.
+    const workonHomeDir = getEnvironmentVariable('WORKON_HOME') || getDefaultVirtualenvwrapperDir();
+    const environmentName = path.basename(path.dirname(path.dirname(interpreterPath)));
 
-    const workonHomeFolder = getEnvironmentVariable('WORKON_HOME') || getDefaultVirtualenvwrapperDir();
+    let environmentDir = path.join(workonHomeDir, environmentName);
 
-    if (!await pathExists(workonHomeFolder)) {
-        return false;
+    if (getOSType() === OSType.Windows) {
+        environmentDir = environmentDir.toUpperCase();
     }
 
-    return interpreterPath.toUpperCase().startsWith(workonHomeFolder.toUpperCase());
+    return await pathExists(environmentDir) && interpreterPath.startsWith(`${environmentDir}${path.sep}`);
 }
