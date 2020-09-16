c04c6f51e6a7bfa2201bbfbc74509e118d911e61
Eric Snow <ericsnowcurrently@gmail.com>
2020-09-16 15:33:39 -0600

    Handle iterator.onUpdated in ComponentAdapter.getInterpreters().

--- src/client/pythonEnvironments/legacyIOC.ts
+++ src/client/pythonEnvironments/legacyIOC.ts
@@ -3,6 +3,7 @@
 
 import { injectable } from 'inversify';
 import * as vscode from 'vscode';
+import { createDeferred } from '../common/utils/async';
 import { Architecture } from '../common/utils/platform';
 import { getVersionString, parseVersion } from '../common/utils/version';
 import {
@@ -293,14 +294,39 @@ class ComponentAdapter implements IComponentAdapter {
             }
         }
 
+        const deferred = createDeferred<PythonEnvironment[]>();
         const envs: PythonEnvironment[] = [];
+        const executableToLegacy: Record<string, PythonEnvironment> = {};
         const iterator = this.api.iterEnvs(query);
+
+        if (iterator.onUpdated !== undefined) {
+            iterator.onUpdated((event) => {
+                if (event === null) {
+                    deferred.resolve(envs);
+                } else {
+                    // Replace the old one.
+                    const old = executableToLegacy[event.old.executable.filename];
+                    if (old !== undefined) {
+                        const index = envs.indexOf(old);
+                        if (index !== -1) {
+                            envs[index] = convertEnvInfo(event.new);
+                        }
+                    }
+                }
+            });
+        } else {
+            deferred.resolve(envs);
+        }
+
         let res = await iterator.next();
         while (!res.done) {
-            envs.push(convertEnvInfo(res.value));
+            const env = convertEnvInfo(res.value);
+            envs.push(env);
+            executableToLegacy[env.path] = env;
             res = await iterator.next(); // eslint-disable-line no-await-in-loop
         }
-        return envs;
+
+        return deferred.promise;
     }
 }
 
