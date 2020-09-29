c383b8cabc7754e424f66b47e0e350dc32ccfa3d
Eric Snow <ericsnowcurrently@gmail.com>
2020-09-28 19:10:25 -0600

    Fix areSimilarVersions() and areIdenticalVersion().

--- src/client/pythonEnvironments/base/info/pythonVersion.ts
+++ src/client/pythonEnvironments/base/info/pythonVersion.ts
@@ -2,13 +2,15 @@
 // Licensed under the MIT License.
 
 import * as path from 'path';
-import { PythonReleaseLevel, PythonVersion, UNKNOWN_PYTHON_VERSION } from '.';
+import { isEqual as areDeepEqual } from 'lodash';
 import { traceError } from '../../../common/logger';
 import {
     EMPTY_VERSION,
     isVersionInfoEmpty,
     parseBasicVersionInfo,
 } from '../../../common/utils/version';
+
+import { PythonReleaseLevel, PythonVersion, UNKNOWN_PYTHON_VERSION } from '.';
 
 export function getPythonVersionFromPath(exe:string): PythonVersion {
     let version = UNKNOWN_PYTHON_VERSION;
@@ -124,8 +126,8 @@ export function isVersionEmpty(version: PythonVersion): boolean {
  * @param {PythonVersion} right
  * @returns {boolean}
  */
-export function areEqualVersions(left: PythonVersion, right:PythonVersion): boolean {
-    return left === right;
+export function areIdenticalVersion(left: PythonVersion, right: PythonVersion): boolean {
+    return areDeepEqual(left, right);
 }
 
 /**
@@ -136,15 +138,31 @@ export function areEqualVersions(left: PythonVersion, right:PythonVersion): bool
  * @param {PythonVersion} right
  * @returns {boolean}
  */
-export function areEquivalentVersions(left: PythonVersion, right:PythonVersion): boolean {
+export function areSimilarVersions(left: PythonVersion, right: PythonVersion): boolean {
     if (left.major === 2 && right.major === 2) {
-        // We are going to assume that if the major version is 2 then the version is 2.7
-        return true;
+        if (left.minor === -1 || right.minor === -1) {
+            // We are going to assume that if the major version is 2 then the version is 2.7
+            return true;
+        }
     }
 
-    // In the case of 3.* if major and minor match we assume that they are equivalent versions
-    return (
-        left.major === right.major
-        && left.minor === right.minor
-    );
+    if (left.major !== right.major) {
+        return false;
+    }
+    if (left.minor === -1 || right.minor === -1) {
+        return true;
+    }
+    if (left.minor !== right.minor) {
+        return false;
+    }
+    if (left.micro === -1 || right.micro === -1) {
+        return true;
+    }
+    if (left.micro !== right.micro) {
+        return false;
+    }
+    if (left.release === undefined || right.release === undefined) {
+        return true;
+    }
+    return areDeepEqual(left.release, right.release);
 }
