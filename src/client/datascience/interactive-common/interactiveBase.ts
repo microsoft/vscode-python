97183b93b2372461438ae4c2cdb45a2dfb548448
rchiodo <rchiodo@users.noreply.github.com>
2020-10-09 16:15:07 -0700

    Try without webview logging as this makes the test pass

--- src/client/datascience/interactive-common/interactiveBase.ts
+++ src/client/datascience/interactive-common/interactiveBase.ts
@@ -762,6 +762,7 @@ export abstract class InteractiveBase extends WebviewPanelHost<IInteractiveWindo
         this.postMessageToListeners(type.toString(), payload);
 
         // Then send it to the webview
+        traceInfo(`Window ${this._id} is posting ${type} ${JSON.stringify(payload)} to Webview UI`);
         return super.postMessage(type, payload);
     }
 
