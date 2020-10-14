5697e17f1ff7cb111fa7d49d98e82b640d22c44d
Karthik Nadig <kanadig@microsoft.com>
2020-10-14 13:26:20 -0700

    Add tests

--- src/client/pythonEnvironments/common/commonUtils.ts
+++ src/client/pythonEnvironments/common/commonUtils.ts
@@ -3,20 +3,24 @@
 
 import * as fsapi from 'fs-extra';
 import * as path from 'path';
+import { getOSType, OSType } from '../../common/utils/platform';
 import { isPosixPythonBin } from './posixUtils';
 import { isWindowsPythonExe } from './windowsUtils';
 
 export async function findInterpretersInDir(root:string, recurseLevels?:number):Promise<string[]> {
-    const interpreters:string[] = [];
+    let interpreters:string[] = [];
     const dirContents = (await fsapi.readdir(root)).map((c) => path.join(root, c));
+    const os = getOSType();
 
     for (const item of dirContents) {
         const stat = await fsapi.lstat(item);
         if (stat.isDirectory()) {
             if (recurseLevels && recurseLevels > 0) {
-                interpreters.concat(await findInterpretersInDir(item, recurseLevels - 1));
+                interpreters = interpreters.concat(await findInterpretersInDir(item, recurseLevels - 1));
             }
-        } else if (isWindowsPythonExe(item) || isPosixPythonBin(item)) {
+        } else if (os === OSType.Windows && isWindowsPythonExe(item)) {
+            interpreters.push(item);
+        } else if (os !== OSType.Windows && isPosixPythonBin(item)) {
             interpreters.push(item);
         }
     }
--- src/test/pythonEnvironments/discovery/locators/virtualenvwrapperLocator.unit.test.ts
+++ src/test/pythonEnvironments/discovery/locators/virtualenvwrapperLocator.unit.test.ts
@@ -5,10 +5,14 @@ import * as assert from 'assert';
 import * as path from 'path';
 import * as sinon from 'sinon';
 import * as platformUtils from '../../../../client/common/utils/platform';
+import { PythonEnvInfo, PythonEnvKind, UNKNOWN_PYTHON_VERSION } from '../../../../client/pythonEnvironments/base/info';
+import { getEnvs } from '../../../../client/pythonEnvironments/base/locatorUtils';
 import * as fileUtils from '../../../../client/pythonEnvironments/common/externalDependencies';
-import { isVirtualenvwrapperEnvironment } from '../../../../client/pythonEnvironments/discovery/locators/services/virtualenvwrapperLocator';
+import { isVirtualenvwrapperEnvironment, VirtualEnvWrapperLocator } from '../../../../client/pythonEnvironments/discovery/locators/services/virtualenvwrapperLocator';
+import { TEST_LAYOUT_ROOT } from '../../common/commonTestConstants';
+import { assertEnvEqual, assertEnvsEqual } from './envTestUtils';
 
-suite('Virtualenvwrapper Locator Tests', () => {
+suite('isVirtualenvwrapperEnvironment', () => {
     const envDirectory = 'myenv';
     const homeDir = path.join('path', 'to', 'home');
     const envRootDirectory = '.virtualenvs';
@@ -59,5 +63,122 @@ suite('Virtualenvwrapper Locator Tests', () => {
         getEnvVariableStub.withArgs('WORKON_HOME').returns(workonHomeDirectory);
 
         assert.deepStrictEqual(await isVirtualenvwrapperEnvironment(interpreter), false);
+    });
+});
+
+suite('VirtualEnvWrapper Locator', () => {
+    const testVirtualEnvWrapperRoot = path.join(TEST_LAYOUT_ROOT, 'workOnHome');
+    const testVirtualEnvsPath = path.join(testVirtualEnvWrapperRoot, '.virtualenvs');
+    let getEnvVariableStub: sinon.SinonStub;
+    let getUserHomeDirStub: sinon.SinonStub;
+    let getOSTypeStub: sinon.SinonStub;
+
+    function createExpectedEnvInfo(interpreterPath:string) {
+        return {
+            name: '',
+            location: '',
+            kind: PythonEnvKind.VirtualEnvWrapper,
+            executable: {
+                filename: interpreterPath,
+                sysPrefix: '',
+                ctime: -1,
+                mtime: -1,
+            },
+            version: UNKNOWN_PYTHON_VERSION,
+            arch: platformUtils.Architecture.Unknown,
+            distro: { org: '' },
+        };
+    }
+
+    setup(() => {
+        getEnvVariableStub = sinon.stub(platformUtils, 'getEnvironmentVariable');
+        getEnvVariableStub.withArgs('WORKON_HOME').returns(path.join(testVirtualEnvWrapperRoot, '.virtualenvs'));
+
+        getUserHomeDirStub = sinon.stub(platformUtils, 'getUserHomeDir');
+        getUserHomeDirStub.returns(testVirtualEnvWrapperRoot);
+
+        getOSTypeStub = sinon.stub(platformUtils, 'getOSType');
+        getOSTypeStub.returns(platformUtils.OSType.Linux);
+    });
+
+    teardown(() => {
+        getEnvVariableStub.restore();
+        getUserHomeDirStub.restore();
+        getOSTypeStub.restore();
+    });
+
+    test('iterEnvs(): Windows', async () => {
+        getOSTypeStub.returns(platformUtils.OSType.Windows);
+
+        const expectedEnvs = [
+            createExpectedEnvInfo(path.join(testVirtualEnvsPath, 'env1', 'python.exe')),
+            createExpectedEnvInfo(path.join(testVirtualEnvsPath, 'env2', 'Scripts', 'python.exe')),
+        ].sort((a, b) => a.executable.filename.localeCompare(b.executable.filename));
+
+        const locator = new VirtualEnvWrapperLocator();
+        const iterator = locator.iterEnvs();
+        const actualEnvs = (await getEnvs(iterator))
+            .sort((a, b) => a.executable.filename.localeCompare(b.executable.filename));
+
+        assertEnvsEqual(actualEnvs, expectedEnvs);
+    });
+
+    test('iterEnvs(): Non-Windows', async () => {
+        const expectedEnvs = [
+            createExpectedEnvInfo(path.join(testVirtualEnvsPath, 'env3', 'python')),
+            createExpectedEnvInfo(path.join(testVirtualEnvsPath, 'env4', 'bin', 'python')),
+        ].sort((a, b) => a.executable.filename.localeCompare(b.executable.filename));
+
+        const locator = new VirtualEnvWrapperLocator();
+        const iterator = locator.iterEnvs();
+        const actualEnvs = (await getEnvs(iterator))
+            .sort((a, b) => a.executable.filename.localeCompare(b.executable.filename));
+
+        assertEnvsEqual(actualEnvs, expectedEnvs);
+    });
+
+    test('resolveEnv(string)', async () => {
+        const interpreterPath = path.join(testVirtualEnvsPath, 'env4', 'bin', 'python');
+        const expected = createExpectedEnvInfo(interpreterPath);
+
+        const locator = new VirtualEnvWrapperLocator();
+        const actual = await locator.resolveEnv(interpreterPath);
+
+        assertEnvEqual(actual, expected);
+    });
+
+    test('resolveEnv(string)', async () => {
+        const interpreterPath = path.join(testVirtualEnvsPath, 'env4', 'bin', 'python');
+        const expected = createExpectedEnvInfo(interpreterPath);
+
+        // Partially filled in env info object
+        const input:PythonEnvInfo = {
+            name: '',
+            location: '',
+            kind: PythonEnvKind.Unknown,
+            distro: { org: '' },
+            arch: platformUtils.Architecture.Unknown,
+            executable: {
+                filename: interpreterPath,
+                sysPrefix: '',
+                ctime: -1,
+                mtime: -1,
+            },
+            version: UNKNOWN_PYTHON_VERSION,
+        };
+
+        const locator = new VirtualEnvWrapperLocator();
+        const actual = await locator.resolveEnv(input);
+
+        assertEnvEqual(actual, expected);
+    });
+
+    test('resolveEnv(string): not VirtualEnvWrapper based environment', async () => {
+        const interpreterPath = path.join('some', 'random', 'env', 'bin', 'python');
+
+        const locator = new VirtualEnvWrapperLocator();
+        const actual = await locator.resolveEnv(interpreterPath);
+
+        assert.deepStrictEqual(actual, undefined);
     });
 });
