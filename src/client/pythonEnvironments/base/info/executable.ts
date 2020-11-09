b179511566cee35eda14632c21cc87b9c17c8a96
Eric Snow <ericsnowcurrently@gmail.com>
2020-11-09 14:21:10 -0700

    Look for the best version in the full executable filename.

--- src/client/pythonEnvironments/base/info/executable.ts
+++ src/client/pythonEnvironments/base/info/executable.ts
@@ -4,7 +4,11 @@
 import * as path from 'path';
 import { traceError } from '../../../common/logger';
 import { getOSType, OSType } from '../../../common/utils/platform';
-import { getEmptyVersion, parseVersion } from './pythonVersion';
+import {
+    getEmptyVersion,
+    isBetterVersion,
+    parseVersion,
+} from './pythonVersion';
 
 import { PythonVersion } from '.';
 
@@ -19,13 +23,18 @@ export function parseExeVersion(
 ): PythonVersion {
     let version: PythonVersion;
     try {
-        version = parseBasename(path.basename(filename));
+        version = walkExecutablePath(filename);
     } catch (err) {
         if (opts.ignoreErrors) {
             traceError(`failed to parse version from "${filename}"`, err);
             return getEmptyVersion();
         }
         throw err; // re-throw
+    }
+
+    if (version.major === -1 && getOSType() !== OSType.Windows) {
+        // We can assume it is 2.7.  (See PEP 394.)
+        return parseVersion('2.7');
     }
 
     if (version.major === 2 && version.minor === -1) {
@@ -42,12 +51,54 @@ function parseBasename(basename: string): PythonVersion {
             return getEmptyVersion();
         }
     } else if (basename === 'python') {
-        // We can assume it is 2.7.  (See PEP 394.)
-        return parseVersion('2.7');
+        // We can assume 2.7 if a version doesn't show up elsewhere
+        // in the full executable filename.
+        return getEmptyVersion();
     }
     if (!basename.startsWith('python')) {
         throw Error(`not a Python executable (expected "python..", got "${basename}")`);
     }
     // If we reach here then we expect it to have a version in the name.
     return parseVersion(basename);
+}
+
+function walkExecutablePath(filename: string): PythonVersion {
+    let best = parseBasename(path.basename(filename));
+    if (best.release !== undefined) {
+        return best;
+    }
+
+    let prev = '';
+    let dirname = path.dirname(filename);
+    while (dirname !== '' && dirname !== prev) {
+        prev = dirname;
+        const basename = path.basename(dirname);
+        dirname = path.dirname(dirname);
+        // We don't worry about checking for a "python" prefix.
+
+        let current: PythonVersion | undefined;
+        try {
+            current = parseVersion(basename);
+        } catch {
+            // The path segment did not look like a version.
+        }
+        if (current !== undefined) {
+            // We should use areSimilarVersions() here instead once we have it.
+            const better = isBetterVersion(current, best);
+            if (better === undefined) {
+                // They are not similar versions.
+                // We prioritize the previously found version.
+                break;
+            }
+            if (better) {
+                best = current;
+                if (current.release !== undefined) {
+                    // It can't get better.
+                    break;
+                }
+            }
+        }
+    }
+
+    return best;
 }
