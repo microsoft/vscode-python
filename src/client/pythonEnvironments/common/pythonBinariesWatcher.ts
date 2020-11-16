2cec7a59f3ed355b53bc6d8b162541ee34e81796
Eric Snow <ericsnowcurrently@gmail.com>
2020-11-16 13:32:39 -0700

    lint

--- src/client/pythonEnvironments/common/pythonBinariesWatcher.ts
+++ src/client/pythonEnvironments/common/pythonBinariesWatcher.ts
@@ -6,8 +6,8 @@
 import * as minimatch from 'minimatch';
 import * as path from 'path';
 import { FileChangeType, watchLocationForPattern } from '../../common/platform/fileSystemWatcher';
-import { IDisposable } from '../../common/utils/resourceLifecycle';
 import { getOSType, OSType } from '../../common/utils/platform';
+import { IDisposable } from '../../common/utils/resourceLifecycle';
 
 const [executable, binName] = getOSType() === OSType.Windows ? ['python.exe', 'Scripts'] : ['python', 'bin'];
 
