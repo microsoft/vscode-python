e025ffe296a0ae3fc2d40ae655cb2e12807b95fd
Eric Snow <ericsnowcurrently@gmail.com>
2020-11-17 10:23:35 -0700

    Process search path entries in parallel.

--- src/client/pythonEnvironments/base/locators/lowLevel/windowsKnownPathsLocator.ts
+++ src/client/pythonEnvironments/base/locators/lowLevel/windowsKnownPathsLocator.ts
@@ -4,6 +4,7 @@
 // tslint:disable-next-line:no-single-line-block-comment
 /* eslint-disable max-classes-per-file */
 
+import { chain } from '../../../../common/utils/async';
 import { getSearchPathEntries, isValidAndExecutable } from '../../../../common/utils/exec';
 import { findInterpretersInDir } from '../../../common/commonUtils';
 import { PythonEnvKind } from '../../info';
@@ -40,26 +41,33 @@ class SimpleLocator extends FoundFilesLocator {
 }
 
 async function* getExecutables(): AsyncIterableIterator<string> {
-    for (const entry of getSearchPathEntries()) {
-        try {
-            for await (const executable of findInterpretersInDir(entry)) {
-                const okay = await isValidAndExecutable(executable);
-                if (okay) {
-                    yield executable;
-                } else if (okay === undefined) {
-                    // We could not determine if the file is executable
-                    // (e.g. on Windows).  So we trust that the ".exe"
-                    // suffix check done elsewhere is sufficient.
-                    yield executable;
+    const entries = getSearchPathEntries();
+    // The entries can be processed in parallel so we get each started
+    // and then chain the async results below.
+    const generators = entries.map((entry) => {
+        async function* generator() {
+            try {
+                for await (const executable of findInterpretersInDir(entry)) {
+                    const okay = await isValidAndExecutable(executable);
+                    if (okay) {
+                        yield executable;
+                    } else if (okay === undefined) {
+                        // We could not determine if the file is executable
+                        // (e.g. on Windows).  So we trust that the ".exe"
+                        // suffix check done elsewhere is sufficient.
+                        yield executable;
+                    }
                 }
+            } catch (err) {
+                if (err.code === 'ENOENT') {
+                    return;
+                }
+                throw err; // re-throw
             }
-        } catch (err) {
-            if (err.code === 'ENOENT') {
-                return;
-            }
-            throw err; // re-throw
         }
-    }
+        return generator();
+    });
+    yield* chain(generators);
 }
 
 /**
