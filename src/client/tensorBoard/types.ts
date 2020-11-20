e8532a2541a808900836ea8aea1fe21c53e39133
Joyce Er <huer@microsoft.com>
2020-11-19 22:43:25 -0800

    Prepopulate and validate logDir Add comments More consistent logging/error handling

--- src/client/tensorBoard/types.ts
+++ src/client/tensorBoard/types.ts
@@ -2,10 +2,9 @@
 // Licensed under the MIT License.
 
 import { IInstaller } from '../common/types';
-import { TensorBoardSession } from './tensorBoardSession';
 
 export const ITensorBoardSessionProvider = Symbol('ITensorBoardSessionProvider');
 
 export interface ITensorBoardSessionProvider {
-    getOrCreate(installer: IInstaller): Promise<TensorBoardSession>;
+    createNewSession(installer: IInstaller): Promise<void>;
 }
