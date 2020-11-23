21348369bf1321ce321b570cee948e1e89c02039
Joyce Er <huer@microsoft.com>
2020-11-22 21:28:21 -0800

    Add disposables to registry and catch errors

--- src/client/tensorBoard/tensorBoardFileWatcher.ts
+++ src/client/tensorBoard/tensorBoardFileWatcher.ts
@@ -5,6 +5,8 @@ import { inject, injectable } from 'inversify';
 import { FileSystemWatcher, RelativePattern, WorkspaceFolder, WorkspaceFoldersChangeEvent } from 'vscode';
 import { IExtensionActivationService } from '../activation/types';
 import { IWorkspaceService } from '../common/application/types';
+import { traceError } from '../common/logger';
+import { IDisposableRegistry } from '../common/types';
 import { TensorBoardPrompt } from './tensorBoardPrompt';
 
 @injectable()
@@ -14,7 +16,8 @@ export class TensorBoardFileWatcher implements IExtensionActivationService {
 
     constructor(
         @inject(IWorkspaceService) private workspaceService: IWorkspaceService,
-        @inject(TensorBoardPrompt) private tensorBoardPrompt: TensorBoardPrompt
+        @inject(TensorBoardPrompt) private tensorBoardPrompt: TensorBoardPrompt,
+        @inject(IDisposableRegistry) private readonly disposables: IDisposableRegistry
     ) {}
 
     public async activate() {
@@ -23,12 +26,9 @@ export class TensorBoardFileWatcher implements IExtensionActivationService {
             return;
         }
 
-        // Look for existing tfevent files. Just one will do
-        this.workspaceService.findFiles(this.globPattern, undefined, 1).then(async (matches) => {
-            if (matches.length > 0) {
-                await this.tensorBoardPrompt.showNativeTensorBoardPrompt();
-            }
-        });
+        // Look for pre-existing tfevent files, as the file watchers will only pick up files
+        // created or changed after they have been registered and hooked up. Just one will do.
+        await this.promptIfWorkspaceHasPreexistingFiles();
 
         // If the user creates or changes tfevent files, listen for those too
         for (const folder of folders) {
@@ -36,7 +36,22 @@ export class TensorBoardFileWatcher implements IExtensionActivationService {
         }
 
         // If workspace folders change, ensure we update our FileSystemWatchers
-        this.workspaceService.onDidChangeWorkspaceFolders((e) => this.updateFileSystemWatchers(e));
+        this.disposables.push(
+            this.workspaceService.onDidChangeWorkspaceFolders((e) => this.updateFileSystemWatchers(e))
+        );
+    }
+
+    private async promptIfWorkspaceHasPreexistingFiles() {
+        try {
+            const matches = await this.workspaceService.findFiles(this.globPattern, undefined, 1);
+            if (matches.length) {
+                await this.tensorBoardPrompt.showNativeTensorBoardPrompt();
+            }
+        } catch (e) {
+            traceError(
+                `Failed to prompt to launch TensorBoard session based on preexisting tfevent files in workspace: ${e}`
+            );
+        }
     }
 
     private async updateFileSystemWatchers(event: WorkspaceFoldersChangeEvent) {
@@ -55,9 +70,14 @@ export class TensorBoardFileWatcher implements IExtensionActivationService {
     private createFileSystemWatcher(folder: WorkspaceFolder) {
         const relativePattern = new RelativePattern(folder, this.globPattern);
         const fileSystemWatcher = this.workspaceService.createFileSystemWatcher(relativePattern);
+
         // When a file is created or changed that matches `this.globPattern`, try to show our prompt
-        fileSystemWatcher.onDidCreate((_uri) => this.tensorBoardPrompt.showNativeTensorBoardPrompt());
-        fileSystemWatcher.onDidChange((_uri) => this.tensorBoardPrompt.showNativeTensorBoardPrompt());
-        this.fileSystemWatchers.set(folder, fileSystemWatcher);
+        this.disposables.push(
+            fileSystemWatcher.onDidCreate((_uri) => this.tensorBoardPrompt.showNativeTensorBoardPrompt())
+        );
+        this.disposables.push(
+            fileSystemWatcher.onDidChange((_uri) => this.tensorBoardPrompt.showNativeTensorBoardPrompt())
+        );
+        this.disposables.push(fileSystemWatcher);
     }
 }
