fd12b5c5a325ba0e84bc1f951bdcea89346b9af9
Kim-Adeline Miguel <kimiguel@microsoft.com>
2020-11-25 10:00:36 -0800

    Uncomment everything + add setAutoSaveDelayInWorkspaceRoot

--- src/test/smoke/runInTerminal.smoke.test.ts
+++ src/test/smoke/runInTerminal.smoke.test.ts
@@ -5,12 +5,11 @@
 
 // tslint:disable:no-invalid-this
 
-// import * as assert from 'assert';
+import * as assert from 'assert';
 import * as fs from 'fs-extra';
 import * as path from 'path';
-// import * as vscode from 'vscode';
-// import { openFile, waitForCondition } from '../common';
-import { waitForCondition } from '../common';
+import * as vscode from 'vscode';
+import { openFile, setAutoSaveDelayInWorkspaceRoot, waitForCondition } from '../common';
 import { EXTENSION_ROOT_DIR_FOR_TESTS, IS_SMOKE_TEST } from '../constants';
 // import { sleep } from '../core';
 import { closeActiveWindows, initialize, initializeTest } from '../initialize';
@@ -23,19 +22,20 @@ suite('Smoke Test: Run Python File In Terminal', () => {
             return this.skip();
         }
         await initialize();
+        await setAutoSaveDelayInWorkspaceRoot(1);
     });
     setup(initializeTest);
     suiteTeardown(closeActiveWindows);
     teardown(closeActiveWindows);
 
     test('Exec', async () => {
-        // const file = path.join(
-        //     EXTENSION_ROOT_DIR_FOR_TESTS,
-        //     'src',
-        //     'testMultiRootWkspc',
-        //     'smokeTests',
-        //     'testExecInTerminal.py'
-        // );
+        const file = path.join(
+            EXTENSION_ROOT_DIR_FOR_TESTS,
+            'src',
+            'testMultiRootWkspc',
+            'smokeTests',
+            'testExecInTerminal.py'
+        );
         const outputFile = path.join(
             EXTENSION_ROOT_DIR_FOR_TESTS,
             'src',
@@ -46,14 +46,14 @@ suite('Smoke Test: Run Python File In Terminal', () => {
         if (await fs.pathExists(outputFile)) {
             await fs.unlink(outputFile);
         }
-        // const textDocument = await openFile(file);
+        const textDocument = await openFile(file);
 
         // Wait here
         // await sleep(1_000);
 
-        // await vscode.commands.executeCommand<void>('python.execInTerminal', textDocument.uri).then(undefined, (err) => {
-        //     assert.fail(`Unhandled failure:  ${err}`);
-        // });
+        await vscode.commands.executeCommand<void>('python.execInTerminal', textDocument.uri).then(undefined, (err) => {
+            assert.fail(`Unhandled failure:  ${err}`);
+        });
 
         const checkIfFileHasBeenCreated = () => fs.pathExists(outputFile);
         await waitForCondition(checkIfFileHasBeenCreated, testTimeout, `"${outputFile}" file not created`);
