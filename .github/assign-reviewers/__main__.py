6e85a86f72c4a6fd9400f1e6fe4db01fb643484a
Brett Cannon <brett@python.org>
2020-11-27 12:46:38 -0800

    Specify the YAML loader to eliminate a warning

--- .github/assign-reviewers/__main__.py
+++ .github/assign-reviewers/__main__.py
@@ -85,7 +85,7 @@ async def add_reviewers(
 async def main(token: str):
     config_file = pathlib.Path(__file__).parent.parent / "assign-reviewers.yml"
     with config_file.open(encoding="utf-8") as file:
-        config = yaml.load(file)
+        config = yaml.load(file, Loader=yaml.FullLoader)
     event = gidgethub.actions.event()
     team_reviewers, reviewers_to_add = select_reviewers(
         author=event["pull_request"]["user"]["login"],
