40a10901f860fd54f097e887090d849da793cb86
Eric Snow <ericsnowcurrently@gmail.com>
2020-12-03 17:24:07 -0700

    Re-implement parseVersionInfo() using parseBasicVersion().

--- src/client/pythonEnvironments/discovery/locators/services/virtualEnvironmentIdentifier.ts
+++ src/client/pythonEnvironments/discovery/locators/services/virtualEnvironmentIdentifier.ts
@@ -9,7 +9,11 @@ import {
 } from '../../../../common/utils/platform';
 import { PythonVersion, UNKNOWN_PYTHON_VERSION } from '../../../base/info';
 import { comparePythonVersionSpecificity } from '../../../base/info/env';
-import { parseBasicVersion, parseVersion } from '../../../base/info/pythonVersion';
+import {
+    parseBasicVersion,
+    parseRelease,
+    parseVersion,
+} from '../../../base/info/pythonVersion';
 import { pathExists, readFile } from '../../../common/externalDependencies';
 
 function getPyvenvConfigPathsFrom(interpreterPath:string): string[] {
@@ -150,8 +154,7 @@ export async function getPythonVersionFromPyvenvCfg(interpreterPath:string): Pro
                                 }
                             } else if (name === 'version_info') {
                                 try {
-                                    const [ver] = parseBasicVersion(value);
-                                    return ver;
+                                    return parseVersionInfo(value);
                                 } catch (ex) {
                                     return undefined;
                                 }
@@ -177,5 +180,34 @@ export async function getPythonVersionFromPyvenvCfg(interpreterPath:string): Pro
         }
     }
 
+    return version;
+}
+
+/**
+ * Convert the given string into the corresponding Python version object.
+ * Example:
+ *   3.9.0.final.0
+ *   3.9.0.alpha.1
+ *   3.9.0.beta.2
+ *   3.9.0.candidate.1
+ *
+ * Does not parse:
+ *   3.9.0
+ *   3.9.0a1
+ *   3.9.0b2
+ *   3.9.0rc1
+ */
+function parseVersionInfo(versionInfoStr: string): PythonVersion {
+    let version: PythonVersion;
+    let after: string;
+    try {
+        [version, after] = parseBasicVersion(versionInfoStr);
+    } catch {
+        // XXX Use getEmptyVersion().
+        return UNKNOWN_PYTHON_VERSION;
+    }
+    if (version.micro !== -1 && after.startsWith('.')) {
+        [version.release] = parseRelease(after);
+    }
     return version;
 }
