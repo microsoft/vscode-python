6c8236edb0fd694a41837c2794f263827173b9c7
Kim-Adeline Miguel <kimiguel@microsoft.com>
2021-01-05 14:35:09 -0800

    Try something

--- src/client/testing/unittest/socketServer.ts
+++ src/client/testing/unittest/socketServer.ts
@@ -44,12 +44,17 @@ export class UnitTestSocketServer extends EventEmitter implements IUnitTestSocke
         if (host.trim.length == 0) {
             host = 'localhost';
         }
-        const socket = net.createConnection({ port, host });
-        this.server!.listen({ port, host }, () => {
+        this.server!.on('connection', (socket: net.Socket) => {
             this.startedDef!.resolve((this.server!.address() as net.AddressInfo).port);
             this.startedDef = undefined;
             this.emit('start', socket);
         });
+        const socket = net.createConnection({ port, host });
+        // this.server!.listen(port, host, () => {
+        //     this.startedDef!.resolve((this.server!.address() as net.AddressInfo).port);
+        //     this.startedDef = undefined;
+        //     this.emit('start', socket);
+        // });
         this.server!.on('close', () => {
             socket.destroy();
         });
