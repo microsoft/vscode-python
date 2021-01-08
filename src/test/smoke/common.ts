86fed66e29453c39217c14d493f2b5f97099be49
Joyce Er <huer@microsoft.com>
2021-01-07 18:52:09 -0800

    Fix linter

--- src/test/smoke/common.ts
+++ src/test/smoke/common.ts
@@ -46,7 +46,7 @@ export async function enableJedi(enable: boolean | undefined): Promise<void> {
     await updateSetting('languageServer', 'Jedi');
 }
 
-export async function openNotebook(file: string) {
+export async function openNotebook(file: string): Promise<vscode.NotebookEditor> {
     await verifyExtensionIsAvailable(JUPYTER_EXTENSION_ID);
     await vscode.commands.executeCommand('vscode.openWith', vscode.Uri.file(file), 'jupyter-notebook');
     const notebook = vscode.notebook.activeNotebookEditor;
