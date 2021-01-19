ddbce41507108f9face57c3b572021d293c37cf6
Kartik Raj <karraj@microsoft.com>
2021-01-19 13:29:41 -0800

    Oops

--- src/client/pythonEnvironments/legacyIOC.ts
+++ src/client/pythonEnvironments/legacyIOC.ts
@@ -352,7 +352,7 @@ export class ComponentAdapter implements IComponentAdapter, IExtensionSingleActi
         if (!this.enabled) {
             return undefined;
         }
-        const workspaceFolder = resource ? vscode.workspace.getWorkspaceFolder(resource) : undefined;
+        const workspaceFolder = vscode.workspace.getWorkspaceFolder(resource);
         if (!workspaceFolder) {
             return [];
         }
