9fe9e7d5f33d2b4701e35945398cfb17aae687bb
Joyce Er <huer@microsoft.com>
2021-01-25 18:40:01 -0800

    Drop A/B test

--- src/client/common/experiments/groups.ts
+++ src/client/common/experiments/groups.ts
@@ -95,12 +95,6 @@ export enum LinterInstallationPromptVariants {
     noPrompt = 'pythonNotDisplayLinterPrompt',
 }
 
-// AB test codeactions vs codelenses as an entrypoint for native TensorBoard sessions
-export enum NativeTensorBoardEntrypoints {
-    codeActions = 'pythonTensorBoardCodeActions',
-    codeLenses = 'pythonTensorBoardCodeLenses',
-}
-
 // Experiment to control which environment discovery mechanism can be used
 export enum DiscoveryVariants {
     discoverWithFileWatching = 'pythonDiscoveryModule',
