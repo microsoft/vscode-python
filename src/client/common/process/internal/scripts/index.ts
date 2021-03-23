83a8d58c48cdd1a6375f5f604478672c9ec90d02
Kim-Adeline Miguel <kimiguel@microsoft.com>
2021-03-23 08:31:56 -0700

    Re-add comment

--- src/client/common/process/internal/scripts/index.ts
+++ src/client/common/process/internal/scripts/index.ts
@@ -180,6 +180,8 @@ export function printEnvVariables(): [string[], (out: string) => NodeJS.ProcessE
     return [args, parse];
 }
 
+// shell_exec.py
+
 // eslint-disable-next-line camelcase
 export function shell_exec(command: string, lockfile: string, shellArgs: string[]): string[] {
     const script = path.join(SCRIPTS_DIR, 'shell_exec.py');
