325289f65a27a854a3d0d2c88e5174ac41d874a8
Kartik Raj <karraj@microsoft.com>
2021-03-29 17:31:03 -0700

    Apply code review suggestion again

--- src/client/common/configSettings.ts
+++ src/client/common/configSettings.ts
@@ -47,6 +47,7 @@ import {
 } from './types';
 import { debounceSync } from './utils/decorators';
 import { SystemVariables } from './variables/systemVariables';
+import { getOSType, OSType } from './utils/platform';
 
 const untildify = require('untildify');
 
@@ -765,7 +766,10 @@ function getPythonExecutable(pythonPath: string): string {
 }
 
 function isValidPythonPath(pythonPath: string): boolean {
-    return fs.existsSync(pythonPath) && path.basename(pythonPath).startsWith('python');
+    return (
+        fs.existsSync(pythonPath) &&
+        path.basename(getOSType() === OSType.Windows ? pythonPath.toLowerCase() : pythonPath).startsWith('python')
+    );
 }
 
 function convertSettingTypeToLogLevel(setting: LoggingLevelSettingType | undefined): LogLevel | 'off' {
