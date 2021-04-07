f8f9369ac3b9f9a3088d63269b519a6d0a8a7a29
Karthik Nadig <karthik.nadig@live.com>
2021-04-07 12:07:44 -0700

    Set kind for global virtual env tests

--- src/test/pythonEnvironments/discovery/locators/globalVirtualEnvironmentLocator.testvirtualenvs.ts
+++ src/test/pythonEnvironments/discovery/locators/globalVirtualEnvironmentLocator.testvirtualenvs.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 import * as path from 'path';
+import { PythonEnvKind } from '../../../../client/pythonEnvironments/base/info';
 import { GlobalVirtualEnvironmentLocator } from '../../../../client/pythonEnvironments/discovery/locators/services/globalVirtualEnvronmentLocator';
 import { TEST_LAYOUT_ROOT } from '../../common/commonTestConstants';
 import { testLocatorWatcher } from './watcherTestUtils';
@@ -14,7 +15,9 @@ suite('GlobalVirtualEnvironment Locator', async () => {
         workonHomeOldValue = process.env.WORKON_HOME;
         process.env.WORKON_HOME = testWorkOnHomePath;
     });
-    testLocatorWatcher(testWorkOnHomePath, async () => new GlobalVirtualEnvironmentLocator());
+    testLocatorWatcher(testWorkOnHomePath, async () => new GlobalVirtualEnvironmentLocator(), {
+        kind: PythonEnvKind.VirtualEnvWrapper,
+    });
     suiteTeardown(() => {
         process.env.WORKON_HOME = workonHomeOldValue;
     });
