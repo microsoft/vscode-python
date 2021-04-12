c78b6ec33ceab0d3d0f62b1bedec0d0cd02a9ed7
Karthik Nadig <karthik.nadig@live.com>
2021-04-12 14:09:05 -0700

    Wrap yields with try catch

--- src/client/pythonEnvironments/discovery/locators/services/condaLocator.ts
+++ src/client/pythonEnvironments/discovery/locators/services/condaLocator.ts
@@ -7,7 +7,7 @@ import { IPythonEnvsIterator, Locator } from '../../../base/locator';
 import { getInterpreterPathFromDir, getPythonVersionFromPath } from '../../../common/commonUtils';
 import { AnacondaCompanyName, Conda } from './conda';
 import { resolveEnvFromIterator } from '../../../base/locatorUtils';
-import { traceVerbose } from '../../../../common/logger';
+import { traceError, traceVerbose } from '../../../../common/logger';
 
 export class CondaEnvironmentLocator extends Locator {
     // Locating conda binary is expensive, since it potentially involves spawning or
@@ -55,7 +55,11 @@ export class CondaEnvironmentLocator extends Locator {
                     info.name = name;
                 }
                 traceVerbose(`Found conda environment: ${executable}`);
-                yield info;
+                try {
+                    yield info;
+                } catch (ex) {
+                    traceError(`Failed to process environment: ${executable}`, ex);
+                }
             }
         }
     }
