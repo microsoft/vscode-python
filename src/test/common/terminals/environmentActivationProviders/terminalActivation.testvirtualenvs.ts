1509cf1a70b3053412fbadd5bdd07746d875bd00
Kim-Adeline Miguel <kimiguel@microsoft.com>
2021-04-21 15:55:10 -0700

    Wait 15s for activation

--- src/test/common/terminals/environmentActivationProviders/terminalActivation.testvirtualenvs.ts
+++ src/test/common/terminals/environmentActivationProviders/terminalActivation.testvirtualenvs.ts
@@ -42,7 +42,7 @@ suite('Activation of Environments in Terminal', () => {
         PYTHON_VIRTUAL_ENVS_LOCATION !== undefined
             ? path.join(EXTENSION_ROOT_DIR_FOR_TESTS, PYTHON_VIRTUAL_ENVS_LOCATION)
             : path.join(EXTENSION_ROOT_DIR_FOR_TESTS, 'src', 'tmp', 'envPaths.json');
-    const waitTimeForActivation = 5000;
+    const waitTimeForActivation = 15000;
     type EnvPath = {
         condaExecPath: string;
         condaPath: string;
