93913a9c42a22aad73b8c7089c8cd2611a8f6892
Karthik Nadig <kanadig@microsoft.com>
2021-05-04 09:57:37 -0700

    Remove default pylint when using jedi (#16074)
    * Remove default pylint when using jedi
    
    * Remove pylint minimal checkers
    
    * Remove more pylint config
    
    * Restore missed configuration file names
    
    * More linting clean up
    
    * Fix tests

--- src/client/common/types.ts
+++ src/client/common/types.ts
@@ -271,7 +271,6 @@ export interface ILintingSettings {
     banditEnabled: boolean;
     banditArgs: string[];
     banditPath: string;
-    readonly pylintUseMinimalCheckers: boolean;
 }
 export interface IFormattingSettings {
     readonly provider: string;
