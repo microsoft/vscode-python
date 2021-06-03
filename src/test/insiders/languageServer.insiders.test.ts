e8ce2dae3e2165f6a22194644d506e5d2844d3da
Joyce Er <huer@microsoft.com>
2021-06-03 11:06:25 -0700

    notebook renamed to notebooks, notebooks symbols moved to workspace namespace

--- src/test/insiders/languageServer.insiders.test.ts
+++ src/test/insiders/languageServer.insiders.test.ts
@@ -88,9 +88,10 @@ suite('Insiders Test: Language Server', () => {
         const notebookDocument = await openNotebookAndWaitForLS(notebookDefinitions);
         let tested = false;
         for (let i = 0; i < 5; i += 1) {
+            const uri = notebookDocument.cellAt(2).document.uri;
             const locations = await vscode.commands.executeCommand<vscode.Location[]>(
                 'vscode.executeDefinitionProvider',
-                notebookDocument.cellAt(2).document.uri, // Second cell should have a function with the decorator on it
+                uri, // Second cell should have a function with the decorator on it
                 startPosition,
             );
             if (locations && locations.length > 0) {
