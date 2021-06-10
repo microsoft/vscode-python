002392389f93693e00c60e9c4d0926cd82ded275
Kartik Raj <karraj@microsoft.com>
2021-06-10 15:37:48 -0700

    Modify the python path deprecation prompt

--- src/test/application/diagnostics/checks/pythonPathDeprecated.unit.test.ts
+++ src/test/application/diagnostics/checks/pythonPathDeprecated.unit.test.ts
@@ -6,13 +6,13 @@
 import { assert, expect } from 'chai';
 import * as sinon from 'sinon';
 import * as typemoq from 'typemoq';
-import { ConfigurationTarget, DiagnosticSeverity, Uri, WorkspaceConfiguration } from 'vscode';
+import { DiagnosticSeverity, Uri, WorkspaceConfiguration } from 'vscode';
 import { BaseDiagnostic, BaseDiagnosticsService } from '../../../../client/application/diagnostics/base';
 import {
     PythonPathDeprecatedDiagnostic,
     PythonPathDeprecatedDiagnosticService,
 } from '../../../../client/application/diagnostics/checks/pythonPathDeprecated';
-import { CommandOption, IDiagnosticsCommandFactory } from '../../../../client/application/diagnostics/commands/types';
+import { IDiagnosticsCommandFactory } from '../../../../client/application/diagnostics/commands/types';
 import { DiagnosticCodes } from '../../../../client/application/diagnostics/constants';
 import {
     DiagnosticCommandPromptHandlerServiceId,
@@ -26,9 +26,8 @@ import {
     IDiagnosticHandlerService,
 } from '../../../../client/application/diagnostics/types';
 import { IWorkspaceService } from '../../../../client/common/application/types';
-import { STANDARD_OUTPUT_CHANNEL } from '../../../../client/common/constants';
 import { DeprecatePythonPath } from '../../../../client/common/experiments/groups';
-import { IDisposableRegistry, IExperimentService, IOutputChannel, Resource } from '../../../../client/common/types';
+import { IDisposableRegistry, IExperimentService, Resource } from '../../../../client/common/types';
 import { Common, Diagnostics } from '../../../../client/common/utils/localize';
 import { IServiceContainer } from '../../../../client/ioc/types';
 
@@ -40,15 +39,10 @@ suite('Application Diagnostics - Python Path Deprecated', () => {
     let workspaceService: typemoq.IMock<IWorkspaceService>;
     let filterService: typemoq.IMock<IDiagnosticFilterService>;
     let experimentsManager: typemoq.IMock<IExperimentService>;
-    let output: typemoq.IMock<IOutputChannel>;
     let serviceContainer: typemoq.IMock<IServiceContainer>;
     function createContainer() {
         serviceContainer = typemoq.Mock.ofType<IServiceContainer>();
         filterService = typemoq.Mock.ofType<IDiagnosticFilterService>();
-        output = typemoq.Mock.ofType<IOutputChannel>();
-        serviceContainer
-            .setup((s) => s.get(typemoq.It.isValue(IOutputChannel), STANDARD_OUTPUT_CHANNEL))
-            .returns(() => output.object);
         experimentsManager = typemoq.Mock.ofType<IExperimentService>();
         messageHandler = typemoq.Mock.ofType<IDiagnosticHandlerService<MessageCommandPrompt>>();
         serviceContainer
@@ -145,8 +139,13 @@ suite('Application Diagnostics - Python Path Deprecated', () => {
             messageHandler.verifyAll();
         });
         test('Python Path Deprecated Diagnostic is handled as expected', async () => {
+            let invoked = false;
             const diagnostic = new PythonPathDeprecatedDiagnostic('message', resource);
-            const ignoreCmd = ({ cmd: 'ignoreCmd' } as any) as IDiagnosticCommand;
+            const ignoreCmd = ({
+                invoke: () => {
+                    invoked = true;
+                },
+            } as any) as IDiagnosticCommand;
             filterService
                 .setup((f) =>
                     f.shouldIgnoreDiagnostic(typemoq.It.isValue(DiagnosticCodes.PythonPathDeprecatedDiagnostic)),
@@ -160,41 +159,29 @@ suite('Application Diagnostics - Python Path Deprecated', () => {
                 .verifiable(typemoq.Times.once());
 
             commandFactory
-                .setup((f) =>
-                    f.createCommand(
-                        typemoq.It.isAny(),
-                        typemoq.It.isObjectWith<CommandOption<'ignore', DiagnosticScope>>({ type: 'ignore' }),
-                    ),
-                )
+                .setup((f) => f.createCommand(typemoq.It.isAny(), typemoq.It.isAny()))
+                .callback((a, b) => {
+                    expect(a).to.be.deep.equal(diagnostic);
+                    expect(b).to.be.deep.equal({
+                        type: 'ignore',
+                        options: DiagnosticScope.Global,
+                    });
+                })
                 .returns(() => ignoreCmd)
                 .verifiable(typemoq.Times.once());
-            const _removePythonPathFromWorkspaceSettings = sinon.stub(
-                PythonPathDeprecatedDiagnosticService.prototype,
-                '_removePythonPathFromWorkspaceSettings',
-            );
-            _removePythonPathFromWorkspaceSettings.resolves();
 
             await diagnosticService.handle([diagnostic]);
 
-            assert(_removePythonPathFromWorkspaceSettings.calledOnceWith(resource));
+            expect(invoked).to.equal(true, 'Command should be invoked');
             messageHandler.verifyAll();
             commandFactory.verifyAll();
-            expect(messagePrompt).not.be.equal(undefined, 'Message prompt not set');
-            expect(messagePrompt!.commandPrompts.length).to.equal(2, 'Incorrect length');
-            expect(messagePrompt!.commandPrompts[0].command).not.be.equal(undefined, 'Command not set');
-            expect(messagePrompt!.commandPrompts[0].command!.diagnostic).to.be.deep.equal(diagnostic);
-            expect(messagePrompt!.commandPrompts[0].prompt).to.be.deep.equal(Common.openOutputPanel());
-            expect(messagePrompt!.commandPrompts[1]).to.be.deep.equal({
-                prompt: Common.doNotShowAgain(),
-                command: ignoreCmd,
+            expect(messagePrompt).to.be.deep.equal({
+                commandPrompts: [
+                    {
+                        prompt: Common.ok(),
+                    },
+                ],
             });
-
-            output
-                .setup((o) => o.show(true))
-                .returns(() => undefined)
-                .verifiable(typemoq.Times.once());
-            await messagePrompt!.commandPrompts[0].command!.invoke();
-            output.verifyAll();
         });
         test('Handling an empty diagnostic should not show a message nor return a command', async () => {
             const diagnostics: IDiagnostic[] = [];
@@ -354,24 +341,6 @@ suite('Application Diagnostics - Python Path Deprecated', () => {
             assert.deepEqual(diagnostics, []);
 
             workspaceService.verifyAll();
-        });
-
-        test('Method _removePythonPathFromWorkspaceSettings() removes `python.pythonPath` setting from Workspace & Workspace Folder', async () => {
-            const workspaceConfig = typemoq.Mock.ofType<WorkspaceConfiguration>();
-            workspaceService.setup((w) => w.workspaceFile).returns(() => Uri.parse('path/to/workspaceFile'));
-            workspaceService.setup((w) => w.getConfiguration('python', resource)).returns(() => workspaceConfig.object);
-            workspaceConfig
-                .setup((w) => w.update('pythonPath', undefined, ConfigurationTarget.Workspace))
-                .returns(() => Promise.resolve())
-                .verifiable(typemoq.Times.once());
-            workspaceConfig
-                .setup((w) => w.update('pythonPath', undefined, ConfigurationTarget.WorkspaceFolder))
-                .returns(() => Promise.resolve())
-                .verifiable(typemoq.Times.once());
-
-            await diagnosticService._removePythonPathFromWorkspaceSettings(resource);
-
-            workspaceConfig.verifyAll();
         });
     });
 });
