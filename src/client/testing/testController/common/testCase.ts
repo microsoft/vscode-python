bac2450d5341731e457954b3a6b25b18ff593790
Karthik Nadig <kanadig@microsoft.com>
2021-06-17 14:33:44 -0700

    Improve test node descriptions

--- src/client/testing/testController/common/testCase.ts
+++ src/client/testing/testController/common/testCase.ts
@@ -2,19 +2,31 @@
 // Licensed under the MIT License.
 
 import * as path from 'path';
-import { test, TestItem } from 'vscode';
+import { Position, Range, test, TestItem, Uri } from 'vscode';
 import { RawTest } from '../../common/services/types';
 
 export class TestCase {
     public static create(testRoot: string, rawData: RawTest): TestItem<TestCase> {
-        const fullPath = path.join(testRoot, rawData.id);
+        const fullId = path.join(testRoot, rawData.id);
+        const documentPath = path.join(testRoot, rawData.source.substr(0, rawData.source.indexOf(':')));
         const item = test.createTestItem<TestCase>({
-            id: fullPath,
+            id: fullId,
             label: rawData.name,
+            uri: Uri.file(documentPath),
         });
 
         item.debuggable = true;
         item.data = new TestCase(item, rawData);
+
+        try {
+            const sourceLine = rawData.source.substr(rawData.source.indexOf(':') + 1);
+            const line = parseInt(sourceLine, 10);
+            // Lines in raw data start at 1, vscode lines start at 0
+            item.range = new Range(new Position(line - 1, 0), new Position(line, 0));
+        } catch (ex) {
+            // ignore
+        }
+
         return item;
     }
 
