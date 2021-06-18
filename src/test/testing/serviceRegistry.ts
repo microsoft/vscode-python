4279db04afff2fe521f19c68814e5a39cea8a9a5
Kim-Adeline Miguel <kimiguel@microsoft.com>
2021-06-18 10:25:08 -0700

    fix new linting issues

--- src/test/testing/serviceRegistry.ts
+++ src/test/testing/serviceRegistry.ts
@@ -117,8 +117,7 @@ export class UnitTestIocContainer extends IocContainer {
     }
 
     public registerTestManagers(): void {
-        this.serviceManager.addFactory<ITestManager>(ITestManagerFactory, (context) => {
-            return (testProvider: TestProvider, workspaceFolder: Uri, rootDirectory: string) => {
+        this.serviceManager.addFactory<ITestManager>(ITestManagerFactory, (context) => (testProvider: TestProvider, workspaceFolder: Uri, rootDirectory: string) => {
                 const serviceContainer = context.container.get<IServiceContainer>(IServiceContainer);
 
                 switch (testProvider) {
@@ -135,8 +134,7 @@ export class UnitTestIocContainer extends IocContainer {
                         throw new Error(`Unrecognized test provider '${testProvider}'`);
                     }
                 }
-            };
-        });
+            });
     }
 
     public registerInterpreterStorageTypes(): void {
@@ -144,13 +142,11 @@ export class UnitTestIocContainer extends IocContainer {
     }
 
     public registerTestManagerService(): void {
-        this.serviceManager.addFactory<ITestManagerService>(ITestManagerServiceFactory, (context) => {
-            return (workspaceFolder: Uri) => {
+        this.serviceManager.addFactory<ITestManagerService>(ITestManagerServiceFactory, (context) => (workspaceFolder: Uri) => {
                 const serviceContainer = context.container.get<IServiceContainer>(IServiceContainer);
                 const testsHelper = context.container.get<ITestsHelper>(ITestsHelper);
                 return new TestManagerService(workspaceFolder, testsHelper, serviceContainer);
-            };
-        });
+            });
     }
 
     public registerMockUnitTestSocketServer(): void {
