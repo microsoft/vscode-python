0f03b274f8f389d6c7ca011b70b172ca5664e98d
Kartik Raj <karraj@microsoft.com>
2021-07-19 14:56:23 -0700

    Ensure we block on autoselection when no interpreter is explictly set by user

--- src/client/common/serviceRegistry.ts
+++ src/client/common/serviceRegistry.ts
@@ -20,6 +20,7 @@ import {
     IToolExecutionPath,
     IsWindows,
     ToolExecutionPath,
+    IInterpreterPathExpHelper,
 } from './types';
 import { IServiceManager } from '../ioc/types';
 import { JupyterExtensionDependencyManager } from '../jupyter/jupyterExtensionDependencyManager';
@@ -118,11 +119,13 @@ import { IMultiStepInputFactory, MultiStepInputFactory } from './utils/multiStep
 import { Random } from './utils/random';
 import { JupyterNotInstalledNotificationHelper } from '../jupyter/jupyterNotInstalledNotificationHelper';
 import { IJupyterNotInstalledNotificationHelper } from '../jupyter/types';
+import { InterpreterPathExpHelper } from './interpreterPathExpHelper';
 
 export function registerTypes(serviceManager: IServiceManager): void {
     serviceManager.addSingletonInstance<boolean>(IsWindows, IS_WINDOWS);
 
     serviceManager.addSingleton<IActiveResourceService>(IActiveResourceService, ActiveResourceService);
+    serviceManager.addSingleton<IInterpreterPathExpHelper>(IInterpreterPathExpHelper, InterpreterPathExpHelper);
     serviceManager.addSingleton<IInterpreterPathService>(IInterpreterPathService, InterpreterPathService);
     serviceManager.addSingleton<IExtensions>(IExtensions, Extensions);
     serviceManager.addSingleton<IRandom>(IRandom, Random);
