09da10b165f1401deb72e25af51d8e0b2f908ec4
Kim-Adeline Miguel <kimiguel@microsoft.com>
2021-07-23 11:42:52 -0700

    Remove cusotm webview implementation

--- src/client/common/constants.ts
+++ src/client/common/constants.ts
@@ -129,7 +129,6 @@ export function isUnitTestExecution(): boolean {
 }
 
 // Temporary constant, used to indicate whether we're using custom editor api or not.
-export const UseCustomEditorApi = Symbol('USE_CUSTOM_EDITOR');
 export const UseProposedApi = Symbol('USE_VSC_PROPOSED_API');
 
 export * from '../constants';
