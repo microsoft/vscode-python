46fe25f47eefd4b82d5419b75c2a505713a57bd7
Kartik Raj <karraj@microsoft.com>
2021-07-30 10:59:17 -0700

    Revert "Replace deprecated ExP methods with the newer ones  (#16486)"
    This reverts commit 16497b6c21edb6b1a49087b44120d020d860c65d.

--- src/test/tensorBoard/tensorBoardSession.test.ts
+++ src/test/tensorBoard/tensorBoardSession.test.ts
@@ -70,7 +70,7 @@ suite('TensorBoard session creation', async () => {
     setup(async () => {
         sandbox = sinon.createSandbox();
         ({ serviceManager } = await initialize());
-        sandbox.stub(ExperimentHelpers, 'inDiscoveryExperiment').resolves(true);
+        sandbox.stub(ExperimentHelpers, 'inDiscoveryExperiment').resolves(false);
         experimentService = serviceManager.get<IExperimentService>(IExperimentService);
 
         const interpreterService = serviceManager.get<IInterpreterService>(IInterpreterService);
