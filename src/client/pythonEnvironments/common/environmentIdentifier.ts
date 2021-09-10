5eba0b44d3aa51956020549982edc7906e80947f
Kartik Raj <karraj@microsoft.com>
2021-09-09 17:31:53 -0700

    Add to cache

--- src/client/pythonEnvironments/common/environmentIdentifier.ts
+++ src/client/pythonEnvironments/common/environmentIdentifier.ts
@@ -4,7 +4,7 @@
 import { PythonEnvKind } from '../base/info';
 import { getPrioritizedEnvKinds } from '../base/info/envKind';
 import { isCondaEnvironment } from './environmentManagers/conda';
-import { isGloballyInstalledEnv } from './environmentManagers/globalEnvs';
+import { isGloballyInstalledEnv } from './environmentManagers/globalInstalledEnvs';
 import { isPipenvEnvironment } from './environmentManagers/pipenv';
 import { isPoetryEnvironment } from './environmentManagers/poetry';
 import { isPyenvEnvironment } from './environmentManagers/pyenv';
