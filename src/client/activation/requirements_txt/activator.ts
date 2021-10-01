34daa99872fd279e0760e6af2d660cd9bdacc9c9
Marisa Machlis <mmachlis@wustl.edu>
2021-10-01 16:45:19 -0400

    update regex for python package links

--- src/client/activation/requirements_txt/activator.ts
+++ src/client/activation/requirements_txt/activator.ts
@@ -7,13 +7,18 @@ export class RequirementsTxtLinkActivator implements IExtensionSingleActivationS
     // eslint-disable-next-line class-methods-use-this
     public async activate(): Promise<void> {
         languages.registerHoverProvider(
-            { pattern: '**/requirements.txt' },
+            {
+                pattern:
+                    '{**/requirements.txt,**/*-requirements.txt,**/requirements-*.txt,**/requirements.in,**/*-requirements.in,**/requirements-*.in,**/constraints.txt,**/*-constraints.txt,**/constraints-*.txt}',
+            },
             {
                 provideHover(document: TextDocument, position: Position) {
-                    const regex = '^(.*)==';
+                    // source for Python distribution names valid characters: https://www.python.org/dev/peps/pep-0508/#names
+                    // Regex to match valid Python package name followed by semicolon, whitespace, or version specifiers
+                    const packageRegex = /^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9]+)(?=(;|\s|$|==|>|>=|<|<=|~=))/i;
 
                     const row = document.lineAt(position.line).text;
-                    const projectName = row.match(regex);
+                    const projectName = row.match(packageRegex);
 
                     if (projectName) {
                         return new Hover(`https://pypi.org/project/${projectName[1]}/`);
