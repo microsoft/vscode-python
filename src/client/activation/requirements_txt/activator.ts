6d41e52d4ade7aa207246e8094228ba9be951441
Marisa Machlis <mmachlis@wustl.edu>
2021-10-01 19:07:27 -0400

    add maximum length to regex

--- src/client/activation/requirements_txt/activator.ts
+++ src/client/activation/requirements_txt/activator.ts
@@ -15,7 +15,8 @@ export class RequirementsTxtLinkActivator implements IExtensionSingleActivationS
                 provideHover(document: TextDocument, position: Position) {
                     // source for Python distribution names valid characters: https://www.python.org/dev/peps/pep-0508/#names
                     // Regex to match valid Python package name followed by semicolon, whitespace, or version specifiers
-                    const packageRegex = /^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9]+)(?=(;|\s|$|==|>|>=|<|<=|~=))/i;
+                    // Matches package names up to 36 characters in length
+                    const packageRegex = /^([A-Z0-9]{0,36}|[A-Z0-9][A-Z0-9._-]{0,34}[A-Z0-9])(?=(;|\s|$|==|>|>=|<|<=|~=))/i;
 
                     const row = document.lineAt(position.line).text;
                     const projectName = row.match(packageRegex);
