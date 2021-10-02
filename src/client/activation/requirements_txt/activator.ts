8951920290198bec4958760ddd42611413a729a6
marisamachlis <91751286+marisamachlis@users.noreply.github.com>
2021-10-01 20:04:38 -0400

    Update src/client/activation/requirements_txt/activator.ts
    Co-authored-by: Brett Cannon <brett@python.org>

--- src/client/activation/requirements_txt/activator.ts
+++ src/client/activation/requirements_txt/activator.ts
@@ -14,7 +14,7 @@ export class RequirementsTxtLinkActivator implements IExtensionSingleActivationS
             {
                 provideHover(document: TextDocument, position: Position) {
                     // https://packaging.python.org/specifications/core-metadata/#name
-                    const packageRegex = /^([A-Z0-9]{0,36}|[A-Z0-9][A-Z0-9._-]{0,34}[A-Z0-9])(?=(;|\s|$|==|>|>=|<|<=|~=))/i;
+                    const packageRegex = /^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])/i;
 
                     const row = document.lineAt(position.line).text;
                     const projectName = row.match(packageRegex);
