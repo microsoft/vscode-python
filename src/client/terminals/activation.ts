0bbde9bc1598aaae7b31e2f683d21175192b7e07
Karthik Nadig <kanadig@microsoft.com>
2021-10-04 12:20:37 -0700

    Fix formatting.

--- src/client/terminals/activation.ts
+++ src/client/terminals/activation.ts
@@ -12,53 +12,55 @@ import { ITerminalAutoActivation } from './types';
 
 @injectable()
 export class TerminalAutoActivation implements ITerminalAutoActivation {
-  private handler?: IDisposable;
+    private handler?: IDisposable;
 
-  private readonly terminalsNotToAutoActivate = new WeakSet<Terminal>();
+    private readonly terminalsNotToAutoActivate = new WeakSet<Terminal>();
 
-  constructor(
-    @inject(ITerminalManager)
-    private readonly terminalManager: ITerminalManager,
-    @inject(IDisposableRegistry) disposableRegistry: IDisposableRegistry,
-    @inject(ITerminalActivator) private readonly activator: ITerminalActivator,
-    @inject(IActiveResourceService)
-    private readonly activeResourceService: IActiveResourceService
-  ) {
-    disposableRegistry.push(this);
-  }
-
-  public dispose(): void {
-    if (this.handler) {
-      this.handler.dispose();
-      this.handler = undefined;
-    }
-  }
-
-  public register(): void {
-    if (this.handler) {
-      return;
-    }
-    this.handler = this.terminalManager.onDidOpenTerminal(this.activateTerminal, this);
-  }
-
-  public disableAutoActivation(terminal: Terminal): void {
-    this.terminalsNotToAutoActivate.add(terminal);
-  }
-
-  private async activateTerminal(terminal: Terminal): Promise<void> {
-    if (this.terminalsNotToAutoActivate.has(terminal)) {
-      return;
-    }
-    if ('hideFromUser' in terminal.creationOptions && terminal.creationOptions.hideFromUser) {
-      return;
+    constructor(
+        @inject(ITerminalManager)
+        private readonly terminalManager: ITerminalManager,
+        @inject(IDisposableRegistry) disposableRegistry: IDisposableRegistry,
+        @inject(ITerminalActivator) private readonly activator: ITerminalActivator,
+        @inject(IActiveResourceService)
+        private readonly activeResourceService: IActiveResourceService,
+    ) {
+        disposableRegistry.push(this);
     }
 
-    const cwd =
-      'cwd' in terminal.creationOptions ? terminal.creationOptions.cwd : this.activeResourceService.getActiveResource();
-    const resource = typeof cwd === 'string' ? Uri.file(cwd) : cwd;
+    public dispose(): void {
+        if (this.handler) {
+            this.handler.dispose();
+            this.handler = undefined;
+        }
+    }
 
-    await this.activator.activateEnvironmentInTerminal(terminal, {
-      resource,
-    });
-  }
+    public register(): void {
+        if (this.handler) {
+            return;
+        }
+        this.handler = this.terminalManager.onDidOpenTerminal(this.activateTerminal, this);
+    }
+
+    public disableAutoActivation(terminal: Terminal): void {
+        this.terminalsNotToAutoActivate.add(terminal);
+    }
+
+    private async activateTerminal(terminal: Terminal): Promise<void> {
+        if (this.terminalsNotToAutoActivate.has(terminal)) {
+            return;
+        }
+        if ('hideFromUser' in terminal.creationOptions && terminal.creationOptions.hideFromUser) {
+            return;
+        }
+
+        const cwd =
+            'cwd' in terminal.creationOptions
+                ? terminal.creationOptions.cwd
+                : this.activeResourceService.getActiveResource();
+        const resource = typeof cwd === 'string' ? Uri.file(cwd) : cwd;
+
+        await this.activator.activateEnvironmentInTerminal(terminal, {
+            resource,
+        });
+    }
 }
