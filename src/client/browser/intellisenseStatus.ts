c9c6a2dd16733d45088e56d1ceba66a3749bd049
Kartik Raj <karraj@microsoft.com>
2021-10-11 11:09:59 -0700

    PR reviews

--- src/client/browser/intellisenseStatus.ts
+++ src/client/browser/intellisenseStatus.ts
@@ -3,19 +3,23 @@
 
 import * as vscode from 'vscode';
 import { Common, LanguageService } from '../common/utils/localize';
+import { noop } from '../common/utils/misc';
 
 export function createStatusItem(): vscode.Disposable {
-    const statusItem = vscode.languages.createLanguageStatusItem('python.projectStatus', {
-        language: 'python',
-    });
-    statusItem.name = LanguageService.statusItem.name();
-    statusItem.severity = vscode.LanguageStatusSeverity.Warning;
-    statusItem.text = LanguageService.statusItem.text();
-    statusItem.detail = LanguageService.statusItem.detail();
-    statusItem.command = {
-        title: Common.learnMore(),
-        command: 'vscode.open',
-        arguments: [vscode.Uri.parse('https://aka.ms/AAdzyh4')],
-    };
-    return statusItem;
+    if (vscode.languages.createLanguageStatusItem) {
+        const statusItem = vscode.languages.createLanguageStatusItem('python.projectStatus', {
+            language: 'python',
+        });
+        statusItem.name = LanguageService.statusItem.name();
+        statusItem.severity = vscode.LanguageStatusSeverity.Warning;
+        statusItem.text = LanguageService.statusItem.text();
+        statusItem.detail = LanguageService.statusItem.detail();
+        statusItem.command = {
+            title: Common.learnMore(),
+            command: 'vscode.open',
+            arguments: [vscode.Uri.parse('https://aka.ms/AAdzyh4')],
+        };
+        return statusItem;
+    }
+    return { dispose: noop };
 }
