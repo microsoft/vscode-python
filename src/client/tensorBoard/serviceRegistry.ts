02dea3563a95203080f323cfff1aeaeaa3d6c1f0
aamunger <aamunger@microsoft.com>
2021-12-01 09:28:52 -0800

    remove expensive filewatcher for tensorboard

--- src/client/tensorBoard/serviceRegistry.ts
+++ src/client/tensorBoard/serviceRegistry.ts
@@ -4,7 +4,6 @@
 import { IExtensionSingleActivationService } from '../activation/types';
 import { IServiceManager } from '../ioc/types';
 import { TensorBoardImportCodeLensProvider } from './tensorBoardImportCodeLensProvider';
-import { TensorBoardFileWatcher } from './tensorBoardFileWatcher';
 import { TensorBoardUsageTracker } from './tensorBoardUsageTracker';
 import { TensorBoardPrompt } from './tensorBoardPrompt';
 import { TensorBoardSessionProvider } from './tensorBoardSessionProvider';
@@ -14,8 +13,6 @@ import { TerminalWatcher } from './terminalWatcher';
 export function registerTypes(serviceManager: IServiceManager): void {
     serviceManager.addSingleton<TensorBoardSessionProvider>(TensorBoardSessionProvider, TensorBoardSessionProvider);
     serviceManager.addBinding(TensorBoardSessionProvider, IExtensionSingleActivationService);
-    serviceManager.addSingleton<TensorBoardFileWatcher>(TensorBoardFileWatcher, TensorBoardFileWatcher);
-    serviceManager.addBinding(TensorBoardFileWatcher, IExtensionSingleActivationService);
     serviceManager.addSingleton<TensorBoardPrompt>(TensorBoardPrompt, TensorBoardPrompt);
     serviceManager.addSingleton<IExtensionSingleActivationService>(
         IExtensionSingleActivationService,
