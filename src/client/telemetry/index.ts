8adbf18c73277fb7524c39aa560fea6dd5ecdd04
Brett Cannon <brcan@microsoft.com>
2021-12-14 14:47:57 -0800

    Add in missing telemetry

--- src/client/telemetry/index.ts
+++ src/client/telemetry/index.ts
@@ -608,7 +608,9 @@ export interface IEventNamePropertyMapping {
      * Telemetry event sent when attaching to child process
      */
     /* __GDPR__
-       "debugger.attach_to_child_process" : { }
+       "debugger.attach_to_child_process" : {
+           "duration" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "isMeasurement": true }
+       }
      */
     [EventName.DEBUGGER_ATTACH_TO_CHILD_PROCESS]: never | undefined;
     /**
@@ -733,7 +735,10 @@ export interface IEventNamePropertyMapping {
        "editor.load" : {
           "codeloadingtime" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" },
           "condaversion" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" },
+          "errorname" : { "classification": "CallstackOrException", "purpose": "PerformanceAndHealth" },
+          "errorstack" : { "classification": "CallstackOrException", "purpose": "PerformanceAndHealth" },
           "pythonversion" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" },
+          "installsource" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" },
           "interpretertype" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" },
           "terminal" : { "classification": "SystemMetaData", "purpose": "PerformanceAndHealth" },
           "workspacefoldercount" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" },
@@ -852,6 +857,9 @@ export interface IEventNamePropertyMapping {
      */
     /* __GDPR__
        "format.format" : {
+          "duration" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "isMeasurement": true },
+          "errorname" : { "classification": "CallstackOrException", "purpose": "PerformanceAndHealth" },
+          "errorstack" : { "classification": "CallstackOrException", "purpose": "PerformanceAndHealth" },
           "tool" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" },
           "hascustomargs" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" },
           "formatselection" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" }
@@ -891,7 +899,9 @@ export interface IEventNamePropertyMapping {
      * Telemetry event sent when sorting imports using formatter
      */
     /* __GDPR__
-       "format.sort_imports" : { }
+       "format.sort_imports" : {
+           "duration" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "isMeasurement": true }
+       }
      */
     [EventName.FORMAT_SORT_IMPORTS]: never | undefined;
     /**
@@ -1007,6 +1017,7 @@ export interface IEventNamePropertyMapping {
      */
     /* __GDPR__
        "linting" : {
+           "duration" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "isMeasurement": true },
           "tool" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" },
           "hascustomargs" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" },
           "trigger" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" },
@@ -1066,7 +1077,9 @@ export interface IEventNamePropertyMapping {
      * Telemetry event sent when 'Select Interpreter' command is invoked.
      */
     /* __GDPR__
-       "select_interpreter" : { }
+       "select_interpreter" : {
+           "duration" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "isMeasurement": true }
+       }
      */
     [EventName.SELECT_INTERPRETER]: never | undefined;
     /**
@@ -1290,6 +1303,7 @@ export interface IEventNamePropertyMapping {
      */
     /* __GDPR__
        "python_interpreter_discovery" : {
+           "duration" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "isMeasurement": true },
           "interpreters" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "isMeasurement": true }
        }
      */
@@ -1750,7 +1764,9 @@ export interface IEventNamePropertyMapping {
      * Telemetry event sent when starting REPL
      */
     /* __GDPR__
-       "repl" : { }
+       "repl" : {
+           "duration" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "isMeasurement": true }
+       }
      */
     [EventName.REPL]: never | undefined;
     /**
