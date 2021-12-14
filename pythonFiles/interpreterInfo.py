a47de59872b3be03c258fdf710412deaed542e90
Kartik Raj <karraj@microsoft.com>
2021-12-14 19:01:20 +0530

    Update tests

--- pythonFiles/interpreterInfo.py
+++ pythonFiles/interpreterInfo.py
@@ -10,6 +10,6 @@ obj["sysPrefix"] = sys.prefix
 obj["sysVersion"] = sys.version
 obj["is64Bit"] = sys.maxsize > 2 ** 32
 
-print('>>>JSON')
+print('>>>JSON') # Printing out markers for our JSON to make it more resilient to pull it out.
 print(json.dumps(obj))
 print('<<<JSON')
