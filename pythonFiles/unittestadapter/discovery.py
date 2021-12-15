52785d695c83446b59d45093825151eb5384db74
Kim-Adeline Miguel <kimiguel@microsoft.com>
2021-12-15 14:52:34 -0800

    Build tree on the Python side + move utils out

--- pythonFiles/unittestadapter/discovery.py
+++ pythonFiles/unittestadapter/discovery.py
@@ -1,10 +1,12 @@
 import getopt
-import inspect
 import os
+from typing import Any, List, Tuple
 import requests
 import sys
 import traceback
 import unittest
+
+from utils import TestNode, build_test_tree
 
 sys.path.insert(0, os.getcwd())
 
@@ -19,10 +21,14 @@ unittest_args, _ = getopt.getopt(
     ["start-directory=", "pattern=", "top-level-directory="],
 )
 
+Arguments = Tuple[str, Any]
 
-# Parse command-line arguments that should be processed by the script,
-# for example the port number that it needs to connect to.
-def parse_script_args(args):
+
+def parse_port(args: List[Arguments]) -> int:
+    """
+    Parse command-line arguments that should be processed by the script,
+    for example the port number that it needs to connect to.
+    """
     # The port is passed to the discovery.py script when it is executed,
     # defaults to 45454 if it can't be parsed.
     port = 45454
@@ -32,9 +38,11 @@ def parse_script_args(args):
     return port
 
 
-# Parse command-line arguments that should be forwarded to unittest.
-# Unittest arguments are: -v, -s, -p, -t, in that order.
-def parse_unittest_args(args):
+def parse_unittest_args(args: List[Arguments]) -> Tuple[str, str, str]:
+    """
+    Parse command-line arguments that should be forwarded to unittest.
+    Unittest arguments are: -v, -s, -p, -t, in that order.
+    """
     pattern = "test*.py"
     top_level_dir = None
 
@@ -49,85 +57,27 @@ def parse_unittest_args(args):
     return start_dir, pattern, top_level_dir
 
 
-# Helper functions
-def get_test_cases(suite):
-    for test in suite:
-        if isinstance(test, unittest.TestCase):
-            yield test
-        else:
-            for test_case in get_test_cases(test):
-                yield test_case
+if __name__ == "__main__":
+    # Test discovery
+    start_dir, pattern, top_level_dir = parse_unittest_args(unittest_args)
+    cwd = os.path.abspath(start_dir)
+    payload = {"cwd": cwd}
+    tests: TestNode = {}
 
-
-def get_source_line(obj):
     try:
-        sourcelines, lineno = inspect.getsourcelines(obj)
+        loader = unittest.TestLoader()
+        suite = loader.discover(start_dir, pattern, top_level_dir)
+
+        tests, errors = build_test_tree(suite, cwd)
     except:
-        try:
-            sourcelines, lineno = inspect.getsourcelines(obj.orig_method)
-        except:
-            return "*"
+        payload["status"] = "error"
+        payload["errors"] = [traceback.format_exc()]
 
-    # Return the line number of the first line of the test case definition.
-    for i, v in enumerate(sourcelines):
-        if v.strip().startswith(("def", "async def")):
-            return str(lineno + i)
-    return "*"
+    payload["tests"] = tests
 
+    if len(errors):
+        payload["errors"] = errors
 
-# Test discovery
-start_dir, pattern, top_level_dir = parse_unittest_args(unittest_args)
-payload = {"cwd": os.path.abspath(start_dir)}
-tests = []
-
-try:
-    loader = unittest.TestLoader()
-    suite = loader.discover(start_dir, pattern, top_level_dir)
-    loader_errors = []
-
-    for test in get_test_cases(suite):
-        test_id = test.id()
-        test_method = getattr(test, test._testMethodName)
-        lineno = get_source_line(test_method)
-
-        # Build payload
-        if test_id.startswith("unittest.loader._FailedTest"):
-            loader_errors.append(test._exception.__str__())
-        else:
-            tests.append({"id": test_id, "lineno": lineno})
-except:
-    payload["status"] = "error"
-    payload["errors"] = [traceback.format_exc()]
-
-payload["tests"] = tests
-
-if len(loader_errors):
-    payload["errors"] = loader_errors
-
-# Connect to the TypeScript side and send payload.
-port = parse_script_args(script_args)
-requests.post(f"http://localhost:{port}", json=payload)
-
-
-# Message formatting:
-#
-# Successful discovery:
-# {
-#     "cwd": <test discovery directory>
-#     "status": "success",
-#     "tests":[
-#         {
-#           "id": <test id>,
-#           "lineno": <source line>,
-#         },
-#         ...
-#     ]
-# }
-#
-# Error:
-# {
-#     "cwd": <test discovery directory>
-#     "errors": [list of errors]
-#     "status": "error",
-# }
-#
+    # Connect to the TypeScript side and send payload.
+    port = parse_port(script_args)
+    requests.post(f"http://localhost:{port}", json=payload)
---
+++ pythonFiles/unittestadapter/utils.py
@@ -0,0 +1,185 @@
+from enum import Enum
+import inspect
+import pathlib
+from typing import List, Tuple, TypedDict
+import unittest
+
+# Types
+
+
+class TestNodeTypeEnum(str, Enum):
+    class_ = "class"
+    file = "file"
+    folder = "folder"
+    test = "test"
+
+
+class TestData(TypedDict):
+    name: str
+    path: str
+    type_: TestNodeTypeEnum
+
+
+class TestItem(TestData):
+    id_: str
+    lineno: str
+
+
+class TestNode(TestData):
+    children: "List[TestNode | TestItem]"
+
+
+# Helper functions for data retrieval.
+
+
+def get_test_case(suite):
+    """
+    Iterate through a unittest test suite and return all test cases.
+    """
+    for test in suite:
+        if isinstance(test, unittest.TestCase):
+            yield test
+        else:
+            for test_case in get_test_case(test):
+                yield test_case
+
+
+def get_source_line(obj) -> str:
+    """
+    Get the line number of a test case start line.
+    """
+    try:
+        sourcelines, lineno = inspect.getsourcelines(obj)
+    except:
+        try:
+            sourcelines, lineno = inspect.getsourcelines(obj.orig_method)
+        except:
+            return "*"
+
+    # Return the line number of the first line of the test case definition.
+    for i, v in enumerate(sourcelines):
+        if v.strip().startswith(("def", "async def")):
+            return str(lineno + i)
+    return "*"
+
+
+# Helper functions for test tree building.
+
+
+def build_test_node(path: str, name: str, type: TestNodeTypeEnum) -> TestNode:
+    """
+    Build a test node with no children. A test node can be a folder, a file or a class.
+    """
+    return {
+        "path": path,
+        "name": name,
+        "type": type,
+        "children": [],
+    }
+
+
+def find_child_node(name: str, type: TestNodeTypeEnum, root: TestNode) -> TestNode:
+    """
+    Find a child node in a test tree given its name and type.
+    """
+    try:
+        result = next(
+            node
+            for node in root["children"]
+            if node["name"] == name and node["type"] == type
+        )
+    except StopIteration:
+        result = build_test_node(f'{root["path"]}/{name}', name, type)
+        root["children"].append(result)
+
+    return result
+
+
+def build_test_tree(
+    suite: unittest.TestSuite, test_directory: str
+) -> Tuple[TestNode, List[str]]:
+    """
+    Build a test tree from a unittest test suite.
+
+    Payload format for a successful discovery:
+    {
+        "status": "success",
+        "cwd": <test discovery directory>,
+        "tests": [
+            {
+                "path": <file or folder path>,
+                "type": "file" or "folder",
+                "name": <file or folder name>,
+                "children": [
+                    { files and folders }
+                    {
+                        "path": <file or folder path>,
+                        "name": <test name>,
+                        "type": "test",
+                        "lineno": <line number>
+                        "id": <test case id>,
+                    }
+                ]
+            }
+        ]
+    }
+
+    Payload format when there are errors:
+    {
+        "cwd": <test discovery directory>
+        "errors": [list of errors]
+        "status": "error",
+    }
+
+
+    """
+
+    errors = []
+    directory_path = pathlib.PurePath(test_directory)
+    root = build_test_node(test_directory, directory_path.name, TestNodeTypeEnum.folder)
+
+    for test_case in get_test_case(suite):
+        test_id = test_case.id()
+        if test_id.startswith("unittest.loader._FailedTest"):
+            errors.append(test_case._exception.__str__())
+        else:
+            # Get test path components
+            folders = test_id.split(".")
+            function_name = folders.pop()
+            class_name = folders.pop()
+            filename = folders.pop()
+            py_filename = f"{filename}.py"
+
+            current_node = root
+
+            # Find/build folder nodes
+            for folder in folders:
+                current_node = find_child_node(
+                    folder, TestNodeTypeEnum.folder, current_node
+                )
+
+            # Find/build file node
+            current_node = find_child_node(
+                py_filename, TestNodeTypeEnum.file, current_node
+            )
+
+            # Find/build class node
+            current_node = find_child_node(
+                class_name, TestNodeTypeEnum.class_, current_node
+            )
+
+            # Get test line number
+            test_method = getattr(test_case, test_case._testMethodName)
+            lineno = get_source_line(test_method)
+
+            # Add test
+            test_node: TestItem = {
+                "id_": test_id,
+                "name": function_name,
+                "path": f'{current_node["path"]}/{function_name}',
+                "lineno": lineno,
+                "type_": TestNodeTypeEnum.test,
+            }
+            current_node["children"].append(test_node)
+
+    return root, errors
