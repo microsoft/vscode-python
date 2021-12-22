5876dde381cc428009d12c5abb1e49072d771393
Kim-Adeline Miguel <kimiguel@microsoft.com>
2021-12-22 15:08:10 -0800

    Add test tree update support

--- src/client/testing/testController/workspaceTestAdapter.ts
+++ src/client/testing/testController/workspaceTestAdapter.ts
@@ -124,13 +124,18 @@ export class WorkspaceTestAdapter {
                 const rootPath = rawTestData.cwd;
                 const workspaceNode = testController.items.get(rootPath);
 
-                // If the test root exists: Workspace refresh, update its children.
-                // Otherwise, it is a freshly discovered workspace, and we need to create a new test root and populate the test tree.
-                if (workspaceNode) {
-                    console.warn(`Update test tree with:\n${rawTestData}`);
-                    // updateTestTree(testController, workspaceNode, parsedData, token);
-                } else if (rawTestData.tests) {
-                    populateTestTree(testController, rawTestData.tests, undefined, token);
+                if (rawTestData.tests) {
+                    // If the test root exists: Workspace refresh, update its children.
+                    // Otherwise, it is a freshly discovered workspace, and we need to create a new test root and populate the test tree.
+                    if (workspaceNode) {
+                        console.warn(`Update test tree with:\n${rawTestData}`);
+                        updateTestTree(testController, rawTestData.tests, this.testData, undefined, token);
+                    } else {
+                        populateTestTree(testController, rawTestData.tests, undefined, token);
+                    }
+                } else {
+                    // Delete everything from the test controller.
+                    testController.items.replace([]);
                 }
 
                 // Save new test data state.
@@ -149,17 +154,97 @@ function isTestItem(test: DiscoveredTestNode | DiscoveredTestItem): test is Disc
     return test.type_ === 'test';
 }
 
-// function updateTestTree(
-//     testController: TestController,
-//     testRoot: TestItem,
-//     parsedTestData: UParsedTestNode,
-//     token?: CancellationToken,
-// ): void {
-// /**
-//  * Algorithm:
-//  *
-//  */
-// }
+function deleteTestTree(testController: TestController, root?: TestItem) {
+    if (root) {
+        const { children } = root;
+
+        children.forEach((child) => {
+            deleteTestTree(testController, child);
+
+            const { id } = child;
+            testController.items.delete(id);
+        });
+
+        testController.items.delete(root.id);
+    }
+}
+
+function updateTestTree(
+    testController: TestController,
+    updatedData: DiscoveredTestNode,
+    localData: DiscoveredTestNode | undefined,
+    testRoot: TestItem | undefined,
+    token?: CancellationToken,
+): void {
+    // If testRoot is undefined, use the info of the root item of testTreeData to create a test item, and append it to the test controller.
+    if (!testRoot) {
+        testRoot = testController.createTestItem(updatedData.path, updatedData.name, Uri.file(updatedData.path));
+        testRoot.canResolveChildren = true;
+        testRoot.tags = [RunTestTag, DebugTestTag];
+
+        testController.items.add(testRoot);
+    }
+
+    // Delete existing items if they don't exist in the updated tree.
+    if (localData) {
+        localData.children.forEach((local) => {
+            if (!token?.isCancellationRequested) {
+                const exists = updatedData.children.find(
+                    (node) => local.name === node.name && local.path === node.path && local.type_ === node.type_,
+                );
+
+                if (!exists) {
+                    // Delete this node and all its children.
+                    const testItem = testController.items.get(local.path);
+                    deleteTestTree(testController, testItem);
+                }
+            }
+        });
+    }
+
+    // Go through the updated tree, update the existing nodes, and create new ones if necessary.
+    updatedData.children.forEach((child) => {
+        if (!token?.isCancellationRequested) {
+            const root = testController.items.get(child.path);
+            if (root) {
+                root.busy = true;
+                // Update existing test node or item.
+                if (isTestItem(child)) {
+                    // Update the only property that can be updated.
+                    root.label = child.name;
+                } else {
+                    const localNode = localData?.children.find(
+                        (node) => child.name === node.name && child.path === node.path && child.type_ === node.type_,
+                    );
+                    updateTestTree(testController, child, localNode as DiscoveredTestNode, root, token);
+                }
+                root.busy = false;
+            } else {
+                // Create new test node or item.
+                let testItem;
+                if (isTestItem(child)) {
+                    testItem = testController.createTestItem(child.id_, child.name, Uri.file(child.path));
+                    const range = new Range(new Position(child.lineno - 1, 0), new Position(child.lineno, 0));
+
+                    testItem.canResolveChildren = false;
+                    testItem.tags = [RunTestTag, DebugTestTag];
+                    testItem.range = range;
+
+                    testRoot!.children.add(testItem);
+                } else {
+                    testItem = testController.createTestItem(child.path, child.name, Uri.file(child.path));
+                    testItem.canResolveChildren = true;
+                    testItem.tags = [RunTestTag, DebugTestTag];
+
+                    testRoot!.children.add(testItem);
+
+                    // Populate the test tree under the newly created node.
+                    populateTestTree(testController, child, testItem, token);
+                }
+            }
+        }
+    });
+}
 
 function populateTestTree(
     testController: TestController,
