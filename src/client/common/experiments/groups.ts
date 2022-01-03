92af252df93a9d8ef4d825d4f3f92a319f2e4fcb
Karthik Nadig <kanadig@microsoft.com>
2022-01-03 12:10:25 -0800

    Remove `pythonTensorboardExperiment` Experiment

--- src/client/common/experiments/groups.ts
+++ src/client/common/experiments/groups.ts
@@ -10,11 +10,6 @@ export enum DeprecatePythonPath {
     experiment = 'pythonDeprecatePythonPath',
 }
 
-// Feature flag for 'Python: Launch TensorBoard' feature
-export enum NativeTensorBoard {
-    experiment = 'pythonTensorboardExperiment',
-}
-
 // Feature gate to control whether we install the PyTorch profiler package
 // torch.profiler release is being delayed till end of March. This allows us
 // to turn on the profiler plugin install functionality between releases
