3d7c2bb8b7aebc25fbd91b327733aabc8a1acae9
Kim-Adeline Miguel <kimiguel@microsoft.com>
2022-01-10 16:33:16 -0800

    I don't know how Python works

--- pythonFiles/tests/unittestadapter/conftest.py
+++ pythonFiles/tests/unittestadapter/conftest.py
@@ -1,6 +1,9 @@
-# content of conftest.py
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 import sys
 
 # Ignore the contents of this folder for Python2 tests.
+collect_ignore = []
 if sys.version_info[0] < 3:
     collect_ignore.append(".")
