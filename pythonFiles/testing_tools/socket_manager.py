f90fc7126555c58f071b4b9de759bb8e9cf2f5be
Kim-Adeline Miguel <kimiguel@microsoft.com>
2022-01-21 11:51:58 -0800

    Add description to the SocketManager class

--- pythonFiles/testing_tools/socket_manager.py
+++ pythonFiles/testing_tools/socket_manager.py
@@ -6,6 +6,18 @@ import sys
 
 
 class SocketManager(object):
+    """Create a socket and connect to the given address.
+
+    The address is a (host: str, port: int) tuple.
+    Example usage:
+
+    ```
+    with SocketManager(("localhost", 6767)) as sock:
+        request = json.dumps(payload)
+        result = s.socket.sendall(request.encode("utf-8"))
+    ```
+    """
+
     def __init__(self, addr):
         self.socket = None
         self.addr = addr
