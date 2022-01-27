a94af8ca8acefb8d349a3f40f7cb10ba9b47c5d0
Karthik Nadig <kanadig@microsoft.com>
2022-01-27 13:07:37 -0800

    Fix linting under conda run.

--- src/client/linters/pylint.ts
+++ src/client/linters/pylint.ts
@@ -25,12 +25,10 @@ export class Pylint extends BaseLinter {
     }
 
     protected async runLinter(document: TextDocument, cancellation: CancellationToken): Promise<ILintMessage[]> {
-        const { uri } = document;
-        const settings = this.configService.getSettings(uri);
-        const args = ['--reports=n', '--output-format=json', uri.fsPath];
+        const args = [document.uri.fsPath];
         const messages = await this.run(args, document, cancellation);
         messages.forEach((msg) => {
-            msg.severity = this.parseMessagesSeverity(msg.type, settings.linting.pylintCategorySeverity);
+            msg.severity = this.parseMessagesSeverity(msg.type, this.pythonSettings.linting.pylintCategorySeverity);
         });
 
         return messages;
