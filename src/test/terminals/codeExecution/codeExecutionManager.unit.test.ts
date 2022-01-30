3dd72cb9628434a5b985b5a405aca139c8a7da93
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-01-30 09:41:20 +0000

    Use IAppShell to display message for testing

--- src/test/terminals/codeExecution/codeExecutionManager.unit.test.ts
+++ src/test/terminals/codeExecution/codeExecutionManager.unit.test.ts
@@ -10,7 +10,10 @@ import { IFileSystem } from '../../../client/common/platform/types';
 import { IServiceContainer } from '../../../client/ioc/types';
 import { CodeExecutionManager } from '../../../client/terminals/codeExecution/codeExecutionManager';
 import { ICodeExecutionHelper, ICodeExecutionManager, ICodeExecutionService } from '../../../client/terminals/types';
-import * as vscode from 'vscode';
+
+interface IAppShell {
+    showErrorMessage(message: string, ...items: string[]): Promise<string | undefined>;
+}
 
 suite('Terminal - Code Execution Manager', () => {
     let executionManager: ICodeExecutionManager;
@@ -275,10 +278,12 @@ suite('Terminal - Code Execution Manager', () => {
         );
         helper.verifyAll();
     }
-    async function checkErrorMessage(this: any): Promise<void> {
+    async function checkErrorMessage(this: any): Promise<Error[] | undefined> {
         const activeEditor = this.documentManager.activeTextEditor;
         if (!activeEditor) {
-            vscode.window.showErrorMessage('Open an active editor before executing code');
+            const appShell: IAppShell = (window as any) as IAppShell;
+            appShell.showErrorMessage('Open an active editor before executing code');
+            return [new Error('No active editor')];
         } else {
             return undefined;
         }
