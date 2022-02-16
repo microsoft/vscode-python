a441e7784cebdbd355a3e8fbf9d056f549e50b2f
luabud <luabud@microsoft.com>
2022-02-16 09:24:27 -0800

    Attempt to fix test

--- src/test/common/application/commands/createNewFileCommand.unit.test.ts
+++ src/test/common/application/commands/createNewFileCommand.unit.test.ts
@@ -1,16 +1,25 @@
-import { anything, instance, mock, verify } from 'ts-mockito';
+import { anything, instance, mock, verify, when } from 'ts-mockito';
 import { Commands } from '../../../../client/common/constants';
 import { CommandManager } from '../../../../client/common/application/commandManager';
 import { CreatePythonFileCommandHandler } from '../../../../client/common/application/commands/createFileCommand';
-import { ICommandManager } from '../../../../client/common/application/types';
+import { ICommandManager, IWorkspaceService } from '../../../../client/common/application/types';
+import { MockWorkspaceConfiguration } from '../../../mocks/mockWorkspaceConfig';
 
 suite('Create New Python File Commmand', () => {
     let createNewFileCommandHandler: CreatePythonFileCommandHandler;
     let cmdManager: ICommandManager;
+    let workspaceService: IWorkspaceService;
 
     setup(async () => {
         cmdManager = mock(CommandManager);
+
         createNewFileCommandHandler = new CreatePythonFileCommandHandler(instance(cmdManager));
+        when(cmdManager.executeCommand(anything())).thenResolve();
+        when(workspaceService.getConfiguration('python')).thenReturn(
+            new MockWorkspaceConfiguration({
+                createNewFileEnabled: true,
+            }),
+        );
         await createNewFileCommandHandler.activate();
     });
 
