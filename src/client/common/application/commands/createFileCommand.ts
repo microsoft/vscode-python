eff117255da6ccc5125d5a01cbf733d15e0179d8
luabud <luabud@microsoft.com>
2022-02-15 18:11:04 -0800

    Add telemetry and add experimental setting

--- src/client/common/application/commands/createFileCommand.ts
+++ src/client/common/application/commands/createFileCommand.ts
@@ -3,6 +3,8 @@ import { IExtensionSingleActivationService } from '../../../activation/types';
 import { Commands } from '../../constants';
 import { ICommandManager } from '../types';
 import * as vscode from 'vscode';
+import { sendTelemetryEvent } from '../../../telemetry';
+import { EventName } from '../../../telemetry/constants';
 
 @injectable()
 export class CreatePythonFileCommandHandler implements IExtensionSingleActivationService {
@@ -11,11 +13,15 @@ export class CreatePythonFileCommandHandler implements IExtensionSingleActivatio
     constructor(@inject(ICommandManager) private readonly commandManager: ICommandManager) {}
 
     public async activate(): Promise<void> {
+        if (!vscode.workspace.getConfiguration('python').get<boolean>('createNewFileEnabled')) {
+            return;
+        }
         this.commandManager.registerCommand(Commands.CreateNewFile, this.createPythonFile, this);
     }
 
     public async createPythonFile(): Promise<void> {
         const newFile = await vscode.workspace.openTextDocument({ language: 'python' });
         vscode.window.showTextDocument(newFile);
+        sendTelemetryEvent(EventName.CREATE_NEW_FILE_COMMAND);
     }
 }
