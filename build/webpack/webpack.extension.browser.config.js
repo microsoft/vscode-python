123913ab6a84da19238b38e229191fa363cd9946
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-02-22 19:56:30 +0000

    Made changes as requested

--- build/webpack/webpack.extension.browser.config.js
+++ build/webpack/webpack.extension.browser.config.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-check
 // @ts-nocheck
 
 'use strict';
