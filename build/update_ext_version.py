5827312076f3a105b1d67ae25ab443557b9b47e5
Karthik Nadig <kanadig@microsoft.com>
2022-02-22 12:29:48 -0800

    Address comments.

--- build/update_ext_version.py
+++ build/update_ext_version.py
@@ -1,13 +1,18 @@
 import argparse
 import datetime
 import json
-import os
+import pathlib
 
-EXT_ROOT = os.path.dirname(os.path.dirname(__file__))
-PACKAGE_JSON_PATH = os.path.join(EXT_ROOT, "package.json")
+EXT_ROOT = pathlib.Path(__file__).parent.parent
+PACKAGE_JSON_PATH = EXT_ROOT / "package.json"
 
 
-def get_argparse():
+def build_arg_parse():
+    """Builds arguments parser for the following arguments.
+        --release: Validates release version number.
+        --build-id: Custom <micro> version for the build.
+        --for-publishing: Ensures that the version is parseable by marketplace.
+    """
     parser = argparse.ArgumentParser(
         description="This script updates the python extension micro version based on the release or pre-release channel."
     )
@@ -21,81 +26,82 @@ def get_argparse():
         action="store",
         type=int,
         default=None,
-        help="If persent will be used as a micro version.",
+        help="If present will be used as a micro version.",
         required=False,
     )
     parser.add_argument(
         "--for-publishing",
         action="store_true",
-        help="Preserves `-dev` or `-rc` suffix.",
+        help="Removes `-dev` or `-rc` suffix.",
     )
     return parser
 
 
 def is_even(v):
-    return int(v) % 2 == 0
+    """Returns True if `v` is even.
+    """
+    return not int(v) % 2
 
 
-def get_build_number():
-    return "1" + datetime.datetime.now(tz=datetime.timezone.utc).strftime("%j%H%M")
+def micro_build_number():
+    """Generates micro build number using the following format:
+    1 + <Julian day> + <hour> + <minute>
+    """
+    return f"1{datetime.datetime.now(tz=datetime.timezone.utc).strftime('%j%H%M')}"
 
 
 def parse_version(version):
-    major, minor, parts = version.split(".")
+    """Returns a tuple of major, minor, micro and suffix from a given string
+    """
+    major, minor, parts = version.split(".",maxsplit=2)
     try:
-        micro, suffix = parts.split("-")
-    except Exception:
+        micro, suffix = parts.split("-", maxsplit=1)
+    except ValueError:
         micro = parts
         suffix = ""
     return (major, minor, micro, suffix)
 
 
 def main():
-    parser = get_argparse()
+    parser = build_arg_parse()
     args = parser.parse_args()
 
-    with open(PACKAGE_JSON_PATH, "r", encoding="utf-8") as f:
-        package = json.load(f)
+    package = json.loads(PACKAGE_JSON_PATH.read_text(encoding="utf-8"))
 
     major, minor, micro, suffix = parse_version(package["version"])
 
     if args.release and not is_even(minor):
-        raise Exception(
-            "Release version should have EVEN numbered minor version: {}".format(
-                package["version"]
+         raise ValueError(
+                f"Release version should have EVEN numbered minor version: {package['version']}"
             )
-        )
-
-    if not args.release and is_even(minor):
-        raise Exception(
-            "Pre-release version should have ODD numbered minor version: {}".format(
-                package["version"]
+    elif not args.release and is_even(minor):
+        raise ValueError(
+                f"Pre-Release version should have ODD numbered minor version: {package['version']}"
             )
-        )
 
-    print("Updating build FROM: {}".format(package["version"]))
+
+    print(f"Updating build FROM: {package['version']}")
     if args.build_id:
         # If build id is provided it should fall within the 0-INT32 max range
-        # that the max allowed value for publishing to market place
+        # that the max allowed value for publishing to Marketplace.
         if args.for_publishing and (
             args.build_id < 0 or args.build_id > ((2**32) - 1)
         ):
             raise Exception("Build ID must be within [0, {}]".format((2**32) - 1))
 
-        package["version"] = "{}.{}.{}".format(major, minor, args.build_id)
+        package["version"] = ".".join((major, minor, args.build_id))
     elif args.release:
-        package["version"] = "{}.{}.{}".format(major, minor, micro)
+        package["version"] = ".".join((major, minor, micro))
     else:
-        # micro version only updated for pre-release
-        package["version"] = "{}.{}.{}".format(major, minor, get_build_number())
+        # micro version only updated for pre-release.
+        package["version"] = ".".join((major, minor, micro_build_number()))
 
-    if not args.for_publishing and not args.release and len(suffix) > 0:
+    if not args.for_publishing and not args.release and len(suffix):
         package["version"] += "-" + suffix
-    print("Updating build TO: {}".format(package["version"]))
+    print(f"Updating build TO: {package['version']}")
 
-    with open(PACKAGE_JSON_PATH, "w", encoding="utf-8") as f:
-        json.dump(package, f, indent=4, ensure_ascii=False)
-        print("", file=f)  # add a new line at the end of the file
+    # Overwrite package.json with new data add a new-line at the end of the file.
+    PACKAGE_JSON_PATH.write_text(json.dumps(package, indent=4, ensure_ascii=False) + "\n", encoding="utf-8")
 
 
 if __name__ == "__main__":
