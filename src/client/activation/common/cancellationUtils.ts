939571cf6fdf12323b8a2c1189f12580f446d1c8
Erik De Bonte <erikd@microsoft.com>
2022-03-21 14:01:16 -0700

    Update to latest LSP packages

--- src/client/activation/common/cancellationUtils.ts
+++ src/client/activation/common/cancellationUtils.ts
@@ -43,7 +43,7 @@ class FileCancellationSenderStrategy implements CancellationSenderStrategy {
         tryRun(() => fs.mkdirSync(folder, { recursive: true }));
     }
 
-    public async sendCancellation(_: MessageConnection, id: CancellationId): Promise<void> {
+    public async sendCancellation(_: MessageConnection, id: CancellationId) {
         const file = getCancellationFilePath(this.folderName, id);
         tryRun(() => fs.writeFileSync(file, '', { flag: 'w' }));
     }
