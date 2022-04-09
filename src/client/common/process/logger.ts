751ad57552e3130fc17e0ca2dd6bfb06ad7cbab0
bill sun <sunyinqi0508@gmail.com>
2022-04-09 10:17:45 +0800

    Only replace '\\' with '(\\|/)' on Windows to save a scan on other OSs.

--- src/client/common/process/logger.ts
+++ src/client/common/process/logger.ts
@@ -54,9 +54,8 @@ function replaceMatchesWithCharacter(original: string, match: string, character:
     // Backslashes, plus signs, brackets and other characters have special meaning in regexes,
     // we need an extra backlash so it's not considered special. Also match both forward and
     // backward slash versions of 'match' for Windows.
-    const seperatorPattern = getOSType() === OSType.Windows ? '(\\\\|/)' : '\\\\';
-    const pattern = escapeRegExp(match).replaceAll('\\\\', seperatorPattern);
+    const pattern = escapeRegExp(match);
 
-    let regex = new RegExp(pattern, 'ig');
+    let regex = new RegExp(getOSType() === OSType.Windows ? pattern.replaceAll('\\\\', '(\\\\|/)') : pattern, 'ig');
     return original.replace(regex, character);
 }
