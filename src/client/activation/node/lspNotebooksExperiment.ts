42bb8b3f14fa88870ab21368288285b47de9b31e
Erik De Bonte <erikd@microsoft.com>
2022-04-26 13:47:34 -0700

    Determine if experiment is supported via extension versions; telemetry for supported case

--- src/client/activation/node/lspNotebooksExperiment.ts
+++ src/client/activation/node/lspNotebooksExperiment.ts
@@ -1,8 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import { inject, injectable } from 'inversify';
+import { extensions } from 'vscode';
+import * as semver from 'semver';
 import { IConfigurationService } from '../../common/types';
 import { IExtensionSingleActivationService } from '../types';
+import { sendTelemetryEvent } from '../../telemetry';
+import { EventName } from '../../telemetry/constants';
+import { JUPYTER_EXTENSION_ID, PYLANCE_EXTENSION_ID } from '../../common/constants';
 
 @injectable()
 export class LspNotebooksExperiment implements IExtensionSingleActivationService {
@@ -10,13 +15,32 @@ export class LspNotebooksExperiment implements IExtensionSingleActivationService
 
     private _isInNotebooksExperiment?: boolean;
 
+    private _jupyterVersion: string | undefined;
+
+    private _pylanceVersion: string | undefined;
+
     constructor(@inject(IConfigurationService) private readonly configurationService: IConfigurationService) {}
 
     public async activate(): Promise<void> {
         this._isInNotebooksExperiment = this.configurationService.getSettings().pylanceLspNotebooksEnabled;
+        this._jupyterVersion = extensions.getExtension(JUPYTER_EXTENSION_ID)?.packageJSON.version;
+        this._pylanceVersion = extensions.getExtension(PYLANCE_EXTENSION_ID)?.packageJSON.version;
+
+        if (this._supportsNotebooksExperiment()) {
+            sendTelemetryEvent(EventName.PYTHON_EXPERIMENTS_LSP_NOTEBOOKS);
+        }
     }
 
     public isInNotebooksExperiment(): boolean | undefined {
-        return this._isInNotebooksExperiment;
+        return this._isInNotebooksExperiment && this._supportsNotebooksExperiment();
+    }
+
+    private _supportsNotebooksExperiment(): boolean {
+        return (
+            this._jupyterVersion !== undefined &&
+            semver.satisfies(this._jupyterVersion, '>=2022.4.100') &&
+            this._pylanceVersion !== undefined &&
+            semver.satisfies(this._pylanceVersion, '>=2022.4.4-pre.1 || 9999.0.0-dev')
+        );
     }
 }
