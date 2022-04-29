07ed0db3c55664a60eb845623067f3c5e9974610
Erik De Bonte <erikd@microsoft.com>
2022-04-29 16:26:44 -0700

    Fix issues in analysisOptions.ts

--- src/client/activation/node/analysisOptions.ts
+++ src/client/activation/node/analysisOptions.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { LanguageClientOptions } from 'vscode-languageclient';
 import { WorkspaceFolder } from 'vscode';
+import { LanguageClientOptions } from 'vscode-languageclient';
 import { DocumentFilter } from 'vscode-languageserver-protocol';
 import { IWorkspaceService } from '../../common/application/types';
 
@@ -12,7 +12,9 @@ import { LspNotebooksExperiment } from './lspNotebooksExperiment';
 
 export class NodeLanguageServerAnalysisOptions extends LanguageServerAnalysisOptionsBase {
     // eslint-disable-next-line @typescript-eslint/no-useless-constructor
-    constructor(lsOutputChannel: ILanguageServerOutputChannel, workspace: IWorkspaceService,
+    constructor(
+        lsOutputChannel: ILanguageServerOutputChannel,
+        workspace: IWorkspaceService,
         private readonly lspNotebooksExperiment: LspNotebooksExperiment,
     ) {
         super(lsOutputChannel, workspace);
@@ -23,14 +25,14 @@ export class NodeLanguageServerAnalysisOptions extends LanguageServerAnalysisOpt
         return ({
             experimentationSupport: true,
             trustedWorkspaceSupport: true,
-            lspNotebooksSupport: this.lspNotebooksExperiment.isInNotebooksExperiment() == true,
+            lspNotebooksSupport: this.lspNotebooksExperiment.isInNotebooksExperiment() === true,
         } as unknown) as LanguageClientOptions;
     }
 
     protected async getDocumentFilters(_workspaceFolder?: WorkspaceFolder): Promise<DocumentFilter[]> {
-        let filters = await super.getDocumentFilters(_workspaceFolder);
+        const filters = await super.getDocumentFilters(_workspaceFolder);
 
-        if (this.lspNotebooksExperiment.isInNotebooksExperiment() == true) {
+        if (this.lspNotebooksExperiment.isInNotebooksExperiment() === true) {
             return [
                 { language: 'python' },
                 {
