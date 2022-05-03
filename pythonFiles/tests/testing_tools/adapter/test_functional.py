f35d4f25d5bf2de468c268f9d578b6114b9f46d1
Karthik Nadig <kanadig@microsoft.com>
2022-05-03 12:00:41 -0700

    Fix test_functional.py

--- pythonFiles/tests/testing_tools/adapter/test_functional.py
+++ pythonFiles/tests/testing_tools/adapter/test_functional.py
@@ -10,8 +10,9 @@ import subprocess
 import sys
 import unittest
 
+from testing_tools.adapter.util import PATH_SEP, fix_path
+
 from ...__main__ import TESTING_TOOLS_ROOT
-from testing_tools.adapter.util import fix_path, PATH_SEP
 
 # Pytest 3.7 and later uses pathlib/pathlib2 for path resolution.
 try:
@@ -166,7 +167,9 @@ class PytestTests(unittest.TestCase):
         result[0]["tests"] = fix_test_order(result[0]["tests"])
 
         self.maxDiff = None
-        self.assertEqual(sorted_object(result), sorted_object(expected))
+        a = sorted_object(result)
+        b = sorted_object(expected)
+        self.assertEqual(a, b)
 
     def test_discover_complex_doctest(self):
         projroot, _ = resolve_testroot("complex")
@@ -1272,6 +1275,13 @@ COMPLEX = {
             "parentid": "./tests/test_pytest.py::test_param_fixture",
         },
         {
+            "id": "./tests/test_pytest.py::test_param_mark_fixture[(1+0j)]",
+            "name": "test_param_mark_fixture[(1+0j)]",
+            "source": fix_path("./tests/test_pytest.py:207"),
+            "markers": [],
+            "parentid": "./tests/test_pytest.py::test_param_mark_fixture",
+        },
+        {
             "id": "./tests/test_pytest.py::test_param_mark_fixture[x0]",
             "name": "test_param_mark_fixture[x0]",
             "source": fix_path("./tests/test_pytest.py:207"),
@@ -1281,13 +1291,6 @@ COMPLEX = {
         {
             "id": "./tests/test_pytest.py::test_param_mark_fixture[x1]",
             "name": "test_param_mark_fixture[x1]",
-            "source": fix_path("./tests/test_pytest.py:207"),
-            "markers": [],
-            "parentid": "./tests/test_pytest.py::test_param_mark_fixture",
-        },
-        {
-            "id": "./tests/test_pytest.py::test_param_mark_fixture[x2]",
-            "name": "test_param_mark_fixture[x2]",
             "source": fix_path("./tests/test_pytest.py:207"),
             "markers": [],
             "parentid": "./tests/test_pytest.py::test_param_mark_fixture",
