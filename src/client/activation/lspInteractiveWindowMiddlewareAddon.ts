1ae783af33aca592de6f621e050823e022be7003
Erik De Bonte <erikd@microsoft.com>
2022-07-14 22:06:00 -0700

    Need to switch to proto objects somehow

--- src/client/activation/lspInteractiveWindowMiddlewareAddon.ts
+++ src/client/activation/lspInteractiveWindowMiddlewareAddon.ts
@@ -1,9 +1,19 @@
 /* eslint-disable class-methods-use-this */
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { Disposable, NotebookCell, NotebookDocument, TextDocument, TextDocumentChangeEvent } from 'vscode';
+import {
+    Disposable,
+    NotebookCell,
+    NotebookDocument,
+    Position,
+    Range,
+    TextDocument,
+    TextDocumentChangeEvent,
+    TextDocumentContentChangeEvent,
+} from 'vscode';
 import {
     DidChangeNotebookDocumentNotification,
+    DidChangeTextDocumentParams,
     LanguageClient,
     Middleware,
     NotebookCellKind,
@@ -17,6 +27,8 @@ interface NotebookMetadata {
 interface InputBoxMetadata {
     textDocument: TextDocument;
 }
+
+type TextContent = Required<Required<Required<NotebookDocumentChangeEvent>['cells']>['textContent']>[0];
 
 /**
  * This class is a temporary solution to handling intellisense and diagnostics in python based notebooks.
@@ -76,7 +88,7 @@ export class LspInteractiveWindowMiddlewareAddon implements Middleware, Disposab
             };
 
             await this.getClient()?.sendNotification(DidChangeNotebookDocumentNotification.type, {
-                notebookDocument: { version: 0, uri: notebookUri.toString() },
+                notebookDocument: { version: 0, uri: notebookUri.toString() }, // TODO: Fix version
                 change: result,
             });
         } catch (error) {
@@ -95,19 +107,96 @@ export class LspInteractiveWindowMiddlewareAddon implements Middleware, Disposab
         const notebookUri = event.document.uri.with({ scheme: 'vscode-interactive', path: notebookPath });
         const notebookMetadata = this.notebookMetadataMap.get(notebookUri.toString());
         if (notebookMetadata) {
-            this.getClient()?.sendNotification(DidChangeNotebookDocumentNotification.method, {
-                notebookDocument: { uri: notebookUri.toString(), version: 0 },
+            this.getClient()?.sendNotification(DidChangeNotebookDocumentNotification.type, {
+                notebookDocument: { uri: notebookUri.toString(), version: 0 }, // TODO: Fix version
                 change: {
                     cells: {
-                        textContent: {
-                            document: event.document,
-                            changes: event.contentChanges,
-                        },
+                        textContent: [this._asTextContentChange(event)],
                     },
                 },
             });
         }
     }
+
+    private _asChangeTextDocumentParams(arg: TextDocumentChangeEvent): DidChangeTextDocumentParams {
+        // if (isTextDocument(arg)) {
+        // 	let result: proto.DidChangeTextDocumentParams = {
+        // 		textDocument: {
+        // 			uri: _uriConverter(arg.uri),
+        // 			version: arg.version
+        // 		},
+        // 		contentChanges: [{ text: arg.getText() }]
+        // 	};
+        // 	return result;
+        // } else if (isTextDocumentChangeEvent(arg)) {
+        const { document } = arg;
+        const result: DidChangeTextDocumentParams = {
+            textDocument: {
+                uri: document.uri.toString(),
+                version: document.version,
+            },
+            contentChanges: arg.contentChanges.map(
+                (change): TextDocumentContentChangeEvent => {
+                    const { range } = change;
+                    return {
+                        range: this.getClient()!.code2ProtocolConverter.asRange(range),
+                        //  new Range(
+                        //     new Position(range.start.line, range.start.character),
+                        //     new Position(range.end.line, range.end.character),
+                        // ),
+                        rangeLength: change.rangeLength,
+                        rangeOffset: change.rangeOffset,
+                        text: change.text,
+                    };
+                },
+            ),
+        };
+        return result;
+        // } else {
+        // 	throw Error('Unsupported text document change parameter');
+        // }
+    }
+
+    private _asTextContentChange(event: TextDocumentChangeEvent): TextContent {
+        const params = this._asChangeTextDocumentParams(event);
+        return { document: params.textDocument, changes: params.contentChanges };
+    }
+
+    // private _asNotebookDocumentChangeEvent(event: NotebookDocumentChangeEvent): NotebookDocumentChangeEvent {
+    //     const result: NotebookDocumentChangeEvent = Object.create(null);
+    //     // if (event.metadata) {
+    //     //     result.metadata = Converter.c2p.asMetadata(event.metadata);
+    //     // }
+    //     if (event.cells !== undefined) {
+    //         const cells: Required<NotebookDocumentChangeEvent>['cells'] = Object.create(null);
+    //         const changedCells = event.cells;
+    //         // if (changedCells.structure) {
+    //         //     cells.structure = {
+    //         //         array: {
+    //         //             start: changedCells.structure.array.start,
+    //         //             deleteCount: changedCells.structure.array.deleteCount,
+    //         //             cells: changedCells.structure.array.cells !== undefined ? changedCells.structure.array.cells.map(cell => Converter.c2p.asNotebookCell(cell, base)) : undefined
+    //         //         },
+    //         //         didOpen: changedCells.structure.didOpen !== undefined
+    //         //             ? changedCells.structure.didOpen.map(cell => base.asOpenTextDocumentParams(cell.document).textDocument)
+    //         //             : undefined,
+    //         //         didClose: changedCells.structure.didClose !== undefined
+    //         //             ? changedCells.structure.didClose.map(cell => base.asCloseTextDocumentParams(cell.document).textDocument)
+    //         //             : undefined
+    //         //     };
+    //         // }
+    //         // if (changedCells.data !== undefined) {
+    //         //     cells.data = changedCells.data.map(cell => Converter.c2p.asNotebookCell(cell, base));
+    //         // }
+    //         if (changedCells.textContent !== undefined) {
+    //             cells.textContent = changedCells.textContent.map((x) => this._asTextContentChange(x));
+    //         }
+    //         // if (Object.keys(cells).length > 0) {
+    //         //     result.cells = cells;
+    //         // }
+    //     }
+    //     return result;
+    // }
 
     public async didClose(document: TextDocument, next: (ev: TextDocument) => void): Promise<void> {
         if (document.uri.scheme !== 'vscode-interactive-input') {
@@ -126,14 +215,20 @@ export class LspInteractiveWindowMiddlewareAddon implements Middleware, Disposab
         cells: NotebookCell[],
         next: (notebookDocument: NotebookDocument, cells: NotebookCell[]) => void,
     ): Promise<void> {
-        if (notebookDocument.uri.scheme === 'vscode-interactive') {
-            this.notebookMetadataMap.set(notebookDocument.uri.toString(), { cellCount: notebookDocument.cellCount });
+        if (notebookDocument.uri.scheme !== 'vscode-interactive') {
+            await next(notebookDocument, cells);
+            return;
+        }
 
-            const inputBoxMetadata = this.unlinkedInputBoxMap.get(notebookDocument.uri.toString());
-            if (inputBoxMetadata) {
-                const inputBoxIndex = notebookDocument.cellCount;
-                // notebookDocument.cellCount += 1;
-                cells.push({
+        this.notebookMetadataMap.set(notebookDocument.uri.toString(), { cellCount: notebookDocument.cellCount });
+
+        const inputBoxMetadata = this.unlinkedInputBoxMap.get(notebookDocument.uri.toString());
+        if (inputBoxMetadata) {
+            const inputBoxIndex = notebookDocument.cellCount;
+            // notebookDocument.cellCount += 1;
+            const newCells = [
+                ...cells,
+                {
                     index: inputBoxIndex,
                     notebook: notebookDocument,
                     kind: NotebookCellKind.Code,
@@ -141,13 +236,13 @@ export class LspInteractiveWindowMiddlewareAddon implements Middleware, Disposab
                     metadata: {},
                     outputs: [],
                     executionSummary: undefined,
-                });
+                },
+            ];
 
-                this.unlinkedInputBoxMap.delete(notebookDocument.uri.toString());
-            }
+            this.unlinkedInputBoxMap.delete(notebookDocument.uri.toString());
+
+            await next(notebookDocument, newCells);
         }
-
-        await next(notebookDocument, cells);
 
         // TODO:
         // What to do with versions?
