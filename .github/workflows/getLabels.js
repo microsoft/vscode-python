2e17fabefe8055adb43f31705770238cea452b26
Kartik Raj <karraj@microsoft.com>
2022-08-08 21:04:42 +0000

    Fix indent

--- .github/workflows/getLabels.js
+++ .github/workflows/getLabels.js
@@ -4,15 +4,14 @@
  * * node .github/workflows/getLabels.js
  */
 
- const { Octokit } = require('@octokit/rest');
- const octokit = new Octokit();
- octokit.rest.issues
-     .listLabelsForRepo({
-         owner: 'microsoft',
-         repo: 'vscode-python',
-     })
-     .then((result) => {
-         const labels = result.data.map((label) => label.name);
-         console.log(JSON.stringify(labels));
-     });
- 
+const { Octokit } = require('@octokit/rest');
+const github = new Octokit();
+github.rest.issues
+    .listLabelsForRepo({
+        owner: 'microsoft',
+        repo: 'vscode-python',
+    })
+    .then((result) => {
+        const labels = result.data.map((label) => label.name);
+        console.log(JSON.stringify(labels));
+    });
