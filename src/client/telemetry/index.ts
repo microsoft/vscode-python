bc50a267d6ce9d2841eb28b498b6ef16ccc4c924
luabud <luabud@microsoft.com>
2022-09-07 15:30:10 -0700

    Format code

--- src/client/telemetry/index.ts
+++ src/client/telemetry/index.ts
@@ -1960,7 +1960,7 @@ export interface IEventNamePropertyMapping {
 				"ABExp.queriedFeature": { "classification": "SystemMetaData", "purpose": "FeatureInsight", "comment": "The experimental feature being queried" }
 			}
 	*/
-      /* __GDPR__
+    /* __GDPR__
 			"call-tas-error" : {
 				"owner": "luabud",
 				"comment": "Logs when calls to the experiment service fails",
