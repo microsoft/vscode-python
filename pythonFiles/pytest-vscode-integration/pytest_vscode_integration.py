ecb2a2faff7a47c84d8642410823e6c4a8b8a85c
eleanorjboyd <eleanorboyd@microsoft.com>
2022-09-16 10:41:43 -0700

    attempting port

--- pythonFiles/pytest-vscode-integration/pytest_vscode_integration.py
+++ pythonFiles/pytest-vscode-integration/pytest_vscode_integration.py
@@ -1,5 +1,33 @@
 # -*- coding: utf-8 -*-
+import json
+import os
+import sys
+
 import pytest
+
+sys.path.append(
+    "/Users/eleanorboyd/.vscode/extensions/ms-python.python-2022.12.1/pythonFiles/lib/python"
+)  #
+# import debugpy
+
+# debugpy.connect(5678)
+
+# Add the path to pythonFiles to sys.path to find testing_tools.socket_manager.
+PYTHON_FILES = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print("ALERT!! in plugin", PYTHON_FILES)
+sys.path.insert(0, PYTHON_FILES)
+
+from testing_tools import socket_manager
+
+# If I use from utils then there will be an import error in test_discovery.py.
+from unittest_adapter.utils import TestNode, build_test_tree, parse_unittest_args
+
+# Add the lib path to sys.path to find the typing_extensions module.
+sys.path.insert(0, os.path.join(PYTHON_FILES, "lib", "python"))
+
+from typing_extensions import NotRequired
+
+DEFAULT_PORT = "45454"
 
 
 def pytest_addoption(parser):
@@ -44,6 +72,7 @@ def pytest_collection_finish(session):
             parentCur = parentCur.parent
         testsList.append(path)
     print("final tests collected", testsList)
+    sendPost()
 
 
 # def pytest_collectstart(collector):
@@ -60,3 +89,13 @@ def pytest_collection_finish(session):
 
 # def get_config(request):
 #     print("ABCD request,", request.config)
+
+
+def sendPost():
+    payload: dict = {"status": "success"}
+    addr = ("localhost", 45454)
+    print("sending post")
+    # socket_manager.send_post("Hello from pytest")  # type: ignore
+    with socket_manager.SocketManager(addr) as s:
+        request = json.dumps(payload)
+        result = s.socket.sendall(request.encode("utf-8"))  # type: ignore
