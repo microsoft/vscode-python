8158fc38e6a55c6ac9cdb8585af9105765532957
Kartik Raj <karraj@microsoft.com>
2022-11-07 10:23:44 -0800

    Monkeypatch terminals created via extension to log commands sent

--- src/client/common/application/terminalManager.ts
+++ src/client/common/application/terminalManager.ts
@@ -3,6 +3,7 @@
 
 import { injectable } from 'inversify';
 import { Event, Terminal, TerminalOptions, window } from 'vscode';
+import { traceLog } from '../../logging';
 import { ITerminalManager } from './types';
 
 @injectable()
@@ -14,6 +15,12 @@ export class TerminalManager implements ITerminalManager {
         return window.onDidOpenTerminal;
     }
     public createTerminal(options: TerminalOptions): Terminal {
-        return window.createTerminal(options);
+        const terminal = window.createTerminal(options);
+        // Monkeypatch the terminal to log commands sent.
+        terminal.sendText = (text: string, addNewLine: boolean = true) => {
+            traceLog(`Send text to terminal: ${text}`);
+            return terminal.sendText(text, addNewLine);
+        };
+        return terminal;
     }
 }
