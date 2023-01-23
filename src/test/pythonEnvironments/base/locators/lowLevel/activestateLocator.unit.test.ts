3381fe5811b52041b1322e5eb7650303182e32cd
mitchell <mitchellb@activestate.com>
2023-01-23 13:23:54 -0500

    Attempt to fix Windows test failure.

--- src/test/pythonEnvironments/base/locators/lowLevel/activestateLocator.unit.test.ts
+++ src/test/pythonEnvironments/base/locators/lowLevel/activestateLocator.unit.test.ts
@@ -24,7 +24,15 @@ suite('ActiveState Locator', () => {
         shellExecute.callsFake((command: string) => {
             if (command === 'state projects -o editor') {
                 return Promise.resolve<ExecutionResult<string>>({
-                    stdout: `[{"name":"test","organization":"test-org","local_checkouts":["does-not-matter"],"executables":["${testActiveStateDir}/c09080d1/exec"]},{"name":"test2","organization":"test-org","local_checkouts":["does-not-matter2"],"executables":["${testActiveStateDir}/2af6390a/exec"]}]\n\0`,
+                    stdout: `[{"name":"test","organization":"test-org","local_checkouts":["does-not-matter"],"executables":["${path.join(
+                        testActiveStateDir,
+                        'c09080d1',
+                        'exec',
+                    )}]},{"name":"test2","organization":"test-org","local_checkouts":["does-not-matter2"],"executables":["${path.join(
+                        testActiveStateDir,
+                        '2af6390a',
+                        'exec',
+                    )}]}]\n\0`,
                 });
             }
             return Promise.reject(new Error('Command failed'));
