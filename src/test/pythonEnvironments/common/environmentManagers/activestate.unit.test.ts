819d16fbe81d8cca742c0814f5f669b0c6bd9e10
mitchell <mitchellb@activestate.com>
2023-01-24 14:51:21 -0500

    Remove unnecessary IFilesystem mocks.

--- src/test/pythonEnvironments/common/environmentManagers/activestate.unit.test.ts
+++ src/test/pythonEnvironments/common/environmentManagers/activestate.unit.test.ts
@@ -3,26 +3,14 @@
 
 import { expect } from 'chai';
 import * as path from 'path';
-import * as TypeMoq from 'typemoq';
-import { IFileSystem } from '../../../../client/common/platform/types';
 import { getOSType, OSType } from '../../../../client/common/utils/platform';
 import { isActiveStateEnvironment } from '../../../../client/pythonEnvironments/common/environmentManagers/activestate';
 import { TEST_LAYOUT_ROOT } from '../commonTestConstants';
 
 suite('isActiveStateEnvironment Tests', () => {
     const testActiveStateDir = path.join(TEST_LAYOUT_ROOT, 'activestate');
-    let fileSystem: TypeMoq.IMock<IFileSystem>;
-
-    setup(() => {
-        fileSystem = TypeMoq.Mock.ofType<IFileSystem>();
-    });
 
     test('Return true if runtime is set up', async () => {
-        const runtimeStorePath = path.join(testActiveStateDir, 'c09080d1', '_runtime_store');
-        fileSystem
-            .setup((f) => f.directoryExists(TypeMoq.It.isValue(runtimeStorePath)))
-            .returns(() => Promise.resolve(true));
-
         const result = await isActiveStateEnvironment(
             path.join(
                 testActiveStateDir,
