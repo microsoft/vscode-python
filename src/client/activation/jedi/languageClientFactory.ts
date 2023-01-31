6cd17b3866c1bbf3d009e7724ec8f2added6c8bc
Karthik Nadig <kanadig@microsoft.com>
2023-01-31 13:54:56 -0800

    Try runnung jedi

--- src/client/activation/jedi/languageClientFactory.ts
+++ src/client/activation/jedi/languageClientFactory.ts
@@ -1,11 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+import { exec } from 'child_process';
 import * as path from 'path';
 import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
 
 import { EXTENSION_ROOT_DIR, PYTHON_LANGUAGE } from '../../common/constants';
 import { Resource } from '../../common/types';
+import { sleep } from '../../common/utils/async';
 import { IInterpreterService } from '../../interpreter/contracts';
 import { PythonEnvironment } from '../../pythonEnvironments/info';
 import { ILanguageClientFactory } from '../types';
@@ -29,6 +31,13 @@ export class JediLanguageClientFactory implements ILanguageClientFactory {
         };
 
         console.log(`Jedi Language Server options: ${JSON.stringify(serverOptions)}`);
+        const child = exec(`${serverOptions.command} ${lsScriptPath}`, (error, stdout, stderr) => {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+            console.log(`ERROR:`, error);
+        });
+        await sleep(5000);
+        child.kill();
 
         return new LanguageClient(PYTHON_LANGUAGE, languageClientName, serverOptions, clientOptions);
     }
