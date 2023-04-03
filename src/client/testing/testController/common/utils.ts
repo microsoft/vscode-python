f90a4da3ef0182bf226d83814efccc1a1885ce3a
eleanorjboyd <eleanorboyd@microsoft.com>
2023-04-03 11:25:39 -0700

    switch to using tcp for comm with server

--- src/client/testing/testController/common/utils.ts
+++ src/client/testing/testController/common/utils.ts
@@ -5,3 +5,34 @@ export function fixLogLines(content: string): string {
     const lines = content.split(/\r?\n/g);
     return `${lines.join('\r\n')}\r\n`;
 }
+export interface IJSONRPCMessage {
+    headers: Map<string, string>;
+    extractedData: string;
+    remainingRawData: string;
+}
+
+export function jsonRPCProcessor(rawData: string): IJSONRPCMessage {
+    const lines = rawData.split('\n');
+    let remainingRawData = '';
+    const headerMap = new Map<string, string>();
+    for (let i = 0; i < lines.length; i += 1) {
+        const line = lines[i];
+        if (line === '') {
+            remainingRawData = lines.slice(i + 1).join('\n');
+            break;
+        }
+        const [key, value] = line.split(':');
+        if (['Content-Length', 'Content-Type', 'Request-uuid'].includes(key)) {
+            headerMap.set(key.trim(), value.trim());
+        }
+    }
+
+    const length = parseInt(headerMap.get('Content-Length') ?? '0', 10);
+    const data = remainingRawData.slice(0, length);
+    remainingRawData = remainingRawData.slice(length);
+    return {
+        headers: headerMap,
+        extractedData: data,
+        remainingRawData,
+    };
+}
