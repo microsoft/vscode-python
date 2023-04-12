53589bd49ac55b87eb83c50dcb7985b831cdfac5
eleanorjboyd <eleanorboyd@microsoft.com>
2023-04-12 14:20:41 -0700

    functioning test

--- pythonFiles/tests/pytestadapter/expected_execution_test_output.py
+++ pythonFiles/tests/pytestadapter/expected_execution_test_output.py
@@ -1,8 +1,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-TEST_SUBTRACT_FUNCTION = "unittest_folder.test_subtract.TestSubtractFunction"
-TEST_ADD_FUNCTION = "unittest_folder.test_add.TestAddFunction"
+TEST_SUBTRACT_FUNCTION = "unittest_folder/test_subtract.py::TestSubtractFunction::"
+TEST_ADD_FUNCTION = "unittest_folder/test_add.py::TestAddFunction::"
 CWD = "/Users/eleanorboyd/Documents/testingFiles/playground_run_tests"
 SUCCESS = "success"
 FAILURE = "failure"
@@ -20,32 +20,36 @@ ASSERTION_ERROR = "<class 'AssertionError'>, AssertionError('1 != 10000'), <trac
 #            ├── test_subtract_negative_numbers: failure
 #            └── test_subtract_positive_numbers: success
 uf_execution_expected_output = {
-    "cwd": CWD,
-    "status": SUCCESS,
-    "result": {
-        f"{TEST_ADD_FUNCTION}.test_add_negative_numbers": {
-            "test": f"{TEST_ADD_FUNCTION}.test_add_negative_numbers",
-            "outcome": SUCCESS,
-            "message": NONE,
-            "traceback": None,
-            "subtest": None,
-        },
-        f"{TEST_ADD_FUNCTION}.test_add_positive_numbers": {
-            "test": f"{TEST_ADD_FUNCTION}.test_add_positive_numbers",
-            "outcome": SUCCESS,
-            "message": NONE,
-            "traceback": None,
-            "subtest": None,
-        },
-        f"{TEST_SUBTRACT_FUNCTION}.test_subtract_negative_numbers": {
-            "test": f"{TEST_SUBTRACT_FUNCTION}.test_subtract_negative_numbers",
-            "outcome": FAILURE,
-            "message": ASSERTION_ERROR,
-            "traceback": None,
-            "subtest": None,
-        },
+    f"{TEST_ADD_FUNCTION}test_add_negative_numbers": {
+        "test": f"{TEST_ADD_FUNCTION}test_add_negative_numbers",
+        "outcome": SUCCESS,
+        "message": None,
+        "traceback": None,
+        "subtest": None,
+    },
+    f"{TEST_ADD_FUNCTION}test_add_positive_numbers": {
+        "test": f"{TEST_ADD_FUNCTION}test_add_positive_numbers",
+        "outcome": SUCCESS,
+        "message": None,
+        "traceback": None,
+        "subtest": None,
+    },
+    f"{TEST_SUBTRACT_FUNCTION}test_subtract_negative_numbers": {
+        "test": f"{TEST_SUBTRACT_FUNCTION}test_subtract_negative_numbers",
+        "outcome": FAILURE,
+        "message": "self = <test_subtract.TestSubtractFunction testMethod=test_subtract_negative_numbers>\n\n    def test_subtract_negative_numbers(  # test_marker--test_subtract_negative_numbers\n        self,\n    ):\n        result = subtract(-2, -3)\n>       self.assertEqual(result, 100000)\nE       AssertionError: 1 != 100000\n\nunittest_folder/test_subtract.py:25: AssertionError",
+        "traceback": None,
+        "subtest": None,
+    },
+    f"{TEST_SUBTRACT_FUNCTION}test_subtract_positive_numbers": {
+        "test": f"{TEST_SUBTRACT_FUNCTION}test_subtract_positive_numbers",
+        "outcome": SUCCESS,
+        "message": None,
+        "traceback": None,
+        "subtest": None,
     },
 }
+
 
 # This is the expected output for the unittest_folder add only execute tests
 # └── unittest_folder
@@ -54,23 +58,19 @@ uf_execution_expected_output = {
 #    │       ├── test_add_negative_numbers: success
 #    │       └── test_add_positive_numbers: success
 uf_single_file_expected_output = {
-    "cwd": CWD,
-    "status": SUCCESS,
-    "result": {
-        f"{TEST_ADD_FUNCTION}.test_add_negative_numbers": {
-            "test": f"{TEST_ADD_FUNCTION}.test_add_negative_numbers",
-            "outcome": SUCCESS,
-            "message": NONE,
-            "traceback": None,
-            "subtest": None,
-        },
-        f"{TEST_ADD_FUNCTION}.test_add_positive_numbers": {
-            "test": f"{TEST_ADD_FUNCTION}.test_add_positive_numbers",
-            "outcome": SUCCESS,
-            "message": NONE,
-            "traceback": None,
-            "subtest": None,
-        },
+    f"{TEST_ADD_FUNCTION}.test_add_negative_numbers": {
+        "test": f"{TEST_ADD_FUNCTION}.test_add_negative_numbers",
+        "outcome": SUCCESS,
+        "message": None,
+        "traceback": None,
+        "subtest": None,
+    },
+    f"{TEST_ADD_FUNCTION}.test_add_positive_numbers": {
+        "test": f"{TEST_ADD_FUNCTION}.test_add_positive_numbers",
+        "outcome": SUCCESS,
+        "message": NONE,
+        "traceback": None,
+        "subtest": None,
     },
 }
 
