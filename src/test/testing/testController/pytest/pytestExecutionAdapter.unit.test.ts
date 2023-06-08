3ca8a6f1058c1fdce0392ed85ce4805e979b807d
eleanorjboyd <eleanorboyd@microsoft.com>
2023-06-08 10:50:49 -0700

    execution adapter changes

--- src/test/testing/testController/pytest/pytestExecutionAdapter.unit.test.ts
+++ src/test/testing/testController/pytest/pytestExecutionAdapter.unit.test.ts
@@ -4,10 +4,8 @@
 import * as assert from 'assert';
 import { TestRun, Uri } from 'vscode';
 import * as typeMoq from 'typemoq';
-import { debug } from 'console';
-import * as net from 'net';
 import { IConfigurationService, ITestOutputChannel } from '../../../../client/common/types';
-import { DataReceivedEvent, ITestServer } from '../../../../client/testing/testController/common/types';
+import { ITestServer } from '../../../../client/testing/testController/common/types';
 import {
     IPythonExecutionFactory,
     IPythonExecutionService,
@@ -16,7 +14,6 @@ import {
 import { createDeferred, Deferred } from '../../../../client/common/utils/async';
 import { PytestTestExecutionAdapter } from '../../../../client/testing/testController/pytest/pytestExecutionAdapter';
 import { ITestDebugLauncher, LaunchOptions } from '../../../../client/testing/common/types';
-import { DebugLauncher } from '../../../../client/testing/common/debugLauncher';
 
 suite('pytest test execution adapter', () => {
     let testServer: typeMoq.IMock<ITestServer>;
@@ -65,83 +62,51 @@ suite('pytest test execution adapter', () => {
         execService.setup((p) => ((p as unknown) as any).then).returns(() => undefined);
         debugLauncher.setup((p) => ((p as unknown) as any).then).returns(() => undefined);
     });
-    // test('onDataReceivedHandler call exec with correct args', async () => {
-    //     const uri = Uri.file('/my/test/path/');
-    //     const uuid = 'uuid123';
-    //     // const data = { status: 'success' };
-    //     testServer
-    //         .setup((t) => t.onDiscoveryDataReceived(typeMoq.It.isAny(), typeMoq.It.isAny()))
-    //         .returns(() => ({
-    //             dispose: () => {
-    //                 /* no-body */
-    //             },
-    //         }));
-    //     testServer.setup((t) => t.createUUID(typeMoq.It.isAny())).returns(() => uuid);
-    //     const outputChannel = typeMoq.Mock.ofType<ITestOutputChannel>();
-    //     const testRun = typeMoq.Mock.ofType<TestRun>();
-    //     adapter = new PytestTestExecutionAdapter(testServer.object, configService, outputChannel.object);
-    //     await adapter.runTests(uri, [], false, testRun.object, execFactory.object);
+    test('pytest execution called with correct args', async () => {
+        const uri = Uri.file('/my/test/path/');
+        const uuid = 'uuid123';
+        // const data = { status: 'success' };
+        testServer
+            .setup((t) => t.onDiscoveryDataReceived(typeMoq.It.isAny(), typeMoq.It.isAny()))
+            .returns(() => ({
+                dispose: () => {
+                    /* no-body */
+                },
+            }));
+        testServer.setup((t) => t.createUUID(typeMoq.It.isAny())).returns(() => uuid);
+        const outputChannel = typeMoq.Mock.ofType<ITestOutputChannel>();
+        const testRun = typeMoq.Mock.ofType<TestRun>();
+        adapter = new PytestTestExecutionAdapter(testServer.object, configService, outputChannel.object);
+        await adapter.runTests(uri, [], false, testRun.object, execFactory.object);
 
-    //     const expectedArgs = [
-    //         '/Users/eleanorboyd/vscode-python/pythonFiles/vscode_pytest/run_pytest_script.py',
-    //         '--rootdir',
-    //         '/my/test/path/',
-    //     ];
-    //     const expectedExtraVariables = {
-    //         PYTHONPATH: '/Users/eleanorboyd/vscode-python/pythonFiles',
-    //         TEST_UUID: 'uuid123',
-    //         TEST_PORT: '12345',
-    //     };
-    //     execService.verify(
-    //         (x) =>
-    //             x.exec(
-    //                 expectedArgs,
-    //                 typeMoq.It.is<SpawnOptions>((options) => {
-    //                     assert.equal(options.extraVariables?.PYTHONPATH, expectedExtraVariables.PYTHONPATH);
-    //                     assert.equal(options.extraVariables?.TEST_UUID, expectedExtraVariables.TEST_UUID);
-    //                     assert.equal(options.extraVariables?.TEST_PORT, expectedExtraVariables.TEST_PORT);
-    //                     assert.strictEqual(typeof options.extraVariables?.RUN_TEST_IDS_PORT, 'string');
-    //                     assert.equal(options.cwd, uri.fsPath);
-    //                     assert.equal(options.throwOnStdErr, true);
-    //                     return true;
-    //                 }),
-    //             ),
-    //         typeMoq.Times.once(),
-    //     );
-    // });
-    // test('debug called if boolean true and debug launch options are correct', async () => {
-    //     const uri = Uri.file('/my/test/path/');
-    //     const uuid = 'uuid123';
-    //     testServer
-    //         .setup((t) => t.onDiscoveryDataReceived(typeMoq.It.isAny(), typeMoq.It.isAny()))
-    //         .returns(() => ({
-    //             dispose: () => {
-    //                 /* no-body */
-    //             },
-    //         }));
-    //     testServer.setup((t) => t.createUUID(typeMoq.It.isAny())).returns(() => uuid);
-    //     const outputChannel = typeMoq.Mock.ofType<ITestOutputChannel>();
-    //     const testRun = typeMoq.Mock.ofType<TestRun>();
-    //     adapter = new PytestTestExecutionAdapter(testServer.object, configService, outputChannel.object);
-    //     await adapter.runTests(uri, [], true, testRun.object, execFactory.object, debugLauncher.object);
-    //     debugLauncher.verify(
-    //         (x) =>
-    //             x.launchDebugger(
-    //                 typeMoq.It.is<LaunchOptions>((launchOptions) => {
-    //                     assert.equal(launchOptions.cwd, uri.fsPath);
-    //                     assert.deepEqual(launchOptions.args, ['--rootdir', '/my/test/path/', '--capture', 'no']);
-    //                     assert.equal(launchOptions.testProvider, 'pytest');
-    //                     assert.equal(launchOptions.pytestPort, '12345');
-    //                     assert.equal(launchOptions.pytestUUID, 'uuid123');
-    //                     assert.strictEqual(typeof launchOptions.runTestIdsPort, 'string');
-    //                     return true;
-    //                 }),
-    //                 typeMoq.It.isAny(),
-    //             ),
-    //         typeMoq.Times.once(),
-    //     );
-    // });
-    test('dafdsaljfj;a4wfadss', async () => {
+        const expectedArgs = [
+            '/Users/eleanorboyd/vscode-python/pythonFiles/vscode_pytest/run_pytest_script.py',
+            '--rootdir',
+            '/my/test/path/',
+        ];
+        const expectedExtraVariables = {
+            PYTHONPATH: '/Users/eleanorboyd/vscode-python/pythonFiles',
+            TEST_UUID: 'uuid123',
+            TEST_PORT: '12345',
+        };
+        execService.verify(
+            (x) =>
+                x.exec(
+                    expectedArgs,
+                    typeMoq.It.is<SpawnOptions>((options) => {
+                        assert.equal(options.extraVariables?.PYTHONPATH, expectedExtraVariables.PYTHONPATH);
+                        assert.equal(options.extraVariables?.TEST_UUID, expectedExtraVariables.TEST_UUID);
+                        assert.equal(options.extraVariables?.TEST_PORT, expectedExtraVariables.TEST_PORT);
+                        assert.strictEqual(typeof options.extraVariables?.RUN_TEST_IDS_PORT, 'string');
+                        assert.equal(options.cwd, uri.fsPath);
+                        assert.equal(options.throwOnStdErr, true);
+                        return true;
+                    }),
+                ),
+            typeMoq.Times.once(),
+        );
+    });
+    test('Debug launched correctly for pytest', async () => {
         const uri = Uri.file('/my/test/path/');
         const uuid = 'uuid123';
         testServer
@@ -154,7 +119,6 @@ suite('pytest test execution adapter', () => {
         testServer.setup((t) => t.createUUID(typeMoq.It.isAny())).returns(() => uuid);
         const outputChannel = typeMoq.Mock.ofType<ITestOutputChannel>();
         const testRun = typeMoq.Mock.ofType<TestRun>();
-
         adapter = new PytestTestExecutionAdapter(testServer.object, configService, outputChannel.object);
         await adapter.runTests(uri, [], true, testRun.object, execFactory.object, debugLauncher.object);
         debugLauncher.verify(
