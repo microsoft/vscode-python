98c36faaae2c58e352ed15021ee4d6536331e03e
eleanorjboyd <eleanorboyd@microsoft.com>
2023-06-07 17:15:54 -0700

    execution for pytest and discovery for unittest

--- src/test/testing/testController/unittest/testDiscoveryAdapter.unit.test.ts
+++ src/test/testing/testController/unittest/testDiscoveryAdapter.unit.test.ts
@@ -1,27 +1,27 @@
-// // Copyright (c) Microsoft Corporation. All rights reserved.
-// // Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-// import * as assert from 'assert';
-// import * as path from 'path';
-// import * as typemoq from 'typemoq';
-// import { Uri } from 'vscode';
-// import { IConfigurationService, ITestOutputChannel } from '../../../../client/common/types';
-// import { EXTENSION_ROOT_DIR } from '../../../../client/constants';
-// import { ITestServer, TestCommandOptions } from '../../../../client/testing/testController/common/types';
-// import { UnittestTestDiscoveryAdapter } from '../../../../client/testing/testController/unittest/testDiscoveryAdapter';
+import * as assert from 'assert';
+import * as path from 'path';
+import * as typemoq from 'typemoq';
+import { Uri } from 'vscode';
+import { IConfigurationService, ITestOutputChannel } from '../../../../client/common/types';
+import { EXTENSION_ROOT_DIR } from '../../../../client/constants';
+import { ITestServer, TestCommandOptions } from '../../../../client/testing/testController/common/types';
+import { UnittestTestDiscoveryAdapter } from '../../../../client/testing/testController/unittest/testDiscoveryAdapter';
 
-// suite('Unittest test discovery adapter', () => {
-//     let stubConfigSettings: IConfigurationService;
-//     let outputChannel: typemoq.IMock<ITestOutputChannel>;
+suite('Unittest test discovery adapter', () => {
+    let stubConfigSettings: IConfigurationService;
+    let outputChannel: typemoq.IMock<ITestOutputChannel>;
 
-//     setup(() => {
-//         stubConfigSettings = ({
-//             getSettings: () => ({
-//                 testing: { unittestArgs: ['-v', '-s', '.', '-p', 'test*'] },
-//             }),
-//         } as unknown) as IConfigurationService;
-//         outputChannel = typemoq.Mock.ofType<ITestOutputChannel>();
-//     });
+    setup(() => {
+        stubConfigSettings = ({
+            getSettings: () => ({
+                testing: { unittestArgs: ['-v', '-s', '.', '-p', 'test*'] },
+            }),
+        } as unknown) as IConfigurationService;
+        outputChannel = typemoq.Mock.ofType<ITestOutputChannel>();
+    });
 
     test('DiscoverTests should send the discovery command to the test server with the correct args', async () => {
         let options: TestCommandOptions | undefined;
@@ -38,11 +38,11 @@
             createUUID: () => '123456789',
         } as unknown) as ITestServer;
 
-//         const uri = Uri.file('/foo/bar');
-//         const script = path.join(EXTENSION_ROOT_DIR, 'pythonFiles', 'unittestadapter', 'discovery.py');
+        const uri = Uri.file('/foo/bar');
+        const script = path.join(EXTENSION_ROOT_DIR, 'pythonFiles', 'unittestadapter', 'discovery.py');
 
-//         const adapter = new UnittestTestDiscoveryAdapter(stubTestServer, stubConfigSettings, outputChannel.object);
-//         adapter.discoverTests(uri);
+        const adapter = new UnittestTestDiscoveryAdapter(stubTestServer, stubConfigSettings, outputChannel.object);
+        adapter.discoverTests(uri);
 
         assert.deepStrictEqual(options, {
             workspaceFolder: uri,
