7f9ef9486af1ec557f52041c44047a9bacb036fe
Eleanor Boyd <eleanorboyd@microsoft.com>
2023-06-20 10:41:42 -0700

    Workspace & resolver tests (#21441)
    This PR
    - moves populateTestTree to utils
    - adds tests for execution adapters (pytest and unittest)
    - resultResolver tests
    - workspaceTestAdapater tests

--- src/test/testing/testController/workspaceTestAdapter.unit.test.ts
+++ src/test/testing/testController/workspaceTestAdapter.unit.test.ts
@@ -1,9 +1,9 @@
-// // Copyright (c) Microsoft Corporation. All rights reserved.
-// // Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-// import * as assert from 'assert';
-// import * as sinon from 'sinon';
-// import * as typemoq from 'typemoq';
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+import * as typemoq from 'typemoq';
 
 import { TestController, TestItem, TestItemCollection, TestRun, Uri } from 'vscode';
 import { IConfigurationService, ITestOutputChannel } from '../../../client/common/types';
@@ -23,15 +23,15 @@ suite('Workspace test adapter', () => {
         let stubConfigSettings: IConfigurationService;
         let stubResultResolver: ITestResultResolver;
 
-//         let discoverTestsStub: sinon.SinonStub;
-//         let sendTelemetryStub: sinon.SinonStub;
-//         let outputChannel: typemoq.IMock<ITestOutputChannel>;
+        let discoverTestsStub: sinon.SinonStub;
+        let sendTelemetryStub: sinon.SinonStub;
+        let outputChannel: typemoq.IMock<ITestOutputChannel>;
 
-//         let telemetryEvent: { eventName: EventName; properties: Record<string, unknown> }[] = [];
+        let telemetryEvent: { eventName: EventName; properties: Record<string, unknown> }[] = [];
 
-//         // Stubbed test controller (see comment around L.40)
-//         let testController: TestController;
-//         let log: string[] = [];
+        // Stubbed test controller (see comment around L.40)
+        let testController: TestController;
+        let log: string[] = [];
 
         setup(() => {
             stubConfigSettings = ({
@@ -40,14 +40,14 @@ suite('Workspace test adapter', () => {
                 }),
             } as unknown) as IConfigurationService;
 
-//             stubTestServer = ({
-//                 sendCommand(): Promise<void> {
-//                     return Promise.resolve();
-//                 },
-//                 onDataReceived: () => {
-//                     // no body
-//                 },
-//             } as unknown) as ITestServer;
+            stubResultResolver = ({
+                resolveDiscovery: () => {
+                    // no body
+                },
+                resolveExecution: () => {
+                    // no body
+                },
+            } as unknown) as ITestResultResolver;
 
             stubResultResolver = ({
                 resolveDiscovery: () => {
@@ -75,46 +75,44 @@ suite('Workspace test adapter', () => {
                 },
             } as unknown) as TestItem;
 
-//             // const vsIdToRunIdGetStub = sinon.stub(stubResultResolver.vsIdToRunId, 'get');
-//             // const expectedRunId = 'expectedRunId';
-//             // vsIdToRunIdGetStub.withArgs(sinon.match.any).returns(expectedRunId);
+            // For some reason the 'tests' namespace in vscode returns undefined.
+            // While I figure out how to expose to the tests, they will run
+            // against a stub test controller and stub test items.
+            const testItem = ({
+                canResolveChildren: false,
+                tags: [],
+                children: {
+                    add: () => {
+                        // empty
+                    },
+                },
+            } as unknown) as TestItem;
 
-//             // For some reason the 'tests' namespace in vscode returns undefined.
-//             // While I figure out how to expose to the tests, they will run
-//             // against a stub test controller and stub test items.
-//             const testItem = ({
-//                 canResolveChildren: false,
-//                 tags: [],
-//                 children: {
-//                     add: () => {
-//                         // empty
-//                     },
-//                 },
-//             } as unknown) as TestItem;
+            testController = ({
+                items: {
+                    get: () => {
+                        log.push('get');
+                    },
+                    add: () => {
+                        log.push('add');
+                    },
+                    replace: () => {
+                        log.push('replace');
+                    },
+                    delete: () => {
+                        log.push('delete');
+                    },
+                },
+                createTestItem: () => {
+                    log.push('createTestItem');
+                    return testItem;
+                },
+                dispose: () => {
+                    // empty
+                },
+            } as unknown) as TestController;
 
-//             testController = ({
-//                 items: {
-//                     get: () => {
-//                         log.push('get');
-//                     },
-//                     add: () => {
-//                         log.push('add');
-//                     },
-//                     replace: () => {
-//                         log.push('replace');
-//                     },
-//                     delete: () => {
-//                         log.push('delete');
-//                     },
-//                 },
-//                 createTestItem: () => {
-//                     log.push('createTestItem');
-//                     return testItem;
-//                 },
-//                 dispose: () => {
-//                     // empty
-//                 },
-//             } as unknown) as TestController;
+            // testController = tests.createTestController('mock-python-tests', 'Mock Python Tests');
 
             discoverTestsStub = sinon.stub(UnittestTestDiscoveryAdapter.prototype, 'discoverTests');
             sendTelemetryStub = sinon.stub(Telemetry, 'sendTelemetryEvent').callsFake(mockSendTelemetryEvent);
@@ -175,10 +173,12 @@ suite('Workspace test adapter', () => {
             sinon.assert.calledWithMatch(buildErrorNodeOptionsStub, Uri.parse('foo'), sinon.match.any, testProvider);
         });
 
-//             discoverTestsStub = sandbox.stub(UnittestTestDiscoveryAdapter.prototype, 'discoverTests');
-//             sendTelemetryStub = sandbox.stub(Telemetry, 'sendTelemetryEvent').callsFake(mockSendTelemetryEvent);
-//             outputChannel = typemoq.Mock.ofType<ITestOutputChannel>();
-//         });
+        teardown(() => {
+            telemetryEvent = [];
+            log = [];
+            testController.dispose();
+            sinon.restore();
+        });
 
             const testDiscoveryAdapter = new UnittestTestDiscoveryAdapter(
                 stubTestServer,
@@ -198,28 +198,42 @@ suite('Workspace test adapter', () => {
                 stubResultResolver,
             );
 
-//         test("When discovering tests, the workspace test adapter should call the test discovery adapter's discoverTest method", async () => {
-//             discoverTestsStub.resolves();
+            const testDiscoveryAdapter = new UnittestTestDiscoveryAdapter(
+                stubTestServer,
+                stubConfigSettings,
+                outputChannel.object,
+            );
+            const testExecutionAdapter = new UnittestTestExecutionAdapter(
+                stubTestServer,
+                stubConfigSettings,
+                outputChannel.object,
+            );
 
-//             const testDiscoveryAdapter = new UnittestTestDiscoveryAdapter(
-//                 stubTestServer,
-//                 stubConfigSettings,
-//                 outputChannel.object,
-//             );
-//             const testExecutionAdapter = new UnittestTestExecutionAdapter(
-//                 stubTestServer,
-//                 stubConfigSettings,
-//                 outputChannel.object,
-//             );
-//             const workspaceTestAdapter = new WorkspaceTestAdapter(
-//                 'unittest',
-//                 testDiscoveryAdapter,
-//                 testExecutionAdapter,
-//                 Uri.parse('foo'),
-//                 stubResultResolver,
-//             );
+            const workspaceTestAdapter = new WorkspaceTestAdapter(
+                'unittest',
+                testDiscoveryAdapter,
+                testExecutionAdapter,
+                Uri.parse('foo'),
+                stubResultResolver,
+            );
 
-//             await workspaceTestAdapter.discoverTests(testController);
+            const blankTestItem = ({
+                canResolveChildren: false,
+                tags: [],
+                children: {
+                    add: () => {
+                        // empty
+                    },
+                },
+            } as unknown) as TestItem;
+            const errorTestItemOptions: testItemUtilities.ErrorTestItemOptions = {
+                id: 'id',
+                label: 'label',
+                error: 'error',
+            };
+            const createErrorTestItemStub = sinon.stub(testItemUtilities, 'createErrorTestItem').returns(blankTestItem);
+            const buildErrorNodeOptionsStub = sinon.stub(util, 'buildErrorNodeOptions').returns(errorTestItemOptions);
+            const testProvider = 'unittest';
 
             const testDiscoveryAdapter = new UnittestTestDiscoveryAdapter(
                 stubTestServer,
@@ -239,43 +253,35 @@ suite('Workspace test adapter', () => {
                 stubResultResolver,
             );
 
-//         test('If discovery is already running, do not call discoveryAdapter.discoverTests again', async () => {
-//             discoverTestsStub.callsFake(
-//                 async () =>
-//                     new Promise<void>((resolve) => {
-//                         setTimeout(() => {
-//                             // Simulate time taken by discovery.
-//                             resolve();
-//                         }, 2000);
-//                     }),
-//             );
+            sinon.assert.calledWithMatch(createErrorTestItemStub, sinon.match.any, sinon.match.any);
+            sinon.assert.calledWithMatch(buildErrorNodeOptionsStub, Uri.parse('foo'), sinon.match.any, testProvider);
+        });
 
-//             const testDiscoveryAdapter = new UnittestTestDiscoveryAdapter(
-//                 stubTestServer,
-//                 stubConfigSettings,
-//                 outputChannel.object,
-//             );
-//             const testExecutionAdapter = new UnittestTestExecutionAdapter(
-//                 stubTestServer,
-//                 stubConfigSettings,
-//                 outputChannel.object,
-//             );
-//             const workspaceTestAdapter = new WorkspaceTestAdapter(
-//                 'unittest',
-//                 testDiscoveryAdapter,
-//                 testExecutionAdapter,
-//                 Uri.parse('foo'),
-//                 stubResultResolver,
-//             );
+        test("When discovering tests, the workspace test adapter should call the test discovery adapter's discoverTest method", async () => {
+            discoverTestsStub.resolves();
 
-//             // Try running discovery twice
-//             const one = workspaceTestAdapter.discoverTests(testController);
-//             const two = workspaceTestAdapter.discoverTests(testController);
+            const testDiscoveryAdapter = new UnittestTestDiscoveryAdapter(
+                stubTestServer,
+                stubConfigSettings,
+                outputChannel.object,
+            );
+            const testExecutionAdapter = new UnittestTestExecutionAdapter(
+                stubTestServer,
+                stubConfigSettings,
+                outputChannel.object,
+            );
+            const workspaceTestAdapter = new WorkspaceTestAdapter(
+                'unittest',
+                testDiscoveryAdapter,
+                testExecutionAdapter,
+                Uri.parse('foo'),
+                stubResultResolver,
+            );
 
-//             Promise.all([one, two]);
+            await workspaceTestAdapter.discoverTests(testController);
 
-//             sinon.assert.calledOnce(discoverTestsStub);
-//         });
+            sinon.assert.calledOnce(discoverTestsStub);
+        });
 
             const workspaceTestAdapter = new WorkspaceTestAdapter(
                 'unittest',
@@ -285,33 +291,35 @@ suite('Workspace test adapter', () => {
                 stubResultResolver,
             );
 
-//             const testDiscoveryAdapter = new UnittestTestDiscoveryAdapter(
-//                 stubTestServer,
-//                 stubConfigSettings,
-//                 outputChannel.object,
-//             );
-//             const testExecutionAdapter = new UnittestTestExecutionAdapter(
-//                 stubTestServer,
-//                 stubConfigSettings,
-//                 outputChannel.object,
-//             );
+            const testDiscoveryAdapter = new UnittestTestDiscoveryAdapter(
+                stubTestServer,
+                stubConfigSettings,
+                outputChannel.object,
+            );
+            const testExecutionAdapter = new UnittestTestExecutionAdapter(
+                stubTestServer,
+                stubConfigSettings,
+                outputChannel.object,
+            );
+            const workspaceTestAdapter = new WorkspaceTestAdapter(
+                'unittest',
+                testDiscoveryAdapter,
+                testExecutionAdapter,
+                Uri.parse('foo'),
+                stubResultResolver,
+            );
 
-//             const workspaceTestAdapter = new WorkspaceTestAdapter(
-//                 'unittest',
-//                 testDiscoveryAdapter,
-//                 testExecutionAdapter,
-//                 Uri.parse('foo'),
-//                 stubResultResolver,
-//             );
+            // Try running discovery twice
+            const one = workspaceTestAdapter.discoverTests(testController);
+            const two = workspaceTestAdapter.discoverTests(testController);
 
-//             await workspaceTestAdapter.discoverTests(testController);
+            Promise.all([one, two]);
 
-//             sinon.assert.calledWith(sendTelemetryStub, EventName.UNITTEST_DISCOVERY_DONE);
-//             assert.strictEqual(telemetryEvent.length, 2);
+            sinon.assert.calledOnce(discoverTestsStub);
+        });
 
-//             const lastEvent = telemetryEvent[1];
-//             assert.strictEqual(lastEvent.properties.failed, false);
-//         });
+        test('If discovery succeeds, send a telemetry event with the "failed" key set to false', async () => {
+            discoverTestsStub.resolves({ status: 'success' });
 
             const workspaceTestAdapter = new WorkspaceTestAdapter(
                 'unittest',
@@ -321,24 +329,15 @@ suite('Workspace test adapter', () => {
                 stubResultResolver,
             );
 
-//             const testDiscoveryAdapter = new UnittestTestDiscoveryAdapter(
-//                 stubTestServer,
-//                 stubConfigSettings,
-//                 outputChannel.object,
-//             );
-//             const testExecutionAdapter = new UnittestTestExecutionAdapter(
-//                 stubTestServer,
-//                 stubConfigSettings,
-//                 outputChannel.object,
-//             );
+            const workspaceTestAdapter = new WorkspaceTestAdapter(
+                'unittest',
+                testDiscoveryAdapter,
+                testExecutionAdapter,
+                Uri.parse('foo'),
+                stubResultResolver,
+            );
 
-//             const workspaceTestAdapter = new WorkspaceTestAdapter(
-//                 'unittest',
-//                 testDiscoveryAdapter,
-//                 testExecutionAdapter,
-//                 Uri.parse('foo'),
-//                 stubResultResolver,
-//             );
+            await workspaceTestAdapter.discoverTests(testController);
 
             const lastEvent = telemetryEvent[1];
             assert.ok(lastEvent.properties.failed);
