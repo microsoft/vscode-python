bfdb6acc7f8be10f0eb8563cc9bf22f74c4df199
Eleanor Boyd <eleanorboyd@microsoft.com>
2023-06-21 10:28:34 -0700

    Result resolver feature branch (#21457)
    fixes https://github.com/microsoft/vscode-python/issues/21394

--- pythonFiles/unittestadapter/execution.py
+++ pythonFiles/unittestadapter/execution.py
@@ -6,11 +6,14 @@ import enum
 import json
 import os
 import pathlib
+import socket
 import sys
 import traceback
 import unittest
 from types import TracebackType
 from typing import Dict, List, Optional, Tuple, Type, Union
+
+from pythonFiles.testing_tools import process_json_util
 
 directory_path = pathlib.Path(__file__).parent.parent / "lib" / "python"
 # Add the path to pythonFiles to sys.path to find testing_tools.socket_manager.
@@ -28,7 +31,7 @@ DEFAULT_PORT = "45454"
 
 def parse_execution_cli_args(
     args: List[str],
-) -> Tuple[int, Union[str, None], List[str]]:
+) -> Tuple[int, Union[str, None]]:
     """Parse command-line arguments that should be processed by the script.
 
     So far this includes the port number that it needs to connect to, the uuid passed by the TS side,
@@ -42,10 +45,9 @@ def parse_execution_cli_args(
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--port", default=DEFAULT_PORT)
     arg_parser.add_argument("--uuid")
-    arg_parser.add_argument("--testids", nargs="+")
     parsed_args, _ = arg_parser.parse_known_args(args)
 
-    return (int(parsed_args.port), parsed_args.uuid, parsed_args.testids)
+    return (int(parsed_args.port), parsed_args.uuid)
 
 
 ErrorType = Union[
@@ -258,8 +260,57 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     index = argv.index("--udiscovery")
 
-    START_DIR, pattern, top_level_dir = parse_unittest_args(argv[index + 1 :])
+    start_dir, pattern, top_level_dir = parse_unittest_args(argv[index + 1 :])
 
-    # Perform test execution.
-    PORT, UUID, testids = parse_execution_cli_args(argv[:index])
-    payload = run_tests(START_DIR, testids, pattern, top_level_dir, UUID)
+    run_test_ids_port = os.environ.get("RUN_TEST_IDS_PORT")
+    run_test_ids_port_int = (
+        int(run_test_ids_port) if run_test_ids_port is not None else 0
+    )
+
+    # get data from socket
+    test_ids_from_buffer = []
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(("localhost", run_test_ids_port_int))
+        buffer = b""
+
+        while True:
+            # Receive the data from the client
+            data = client_socket.recv(1024 * 1024)
+            if not data:
+                break
+
+            # Append the received data to the buffer
+            buffer += data
+
+            try:
+                # Try to parse the buffer as JSON
+                test_ids_from_buffer = process_json_util.process_rpc_json(
+                    buffer.decode("utf-8")
+                )
+                # Clear the buffer as complete JSON object is received
+                buffer = b""
+
+                # Process the JSON data
+                break
+            except json.JSONDecodeError:
+                # JSON decoding error, the complete JSON object is not yet received
+                continue
+    except socket.error as e:
+        print(f"Error: Could not connect to runTestIdsPort: {e}")
+        print("Error: Could not connect to runTestIdsPort")
+
+    port, uuid = parse_execution_cli_args(argv[:index])
+    if test_ids_from_buffer:
+        # Perform test execution.
+        payload = run_tests(
+            start_dir, test_ids_from_buffer, pattern, top_level_dir, uuid
+        )
+    else:
+        cwd = os.path.abspath(start_dir)
+        status = TestExecutionStatus.error
+        payload: PayloadDict = {
+            "cwd": cwd,
+            "status": status,
+            "error": "No test ids received from buffer",
+        }
