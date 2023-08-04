77c7996d6e4589ef7227265eca69045fd5cd395f
eleanorjboyd <eleanorboyd@microsoft.com>
2023-08-04 10:10:04 -0700

    remove log

--- src/test/mocks/helper.ts
+++ src/test/mocks/helper.ts
@@ -4,9 +4,8 @@
 import { Readable } from 'stream';
 
 export class MyReadableStream extends Readable {
-    _read(size: unknown): void | null {
+    _read(_size: unknown): void | null {
         // custom reading logic here
-        console.log(size);
         this.push(null); // end the stream
     }
 }
