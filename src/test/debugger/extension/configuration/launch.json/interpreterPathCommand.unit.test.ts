18f159a0412024cfbeb1712d9df64978b2aa1d77
Kartik Raj <karraj@microsoft.com>
2023-09-12 16:56:50 -0700

    Fix

--- src/test/debugger/extension/configuration/launch.json/interpreterPathCommand.unit.test.ts
+++ src/test/debugger/extension/configuration/launch.json/interpreterPathCommand.unit.test.ts
@@ -45,7 +45,7 @@ suite('Interpreter Path Command', () => {
     test('If `workspaceFolder` property exists in `args`, it is used to retrieve setting from config', async () => {
         const args = { workspaceFolder: 'folderPath' };
         when(interpreterService.getActiveInterpreter(anything())).thenCall((arg) => {
-            assert.deepEqual(arg, Uri.parse('folderPath'));
+            assert.deepEqual(arg, Uri.file('folderPath'));
 
             return Promise.resolve({ path: 'settingValue' }) as unknown;
         });
