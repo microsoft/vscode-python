143ed5a8117347e19495edd39c00ceb42b2f47bb
Anthony Kim <anthonykim@microsoft.com>
2023-09-13 00:08:09 -0700

    format python files for ruff compliance

--- pythonFiles/tests/test_normalize_selection.py
+++ pythonFiles/tests/test_normalize_selection.py
@@ -1,8 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import os
-import sys
+
 import textwrap
 
 # __file__ = "/Users/anthonykim/Desktop/vscode-python/pythonFiles/normalizeSelection.py"
--- pythonFiles/tests/test_smart_selection.py
+++ pythonFiles/tests/test_smart_selection.py
@@ -1,7 +1,4 @@
-import ast
 import importlib
-import os
-import sys
 import textwrap
 
 import normalizeSelection
@@ -9,7 +6,7 @@ import normalizeSelection
 
 def test_part_dictionary():
     importlib.reload(normalizeSelection)
-    src = 'import textwrap\nimport ast\n\nprint("Audi")\nprint("Genesis")\n\n\nprint("Audi");print("BMW");print("Mercedes")\n\nmy_dict = {\n    "key1": "value1",\n    "key2": "value2"\n}\n\n\nsrc = """\nmy_dict = {\n"key1": "value1",\n"key2": "value2"\n}\n"""\n\ntop_level_nodes = []\n\nparsed_file_content = ast.parse(src)\nprint(ast.dump(parsed_file_content))\n\nparsed_dict_content2 = ast.parse(str(my_dict))\nprint(ast.dump(parsed_dict_content2))\n\n\nfor node in ast.iter_child_nodes(parsed_file_content):\n        top_level_nodes.append(node)\n        line_start = node.lineno\n        line_end = node.end_lineno\n        code_of_node = ast.get_source_segment(wholeFileContent, node)\n        ast.get_source_segment(wholeFileContent, node) # This is way to get original code of the selected node\n\n################################################################################\n# New test case(s):\n# what should happen when shift enter at line 5? \n# follow ast says ----- TODO \n\n# execute individually line 5 bc two statements ---- TODO \n#################################################################################'
+    src = 'import textwrap\nimport ast\n\nprint("Audi")\nprint("Genesis")\n\n\nprint("Audi");print("BMW");print("Mercedes")\n\nmy_dict = {\n    "key1": "value1",\n    "key2": "value2"\n}\n\n\nsrc = """\nmy_dict = {\n"key1": "value1",\n"key2": "value2"\n}\n"""\n\ntop_level_nodes = []\n\nparsed_file_content = ast.parse(src)\nprint(ast.dump(parsed_file_content))\n\nparsed_dict_content2 = ast.parse(str(my_dict))\nprint(ast.dump(parsed_dict_content2))\n\n\nfor node in ast.iter_child_nodes(parsed_file_content):\n        top_level_nodes.append(node)\n        line_start = node.lineno\n        line_end = node.end_lineno\n        code_of_node = ast.get_source_segment(wholeFileContent, node)\n        ast.get_source_segment(wholeFileContent, node) # This is way to get original code of the selected node\n\n################################################################################\n# New test case(s):\n# what should happen when shift enter at line 5? \n# follow ast says ----- TODO \n\n# execute individually line 5 bc two statements ---- TODO \n#################################################################################'  # noqa: E501
 
     expected = 'my_dict = {\n    "key1": "value1",\n    "key2": "value2"\n}\n'
     # parsed_file_content = ast.parse(src)
