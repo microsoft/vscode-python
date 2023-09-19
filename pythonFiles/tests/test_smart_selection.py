942857837d30e7894f654d23a2987b3a220aa756
Anthony Kim <anthonykim@microsoft.com>
2023-09-19 13:58:37 -0700

    fix test_part_dictionary test

--- pythonFiles/tests/test_smart_selection.py
+++ pythonFiles/tests/test_smart_selection.py
@@ -6,14 +6,27 @@ import normalizeSelection
 
 def test_part_dictionary():
     importlib.reload(normalizeSelection)
-    src = 'import textwrap\nimport ast\n\nprint("Audi")\nprint("Genesis")\n\n\nprint("Audi");print("BMW");print("Mercedes")\n\nmy_dict = {\n    "key1": "value1",\n    "key2": "value2"\n}\n\n\nsrc = """\nmy_dict = {\n"key1": "value1",\n"key2": "value2"\n}\n"""\n\ntop_level_nodes = []\n\nparsed_file_content = ast.parse(src)\nprint(ast.dump(parsed_file_content))\n\nparsed_dict_content2 = ast.parse(str(my_dict))\nprint(ast.dump(parsed_dict_content2))\n\n\nfor node in ast.iter_child_nodes(parsed_file_content):\n        top_level_nodes.append(node)\n        line_start = node.lineno\n        line_end = node.end_lineno\n        code_of_node = ast.get_source_segment(wholeFileContent, node)\n        ast.get_source_segment(wholeFileContent, node) # This is way to get original code of the selected node\n\n################################################################################\n# New test case(s):\n# what should happen when shift enter at line 5? \n# follow ast says ----- TODO \n\n# execute individually line 5 bc two statements ---- TODO \n#################################################################################'  # noqa: E501
+    src = textwrap.dedent(
+        """\
+        not_dictionary = 'hi'
+        my_dict = {
+            "key1": "value1",
+            "key2": "value2"
+        }
+        print('only send the dictionary')
+        """
+    )
 
-    expected = 'my_dict = {\n    "key1": "value1",\n    "key2": "value2"\n}\n'
-    # parsed_file_content = ast.parse(src)
-    # top_level_nodes = []
-    # for node in ast.iter_child_nodes(parsed_file_content):
-    #     print(node.__dir__())
-    result = normalizeSelection.traverse_file(src, 10, 11, False)
+    expected = textwrap.dedent(
+        """\
+        my_dict = {
+            "key1": "value1",
+            "key2": "value2"
+        }
+        """
+    )
+
+    result = normalizeSelection.traverse_file(src, 3, 3, False)
     assert result == expected
 
 
