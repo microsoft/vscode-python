83934c789ea6daf0e154d0e431b9d31aabaab1f2
eleanorjboyd <eleanorboyd@microsoft.com>
2023-09-25 10:51:50 -0700

    add test python side

--- pythonFiles/tests/pytestadapter/test_execution.py
+++ pythonFiles/tests/pytestadapter/test_execution.py
@@ -1,5 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+import json
 import os
 import shutil
 from typing import Any, Dict, List
@@ -9,6 +10,9 @@ import pytest
 from tests.pytestadapter import expected_execution_test_output
 
 from .helpers import TEST_DATA_PATH, runner, runner_with_cwd
+
+# uncomment this line to skip all tests in this module
+# pytestmark = pytest.mark.skip(reason="Skipping all tests in this module")
 
 
 def test_config_file():
@@ -217,7 +221,7 @@ def test_bad_id_error_execution():
         ),
         (
             ["test_logging.py::test_logging2", "test_logging.py::test_logging"],
-            expected_execution_test_output.doctest_pytest_expected_execution_output,
+            expected_execution_test_output.logging_test_expected_execution_output,
         ),
     ],
 )
@@ -225,23 +229,27 @@ def test_pytest_execution(test_ids, expected_const):
     """
     Test that pytest discovery works as expected where run pytest is always successful
     but the actual test results are both successes and failures.:
-    1. uf_execution_expected_output: unittest tests run on multiple files.
-    2. uf_single_file_expected_output: test run on a single file.
-    3. uf_single_method_execution_expected_output: test run on a single method in a file.
-    4. uf_non_adjacent_tests_execution_expected_output: test run on unittests in two files with single selection in test explorer.
-    5. unit_pytest_same_file_execution_expected_output: test run on a file with both unittest and pytest tests.
-    6. dual_level_nested_folder_execution_expected_output: test run on a file with one test file
+    1: skip_tests_execution_expected_output: test run on a file with skipped tests.
+    2. error_raised_exception_execution_expected_output: test run on a file that raises an exception.
+    3. uf_execution_expected_output: unittest tests run on multiple files.
+    4. uf_single_file_expected_output: test run on a single file.
+    5. uf_single_method_execution_expected_output: test run on a single method in a file.
+    6. uf_non_adjacent_tests_execution_expected_output: test run on unittests in two files with single selection in test explorer.
+    7. unit_pytest_same_file_execution_expected_output: test run on a file with both unittest and pytest tests.
+    8. dual_level_nested_folder_execution_expected_output: test run on a file with one test file
     at the top level and one test file in a nested folder.
-    7. double_nested_folder_expected_execution_output: test run on a double nested folder.
-    8. parametrize_tests_expected_execution_output: test run on a parametrize test with 3 inputs.
-    9. single_parametrize_tests_expected_execution_output: test run on single parametrize test.
-    10. doctest_pytest_expected_execution_output: test run on doctest file.
+    9. double_nested_folder_expected_execution_output: test run on a double nested folder.
+    10. parametrize_tests_expected_execution_output: test run on a parametrize test with 3 inputs.
+    11. single_parametrize_tests_expected_execution_output: test run on single parametrize test.
+    12. doctest_pytest_expected_execution_output: test run on doctest file.
+    13. logging_test_expected_execution_output: test run on a file with logging.
 
 
     Keyword arguments:
     test_ids -- an array of test_ids to run.
     expected_const -- a dictionary of the expected output from running pytest discovery on the files.
     """
+    print("Test IDs: ", test_ids)
     args = test_ids
     actual = runner(args)
     assert actual
