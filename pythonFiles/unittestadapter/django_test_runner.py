395e36613abacf5de67b340e23eb546b6232ed9c
eleanorjboyd <eleanorboyd@microsoft.com>
2023-10-18 13:12:24 -0700

    add result class error msg

--- pythonFiles/unittestadapter/django_test_runner.py
+++ pythonFiles/unittestadapter/django_test_runner.py
@@ -13,5 +13,9 @@ class CustomTestRunner(DiscoverRunner):
     def get_test_runner_kwargs(self):
         print("get_test_runner_kwargs")
         kwargs = super().get_test_runner_kwargs()
+        if kwargs["resultclass"] is not None:
+            raise ValueError(
+                "Resultclass already set, cannot use custom test runner design for VS Code compatibility."
+            )
         kwargs["resultclass"] = UnittestTestResult
         return kwargs
