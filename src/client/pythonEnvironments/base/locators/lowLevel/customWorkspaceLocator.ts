761d94543e8e6006e3a7510890bd12d3baa68a43
Kartik Raj <karraj@microsoft.com>
2023-10-30 23:38:14 +0000

    Ensure Python environment defined in `python.defaultInterpreterPath` is returned via discovery API

---
+++ src/client/pythonEnvironments/base/locators/lowLevel/customWorkspaceLocator.ts
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { PythonEnvKind } from '../../info';
+import { BasicEnvInfo, IPythonEnvsIterator } from '../../locator';
+import { FSWatchingLocator } from './fsWatchingLocator';
+import { getPythonSetting, onDidChangePythonSetting } from '../../../common/externalDependencies';
+import '../../../../common/extensions';
+import { traceVerbose } from '../../../../logging';
+
+export const DEFAULT_INTERPRETER_PATH = 'defaultInterpreterPath';
+
+/**
+ * Finds and resolves custom virtual environments that users have provided.
+ */
+export class CustomWorkspaceLocator extends FSWatchingLocator {
+    public readonly providerId: string = 'custom-workspace-locator';
+
+    constructor(private readonly root: string) {
+        super(
+            () => [],
+            async () => PythonEnvKind.Unknown,
+        );
+    }
+
+    protected async initResources(): Promise<void> {
+        this.disposables.push(onDidChangePythonSetting(DEFAULT_INTERPRETER_PATH, () => this.fire(), this.root));
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    protected doIterEnvs(): IPythonEnvsIterator<BasicEnvInfo> {
+        const iterator = async function* (root: string) {
+            traceVerbose('Searching for custom workspace envs');
+            const filename = getPythonSetting<string>(DEFAULT_INTERPRETER_PATH, root);
+            if (!filename || filename === 'python') {
+                return;
+            }
+            yield { kind: PythonEnvKind.Unknown, executablePath: filename };
+            traceVerbose(`Finished searching for windows store envs`);
+        };
+        return iterator(this.root);
+    }
+}
