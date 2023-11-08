03b45670af70b42a20a96e90a8e5a25d106ed26e
Kartik Raj <karraj@microsoft.com>
2023-11-08 14:56:36 -0800

    Restore all env vars directly for powershell (to keep things simple)

--- pythonFiles/deactivate/powershell/deactivate.ps1
+++ pythonFiles/deactivate/powershell/deactivate.ps1
@@ -1,26 +1,4 @@
-# Same as deactivate in "Activate.ps1"
-function global:deactivate ([switch]$NonDestructive) {
-    if (Test-Path function:_OLD_VIRTUAL_PROMPT) {
-        copy-item function:_OLD_VIRTUAL_PROMPT function:prompt
-        remove-item function:_OLD_VIRTUAL_PROMPT
-    }
-    if (Test-Path env:_OLD_VIRTUAL_PYTHONHOME) {
-        copy-item env:_OLD_VIRTUAL_PYTHONHOME env:PYTHONHOME
-        remove-item env:_OLD_VIRTUAL_PYTHONHOME
-    }
-    if (Test-Path env:_OLD_VIRTUAL_PATH) {
-        copy-item env:_OLD_VIRTUAL_PATH env:PATH
-        remove-item env:_OLD_VIRTUAL_PATH
-    }
-    if (Test-Path env:VIRTUAL_ENV) {
-        remove-item env:VIRTUAL_ENV
-    }
-    if (!$NonDestructive) {
-        remove-item function:deactivate
-    }
-}
-
-# Load dotenv-style file and set environment variables
+# Load dotenv-style file and restore environment variables
 Get-Content -Path "$PSScriptRoot\envVars.txt" | ForEach-Object {
     # Split each line into key and value at the first '='
     $parts = $_ -split '=', 2
