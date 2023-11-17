49758ea10aadfa5dc58bdbfcdb02e6604893536a
Kartik Raj <karraj@microsoft.com>
2023-11-17 08:58:57 +0000

    console. logging

--- src/client/logging/outputChannelLogger.ts
+++ src/client/logging/outputChannelLogger.ts
@@ -9,22 +9,27 @@ export class OutputChannelLogger implements ILogging {
     constructor(private readonly channel: LogOutputChannel) {}
 
     public traceLog(...data: Arguments): void {
+        console.log(util.format(...data));
         this.channel.appendLine(util.format(...data));
     }
 
     public traceError(...data: Arguments): void {
+        console.error(util.format(...data));
         this.channel.error(util.format(...data));
     }
 
     public traceWarn(...data: Arguments): void {
+        console.warn(util.format(...data));
         this.channel.warn(util.format(...data));
     }
 
     public traceInfo(...data: Arguments): void {
+        console.info(util.format(...data));
         this.channel.info(util.format(...data));
     }
 
     public traceVerbose(...data: Arguments): void {
+        console.debug(util.format(...data));
         this.channel.debug(util.format(...data));
     }
 }
