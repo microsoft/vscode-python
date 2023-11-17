739f65f769f64c8403ee17923f3ac9091e853bf8
Kartik Raj <karraj@microsoft.com>
2023-11-17 10:00:38 +0000

    s

--- src/client/common/process/worker/main.ts
+++ src/client/common/process/worker/main.ts
@@ -2,13 +2,16 @@
 // Licensed under the MIT License.
 
 import { Worker } from 'worker_threads';
-import { traceError } from '../../../logging';
+import { traceError, traceVerbose } from '../../../logging';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
 export async function executeWorkerFile(workerFileName: string, workerData: any): Promise<any> {
     return new Promise((resolve, reject) => {
+        traceVerbose(`Starting worker ${workerFileName} with data ${JSON.stringify(workerData)}`);
         const worker = new Worker(workerFileName, { workerData });
+        traceVerbose(`Started worker ${workerFileName}`);
         worker.on('message', (msg: { err: Error; res: unknown }) => {
+            traceVerbose(`Worker ${workerFileName} sent message ${JSON.stringify(msg)}`);
             if (msg.err) {
                 reject(msg.err);
             }
@@ -19,6 +22,7 @@ export async function executeWorkerFile(workerFileName: string, workerData: any)
             reject(ex);
         });
         worker.on('exit', (code) => {
+            traceVerbose(`Worker ${workerFileName} exited with code ${code}`);
             if (code !== 0) {
                 reject(new Error(`Worker ${workerFileName} stopped with exit code ${code}`));
             }
