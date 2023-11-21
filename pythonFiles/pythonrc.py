8477b172f7c6a82e422850c094dcf47a2d40afbc
Anthony Kim <anthonykim@microsoft.com>
2023-11-21 00:16:35 -0800

    clean up

--- pythonFiles/pythonrc.py
+++ pythonFiles/pythonrc.py
@@ -14,7 +14,6 @@ class repl_hooks:
 
     def my_displayhook(self, value):
         if value is None:
-            # did not fail
             self.failure_flag = False
 
         self.original_displayhook(value)
@@ -31,6 +30,7 @@ class ps1:
     sys.excepthook = hooks.my_excepthook
     sys.displayhook = hooks.my_displayhook
 
+    # str will get called for every prompt with exit code to show success/failure
     def __str__(self):
         exit_code = 0
         if self.hooks.failure_flag:
@@ -38,8 +38,7 @@ class ps1:
         else:
             exit_code = 0
 
-        result = f"{chr(27)}]633;D;{exit_code}0{chr(7)}{chr(27)}]633;A{chr(7)}{original_ps1}{chr(27)}]633;B{chr(7)}{chr(27)}]633;C{chr(7)}"
-
+        # Guide following official VS Code doc for shell integration sequence:
         # result = "{command_finished}{prompt_started}{prompt}{command_start}{command_executed}".format(
         #     command_finished="\x1b]633;D;" + str(exit_code) + "0\x07",
         #     prompt_started="\x1b]633;A\x07",
@@ -47,6 +46,8 @@ class ps1:
         #     command_start="\x1b]633;B\x07",
         #     command_executed="\x1b]633;C\x07",
         # )
+        result = f"{chr(27)}]633;D;{exit_code}0{chr(7)}{chr(27)}]633;A{chr(7)}{original_ps1}{chr(27)}]633;B{chr(7)}{chr(27)}]633;C{chr(7)}"
+
         return result
 
 
