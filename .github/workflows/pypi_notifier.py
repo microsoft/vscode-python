e29d80b59fc74af269008550d3765adaff6229d4
Anthony Kim <anthonykim@microsoft.com>
2024-01-30 13:43:43 -0800

    get dictionary of package and version

--- .github/workflows/pypi_notifier.py
+++ .github/workflows/pypi_notifier.py
@@ -11,15 +11,20 @@ import pathlib
 def main():
     # load in list of packages and its version from requirements.txt that is in one directory above
     root_path = pathlib.Path(__file__).parent.parent.parent
-    requirement_path = pathlib.Path(root_path, "requirements.txt").read_text(
+    requirement_content = pathlib.Path(root_path, "requirements.txt").read_text(
         encoding="utf-8"
     )
-    # with open(requirement_path, "r") as f:
-    #     packages = f.readlines()
-    #     packages = [package.strip() for package in packages]
-    #     packages = [package.split("==") for package in packages]
-    # print(packages)
-    print(requirement_path)
+    # Dictionary of package name and its version
+    packages = {}
+    for line in requirement_content.splitlines():
+        # print(line)
+        # If line has == and \ then perform line split to store package and version
+        if "==" in line and "\\" in line:
+            package, version = line.split("==")
+            # remove \ in version string and blank spaces
+            version = version.replace("\\", "").strip()
+            print(version)
+            packages[package] = version
 
 
 if __name__ == "__main__":
