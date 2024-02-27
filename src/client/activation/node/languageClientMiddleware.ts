b8a3d3fb22b29d175d04866ab3e8547f00828fb9
Erik De Bonte <erikd@microsoft.com>
2024-02-27 12:37:23 -0800

    Remove old Pylance code

--- src/client/activation/node/languageClientMiddleware.ts
+++ src/client/activation/node/languageClientMiddleware.ts
@@ -2,64 +2,35 @@
 // Licensed under the MIT License.
 
 import { Uri } from 'vscode';
-import { ConfigurationItem, LanguageClient, LSPObject } from 'vscode-languageclient/node';
+import { ConfigurationItem, LSPObject } from 'vscode-languageclient/node';
 import { IJupyterExtensionDependencyManager, IWorkspaceService } from '../../common/application/types';
 import { IServiceContainer } from '../../ioc/types';
 import { JupyterExtensionIntegration } from '../../jupyter/jupyterIntegration';
 import { traceLog } from '../../logging';
 import { LanguageClientMiddleware } from '../languageClientMiddleware';
-import { LspInteractiveWindowMiddlewareAddon } from './lspInteractiveWindowMiddlewareAddon';
 
 import { LanguageServerType } from '../types';
 
-import { LspNotebooksExperiment } from './lspNotebooksExperiment';
-
 export class NodeLanguageClientMiddleware extends LanguageClientMiddleware {
-    private readonly lspNotebooksExperiment: LspNotebooksExperiment;
-
     private readonly jupyterExtensionIntegration: JupyterExtensionIntegration;
 
     private readonly workspaceService: IWorkspaceService;
 
-    public constructor(
-        serviceContainer: IServiceContainer,
-        private getClient: () => LanguageClient | undefined,
-        serverVersion?: string,
-    ) {
+    public constructor(serviceContainer: IServiceContainer, serverVersion?: string) {
         super(serviceContainer, LanguageServerType.Node, serverVersion);
 
         this.workspaceService = serviceContainer.get<IWorkspaceService>(IWorkspaceService);
 
-        this.lspNotebooksExperiment = serviceContainer.get<LspNotebooksExperiment>(LspNotebooksExperiment);
         this.setupHidingMiddleware(serviceContainer);
 
         this.jupyterExtensionIntegration = serviceContainer.get<JupyterExtensionIntegration>(
             JupyterExtensionIntegration,
         );
-        if (!this.notebookAddon) {
-            this.notebookAddon = new LspInteractiveWindowMiddlewareAddon(
-                this.getClient,
-                this.jupyterExtensionIntegration,
-            );
-        }
     }
 
     // eslint-disable-next-line class-methods-use-this
     protected shouldCreateHidingMiddleware(_: IJupyterExtensionDependencyManager): boolean {
         return false;
-    }
-
-    protected async onExtensionChange(jupyterDependencyManager: IJupyterExtensionDependencyManager): Promise<void> {
-        if (jupyterDependencyManager && jupyterDependencyManager.isJupyterExtensionInstalled) {
-            await this.lspNotebooksExperiment.onJupyterInstalled();
-        }
-
-        if (!this.notebookAddon) {
-            this.notebookAddon = new LspInteractiveWindowMiddlewareAddon(
-                this.getClient,
-                this.jupyterExtensionIntegration,
-            );
-        }
     }
 
     protected async getPythonPathOverride(uri: Uri | undefined): Promise<string | undefined> {
--- src/client/jupyter/jupyterIntegration.ts
+++ src/client/jupyter/jupyterIntegration.ts
@@ -57,14 +57,6 @@ type PythonApiForJupyterExtension = {
      * @param func : The function that Python should call when requesting the Python path.
      */
     registerJupyterPythonPathFunction(func: (uri: Uri) => Promise<string | undefined>): void;
-
-    /**
-     * Call to provide a function that the Python extension can call to request the notebook
-     * document URI related to a particular text document URI, or undefined if there is no
-     * associated notebook.
-     * @param func : The function that Python should call when requesting the notebook URI.
-     */
-    registerGetNotebookUriForTextDocumentUriFunction(func: (textDocumentUri: Uri) => Uri | undefined): void;
 };
 
 type JupyterExtensionApi = {
@@ -82,8 +74,6 @@ export class JupyterExtensionIntegration {
     private pylanceExtension: Extension<PylanceApi> | undefined;
 
     private jupyterPythonPathFunction: ((uri: Uri) => Promise<string | undefined>) | undefined;
-
-    private getNotebookUriForTextDocumentUriFunction: ((textDocumentUri: Uri) => Uri | undefined) | undefined;
 
     constructor(
         @inject(IExtensions) private readonly extensions: IExtensions,
@@ -123,8 +113,6 @@ export class JupyterExtensionIntegration {
             getCondaVersion: () => this.condaService.getCondaVersion(),
             registerJupyterPythonPathFunction: (func: (uri: Uri) => Promise<string | undefined>) =>
                 this.registerJupyterPythonPathFunction(func),
-            registerGetNotebookUriForTextDocumentUriFunction: (func: (textDocumentUri: Uri) => Uri | undefined) =>
-                this.registerGetNotebookUriForTextDocumentUriFunction(func),
         });
         return undefined;
     }
@@ -179,18 +167,5 @@ export class JupyterExtensionIntegration {
 
     public getJupyterPythonPathFunction(): ((uri: Uri) => Promise<string | undefined>) | undefined {
         return this.jupyterPythonPathFunction;
-    }
-
-    public registerGetNotebookUriForTextDocumentUriFunction(func: (textDocumentUri: Uri) => Uri | undefined): void {
-        this.getNotebookUriForTextDocumentUriFunction = func;
-
-        const api = this.getPylanceApi();
-        if (api) {
-            api.notebook!.registerGetNotebookUriForTextDocumentUriFunction(func);
-        }
-    }
-
-    public getGetNotebookUriForTextDocumentUriFunction(): ((textDocumentUri: Uri) => Uri | undefined) | undefined {
-        return this.getNotebookUriForTextDocumentUriFunction;
     }
 }
