6fbde074d6ced47fbc435e716febaad9af327420
LouisGobert <louis.gobert2000@gmail.com>
2024-04-24 17:01:16 +0200

    Add repo regex test

--- src/client/activation/requirementsTxtLinkActivator.ts
+++ src/client/activation/requirementsTxtLinkActivator.ts
@@ -2,23 +2,24 @@ import { injectable } from 'inversify';
 import { Hover, languages, TextDocument, Position } from 'vscode';
 import { IExtensionSingleActivationService } from './types';
 
+const PYPI_PROJECT_URL = 'https://pypi.org/project';
 @injectable()
 export class RequirementsTxtLinkActivator implements IExtensionSingleActivationService {
     // eslint-disable-next-line class-methods-use-this
     public readonly supportedWorkspaceTypes = { untrustedWorkspace: true, virtualWorkspace: true };
+
     public async activate(): Promise<void> {
-        languages.registerHoverProvider(
-            [{ pattern: '**/*requirement*.txt' },{ pattern: '**/requirements/*.txt' }],
-            {
-                provideHover(document: TextDocument, position: Position) {
-                    // Regex to allow to find every possible pypi package (base regex from https://peps.python.org/pep-0508/#names)
-                    const regex = /^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*)($|=| |;|\[)/i
-                    const row = document.lineAt(position.line).text;
-                    const projectName = row.match(regex);
-                    return(projectName) ? new Hover(`https://pypi.org/project/${projectName[1]}/`) : null
-                },
+        languages.registerHoverProvider([{ pattern: '**/*requirement*.txt' }, { pattern: '**/requirements/*.txt' }], {
+            provideHover(document: TextDocument, position: Position) {
+                const link = RequirementsTxtLinkActivator.generatePyPiLink(document.lineAt(position.line).text);
+                return link ? new Hover(link) : null;
             },
-        );
+        });
+    }
+
+    public static generatePyPiLink(name: string): string | null {
+        // Regex to allow to find every possible pypi package (base regex from https://peps.python.org/pep-0508/#names)
+        const projectName = name.match(/^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*)($|=| |;|\[)/i);
+        return projectName ? `${PYPI_PROJECT_URL}/${projectName[1]}/` : null;
     }
 }
-
---
+++ src/test/activation/requirementsTxtLinkActivator.unit.test.ts
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
+import { expect } from 'chai';
+import { RequirementsTxtLinkActivator } from '../../client/activation/requirementsTxtLinkActivator';
+
+/** *
+requests [security] >= 2.8.1, == 2.8.* ; python_version < "2.7"
+urllib3 @ https://github.com/urllib3/urllib3/archive/refs/tags/1.26.8.zip
+
+# It is possible to refer to other requirement files or constraints files.
+-r other-requirements.txt
+-c constraints.txt
+
+# It is possible to refer to specific local distribution paths.
+./downloads/numpy-1.9.2-cp34-none-win32.whl
+
+# It is possible to refer to URLs.
+http://wxpython.org/Phoenix/snapshot-builds/wxPython_Phoenix-3.0.3.dev1820+49a8884-cp34-none-win_amd64.whl
+
+ */
+
+suite('Link to PyPi in requiements test', () => {
+    test('Check if all possible project name are matched', () => {
+        const rows = [
+            ['pytest', 'pytest'],
+            ['pytest-cov', 'pytest-cov'],
+            ['pytest_cov', 'pytest_cov'],
+            ['pytest_cov[an_extra]', 'pytest_cov'],
+            ['pytest == 0.6.1', 'pytest'],
+            ['pytest== 0.6.1', 'pytest'],
+            ['requests [security] >= 2.8.1, == 2.8.* ; python_version < "2.7"', 'requests'],
+            ['# a comment', null],
+            ['', null],
+        ];
+
+        rows.forEach(([input, expected]) => {
+            expect(RequirementsTxtLinkActivator.generatePyPiLink(input)).equal(expected ? `https://pypi.org/project/${expected}/`: null);
+        });
+    });
+});
