7e1cddd24a68906f616acde60d626dcda2f38f3e
LouisGobert <45512639+LouisGobert@users.noreply.github.com>
2024-04-24 20:05:40 +0200

    Update src/client/activation/requirementsTxtLinkActivator.ts
    Co-authored-by: Karthik Nadig <kanadig@microsoft.com>

--- src/client/activation/requirementsTxtLinkActivator.ts
+++ src/client/activation/requirementsTxtLinkActivator.ts
@@ -3,6 +3,13 @@ import { Hover, languages, TextDocument, Position } from 'vscode';
 import { IExtensionSingleActivationService } from './types';
 
 const PYPI_PROJECT_URL = 'https://pypi.org/project';
+
+export function generatePyPiLink(name: string): string | null {
+    // Regex to allow to find every possible pypi package (base regex from https://peps.python.org/pep-0508/#names)
+    const projectName = name.match(/^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*)($|=| |;|\[)/i);
+    return projectName ? `${PYPI_PROJECT_URL}/${projectName[1]}/` : null;
+}
+    
 @injectable()
 export class RequirementsTxtLinkActivator implements IExtensionSingleActivationService {
     // eslint-disable-next-line class-methods-use-this
@@ -11,15 +18,9 @@ export class RequirementsTxtLinkActivator implements IExtensionSingleActivationS
     public async activate(): Promise<void> {
         languages.registerHoverProvider([{ pattern: '**/*requirement*.txt' }, { pattern: '**/requirements/*.txt' }], {
             provideHover(document: TextDocument, position: Position) {
-                const link = RequirementsTxtLinkActivator.generatePyPiLink(document.lineAt(position.line).text);
+                const link = generatePyPiLink(document.lineAt(position.line).text);
                 return link ? new Hover(link) : null;
             },
         });
-    }
-
-    public static generatePyPiLink(name: string): string | null {
-        // Regex to allow to find every possible pypi package (base regex from https://peps.python.org/pep-0508/#names)
-        const projectName = name.match(/^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*)($|=| |;|\[)/i);
-        return projectName ? `${PYPI_PROJECT_URL}/${projectName[1]}/` : null;
     }
 }
