2fede160a7cfdf8d11d7a1945fdc4ddf14e60317
Anthony Kim <anthonykim@microsoft.com>
2024-05-01 10:54:15 -0700

    save comment of TODO

--- src/client/repl/replCommands.ts
+++ src/client/repl/replCommands.ts
@@ -21,6 +21,8 @@ import { createReplController } from './replController';
 
 let notebookController: NotebookController | undefined;
 let notebookEditor: NotebookEditor | undefined;
+let allNotebookEditors: [NotebookEditor] | undefined;
+let mapUriToNotebookEditor: Map<Uri, NotebookEditor> | undefined;
 
 // TODO: Need to figure out making separate REPL for each file:
 // a.py in REPL.
@@ -32,6 +34,8 @@ let notebookEditor: NotebookEditor | undefined;
 // cache binding uri to to REPL instance or notebookEditor.
 
 // TODO: figure out way to put markdown telling user kernel has been dead and need to pick again.
+
+// TODO: FIGURE OUT WHY INTELLISENSE IS NOT WORKING
 
 async function getSelectedTextToExecute(textEditor: TextEditor): Promise<string | undefined> {
     if (!textEditor) {
@@ -68,16 +72,27 @@ export async function registerReplCommands(
                 const activeEditor = window.activeTextEditor as TextEditor;
 
                 const code = await getSelectedTextToExecute(activeEditor);
-                const ourResource = Uri.from({ scheme: 'untitled', path: 'repl.interactive' });
+                // const ourResource = Uri.from({ scheme: 'untitled', path: 'repl.interactive' });
+                const ourResource2 = Uri.file(uri.path);
+
                 // How to go from user clicking Run Python --> Run selection/line via Python REPL -> IW opening
-                const notebookDocument = await workspace.openNotebookDocument(ourResource);
+                const notebookDocument = await workspace.openNotebookDocument(ourResource2);
 
                 // We want to keep notebookEditor, whenever we want to run.
                 // Find interactive window, or open it.
-                if (!notebookEditor) {
+
+                // if (!notebookEditor) {
+                //     notebookEditor = await window.showNotebookDocument(notebookDocument, {
+                //         viewColumn: ViewColumn.Beside,
+                //     });
+                // }
+                // Instead we need to first check if notebookEditor for given file Uri exist.
+                // If it doesnt, we create notebookEditor and add to Map <Uri, NotebookEditor>
+                if (!mapUriToNotebookEditor?.get(ourResource2) || !mapUriToNotebookEditor.get(ourResource2)) {
                     notebookEditor = await window.showNotebookDocument(notebookDocument, {
                         viewColumn: ViewColumn.Beside,
                     });
+                    mapUriToNotebookEditor?.set(ourResource2, notebookEditor);
                 }
 
                 notebookController!.updateNotebookAffinity(notebookDocument, NotebookControllerAffinity.Default);
@@ -100,7 +115,7 @@ export async function registerReplCommands(
                 // Execute the cell
                 commands.executeCommand('notebook.cell.execute', {
                     ranges: [{ start: cellCount, end: cellCount + 1 }],
-                    document: ourResource,
+                    document: ourResource2,
                 });
             }
         }),
