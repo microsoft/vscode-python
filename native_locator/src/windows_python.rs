5dc43f924a0a19529684e59c89e2eca0399a41a0
Don Jayamanne <don.jayamanne@outlook.com>
2024-05-08 11:47:35 +1000

    Minor tweeks to Windows Locator

--- native_locator/src/windows_python.rs
+++ native_locator/src/windows_python.rs
@@ -22,41 +22,26 @@ fn report_path_python(path: &str, dispatcher: &mut impl messaging::MessageDispat
 fn report_windows_store_python(
     dispatcher: &mut impl messaging::MessageDispatcher,
     environment: &impl known::Environment,
-) {
-    let home = environment.get_user_home();
-    match home {
-        Some(home) => {
-            let apps_path = Path::new(&home)
-                .join("AppData")
-                .join("Local")
-                .join("Microsoft")
-                .join("WindowsApps");
-            let files = std::fs::read_dir(apps_path);
-            match files {
-                Ok(files) => {
-                    for file in files {
-                        match file {
-                            Ok(file) => {
-                                let path = file.path();
-                                match path.file_name() {
-                                    Some(name) => {
-                                        let name = name.to_string_lossy().to_lowercase();
-                                        if name.starts_with("python3.") && name.ends_with(".exe") {
-                                            report_path_python(&path.to_string_lossy(), dispatcher);
-                                        }
-                                    }
-                                    None => {}
-                                }
-                            }
-                            Err(_) => {}
-                        }
-                    }
+) -> Option<()> {
+    let home = environment.get_user_home()?;
+    let apps_path = Path::new(&home)
+        .join("AppData")
+        .join("Local")
+        .join("Microsoft")
+        .join("WindowsApps");
+    let files = std::fs::read_dir(apps_path).ok()?;
+    for file in files {
+        if let Ok(file) = file {
+            let path = file.path();
+            if let Some(name) = path.file_name() {
+                let name = name.to_string_lossy().to_lowercase();
+                if name.starts_with("python3.") && name.ends_with(".exe") {
+                    report_path_python(&path.to_string_lossy(), dispatcher);
                 }
-                Err(_) => {}
             }
         }
-        None => {}
     }
+    None
 }
 
 fn report_registry_pythons() {}
