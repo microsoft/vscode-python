6ff2dce9c1e180d421a72143996d36737c7aedac
Anthony Kim <anthonykim@microsoft.com>
2024-05-10 02:44:00 -0700

    clean up

--- src/client/repl/replCommands.ts
+++ src/client/repl/replCommands.ts
@@ -22,21 +22,7 @@ import { createReplController } from './replController';
 
 let notebookController: NotebookController | undefined;
 let notebookEditor: NotebookEditor | undefined;
-// let allNotebookEditors: [NotebookEditor] | undefined;
-// let mapUriToNotebookEditor: Map<Uri, NotebookEditor> | undefined;
-
-// TODO: Need to figure out making separate REPL for each file:
-// a.py in REPL.
-// b.py run in REPL
-// MAPPING Uri to notebookEditor if we want separate REPL for each file.
-// Currently: Everything gets sent into one single REPL.
-
-// TODO: when you reload window, is the REPL still binded to same Python file?
-// cache binding uri to to REPL instance or notebookEditor.
-
 // TODO: figure out way to put markdown telling user kernel has been dead and need to pick again.
-
-// TODO: FIGURE OUT WHY INTELLISENSE IS NOT WORKING
 
 async function getSelectedTextToExecute(textEditor: TextEditor): Promise<string | undefined> {
     if (!textEditor) {
@@ -70,7 +56,7 @@ export async function registerReplCommands(
             }
             if (interpreter) {
                 const interpreterPath = interpreter.path;
-                // How do we get instance of interactive window from Python extension?
+
                 if (!notebookController) {
                     notebookController = createReplController(interpreterPath);
                 }
@@ -78,10 +64,9 @@ export async function registerReplCommands(
 
                 const code = await getSelectedTextToExecute(activeEditor);
                 const ourResource = Uri.from({ scheme: 'untitled', path: 'repl.interactive' });
-                // const ourResource2 = Uri.file(uri.path);
 
-                // How to go from user clicking Run Python --> Run selection/line via Python REPL -> IW opening
                 const notebookDocument = await workspace.openNotebookDocument(ourResource);
+                // commands.executeCommand('_interactive.open'); command to open interactive window so intellisense is registered.
 
                 // We want to keep notebookEditor, whenever we want to run.
                 // Find interactive window, or open it.
@@ -90,15 +75,6 @@ export async function registerReplCommands(
                         viewColumn: ViewColumn.Beside,
                     });
                 }
-
-                // Instead we need to first check if notebookEditor for given file Uri exist.
-                // If it doesnt, we create notebookEditor and add to Map <Uri, NotebookEditor>
-                // if (!mapUriToNotebookEditor?.get(ourResource2) || !mapUriToNotebookEditor.get(ourResource2)) {
-                //     notebookEditor = await window.showNotebookDocument(notebookDocument, {
-                //         viewColumn: ViewColumn.Beside,
-                //     });
-                //     mapUriToNotebookEditor?.set(ourResource2, notebookEditor);
-                // }
 
                 notebookController!.updateNotebookAffinity(notebookDocument, NotebookControllerAffinity.Default);
 
