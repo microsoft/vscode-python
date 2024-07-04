d18042b47b8a01eeceb3f9dd98b9ca996c52d60f
Brett Cannon <brcan@microsoft.com>
2024-07-03 17:12:08 -0700

    Pylance didn't succeed at fixing all calls

--- python_files/testing_tools/adapter/info.py
+++ python_files/testing_tools/adapter/info.py
@@ -29,10 +29,10 @@ class SingleTestPath(namedtuple("TestPath", "root relfile func sub")):
 class ParentInfo(namedtuple("ParentInfo", "id kind name root relpath parentid")):
     KINDS = ("folder", "file", "suite", "function", "subtest")
 
-    def __new__(cls, id_, kind, name, root=None, relpath=None, parentid=None):
+    def __new__(cls, id, kind, name, root=None, relpath=None, parentid=None):  # noqa: A002
         self = super().__new__(
             cls,
-            id=str(id_) if id_ else None,
+            id=str(id) if id else None,
             kind=str(kind) if kind else None,
             name=str(name) if name else None,
             root=str(root) if root else None,
@@ -67,10 +67,10 @@ class SingleTestInfo(namedtuple("TestInfo", "id name path source markers parenti
     MARKERS = ("skip", "skip-if", "expected-failure")
     KINDS = ("function", "doctest")
 
-    def __new__(cls, id_, name, path, source, markers, parentid, kind="function"):
+    def __new__(cls, id, name, path, source, markers, parentid, kind="function"):  # noqa: A002
         self = super().__new__(
             cls,
-            str(id_) if id_ else None,
+            str(id) if id else None,
             str(name) if name else None,
             path or None,
             str(source) if source else None,
