7d339e90da871e15f765080db118d93709c53981
Anthony Kim <anthonykim@microsoft.com>
2024-07-29 17:02:31 -0400

    ensure getNativeREPL gets called

--- src/test/repl/replCommand.test.ts
+++ src/test/repl/replCommand.test.ts
@@ -8,7 +8,9 @@ import { ICommandManager } from '../../client/common/application/types';
 import { ICodeExecutionHelper } from '../../client/terminals/types';
 import * as replCommands from '../../client/repl/replCommands';
 import * as replUtils from '../../client/repl/replUtils';
+import * as nativeRepl from '../../client/repl/nativeRepl';
 import { Commands } from '../../client/common/constants';
+import { PythonEnvironment } from '../../client/pythonEnvironments/info';
 // import { executeInTerminal, getActiveInterpreter, getSendToNativeREPLSetting } from '../../client/repl/replUtils';
 
 suite('REPL - register native repl command', () => {
@@ -20,8 +22,12 @@ suite('REPL - register native repl command', () => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     let registerCommandSpy: sinon.SinonSpy; // Need this for getSendToNativeREPLSetting test #ignore unused variable error on this line // @ts-ignore: TS6133
     let executeInTerminalStub: sinon.SinonStub;
+    let getNativeReplStub: sinon.SinonStub;
     setup(() => {
         interpreterService = TypeMoq.Mock.ofType<IInterpreterService>();
+        interpreterService
+            .setup((i) => i.getActiveInterpreter(TypeMoq.It.isAny()))
+            .returns(() => Promise.resolve(({ path: 'ps' } as unknown) as PythonEnvironment));
         commandManager = TypeMoq.Mock.ofType<ICommandManager>();
         executionHelper = TypeMoq.Mock.ofType<ICodeExecutionHelper>();
         // Define the registerCommand method on the mock object
@@ -116,5 +122,34 @@ suite('REPL - register native repl command', () => {
 
         // Check to see if executeInTerminal was called
         sinon.assert.calledOnce(executeInTerminalStub);
+    });
+
+    test('Ensure we call getNativeREPL() when interpreter exist', async () => {
+        getSendToNativeREPLSettingStub.returns(true);
+        getNativeReplStub = sinon.stub(nativeRepl, 'getNativeRepl');
+
+        let commandHandler: undefined | ((uri: string) => Promise<void>);
+        commandManager
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            .setup((c) => c.registerCommand as any)
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            .returns(() => (command: string, callback: (...args: any[]) => any, _thisArg?: any) => {
+                if (command === Commands.Exec_In_REPL) {
+                    commandHandler = callback;
+                }
+                // eslint-disable-next-line no-void
+                return { dispose: () => void 0 };
+            });
+        replCommands.registerReplCommands(
+            [TypeMoq.Mock.ofType<Disposable>().object],
+            interpreterService.object,
+            executionHelper.object,
+            commandManager.object,
+        );
+
+        expect(commandHandler).not.to.be.an('undefined', 'Command handler not initialized');
+
+        await commandHandler!('uri');
+        sinon.assert.calledOnce(getNativeReplStub);
     });
 });
