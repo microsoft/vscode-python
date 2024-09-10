81be0a9f41dd070ed237242c9675d83edfceb091
Anthony Kim <anthonykim@microsoft.com>
2024-09-10 11:42:00 -0700

    need to change from hard-coded file to current open Python

--- src/client/taskProblemMatcher.ts
+++ src/client/taskProblemMatcher.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 import { ShellExecution, Task, TaskScope, tasks, Disposable } from 'vscode';
+import { traceLog } from './logging';
 
 export function registerPythonTaskProvider(): Disposable {
     const taskProvider = tasks.registerTaskProvider('pythonTask', {
@@ -21,5 +22,27 @@ export function registerPythonTaskProvider(): Disposable {
             return undefined;
         },
     });
+
+    tasks.onDidStartTask((e) => {
+        traceLog(`Task started: ${e.execution.task.name}`);
+
+        // TODO: If the task started is for Python extension,
+        // Create new task,
+        // execute that new task with their current python file
+
+        const runCurrentFileCommand = '';
+
+        const currentPythonFileTask = new Task(
+            { type: 'pythonTask', task: 'defaultTask' },
+            TaskScope.Workspace,
+            'Run current Python file',
+            'pythonTask',
+            new ShellExecution('python joke.py'), // TODO: Get the current active Python file and make command to run this
+            '$pythonCustomMatcher', // Use the custom problem matcher defined in package.json
+        );
+
+        tasks.executeTask(currentPythonFileTask);
+    });
+
     return taskProvider;
 }
