56526ad0743c7d588d7e2e004bd48233ff08f212
Anthony Kim <anthonykim@microsoft.com>
2024-09-20 23:35:17 -0700

    I win

--- src/test/terminals/shellIntegration/pythonStartup.test.ts
+++ src/test/terminals/shellIntegration/pythonStartup.test.ts
@@ -2,57 +2,35 @@
 // Licensed under the MIT License.
 import * as sinon from 'sinon';
 import * as TypeMoq from 'typemoq';
-import {
-    EnvironmentVariableCollection,
-    GlobalEnvironmentVariableCollection,
-    Uri,
-    WorkspaceConfiguration,
-} from 'vscode';
+import { GlobalEnvironmentVariableCollection, Uri, WorkspaceConfiguration } from 'vscode';
 import * as workspaceApis from '../../../client/common/vscodeApis/workspaceApis';
 import { registerPythonStartup } from '../../../client/terminals/pythonStartup';
 import { IExtensionContext } from '../../../client/common/types';
 
 suite('Terminal - Shell Integration with PYTHONSTARTUP', () => {
-    // let context: IExtensionContext;
     let getConfigurationStub: sinon.SinonStub;
     let pythonConfig: TypeMoq.IMock<WorkspaceConfiguration>;
     let editorConfig: TypeMoq.IMock<WorkspaceConfiguration>;
     let context: TypeMoq.IMock<IExtensionContext>;
     let createDirectoryStub: sinon.SinonStub;
     let copyStub: sinon.SinonStub;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    let environmentVariableCollection: TypeMoq.IMock<EnvironmentVariableCollection>;
     let globalEnvironmentVariableCollection: TypeMoq.IMock<GlobalEnvironmentVariableCollection>;
 
     setup(() => {
         context = TypeMoq.Mock.ofType<IExtensionContext>();
-        // context.setup((c) => c).returns(() => context.object);
-        environmentVariableCollection = TypeMoq.Mock.ofType<EnvironmentVariableCollection>();
         globalEnvironmentVariableCollection = TypeMoq.Mock.ofType<GlobalEnvironmentVariableCollection>();
 
         // Question: Why do we have to set up environmentVariableCollection and globalEnvironmentVariableCollection in this flip-flop way?
         // Reference: /vscode-python/src/test/interpreters/activation/terminalEnvVarCollectionService.unit.test.ts
         context.setup((c) => c.environmentVariableCollection).returns(() => globalEnvironmentVariableCollection.object);
-        globalEnvironmentVariableCollection
-            .setup((c) => c.getScoped(TypeMoq.It.isAny()))
-            .returns(() => environmentVariableCollection.object);
         context.setup((c) => c.storageUri).returns(() => Uri.parse('a'));
 
         globalEnvironmentVariableCollection
             .setup((c) => c.replace(TypeMoq.It.isAny(), TypeMoq.It.isAny(), TypeMoq.It.isAny()))
             .returns(() => Promise.resolve());
 
-        environmentVariableCollection
-            .setup((c) => c.replace(TypeMoq.It.isAny(), TypeMoq.It.isAny(), TypeMoq.It.isAny()))
-            .returns(() => Promise.resolve());
+        globalEnvironmentVariableCollection.setup((c) => c.delete(TypeMoq.It.isAny())).returns(() => Promise.resolve());
 
-        // Commenting out 41 to 45 will pas first couple test. "c.environmentVariableCollection is not a function" crashes everything.
-        // context
-        //     .setup((c) =>
-        //         c.environmentVariableCollection.replace(TypeMoq.It.isAny(), TypeMoq.It.isAny(), TypeMoq.It.isAny()),
-        //     )
-        //     .returns(() => Promise.resolve());
-        // Marker -----
         getConfigurationStub = sinon.stub(workspaceApis, 'getConfiguration');
         createDirectoryStub = sinon.stub(workspaceApis, 'createDirectory');
         copyStub = sinon.stub(workspaceApis, 'copy');
@@ -79,7 +57,6 @@ suite('Terminal - Shell Integration with PYTHONSTARTUP', () => {
 
         await registerPythonStartup(context.object);
 
-        // Verify createDirectoryStub has been called
         sinon.assert.calledOnce(createDirectoryStub);
     });
 
@@ -88,7 +65,6 @@ suite('Terminal - Shell Integration with PYTHONSTARTUP', () => {
 
         await registerPythonStartup(context.object);
 
-        // Verify createDirectoryStub has not been called
         sinon.assert.notCalled(createDirectoryStub);
     });
 
@@ -97,7 +73,6 @@ suite('Terminal - Shell Integration with PYTHONSTARTUP', () => {
 
         await registerPythonStartup(context.object);
 
-        // Verify copyStub has been called
         sinon.assert.calledOnce(copyStub);
     });
 
@@ -106,20 +81,44 @@ suite('Terminal - Shell Integration with PYTHONSTARTUP', () => {
 
         await registerPythonStartup(context.object);
 
-        // Verify copyStub has not been called
         sinon.assert.notCalled(copyStub);
     });
 
-    // TODO: figure out what is wrong. How is environmentVariableCollection not a function in context?
-    test('Verify environment collection.replace is called when shell integration is enabled', async () => {
+    test('PYTHONSTARTUP is set when enableShellIntegration setting is true', async () => {
         pythonConfig.setup((p) => p.get('REPL.enableShellIntegration')).returns(() => true);
 
         await registerPythonStartup(context.object);
 
-        // Verify environment collection.replace has been called
         globalEnvironmentVariableCollection.verify(
             (c) => c.replace('PYTHONSTARTUP', TypeMoq.It.isAny(), TypeMoq.It.isAny()),
             TypeMoq.Times.once(),
         );
+    });
+
+    test('environmentCollection should not remove PYTHONSTARTUP when enableShellIntegration setting is true', async () => {
+        pythonConfig.setup((p) => p.get('REPL.enableShellIntegration')).returns(() => true);
+
+        await registerPythonStartup(context.object);
+
+        globalEnvironmentVariableCollection.verify((c) => c.delete('PYTHONSTARTUP'), TypeMoq.Times.never());
+    });
+
+    test('PYTHONSTARTUP is not set when enableShellIntegration setting is false', async () => {
+        pythonConfig.setup((p) => p.get('REPL.enableShellIntegration')).returns(() => false);
+
+        await registerPythonStartup(context.object);
+
+        globalEnvironmentVariableCollection.verify(
+            (c) => c.replace('PYTHONSTARTUP', TypeMoq.It.isAny(), TypeMoq.It.isAny()),
+            TypeMoq.Times.never(),
+        );
+    });
+
+    test('PYTHONSTARTUP is deleted when enableShellIntegration setting is false', async () => {
+        pythonConfig.setup((p) => p.get('REPL.enableShellIntegration')).returns(() => false);
+
+        await registerPythonStartup(context.object);
+
+        globalEnvironmentVariableCollection.verify((c) => c.delete('PYTHONSTARTUP'), TypeMoq.Times.once());
     });
 });
