d3279035b445c453e3511b64b9b683cb80e82739
Anthony Kim <anthonykim@microsoft.com>
2024-10-29 12:23:51 -0700

    try to add more test

--- src/test/common/terminals/helper.unit.test.ts
+++ src/test/common/terminals/helper.unit.test.ts
@@ -107,6 +107,21 @@ suite('Terminal Service helpers', () => {
             const safeTerminalOptions = terminalOptions || {};
             expect(safeTerminalOptions).to.not.have.property('PYTHONSTARTUP');
         });
+        test('Create terminal should respect env passed in', () => {
+            const terminal = 'Terminal Created';
+            when(terminalManager.createTerminal(anything())).thenReturn(terminal as any);
+            terminalManager.createTerminal({
+                name:  'Python',
+                env: {},
+                hideFromUser: undefined,
+            });
+            const args = capture(terminalManager.createTerminal).first()[0];
+            const terminalOptions = args.env;
+            const safeTerminalOptions = terminalOptions || {};
+            expect(safeTerminalOptions).to.have.property('myCustomEnv', '123');
+            });
+        })
+
         test('Create terminal without a title', () => {
             const terminal = 'Terminal Created';
             when(terminalManager.createTerminal(anything())).thenReturn(terminal as any);
