1187591e79816bda4e0a07fb0e368801dcc97ef2
Anthony Kim <anthonykim@microsoft.com>
2025-01-30 10:42:52 -0800

    use sendText for >=3.13

--- src/client/common/terminal/service.ts
+++ src/client/common/terminal/service.ts
@@ -25,6 +25,7 @@ import { useEnvExtension } from '../../envExt/api.internal';
 import { ensureTerminalLegacy } from '../../envExt/api.legacy';
 import { sleep } from '../utils/async';
 import { isWindows } from '../utils/platform';
+import { getActiveInterpreter } from '../../repl/replUtils';
 
 @injectable()
 export class TerminalService implements ITerminalService, Disposable {
@@ -108,7 +109,17 @@ export class TerminalService implements ITerminalService, Disposable {
 
         const config = getConfiguration('python');
         const pythonrcSetting = config.get<boolean>('terminal.shellIntegration.enabled');
-        if ((isPythonShell && !pythonrcSetting) || (isPythonShell && isWindows())) {
+
+        // Need to check for if Python version is >= 3.13 since we have turned off SI on python side.
+        // Because we are not sending explicit commandline info to core, it will inject ^C. (We want to avoid)
+        if (this.options && this.options.resource) {
+            const pythonVersion = await getActiveInterpreter(
+                this.options.resource,
+                this.serviceContainer.get<IInterpreterService>(IInterpreterService),
+            );
+            const minorVersion = pythonVersion?.version?.minor;
+
+        if ((isPythonShell && !pythonrcSetting) || (isPythonShell && isWindows()) || ((minorVersion ?? 0) >= 13)) {
             // If user has explicitly disabled SI for Python, use sendText for inside Terminal REPL.
             terminal.sendText(commandLine);
             return undefined;
