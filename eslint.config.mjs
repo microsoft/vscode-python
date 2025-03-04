1addd22537bd97b854dce6c8686bf19c53394d83
eleanorjboyd <26030610+eleanorjboyd@users.noreply.github.com>
2025-03-04 13:57:48 -0800

    fix redundancies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -48,7 +48,6 @@ export default [
             '@typescript-eslint/no-empty-interface': 'off',
             '@typescript-eslint/no-explicit-any': 'error',
             '@typescript-eslint/no-non-null-assertion': 'off',
-            'no-unused-vars': 'off',
             '@typescript-eslint/no-unused-vars': [
                 'error',
                 {
@@ -56,7 +55,6 @@ export default [
                     argsIgnorePattern: '^_',
                 },
             ],
-            'no-use-before-define': 'off',
             '@typescript-eslint/no-use-before-define': [
                 'error',
                 {
