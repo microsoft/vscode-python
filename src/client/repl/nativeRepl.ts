265a273e4201865668c91897aa22893f562ad72e
Paul Donnelly <donnelpa@gmail.com>
2025-03-07 19:06:01 -0600

    add new pythonServer to disposables array

--- src/client/repl/nativeRepl.ts
+++ src/client/repl/nativeRepl.ts
@@ -76,6 +76,7 @@ export class NativeRepl implements Disposable {
                     await updateWorkspaceStateValue<string | undefined>(NATIVE_REPL_URI_MEMENTO, undefined);
                     this.pythonServer.dispose();
                     this.pythonServer = createPythonServer([this.interpreter.path as string], this.cwd);
+                    this.disposables.push(this.pythonServer);
                     if (this.replController) {
                         this.replController.dispose();
                     }
