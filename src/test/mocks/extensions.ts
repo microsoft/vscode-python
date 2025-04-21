0a0cb6085d2ce47f1fba914023257d64ac4c5ea2
Kyle Into <kylei@fb.com>
2025-04-21 13:35:31 -0500

    fix test
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- src/test/mocks/extensions.ts
+++ src/test/mocks/extensions.ts
@@ -7,7 +7,7 @@ export class MockExtensions implements IExtensions {
     all: readonly Extension<unknown>[] = [];
     onDidChange: Event<void> = (() => {
         throw new Error('Method not implemented');
-    })();
+    });
     getExtension(extensionId: string): Extension<unknown> | undefined;
     getExtension<T>(extensionId: string): Extension<T> | undefined;
     getExtension(_extensionId: unknown): import('vscode').Extension<unknown> | undefined {
