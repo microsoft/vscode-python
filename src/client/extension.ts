f4b4c04dfc028d23c4dd4c2cb30a43ce933d8887
Don Jayamanne <don.jayamanne@outlook.com>
2025-05-20 10:26:04 +1000

    Ability to track the user selected Environment

--- src/client/extension.ts
+++ src/client/extension.ts
@@ -44,6 +44,8 @@ import { ProposedExtensionAPI } from './proposedApiTypes';
 import { buildProposedApi } from './proposedApi';
 import { GLOBAL_PERSISTENT_KEYS } from './common/persistentState';
 import { registerTools } from './chat';
+import { RecommendedEnvironmentService } from './interpreter/configuration/recommededEnvironmentService';
+import { IRecommendedEnvironmentService } from './interpreter/configuration/types';
 
 durations.codeLoadingTime = stopWatch.elapsedTime;
 
@@ -164,6 +166,9 @@ async function activateUnsafe(
     );
     const proposedApi = buildProposedApi(components.pythonEnvs, ext.legacyIOC.serviceContainer);
     registerTools(context, components.pythonEnvs, api.environments, ext.legacyIOC.serviceContainer);
+    ext.legacyIOC.serviceContainer
+        .get<IRecommendedEnvironmentService>(RecommendedEnvironmentService)
+        .registerEnvApi(api.environments);
     return [{ ...api, ...proposedApi }, activationPromise, ext.legacyIOC.serviceContainer];
 }
 
