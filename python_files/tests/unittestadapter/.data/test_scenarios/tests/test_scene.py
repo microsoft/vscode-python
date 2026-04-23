3410532519e83e17820cd9b134d8fefce570bf0a
eleanorjboyd <26030610+eleanorjboyd@users.noreply.github.com>
2026-04-23 08:38:33 -0700

    fixing

--- python_files/tests/unittestadapter/.data/test_scenarios/tests/test_scene.py
+++ python_files/tests/unittestadapter/.data/test_scenarios/tests/test_scene.py
@@ -3,11 +3,12 @@
 
 from testscenarios import TestWithScenarios
 
+
 class TestMathOperations(TestWithScenarios):
     scenarios = [
         ('add', {'test_id': 'test_add', 'a': 5, 'b': 3, 'expected': 8}),
         ('subtract', {'test_id': 'test_subtract', 'a': 5, 'b': 3, 'expected': 2}),
-        ('multiply', {'test_id': 'test_multiply', 'a': 5, 'b': 3, 'expected': 15})
+        ('multiply', {'test_id': 'test_multiply', 'a': 5, 'b': 3, 'expected': 15}),
     ]
     a: int = 0
     b: int = 0
