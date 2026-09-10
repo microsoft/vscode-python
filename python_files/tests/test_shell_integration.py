6d682f971dd420e8dbab779fb79a9d36462a5b3c
Eduardo Villalpando Mello <eduardovil@microsoft.com>
2026-09-10 10:49:09 -0700

    Fix tests

--- python_files/tests/test_shell_integration.py
+++ python_files/tests/test_shell_integration.py
@@ -1,6 +1,7 @@
 import importlib
 import platform
 import sys
+from typing import Protocol, cast
 from unittest.mock import Mock
 
 import pythonrc
@@ -8,42 +9,43 @@ import pythonrc
 is_wsl = "microsoft-standard-WSL" in platform.release()
 
 
+class _Hooks(Protocol):
+    failure_flag: bool
+
+
+class _PS1(Protocol):
+    hooks: _Hooks
+
+
 def test_decoration_success():
     importlib.reload(pythonrc)
-    ps1 = pythonrc.PS1()
-
-    ps1.hooks.failure_flag = False
-    result = str(ps1)
     if sys.platform != "win32" and (not is_wsl):
+        ps1 = cast(_PS1, sys.ps1)
+        ps1.hooks.failure_flag = False
+        result = str(ps1)
         assert (
             result
             == "\x01\x1b]633;C\x07\x1b]633;E;None\x07\x1b]633;D;0\x07\x1b]633;A\x07\x02>>> \x01\x1b]633;B\x07\x02"
         )
-    else:
-        pass
 
 
 def test_decoration_failure():
     importlib.reload(pythonrc)
-    ps1 = pythonrc.PS1()
-
-    ps1.hooks.failure_flag = True
-    result = str(ps1)
     if sys.platform != "win32" and (not is_wsl):
+        ps1 = cast(_PS1, sys.ps1)
+        ps1.hooks.failure_flag = True
+        result = str(ps1)
         assert (
             result
             == "\x01\x1b]633;C\x07\x1b]633;E;None\x07\x1b]633;D;1\x07\x1b]633;A\x07\x02>>> \x01\x1b]633;B\x07\x02"
         )
-    else:
-        pass
 
 
 def test_displayhook_call():
     importlib.reload(pythonrc)
-    pythonrc.PS1()
     mock_displayhook = Mock()
 
-    hooks = pythonrc.REPLHooks()
+    hooks = sys.displayhook.__self__
     hooks.original_displayhook = mock_displayhook
 
     hooks.vscode_displayhook("mock_value")
@@ -53,14 +55,14 @@ def test_displayhook_call():
 
 def test_excepthook_call():
     importlib.reload(pythonrc)
-    pythonrc.PS1()
     mock_excepthook = Mock()
 
-    hooks = pythonrc.REPLHooks()
+    hooks = sys.excepthook.__self__
     hooks.original_excepthook = mock_excepthook
 
     hooks.vscode_excepthook("mock_type", "mock_value", "mock_traceback")
     mock_excepthook.assert_called_once_with("mock_type", "mock_value", "mock_traceback")
+
 
 def test_does_not_pollute_namespace():
     importlib.reload(pythonrc)
